ea97be01c7083ffae5b1b9c5b500ac5bd3fd823b
Patrick Riley <priley86@gmail.com>
2017-08-14 11:46:58 -0400

    feat(toastNotification): adds toast notification component
    Closes #47

---
+++ src/common/Timer.js
@@ -0,0 +1,23 @@
+/**
+ * Timer class implements a simple timeout mechanism
+ */
+class Timer {
+  constructor(func, delay) {
+    this.timer = null
+    this.delay = delay
+    this.execute = func
+  }
+
+  startTimer() {
+    this.clearTimer()
+
+    this.timer = setTimeout(this.execute, this.delay)
+  }
+  clearTimer() {
+    if (this.timer) {
+      clearTimeout(this.timer)
+    }
+  }
+}
+
+export default Timer
