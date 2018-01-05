84879f03b8603ca363c43429190eadfb17a22c42
Avi Sharvit <sharvita@gmail.com>
2018-01-05 13:59:47 +0200

    refactor(ToastNotification): Reuse Alert inside ToastNotification (#146)

--- src/components/ToastNotification/ToastNotification.test.js
+++ src/components/ToastNotification/ToastNotification.test.js
@@ -6,39 +6,28 @@ import renderer from 'react-test-renderer';
 import ToastNotification from './ToastNotification';
 import TimedToastNotification from './TimedToastNotification';
 
-test('ToastNotification renders properly', () => {
+const testToastNotificationSnapshot = Component => {
   const handleNotificationClose = jest.fn();
   const handleOnMouseEnter = jest.fn();
   const handleOnMouseLeave = jest.fn();
   const component = renderer.create(
-    <ToastNotification
+    <Component
       type="danger"
       onDismiss={handleNotificationClose}
       onMouseEnter={handleOnMouseEnter}
       onMouseLeave={handleOnMouseLeave}
     >
       <span>Danger Will Robinson!</span>
-    </ToastNotification>
+    </Component>
   );
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
+};
+
+test('ToastNotification renders properly', () => {
+  testToastNotificationSnapshot(ToastNotification);
 });
 
 test('TimedToastNotification renders properly', () => {
-  const handleNotificationClose = jest.fn();
-  const handleOnMouseEnter = jest.fn();
-  const handleOnMouseLeave = jest.fn();
-  const component = renderer.create(
-    <TimedToastNotification
-      type="danger"
-      onDismiss={handleNotificationClose}
-      onMouseEnter={handleOnMouseEnter}
-      onMouseLeave={handleOnMouseLeave}
-      paused={false}
-    >
-      <span>Danger Will Robinson!</span>
-    </TimedToastNotification>
-  );
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  testToastNotificationSnapshot(TimedToastNotification);
 });
