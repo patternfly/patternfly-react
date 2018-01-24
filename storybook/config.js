d80ff413b0bf30b80b13f7cfbac3acd02e9beefa
Alberto Jesus Gutierrez Juanes <aljesusg@gmail.com>
2018-01-24 13:47:09 +0100

    Set title in storybook

--- storybook/config.js
+++ storybook/config.js
@@ -1,4 +1,5 @@
 import { configure, setAddon } from '@storybook/react';
+import { setOptions } from '@storybook/addon-options';
 import infoAddon from '@storybook/addon-info';
 import './sass/base.scss';
 
@@ -9,5 +10,20 @@ const req = require.context('../src', true, /.stories.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+setOptions({
+  /**
+   * name to display in the top left corner
+   * default: Storybook
+   * @type {String}
+   */
+  name: 'Patternfly-React',
+  /**
+   * sorts stories
+   * default: false
+   * @type {Boolean}
+   */
+  sortStoriesByKind: true
+});
 
 configure(loadStories, module);
