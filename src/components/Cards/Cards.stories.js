27bfef07b660c0867fdbe4dacee1521216a7da07
Gilad Lekner <glekner@redhat.com>
2018-01-30 17:27:36 +0200

    more fixes

--- src/components/Cards/Cards.stories.js
+++ src/components/Cards/Cards.stories.js
@@ -3,20 +3,26 @@ import { storiesOf } from '@storybook/react';
 import { defaultTemplate } from '../../../storybook/decorators/storyTemplates';
 import { DOCUMENTATION_URL } from '../../../storybook/constants';
 import { Card } from './index';
+import { Icon } from '../Icon';
+import { Button } from '../Button';
 
 const stories = storiesOf('Cards', module);
 
 stories.addDecorator(
   defaultTemplate({
     title: 'Card',
-    documentationLink: DOCUMENTATION_URL.PATTERNFLY_ORG_CARDS
+    documentationLink: DOCUMENTATION_URL.PATTERNFLY_ORG_CARDS + 'base-card/'
   })
 );
 
 stories.addWithInfo('Card', () => {
   return (
     <Card>
-      <Card.Title>Card Title</Card.Title>
+      <Card.Title>
+        <span>
+          <Icon name="shield" /> Card Title
+        </span>
+      </Card.Title>
       <Card.Body>This is the Card component.</Card.Body>
     </Card>
   );
