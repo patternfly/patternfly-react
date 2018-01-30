740fe0c8c067e182b9ac38c8943f4ae22e08c109
Gilad Lekner <glekner@redhat.com>
2018-01-30 16:42:22 +0200

    Added storybook template

--- src/components/Cards/Cards.stories.js
+++ src/components/Cards/Cards.stories.js
@@ -2,27 +2,22 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { defaultTemplate } from '../../../storybook/decorators/storyTemplates';
 import { DOCUMENTATION_URL } from '../../../storybook/constants';
-import { Badge } from './index';
+import { Card } from './index';
 
 const stories = storiesOf('Cards', module);
 
-// stories.addDecorator(
-//   defaultTemplate({
-//     title: 'Label',
-//     documentationLink: DOCUMENTATION_URL.PATTERNFLY_ORG_WIDGETS + '#labels',
-//     reactBootstrapDocumentationLink:
-//       DOCUMENTATION_URL.REACT_BOOTSTRAP_COMPONENT + 'label/'
-//   })
-// );
 stories.addDecorator(
   defaultTemplate({
-    title: 'Cards',
-    documentationLink: DOCUMENTATION_URL.PATTERNFLY_ORG_CARDS + 'base-card/'
+    title: 'Card',
+    documentationLink: DOCUMENTATION_URL.PATTERNFLY_ORG_CARDS
   })
 );
 
-stories.addWithInfo('Cards', '', () => (
-  <a href="#">
-    Inbox <Badge>42</Badge>
-  </a>
-));
+stories.addWithInfo('Card', () => {
+  return (
+    <Card>
+      <Card.Title>Card Title</Card.Title>
+      <Card.Body>This is the Card component.</Card.Body>
+    </Card>
+  );
+});
