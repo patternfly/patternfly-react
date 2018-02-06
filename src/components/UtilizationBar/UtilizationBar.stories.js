ab9804fffa3a1c6abc9e66f2273d83d73a46b2d1
Zita Nemeckova <znemecko@redhat.com>
2018-02-06 10:00:41 +0100

    Add Knobs

--- src/components/UtilizationBar/UtilizationBar.stories.js
+++ src/components/UtilizationBar/UtilizationBar.stories.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { defaultTemplate } from '../../../storybook/decorators/storyTemplates';
+import { withKnobs, number} from '@storybook/addon-knobs';
 import { DOCUMENTATION_URL } from '../../../storybook/constants';
 import { UtilizationBar } from './index';
 
 const stories = storiesOf('UtilizationBar', module);
+stories.addDecorator(withKnobs);
 stories.addDecorator(
   defaultTemplate({
     title: 'Utilization Bar',
@@ -19,7 +21,13 @@ stories.addWithInfo(
   () => (
     <div>
       <h1>Default utilization bar</h1>
-        <UtilizationBar units="UNIT"/>
+        <UtilizationBar now={42} />
+      <h1>Utilization bar with thresholds</h1>
+        <UtilizationBar now={number("Actual value", 70)}
+                        min={number("Minimal value", 0)}
+                        max={number("Maximal value", 100)}
+                        thresholdWarning={number("Warning threshold value", 40)}
+                        thresholdError={number("Error threshold value", 80)} />
     </div>
   )
 );
