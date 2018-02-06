2214ac07ffce2827c122b8c4d3e21aaa67db7b02
Zita Nemeckova <znemecko@redhat.com>
2018-02-06 09:55:15 +0100

    Add options to UtilizationBar

--- src/components/UtilizationBar/UtilizationBar.js
+++ src/components/UtilizationBar/UtilizationBar.js
@@ -1,37 +1,64 @@
 import { ProgressBar } from '../ProgressBar';
+import PropTypes from 'prop-types';
 import { Tooltip } from '../Tooltip';
 import { OverlayTrigger } from '../OverlayTrigger';
 import React from 'react';
 //import PropTypes from 'prop-types';
 
-const UtilizationBar = (...props) => {
+const UtilizationBar = ({...props}) => {
     const availableTooltip = () => {
-        debugger;
       return (
           <Tooltip id="availableTooltip">
-            Available {props.units}
+              Available {props.max - props.now} %
           </Tooltip>
       );
     };
     const usedTooltip = () => {
-        debugger;
         return (
             <Tooltip id="usedTooltip">
-              Used {props.units}
+              Used {props.now} %
             </Tooltip>
         );
+    };
+    const barStyle = () => {
+      if(props.thresholdWarning && props.thresholdError) {
+          var style = "success";
+          if (props.thresholdWarning <= props.now) {
+              style = "warning";
+          }
+          if (props.thresholdError <= props.now) {
+              style = "danger";
+          }
+          return style;
+      } else {
+          return null;
+
+      };
     };
 
     return (
         <div className="progress">
           <OverlayTrigger overlay={usedTooltip()} placement="top" trigger={["hover", "focus"]} rootClose={false}>
-            <ProgressBar min={0} max={100} now={70} key={1} isChild={true}></ProgressBar>
+            <ProgressBar bsStyle={barStyle()} min={props.min} max={props.max} now={props.now} key={1} isChild={true}></ProgressBar>
           </OverlayTrigger>
           <OverlayTrigger overlay={availableTooltip()} placement="top" trigger={["hover", "focus"]} rootClose={false}>
-            <ProgressBar now={30} key={2} bsClass="progress-bar progress-bar-remaining" isChild={true}></ProgressBar>
+            <ProgressBar min={props.min} max={props.max} now={props.max - props.now} key={2} bsClass="progress-bar progress-bar-remaining" isChild={true}></ProgressBar>
           </OverlayTrigger>
         </div>
     );
+};
+
+UtilizationBar.propTypes = {
+    min: PropTypes.number,
+    max: PropTypes.number,
+    now: PropTypes.number,
+    thresholdWarning: PropTypes.number,
+    thresholdError: PropTypes.number,
+};
+
+UtilizationBar.defaultProps = {
+    min: 0,
+    max: 100,
 };
 
 export default UtilizationBar;
