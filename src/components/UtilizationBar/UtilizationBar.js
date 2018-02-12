e3ba67ce3abd40d452c45b286a71a620390082d3
Zita Nemeckova <znemecko@redhat.com>
2018-02-12 12:21:13 +0100

    Add label and description

--- src/components/UtilizationBar/UtilizationBar.js
+++ src/components/UtilizationBar/UtilizationBar.js
@@ -1,4 +1,5 @@
 import { ProgressBar } from '../ProgressBar';
+import ClassNames from 'classnames';
 import PropTypes from 'prop-types';
 import { OverlayTrigger } from '../OverlayTrigger';
 import { Tooltip } from '../Tooltip';
@@ -28,66 +29,117 @@ const UtilizationBar = props => {
     }
   };
 
-const mainDivClasses = mainDivClasses => {
-  if (mainDivClasses) {
-    return mainDivClasses + " progress";
-  } else {
-    return "progress";
-  }
-};
+  const labelClasses = top => {
+    return ClassNames(
+      { 'progress-label-top-right': top, 'progress-label-right': !top },
+      'pull-right'
+    );
+  };
+
+  const mainDivClasses = (onSide, mainDivClasses) => {
+    return ClassNames({
+      'progress-container': onSide && props.description,
+      'progress-description-left': onSide && props.description,
+      'progress-label-right': onSide && props.label,
+      mainDivClasses: mainDivClasses
+    });
+  };
 
   return (
-    <div className={mainDivClasses(props.mainDivClasses)}>
-      <OverlayTrigger
-        overlay={props.usedTooltipFunction(props.now)}
-        placement="top"
-        trigger={['hover', 'focus']}
-        rootClose={false}
-      >
-        <ProgressBar
-          bsStyle={barStyle()}
-          min={props.min}
-          max={props.max}
-          now={props.now}
-          key={1}
-          isChild
-        />
-      </OverlayTrigger>
-      <OverlayTrigger
-        overlay={props.availableTooltipFunction(props.max, props.now)}
-        placement="top"
-        trigger={['hover', 'focus']}
-        rootClose={false}
-      >
-        <ProgressBar
-          min={props.min}
-          max={props.max}
-          now={props.max - props.now}
-          key={2}
-          bsClass="progress-bar progress-bar-remaining"
-          isChild
-        />
-      </OverlayTrigger>
+    <div
+      className={mainDivClasses(
+        !props.descriptionPlacementTop,
+        props.mainDivClasses
+      )}
+    >
+      <div className={props.descriptionPlacementTop ? null : 'progress-bar'}>
+        {props.label && (
+          <span
+            className={labelClasses(props.descriptionPlacementTop)}
+            style={{ display: 'inline-block', fontSize: '12px' }}
+          >
+            <strong style={{ fontWeight: 600 }}>
+              {props.now + ' of ' + props.max}
+            </strong>{' '}
+            {props.label}
+          </span>
+        )}
+        {props.description && (
+          <div
+            className="progress-description"
+            style={{ color: 'black', fontSize: '12px' }}
+          >
+            {props.description}
+          </div>
+        )}
+      </div>
+      <div className="progress">
+        <OverlayTrigger
+          overlay={props.usedTooltipFunction(props.now)}
+          placement="top"
+          trigger={['hover', 'focus']}
+          rootClose={false}
+        >
+          <ProgressBar
+            bsStyle={barStyle()}
+            min={props.min}
+            max={props.max}
+            now={props.now}
+            key={1}
+            isChild
+          />
+        </OverlayTrigger>
+        <OverlayTrigger
+          overlay={props.availableTooltipFunction(props.max, props.now)}
+          placement="top"
+          trigger={['hover', 'focus']}
+          rootClose={false}
+        >
+          <ProgressBar
+            min={props.min}
+            max={props.max}
+            now={props.max - props.now}
+            key={2}
+            bsClass="progress-bar progress-bar-remaining"
+            isChild
+          />
+        </OverlayTrigger>
+      </div>
     </div>
   );
 };
 
 UtilizationBar.propTypes = {
+  /** Minimal value */
   min: PropTypes.number,
+  /** Maximal value */
   max: PropTypes.number,
+  /** Current value */
   now: PropTypes.number.isRequired,
+  /** Threshold value. Bar will change value to orange if it's surpassed. */
   thresholdWarning: PropTypes.number,
+  /** Threshold value. Bar will change value to red if it's surpassed.  */
   thresholdError: PropTypes.number,
+  /** Function that renders tooltip for available part of bar. */
   availableTooltipFunction: PropTypes.func,
+  /** Function that renders tooltip for used part of bar. */
   usedTooltipFunction: PropTypes.func,
-  mainDivClasses: PropTypes.string
+  /** Classes for main div */
+  mainDivClasses: PropTypes.string,
+  /** Description that is displayed on the right side */
+  description: PropTypes.string,
+  /** Units */
+  label: PropTypes.string,
+  /** If set labels will be placed above utilization bar */
+  descriptionPlacementTop: PropTypes.bool
 };
 
 UtilizationBar.defaultProps = {
   min: 0,
   max: 100,
   availableTooltipFunction: AvailableTooltipFunction,
-  usedTooltipFunction: UsedTooltipFunction
+  usedTooltipFunction: UsedTooltipFunction,
+  descriptionPlacementTop: false
 };
 
 export default UtilizationBar;
--- src/components/UtilizationBar/UtilizationBar.stories.js
+++ src/components/UtilizationBar/UtilizationBar.stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { defaultTemplate } from '../../../storybook/decorators/storyTemplates';
-import { withKnobs, number } from '@storybook/addon-knobs';
+import { withKnobs, number, boolean, text } from '@storybook/addon-knobs';
 import { UtilizationBar } from './index';
 import { Tooltip } from '../Tooltip';
 
@@ -9,7 +9,9 @@ const stories = storiesOf('UtilizationBar', module);
 stories.addDecorator(withKnobs);
 stories.addDecorator(
   defaultTemplate({
-    title: 'Utilization Bar'
+    title: 'Utilization Bar',
+    documentationLink:
+      'http://www.patternfly.org/pattern-library/cards/utilization-bar-card/'
   })
 );
 
@@ -17,27 +19,20 @@ const overriddenTooltip = () => {
   return <Tooltip id="usedTooltip">This tooltip is overridden.</Tooltip>;
 };
 
-stories.addWithInfo(
-  'Utilization Bar types',
-  'Those are the available progress bar types',
-  () => (
-    <div>
-      <h1>Default utilization bar</h1>
-      <UtilizationBar now={42} />
-      <h1>Utilization bar with thresholds</h1>
-      <UtilizationBar
-        now={number('Actual value', 70)}
-        min={number('Minimal value', 0)}
-        max={number('Maximal value', 100)}
-        thresholdWarning={number('Warning threshold value', 40)}
-        thresholdError={number('Error threshold value', 80)}
-      />
-      <h1>Utilization bar with overridden tooltips</h1>
-      <UtilizationBar
-        now={65}
-        availableTooltipFunction={overriddenTooltip}
-        usedTooltipFunction={overriddenTooltip}
-      />
-    </div>
-  )
-);
+stories.addWithInfo('Utilization Bar', 'Utilization Bar', () => (
+  <div>
+    <h1>Utilization bar</h1>
+    <UtilizationBar
+      now={number('Actual value', 70)}
+      min={number('Minimal value', 0)}
+      max={number('Maximal value', 100)}
+      thresholdWarning={number('Warning threshold value', 40)}
+      thresholdError={number('Error threshold value', 80)}
+      availableTooltipFunction={overriddenTooltip}
+      usedTooltipFunction={overriddenTooltip}
+      description={text('Description', 'Label')}
+      label={text('Label', '%')}
+      descriptionPlacementTop={boolean('Description placement', false)}
+    />
+  </div>
+));
