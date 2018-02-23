c926ec9cfea68b84e028e13cbd641ff6f914bf08
Jeffrey Phillips <jephilli@redhat.com>
2018-02-23 11:03:55 -0500

    fix(Button): Allow bsStyle success (#249)

--- src/components/Button/ButtonConstants.js
+++ src/components/Button/ButtonConstants.js
@@ -1,1 +1,9 @@
-export const BUTTON_BS_STYLES = ['danger', 'default', 'primary', 'link'];
+export const BUTTON_BS_STYLES = [
+  'default',
+  'primary',
+  'success',
+  'warning',
+  'danger',
+  'info',
+  'link'
+];
