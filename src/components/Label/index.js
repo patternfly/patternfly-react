177a007b6e23a021da59b4723815b896322de0ee
Karel Hala <ch4rlien@gmail.com>
2018-03-12 19:02:23 +0100

    feat(component): Create Label with remove button (#223)
    fix #222

--- src/components/Label/index.js
+++ src/components/Label/index.js
@@ -1,1 +1,5 @@
+import DisposableLabel from './DisposableLabel';
+import RemoveButton from './RemoveButton';
+
 export { default as Label } from './Label';
+export { DisposableLabel, RemoveButton };
