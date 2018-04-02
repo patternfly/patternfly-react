dac403891203860851dd6e1249d59b8f7c75a5b8
Doug Miller <dmiller9911@gmail.com>
2018-04-02 12:06:59 -0400

    test(Label): Refactor Label tests to use shallow rendering
    * Modified tests under Label to use shallow rendering and targeted snapshots
    * Removed circular dependncy for Label and DisposableLabel.  This fixes importing DisposableLabel directly.
    * Added tests for Label and RemoveButton

--- src/components/Label/Label.js
+++ src/components/Label/Label.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import { Label as BsLabel } from 'react-bootstrap';
-import DisposableLabel from './DisposableLabel';
+import PropTypes from 'prop-types';
+import RemoveButton from './RemoveButton';
 
-const Label = props => {
-  if (props.onRemoveClick) {
-    return <DisposableLabel {...props} />;
-  }
-  return <BsLabel {...props} />;
-};
+const Label = ({ children, onRemoveClick, type, ...rest }) => (
+  <BsLabel bsStyle={type} {...rest}>
+    {children}
+    {!!onRemoveClick && (
+      <RemoveButton onRemoveClick={onRemoveClick} title="Remove" />
+    )}
+  </BsLabel>
+);
 
 Label.propTypes = {
   ...BsLabel.propTypes,
-  ...DisposableLabel.propTypes
+  /** Children nodes */
+  children: PropTypes.node,
+  /** Label type */
+  type: PropTypes.string,
+  /** callback when Label is removed  */
+  onRemoveClick: PropTypes.func
+};
+
+Label.defaultProps = {
+  children: null,
+  type: 'default',
+  onRemoveClick: undefined
 };
 
 export default Label;
