d1aa8ef9c22e425eb70832ea61291756bec8e34a
Doug Miller <doug@dougmiller.io>
2018-04-04 13:42:46 -0400

    test(InfoTip): Update InfoTip tests for more complete coverage (#292)
    Updated InfoTip tests to cover component functionality better.  Switched to use shallow render, and
    avoided calling instance methods directly. Added test suites for each individual component instead
    of 1 test suite for all.
    
    re #287

--- src/components/InfoTip/InfoTipToggle.js
+++ src/components/InfoTip/InfoTipToggle.js
@@ -3,26 +3,17 @@ import PropTypes from 'prop-types';
 import { Dropdown } from '../Dropdown';
 import { Icon } from '../Icon';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class InfoTipToggle extends React.Component {
-  render() {
-    const { children, className, bsRole, bsClass, open, ...props } = this.props;
-
-    return (
-      <a href="#" className={className} aria-expanded={open} {...props}>
-        <Icon type="pf" name="info" /> {children}
-      </a>
-    );
-  }
-}
-
+const InfoTipToggle = ({ bsClass, bsRole, children, open, ...props }) => (
+  <a href="#" aria-expanded={open} {...props}>
+    <Icon type="pf" name="info" /> {children}
+  </a>
+);
 InfoTipToggle.propTypes = {
   ...Dropdown.propTypes,
   children: PropTypes.node,
   open: PropTypes.bool,
   className: PropTypes.string
 };
-
 InfoTipToggle.defaultProps = {
   bsRole: 'toggle', // eslint-disable-line react/default-props-match-prop-types
   children: null,
