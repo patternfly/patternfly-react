d5dabfa5f83910ad201d45a335a0b402f0e6b292
Doug Miller <dmiller9911@gmail.com>
2018-04-09 09:41:18 -0400

    fix(InfoTip): Fix ref errors from react-boostrap for InfoTip components.
    react-bootstrap's Dropdown component passes, and tries to use, a ref passed to InfoTipMenu and
    InfoTipToggle. Since SFC's cannot have refs passed to them, I converted these two components back to
    class components.
    
    fix #297

--- src/components/InfoTip/InfoTipToggle.js
+++ src/components/InfoTip/InfoTipToggle.js
@@ -3,22 +3,32 @@ import PropTypes from 'prop-types';
 import { Dropdown } from '../Dropdown';
 import { Icon } from '../Icon';
 
-const InfoTipToggle = ({ bsClass, bsRole, children, open, ...props }) => (
-  <a href="#" aria-expanded={open} {...props}>
-    <Icon type="pf" name="info" /> {children}
-  </a>
-);
-InfoTipToggle.propTypes = {
-  ...Dropdown.propTypes,
-  children: PropTypes.node,
-  open: PropTypes.bool,
-  className: PropTypes.string
-};
-InfoTipToggle.defaultProps = {
-  bsRole: 'toggle', // eslint-disable-line react/default-props-match-prop-types
-  children: null,
-  open: false,
-  className: ''
-};
+// Must be a class component react-bootstrap passes a ref to this.
+// eslint-disable-next-line react/prefer-stateless-function
+class InfoTipToggle extends React.Component {
+  static propTypes = {
+    ...Dropdown.propTypes,
+    children: PropTypes.node,
+    open: PropTypes.bool,
+    className: PropTypes.string
+  };
+
+  static defaultProps = {
+    bsRole: 'toggle', // eslint-disable-line react/default-props-match-prop-types
+    children: null,
+    open: false,
+    className: ''
+  };
+
+  render() {
+    const { bsClass, bsRole, children, open, ...props } = this.props;
+
+    return (
+      <a href="#" aria-expanded={open} {...props}>
+        <Icon type="pf" name="info" /> {children}
+      </a>
+    );
+  }
+}
 
 export default InfoTipToggle;
