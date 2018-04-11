c3d7a554d00847fc7480e838a63c8fff0d599712
laviro <1ronlavi@gmail.com>
2018-04-11 11:29:20 +0300

    - Add mock for the 'bootstrap-slider-without-jquery' module. - Change snapshot was decreased from 360 lines to 100. - Add test for slide and slideStop

--- src/components/Slider/Slider.test.js
+++ src/components/Slider/Slider.test.js
@@ -1,22 +1,44 @@
 import React from 'react';
-import { mount } from 'enzyme';
-import { Slider } from './index';
-import BootstrapSlider from './BootstrapSlider';
+import { shallow, mount } from 'enzyme';
+import BSSlider from 'bootstrap-slider-without-jquery';
+import Slider from './Slider';
+import BSSliderWrapper from './BootstrapSlider';
 
-test('horizontal slider match snapshot and respond to simulated events', () => {
-  const props = {
-    id: 'slider-pf',
-    label: 'Size',
-    value: 50,
-    min: 0,
-    max: 100,
-    tooltip: 'show',
-    inputFormat: 'MB',
-    dropdownList: ['MB', 'GB']
-  };
+jest.mock('bootstrap-slider-without-jquery');
+
+beforeEach(() => {
+  BSSlider.mockClear();
+});
+
+const props = {
+  id: 'slider-pf',
+  label: 'Size',
+  value: 50,
+  min: 0,
+  max: 100,
+  tooltip: 'show',
+  inputFormat: 'MB',
+  dropdownList: ['MB', 'GB']
+};
+
+test('listens for slide', () => {
+  shallow(<BSSliderWrapper {...props} />);
+  expect(BSSlider.mock.instances[0].on).toBeCalledWith(
+    'slide',
+    expect.any(Function)
+  );
+});
+
+test('listens for slideStop', () => {
+  shallow(<BSSliderWrapper {...props} />);
+  expect(BSSlider.mock.instances[0].on).toBeCalledWith(
+    'slideStop',
+    expect.any(Function)
+  );
+});
+
+test('listens for input change', () => {
   const wrapper = mount(<Slider dropup input showBoundaries {...props} />);
-  expect(wrapper).toMatchSnapshot();
-
   const inputValue = 5;
   wrapper
     .find('.slider-input-pf')
@@ -25,12 +47,9 @@ test('horizontal slider match snapshot and respond to simulated events', () => {
 
   const { value } = wrapper.state();
   expect(value).toEqual(inputValue);
+});
 
-  const BSSlider = mount(<BootstrapSlider {...props} />);
-  const componentWillReceiveProps = jest.spyOn(
-    BSSlider.instance(),
-    'componentWillReceiveProps'
-  );
-  BSSlider.setProps({ value: 60 });
-  expect(componentWillReceiveProps).toHaveBeenCalled();
+test('Slider match snapshot', () => {
+  const wrapper = shallow(<Slider {...props} />);
+  expect(wrapper).toMatchSnapshot();
 });
