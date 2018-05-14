a207c2c7c4fa012c0c0b0842168cb0735465deb8
Mike Ro <mikerodev@gmail.com>
2018-05-14 10:15:19 -0400

    feat(TreeView Component): address accessibility concerns
    affects: patternfly-react
    
    * Add aria roles and attributes
    * Enable keyboard navigation
    
    ISSUES CLOSED: #321

--- packages/patternfly-react/src/components/TreeView/TreeView.js
+++ packages/patternfly-react/src/components/TreeView/TreeView.js
@@ -5,33 +5,68 @@ import { noop } from '../../common/helpers';
 
 import TreeViewNode from './TreeViewNode';
 
-const TreeView = ({
-  nodes,
-  selectNode,
-  highlightOnSelect,
-  highlightOnHover
-}) => {
-  const classes = classNames('list-group', {
-    'treeview-pf-select': highlightOnSelect,
-    'treeview-pf-hover': highlightOnHover
-  });
+class TreeView extends React.Component {
+  state = {
+    focusedNodeId: ''
+  };
 
-  return (
-    <div className="treeview">
-      <ul className={classes}>
-        {nodes &&
-          nodes.map((node, index) => (
-            <TreeViewNode
-              node={node}
-              key={index}
-              level={1}
-              selectNode={selectNode}
-            />
-          ))}
-      </ul>
-    </div>
-  );
-};
+  onFocus = node => {
+    this.setState(() => ({ focusedNodeId: node.id }));
+  };
+
+  onKeyDown = event => {
+    const nodes = this.getVisibleNodes([...this.treeRef.current.children]);
+    const currentNodePosition = nodes.findIndex(
+      element => element.id === this.state.focusedNodeId
+    );
+    if (event.key === 'ArrowDown' && currentNodePosition !== nodes.length - 1) {
+      nodes[currentNodePosition + 1].focus();
+    } else if (event.key === 'ArrowUp' && currentNodePosition !== 0) {
+      nodes[currentNodePosition - 1].focus();
+    }
+  };
+
+  getVisibleNodes = nodes =>
+    nodes.filter(node => !node.className.match(/node-hidden/));
+
+  treeRef = React.createRef();
+
+  render() {
+    const {
+      nodes,
+      selectNode,
+      highlightOnHover,
+      highlightOnSelect
+    } = this.props;
+    const classes = classNames('list-group', {
+      'treeview-pf-select': highlightOnSelect,
+      'treeview-pf-hover': highlightOnHover
+    });
+
+    return (
+      <div className="treeview">
+        <ul
+          className={classes}
+          ref={this.treeRef}
+          onKeyDown={this.onKeyDown}
+          role="tree"
+        >
+          {nodes &&
+            nodes.map((node, index) => (
+              <TreeViewNode
+                node={node}
+                key={index}
+                index={index}
+                level={1}
+                selectNode={selectNode}
+                onFocus={this.onFocus}
+              />
+            ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 TreeView.propTypes = {
   /** Array of node objects */
--- packages/patternfly-react/src/components/TreeView/TreeViewNode.js
+++ packages/patternfly-react/src/components/TreeView/TreeViewNode.js
@@ -7,50 +7,73 @@ import TreeViewIcon from './TreeViewIcon';
 import TreeViewIndents from './TreeViewIndents';
 
 class TreeViewNode extends Component {
-  constructor(props) {
-    super(props);
-
-    // A node can be set to be expanded by default
-    this.state = {
-      expanded:
-        (props.node.hasOwnProperty('state') && props.node.state.expanded) ||
-        false
-    };
-
-    this.toggleExpand = this.toggleExpand.bind(this);
-    this.handleSelect = this.handleSelect.bind(this);
-  }
-
-  // Collapse the current node if any of its parents is collapsed. This should
-  // only fire for nodes that are level 2 or greater
-  componentWillReceiveProps(nextProps) {
+  static getDerivedStateFromProps(nextProps) {
+    // Collapse the current node if any of its parents is collapsed. This should
+    // only fire for nodes that are level 2 or greater
     if (!nextProps.visible && nextProps.level > 1) {
-      this.setState(() => ({ expanded: false }));
+      return { expanded: false };
     }
+    return null;
   }
 
-  handleSelect() {
+  // A node can be set to be expanded by default
+  state = {
+    expanded:
+      (this.props.node.hasOwnProperty('state') &&
+        this.props.node.state.expanded) ||
+      false
+  };
+
+  onKeyPress = e => {
+    e.stopPropagation();
+    if (e.key === ' ') {
+      this.toggleExpandedState();
+    } else if (e.key === 'Enter') {
+      this.handleSelect();
+    }
+  };
+
+  toggleExpandedState = () => {
+    this.setState(prevState => ({ expanded: !prevState.expanded }));
+  };
+
+  toggleExpand = e => {
+    e.stopPropagation();
+    this.toggleExpandedState();
+  };
+
+  handleSelect = () => {
     const { node, selectNode } = this.props;
     if (node.selectable) {
+      this.nodeRef.current.focus();
       selectNode(node);
     }
-  }
+  };
 
-  toggleExpand(e) {
-    e.stopPropagation();
-    this.setState(prevState => ({ expanded: !prevState.expanded }));
-  }
+  nodeRef = React.createRef();
 
   render() {
-    const { node, level, visible, selectNode } = this.props;
+    const { node, level, visible, selectNode, index, onFocus } = this.props;
     const { expanded } = this.state;
     const classes = classNames('list-group-item', {
       'node-hidden': level > 1 ? !visible : false,
       'node-selected': node.selected
     });
+    const tabIndex = index === 0 && level === 1 ? 0 : -1;
+
     return (
       <React.Fragment>
-        <li className={classes} onClick={this.handleSelect}>
+        <li
+          className={classes}
+          onClick={this.handleSelect}
+          onFocus={() => onFocus(this.nodeRef.current)}
+          onKeyPress={this.onKeyPress}
+          ref={this.nodeRef}
+          tabIndex={tabIndex}
+          id={`${level}-${index}`}
+          role={node.nodes ? 'group' : 'treeitem'}
+          aria-expanded={node.nodes && expanded}
+        >
           <TreeViewIndents level={level} />
           <TreeViewExpand
             nodes={node.nodes}
@@ -61,13 +84,15 @@ class TreeViewNode extends Component {
           {node.text}
         </li>
         {node.nodes &&
-          node.nodes.map((childNode, index) => (
+          node.nodes.map((childNode, idx) => (
             <TreeViewNode
               node={childNode}
-              key={index}
+              key={idx}
+              index={idx}
               level={level + 1}
               visible={expanded}
               selectNode={selectNode}
+              onFocus={onFocus}
             />
           ))}
       </React.Fragment>
@@ -79,13 +104,16 @@ TreeViewNode.propTypes = {
   node: PropTypes.object,
   level: PropTypes.number.isRequired,
   visible: PropTypes.bool,
-  selectNode: PropTypes.func
+  selectNode: PropTypes.func,
+  index: PropTypes.number.isRequired,
+  onFocus: PropTypes.func
 };
 
 TreeViewNode.defaultProps = {
   node: {},
   visible: false,
-  selectNode: noop
+  selectNode: noop,
+  onFocus: noop
 };
 
 export default TreeViewNode;
