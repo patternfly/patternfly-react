24b35bc97adbb37adbb5e2b2e9ce7985ccfd02da
Patrick Riley <priley86@gmail.com>
2018-05-29 14:28:09 -0400

    fix(console): update pf-react and yarn.lock for addressing table issues (#369)
    affects: @patternfly/react-console, patternfly-react

--- packages/core/src/components/Chart/DonutChart.js
+++ packages/core/src/components/Chart/DonutChart.js
@@ -22,7 +22,7 @@ const setDonutTitle = obj => {
 
   switch (title.type) {
     case 'percent':
-      primary = `${Math.round(100 * columns[iMax][1] / sum).toString()}%`;
+      primary = `${Math.round((100 * columns[iMax][1]) / sum).toString()}%`;
       [secondary] = columns[iMax];
       break;
     case 'max':
@@ -52,10 +52,11 @@ const addDonutTitle = lifecycle({
   }
 });
 
-const DonutChart = compose(getComposer('DONUT_CHART'), addDonutTitle)(
-  ({ className, type, data, ...props }) => (
-    <C3Chart className={className} type={type} data={data} {...props} />
-  )
-);
+const DonutChart = compose(
+  getComposer('DONUT_CHART'),
+  addDonutTitle
+)(({ className, type, data, ...props }) => (
+  <C3Chart className={className} type={type} data={data} {...props} />
+));
 
 export default DonutChart;
