b4de1c02f18ce46bf081cd6208596427ff7df58c
PanSpagetka <rknaur@redhat.com>
2018-06-12 12:50:19 +0200

    Use storybook default template

--- packages/tagging/src/stories/TaggingStoryComponent.stories.js
+++ packages/tagging/src/stories/TaggingStoryComponent.stories.js
@@ -3,7 +3,16 @@ import { storiesOf } from '@storybook/react';
 import TaggingStoryComponent from '../components/InnerComponents/TaggingStoryComponent';
 import { name } from '../../package.json';
 import './index.scss';
+import { withInfo } from '@storybook/addon-info';
+import { defaultTemplate, inlineTemplate } from 'storybook/decorators/storyTemplates';
 
-storiesOf(`${name}/Tagging`, module).add('Tagging example', () => (
-  <TaggingStoryComponent />
-));
+const stories = storiesOf(`${name}/Tagging`, module);
+stories.addDecorator(
+  defaultTemplate({
+    title: 'Tagging',
+    description:
+      'This is an example of the Tagging component.'
+  })
+);
+
+stories.addWithInfo('TaggingStoryComponent', () => <TaggingStoryComponent />);
