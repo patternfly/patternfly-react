0334b80b010905c03485d1bd2ccf7ea444ccaaa9
Gilad Lekner <gilad215@gmail.com>
2018-06-14 18:49:47 +0300

    feat(ApplicationLauncher): application Launcher Component (#319)
    affects: patternfly-react
    
    fix #184
    
    ISSUES CLOSED: #184

---
+++ packages/core/src/components/ApplicationLauncher/Stories/index.js
@@ -0,0 +1,6 @@
+export {
+  default as NavApplicationLauncherStory
+} from './NavApplicationLauncherStory';
+export {
+  default as WrapperNavApplicationLauncherStory
+} from './WrapperNavApplicationLauncherStory';
---
+++ packages/core/src/components/ApplicationLauncher/index.js
@@ -0,0 +1,12 @@
+import ApplicationLauncherToggle from './ApplicationLauncherToggle';
+import ApplicationLauncher from './ApplicationLauncher';
+import ApplicationLauncherItem from './ApplicationLauncherItem';
+
+ApplicationLauncher.Toggle = ApplicationLauncherToggle;
+ApplicationLauncher.Item = ApplicationLauncherItem;
+
+export {
+  ApplicationLauncher,
+  ApplicationLauncherItem,
+  ApplicationLauncherToggle
+};
