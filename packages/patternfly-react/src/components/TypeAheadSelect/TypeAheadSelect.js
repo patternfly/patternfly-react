ec3d025a345caa6bfb838a3219db0584f3a33848
Pavel Odvody <podvody@redhat.com>
2018-06-25 15:22:18 +0200

    Do not wrap Typeahed into functional component
    The original component is class-based an allows `ref` to
    be controlled and we want to preserve that behavior
    
    Fixes #427

--- packages/patternfly-react/src/components/TypeAheadSelect/TypeAheadSelect.js
+++ packages/patternfly-react/src/components/TypeAheadSelect/TypeAheadSelect.js
@@ -1,6 +1,1 @@
-import React from 'react';
-import { Typeahead } from 'react-bootstrap-typeahead';
-
-const TypeAheadSelect = props => <Typeahead {...props} />;
-
-export default TypeAheadSelect;
+export { Typeahead as TypeAheadSelect } from 'react-bootstrap-typeahead';
