47dd2cfbb36a7b8803da83e01a52b85ee2063256
Karel Hala <khala@redhat.com>
2018-06-29 15:38:36 +0200

    feat(react-core/Badge): introduce patternfly 4 badge component
    affects: @patternfly/react-core, @patternfly/react-docs
    
    ISSUES CLOSED: 384

---
+++ packages/react-core/src/components/Badge/Badge.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { css } from '@patternfly/react-styles';
+import styles from '@patternfly/patternfly-next/components/Badge/styles.css';
+
+export const propTypes = {
+  isRead: PropTypes.bool,
+  children: PropTypes.node,
+  className: PropTypes.string
+};
+
+export const defaultProps = {
+  isRead: false,
+  children: '',
+  className: ''
+};
+
+const Badge = ({ isRead, className, children, ...props }) => (
+  <span
+    {...props}
+    className={css(
+      styles.badge,
+      isRead ? styles.modifiers.read : styles.modifiers.unread,
+      className
+    )}
+  >
+    {children}
+  </span>
+);
+
+Badge.propTypes = propTypes;
+Badge.defaultProps = defaultProps;
+export default Badge;
--- packages/react-core/src/components/index.js
+++ packages/react-core/src/components/index.js
@@ -1,2 +1,3 @@
+export * from './Badge';
 export * from './Button';
 export * from './Title';
