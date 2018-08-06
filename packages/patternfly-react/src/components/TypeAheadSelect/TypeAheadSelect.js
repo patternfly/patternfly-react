fa16685fee3b2145db4569cfaf8b8fdaedf69e23
Ron Lavi <26363699+Laviro@users.noreply.github.com>
2018-08-06 22:07:56 +0300

    fix(TypeAheadSelect): expose inner components of react-bootstrap-typeahead
    affects: patternfly-react

--- packages/patternfly-react/src/components/TypeAheadSelect/TypeAheadSelect.js
+++ packages/patternfly-react/src/components/TypeAheadSelect/TypeAheadSelect.js
@@ -1,1 +1,22 @@
-export { Typeahead as TypeAheadSelect } from 'react-bootstrap-typeahead';
+import {
+  Typeahead as TypeAheadSelect,
+  Highlighter,
+  Menu,
+  MenuItem,
+  Token,
+  TypeaheadMenu,
+  asyncContainer,
+  menuItemContainer,
+  tokenContainer
+} from 'react-bootstrap-typeahead';
+
+TypeAheadSelect.Highlighter = Highlighter;
+TypeAheadSelect.Menu = Menu;
+TypeAheadSelect.MenuItem = MenuItem;
+TypeAheadSelect.Token = Token;
+TypeAheadSelect.TypeaheadMenu = TypeaheadMenu;
+TypeAheadSelect.asyncContainer = asyncContainer;
+TypeAheadSelect.menuItemContainer = menuItemContainer;
+TypeAheadSelect.tokenContainer = tokenContainer;
+
+export default TypeAheadSelect;
