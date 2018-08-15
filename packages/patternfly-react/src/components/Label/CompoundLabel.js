8562a163ebe35028bb9df8adcb0bc040b584de5c
PanSpagetka <rknaur@redhat.com>
2018-08-15 13:01:25 +0200

    Add description and allow set overlayPlacement in props

--- packages/patternfly-react/src/components/Label/CompoundLabel.js
+++ packages/patternfly-react/src/components/Label/CompoundLabel.js
@@ -14,6 +14,7 @@ class CompoundLabel extends React.Component {
       truncate={this.props.valueTruncate}
       bsStyle={this.props.bsStyle}
       className={this.props.innerClassName}
+      overlayPlacement={this.props.overlayPlacement}
     />
   );
 
@@ -25,7 +26,10 @@ class CompoundLabel extends React.Component {
     );
     return (
       <span className="label label-primary compound-label-pf">
-        <OverlayTrigger placement="bottom" overlay={categoryTooltip}>
+        <OverlayTrigger
+          placement={this.props.overlayPlacement}
+          overlay={categoryTooltip}
+        >
           <span className="category-label-pf">
             {this.props.categoryTruncate(this.props.category.label)}
           </span>
@@ -41,22 +45,34 @@ class CompoundLabel extends React.Component {
 }
 
 CompoundLabel.propTypes = {
+  /** Category in CATEGORY: value(s) pair */
+  /**  Parent of label, it does not get displayed in this component */
   category: PropTypes.shape({
     id: PropTypes.any.isRequired,
     label: PropTypes.string.isRequired
   }).isRequired,
+  /** Array of Values in Category:VALUE(S) pair  */
+  /** id uniquily identify value within its category, label is text which is displayed */
   values: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.any.isRequired,
       label: PropTypes.string.isRequired
     }).isRequired
   ).isRequired,
+  /** Fuction callback called when X button is clicked */
   onDeleteClick: PropTypes.func.isRequired,
+  /** Function used to truncate category label */
   categoryTruncate: PropTypes.func,
+  /** Function used to truncate value label */
   valueTruncate: PropTypes.func,
+  /** Name of CSS class(es) which are set to outer label */
   className: PropTypes.string,
+  /** Bootstrap style which is set to label */
   bsStyle: PropTypes.string,
-  innerClassName: PropTypes.string
+  /** Name of CSS class(es) which are set to inner label(s) */
+  innerClassName: PropTypes.string,
+  /** Placement of the overlay */
+  overlayPlacement: PropTypes.oneOf(['top', 'right', 'bottom', 'left'])
 };
 
 CompoundLabel.defaultProps = {
@@ -65,7 +81,8 @@ CompoundLabel.defaultProps = {
   valueTruncate: str => (str.length > 18 ? `${str.substring(0, 18)}...` : str),
   className: '',
   bsStyle: 'primary',
-  innerClassName: ''
+  innerClassName: '',
+  overlayPlacement: 'bottom'
 };
 
 export default CompoundLabel;
--- packages/patternfly-react/src/components/Label/LabelWithTooltip.js
+++ packages/patternfly-react/src/components/Label/LabelWithTooltip.js
@@ -12,10 +12,11 @@ const LabelWithTooltip = ({
   value,
   truncate,
   bsStyle,
-  className
+  className,
+  overlayPlacement
 }) => (
   <li key={value.id}>
-    <OverlayTrigger placement="bottom" overlay={tooltip(value.label)}>
+    <OverlayTrigger placement={overlayPlacement} overlay={tooltip(value.label)}>
       <Label
         key={value.id}
         onRemoveClick={() => onDeleteClick(category, value)}
@@ -29,22 +30,33 @@ const LabelWithTooltip = ({
 );
 
 LabelWithTooltip.propTypes = {
+  /** Fuction callback called when X button is clicked */
   onDeleteClick: PropTypes.func.isRequired,
+  /** Category in CATEGORY: value(s) pair */
+  /**  Parent of label, it does not get displayed in this component */
   category: PropTypes.shape({
     id: PropTypes.any.isRequired,
     label: PropTypes.string.isRequired
   }).isRequired,
+  /** Individual Value in Category:VALUE(s) pair  */
+  /** id uniquily identify value within its category, label is text which is displayed */
   value: PropTypes.PropTypes.shape({
     id: PropTypes.any.isRequired,
     label: PropTypes.string.isRequired
   }).isRequired,
+  /** Function used to truncate value label */
   truncate: PropTypes.func.isRequired,
+  /** Name of CSS class(es) which are set to label */
   className: PropTypes.string,
-  bsStyle: PropTypes.string
+  /** Bootstrap style which is set to label */
+  bsStyle: PropTypes.string,
+  /** Placement of the overlay */
+  overlayPlacement: PropTypes.oneOf(['top', 'right', 'bottom', 'left'])
 };
 
 LabelWithTooltip.defaultProps = {
   className: '',
-  bsStyle: 'primary'
+  bsStyle: 'primary',
+  overlayPlacement: 'bottom'
 };
 export default LabelWithTooltip;
