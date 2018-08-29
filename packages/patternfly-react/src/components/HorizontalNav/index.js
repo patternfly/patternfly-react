8e17ef609fc80d783f17b1360ad7d334ce7ae0a8
Alberto Gutierrez <aljesusg@gmail.com>
2018-08-29 19:24:36 +0200

    feat(HorizontalNav): Add Horizontal Navigation (#171)
    affects: patternfly-react

---
+++ packages/patternfly-react/src/components/HorizontalNav/index.js
@@ -0,0 +1,3 @@
+export { default as HorizontalNav } from './HorizontalNav';
+export { default as HorizontalNavMenu } from './HorizontalNavMenu';
+export { default as HorizontalNavMenuItem } from './HorizontalNavMenuItem';
