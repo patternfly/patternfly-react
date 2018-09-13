8c5d6dddf7274f02779efae5556483326c2c4d88
Jeffrey Phillips <jephilli@redhat.com>
2018-09-13 10:46:53 -0400

    feat(vertical-tabs): add sub-tab support, with restricted mode (#611)
    affects: patternfly-react-extensions
    
    This adds the necessary properties and css for using sub-tabs in the VerticalTabs component.
    Restricted mode resticts the tabs being shown to the active tab along with its parents, sibilings,
    and direct sub tabs.

--- packages/patternfly-3/patternfly-react-extensions/src/components/VerticalTabs/VerticalTabs.js
+++ packages/patternfly-3/patternfly-react-extensions/src/components/VerticalTabs/VerticalTabs.js
@@ -6,8 +6,8 @@ import VerticalTabsTab from './VerticalTabsTab';
 /**
  * VerticalTabs Component for PatternFly
  */
-const VerticalTabs = ({ children, className, ...props }) => {
-  const classes = classNames('vertical-tabs-pf', className);
+const VerticalTabs = ({ children, className, restrictTabs, activeTab, ...props }) => {
+  const classes = classNames('vertical-tabs-pf', { 'restrict-tabs': restrictTabs, 'active-tab': activeTab }, className);
   return (
     <ul className={classes} {...props}>
       {children}
@@ -19,11 +19,17 @@ VerticalTabs.propTypes = {
   /** Children nodes */
   children: PropTypes.node,
   /** Additional css classes */
-  className: PropTypes.string
+  className: PropTypes.string,
+  /** Flag to restrict shown tabs to active tabs, their parents, their siblings, and direct children */
+  restrictTabs: PropTypes.bool,
+  /** Flag if a direct child is active (only used in restrictTabs mode) */
+  activeTab: PropTypes.bool
 };
 VerticalTabs.defaultProps = {
   children: null,
-  className: ''
+  className: '',
+  restrictTabs: false,
+  activeTab: false
 };
 
 VerticalTabs.Tab = VerticalTabsTab;
--- packages/patternfly-3/patternfly-react-extensions/src/components/VerticalTabs/VerticalTabs.stories.js
+++ packages/patternfly-3/patternfly-react-extensions/src/components/VerticalTabs/VerticalTabs.stories.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info/dist/index';
-import { defaultTemplate } from 'storybook/decorators/storyTemplates';
+import { inlineTemplate } from 'storybook/decorators/storyTemplates';
 import { storybookPackageName, STORYBOOK_CATEGORY } from 'storybook/constants/siteConstants';
 import { VerticalTabs, VerticalTabsTab } from './index';
 import { MockVerticalTabsExample, MockVerticalTabsExampleSource } from './_mocks_/mockVerticalTabsExample';
 
 import { name } from '../../../package.json';
+import { boolean, withKnobs } from '@storybook/addon-knobs';
 
 const stories = storiesOf(`${storybookPackageName(name)}/${STORYBOOK_CATEGORY.WIDGETS}/Vertical Tabs`, module);
 
-stories.addDecorator(
-  defaultTemplate({
-    title: 'Vertical Tabs'
-  })
+const description = (
+  <div>
+    Vertical tabs can be used in a restricted mode by setting the <b>restrictTabs</b> flag on the <i>VerticalTabs</i>{' '}
+    component. This is useful when trying to restrict the vertical space used by the <i>VerticalTabs</i> component.
+    <br />
+    <br />
+    When not in restricted mode, all tabs are shown. In restricted mode, only the active tab along with the path
+    (parents) siblings of the active tab, and first level of children for the active tab are shown.
+    <br />
+    <br />
+    The application is responsible for setting the <b>activeTab</b> property (meaning a direct child is active) on the
+    <i>VerticalTab</i> as well as the <b>active</b> (this tab is active) and <b>hasActiveDescendant</b> (a child of this
+    tab is active) properties on the <i>VerticalTab.Tab</i>. Setting the <b>shown</b> property on a
+    <i>VerticalTab.Tab</i> will always show the tab if its parent tab is shown.
+  </div>
 );
-
+stories.addDecorator(withKnobs);
 stories.add(
   'Vertical Tabs',
   withInfo({
@@ -28,5 +40,13 @@ stories.add(
         <pre>{MockVerticalTabsExampleSource}</pre>
       </div>
     )
-  })(() => <MockVerticalTabsExample />)
+  })(() => {
+    const restrictTabs = boolean('Restrict Tabs', false);
+    const story = <MockVerticalTabsExample restrictTabs={restrictTabs} />;
+    return inlineTemplate({
+      title: 'Vertical Tabs',
+      description,
+      story
+    });
+  })
 );
