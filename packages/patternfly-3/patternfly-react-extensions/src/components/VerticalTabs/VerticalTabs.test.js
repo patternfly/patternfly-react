0a62e33860a31dd5e596fc262a1818e04b5039a6
Jeffrey Phillips <jephilli@redhat.com>
2018-09-18 13:22:16 -0400

    chore(tests): fix test warnings in vertical tabs component
    affects: patternfly-react-extensions

--- packages/patternfly-3/patternfly-react-extensions/src/components/VerticalTabs/VerticalTabs.test.js
+++ packages/patternfly-3/patternfly-react-extensions/src/components/VerticalTabs/VerticalTabs.test.js
@@ -7,22 +7,30 @@ test('Vertical Tabs renders tabs properly', () => {
     <VerticalTabs id="vertical-tabs" className="test-vertical-tabs">
       <VerticalTabs.Tab id="all" className="test-vertical-tabs-tab" title="All" />
       <VerticalTabs.Tab id="one" title="Tab One">
-        <VerticalTabs.Tab key="one-one" id="one-one" title="Tab One-One">
-          <VerticalTabs.Tab key="one-one-one" id="one-one-one" title="Tab One-One-One" active />
-          <VerticalTabs.Tab key="one-one-two" id="one-one-two" title="Tab One-One-Two" active={false} />
-          <VerticalTabs.Tab key="one-one-three" id="one-one-three" title="Tab One-One-Three" />
-        </VerticalTabs.Tab>
-        <VerticalTabs.Tab key="one-two" id="one-two" title="Tab One-Two" />
-        <VerticalTabs.Tab key="one-three" id="one-three" title="Tab One-Three" />
+        <VerticalTabs>
+          <VerticalTabs.Tab key="one-one" id="one-one" title="Tab One-One">
+            <VerticalTabs activeTab>
+              <VerticalTabs.Tab key="one-one-one" id="one-one-one" title="Tab One-One-One" active />
+              <VerticalTabs.Tab key="one-one-two" id="one-one-two" title="Tab One-One-Two" active={false} />
+              <VerticalTabs.Tab key="one-one-three" id="one-one-three" title="Tab One-One-Three" />
+            </VerticalTabs>
+          </VerticalTabs.Tab>
+          <VerticalTabs.Tab key="one-two" id="one-two" title="Tab One-Two" />
+          <VerticalTabs.Tab key="one-three" id="one-three" title="Tab One-Three" />
+        </VerticalTabs>
       </VerticalTabs.Tab>
       <VerticalTabs.Tab id="two" title="Tab Two" className="test-tab-class">
-        <VerticalTabs.Tab key="one-one-one" id="two-one" title="Tab Two-One" />
-        <VerticalTabs.Tab key="one-one-two" id="two-two" title="Tab Two-Two" />
-        <VerticalTabs.Tab key="one-one-three" id="two-three" title="Tab Two-Three" />
+        <VerticalTabs>
+          <VerticalTabs.Tab key="one-one-one" id="two-one" title="Tab Two-One" />
+          <VerticalTabs.Tab key="one-one-two" id="two-two" title="Tab Two-Two" />
+          <VerticalTabs.Tab key="one-one-three" id="two-three" title="Tab Two-Three" />
+        </VerticalTabs>
       </VerticalTabs.Tab>
       <VerticalTabs.Tab id="three" title="Tab Three">
-        <VerticalTabs.Tab key="three-one" id="three-one" title="Tab Three-One" />
-        <VerticalTabs.Tab key="three-two" id="three-two" title="Tab Three-Two" />
+        <VerticalTabs>
+          <VerticalTabs.Tab key="three-one" id="three-one" title="Tab Three-One" />
+          <VerticalTabs.Tab key="three-two" id="three-two" title="Tab Three-Two" />
+        </VerticalTabs>
       </VerticalTabs.Tab>
       <VerticalTabs.Tab id="four" title="Tab Four" />
       <VerticalTabs.Tab id="five" title="Tab Five" />
