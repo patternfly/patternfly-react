936b655f8bf147cc9c3d5b310588d85a15fa46fc
Amineta Lo <amilo@redhat.com>
2018-09-19 14:42:38 -0400

    feat(pf4-brand): introduce brand component
    affects: @patternfly/react-core, @patternfly/react-docs

--- packages/patternfly-4/react-core/src/components/Avatar/examples/SimpleAvatar.js
+++ packages/patternfly-4/react-core/src/components/Avatar/examples/SimpleAvatar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Avatar } from '@patternfly/react-core';
-import avatarImg from '../../../../../react-docs/src/assets/img_avatar.png';
+import avatarImg from './img_avatar.png';
 
 class SimpleAvatar extends React.Component {
   static title = 'Simple Avatar';
---
+++ packages/patternfly-4/react-core/src/components/Brand/Brand.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Brand from './Brand';
+
+test('simple brand', () => {
+  const view = shallow(<Brand alt="brand" />);
+  expect(view).toMatchSnapshot();
+});
