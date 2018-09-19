263b800df5e088957e3f96c8ffd9b7d854eca9d3
Joachim Schuler <jschuler@redhat.com>
2018-09-19 18:13:17 -0400

    fix(@patternfly/react-docs): use emotion to load in global pf core styles into react-docs
    affects: @patternfly/react-docs

--- packages/patternfly-4/react-docs/gatsby-ssr.js
+++
@@ -1,8 +0,0 @@
-import { renderToString } from 'react-dom/server';
-import { renderStatic } from '@patternfly/react-styles/server';
-
-exports.replaceRenderer = ({ bodyComponent, replaceBodyHTMLString, setHeadComponents }) => {
-  const { html } = renderStatic(() => renderToString(bodyComponent));
-
-  replaceBodyHTMLString(html);
-};
--- packages/patternfly-4/react-docs/src/layouts/index.js
+++ packages/patternfly-4/react-docs/src/layouts/index.js
@@ -1,12 +1,18 @@
-// This is a gatsby limitation will be fixed in newer version
-// eslint-disable-next-line
-import '@patternfly/react-core/../dist/styles/base.css';
-import './index.css';
 import React from 'react';
 import Helmet from 'react-helmet';
 import Page from '../components/page';
 import Navigation from '../components/navigation';
 import PropTypes from 'prop-types';
+
+// This is a gatsby limitation will be fixed in newer version
+const globalStyles = require(`!raw-loader!@patternfly/react-core/../dist/styles/base.css`);
+import { injectGlobal } from 'emotion';
+
+// eslint-disable-next-line
+injectGlobal`${globalStyles}`;
+
+// Load other styles after global styles
+import './index.css';
 
 const propTypes = {
   children: PropTypes.func.isRequired,
