d5f94c89816a8d3c8a976ab7e592c5e44bb081aa
Dana Gutride <dgutride@redhat.com>
2018-09-21 22:01:48 -0400

    Revert "fix(logging): add travis loggin for list"
    This reverts commit c15c228ef7c94e8e4486b271415407a221a17a9a.

--- scripts/test.sh
+++
@@ -1,9 +0,0 @@
-DEPLOY_DOMAIN=4-pr-patternfly-react-dgutride.surge.sh
-TRAVIS_PULL_REQUEST=true
-ALREADY_DEPLOYED=`surge list | grep $DEPLOY_DOMAIN`
-if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -z "${ALREADY_DEPLOYED// }" ]
-then
-  echo 'hi mom'
-fi
-
-echo 'Already deployed:' $ALREADY_DEPLOYED
