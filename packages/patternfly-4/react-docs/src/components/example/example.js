514ce425b91d6f77d30dd1f49e003a26f9bd9415
Dan Labrecque <dlabrecq@redhat.com>
2018-09-28 10:24:34 -0400

    feat(full-page): add functionality to show components in full page mode (#658)
    If the fullPageOnly property is added to any `.docs.js` file, Gatsby will render a link to show the example in full page mode, similar to patternfly-next.
    
    BackgroundImage.docs.js Example:
    ```
    export default {
      title: 'Background Image',
      components: {
        BackgroundImage
      },
      examples: [SimpleBackgroundImage],
      fullPageOnly: true
    };
    ```
    
    affects: @patternfly/react-docs

--- packages/patternfly-4/react-docs/src/components/example/example.js
+++ packages/patternfly-4/react-docs/src/components/example/example.js
@@ -4,12 +4,15 @@ import styles from './example.styles';
 import PropTypes from 'prop-types';
 import { Title } from '@patternfly/react-core';
 import LiveDemo from './liveDemo';
+import Link from 'gatsby-link';
 
 const propTypes = {
   children: PropTypes.node.isRequired,
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
   description: PropTypes.string,
   className: PropTypes.string,
+  fullPageOnly: PropTypes.bool,
+  name: PropTypes.string,
   raw: PropTypes.string,
   images: PropTypes.array
 };
@@ -17,25 +20,46 @@ const propTypes = {
 const defaultProps = {
   className: '',
   description: '',
+  fullPageOnly: false,
+  title: '',
+  name: '',
   raw: '',
   images: []
 };
 
 const LIVE_EXAMPLES = /true/i.test(process.env.LIVE_EXAMPLES);
 
-const Example = ({ children, title, className, description, raw, images, ...props }) => (
-  <div>
-    <Title size="lg">{title}</Title>
-    {Boolean(description) && <p className={css(styles.description)}>{description}</p>}
-    {LIVE_EXAMPLES ? (
-      <LiveDemo raw={raw.trim()} images={images} className={className} />
-    ) : (
+const Example = ({ children, title, className, description, name, fullPageOnly, raw, images, ...props }) => {
+  // Display full page link
+  if (fullPageOnly) {
+    const pathName = typeof window !== 'undefined' ? `${window.location.pathname}` : '';
+    const exampleName = name.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+    const separator = pathName.endsWith('/') ? '' : '/';
+    const path = `${pathName}${separator}examples/${exampleName}`;
+    return (
+      <div className={css(className, styles.example)} {...props}>
+        This layout can only be accessed in&nbsp;
+        <Link target="_blank" to={path}>
+          full page mode
+        </Link>
+        .
+      </div>
+    );
+  }
+  return (
+    <div>
+      <Title size="lg">{title}</Title>
+      {Boolean(description) && <p className={css(styles.description)}>{description}</p>}
+      {LIVE_EXAMPLES ? (
+        <LiveDemo raw={raw.trim()} images={images} className={className} />
+      ) : (
         <div className={css(className, styles.example)} {...props}>
           {children}
         </div>
       )}
-  </div>
-);
+    </div>
+  );
+};
 
 Example.propTypes = propTypes;
 Example.defaultProps = defaultProps;
