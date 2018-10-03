c690b3e95660119a707b690d6462912dfccda97f
boaz1337 <ripcurld.github@gmail.com>
2018-10-03 22:43:08 +0300

    fix(pf-4): checkboxProps to be consistent with Checkbox.propTypes (#696)
    affects: patternfly4-react-lerna-root, @patternfly/react-core
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- packages/patternfly-4/react-core/src/components/Checkbox/Checkbox.d.ts
+++ packages/patternfly-4/react-core/src/components/Checkbox/Checkbox.d.ts
@@ -3,9 +3,9 @@ import { Omit } from '../../typeUtils';
 
 export interface CheckboxProps
   extends Omit<HTMLProps<HTMLInputElement>, 'type' | 'onChange' | 'disabled'> {
-  isDisabled: boolean;
-  isValid: boolean;
-  onChange(checked: boolean, event: FormEvent<HTMLInputElement>): void;
+  isDisabled?: boolean;
+  isValid?: boolean;
+  onChange?(checked: boolean, event: FormEvent<HTMLInputElement>): void;
 }
 
 declare const Checkbox: React.SFC<CheckboxProps>;
