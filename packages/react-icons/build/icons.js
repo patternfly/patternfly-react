bdc00c48ae86ec4d121c31c93534e53124930da1
Karel Hala <ch4rlien@gmail.com>
2018-10-05 15:57:01 +0200

    feat(pf-icons): add all PF icons and update font awesome pckg (#654)
    affects: @patternfly/react-icons
    
    ISSUES CLOSED: #730

--- packages/react-icons/build/icons.js
+++ packages/react-icons/build/icons.js
@@ -1,7 +1,9 @@
 const { fas } = require('@fortawesome/free-solid-svg-icons');
-
+const { pfIcons } = require('./pfIcons');
 module.exports = {
   fontAwesome: {
     solid: Object.keys(fas)
-  }
+  },
+
+  pfIcons
 };
