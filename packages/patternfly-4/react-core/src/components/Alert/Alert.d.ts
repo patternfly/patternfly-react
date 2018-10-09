a899d9188db326cf1d639b02ac5ac3b02426d1e0
Boaz Shuster <boaz.shuster.github@gmail.com>
2018-10-09 18:44:58 +0300

    fix(Alert): [TS] action type is string while it should've been node
    When adding an action button to Alert Typescript fails to
    compile saying action is supposed to be a string.
    
    This commit fixes Alert.d.ts so action accepts ReactNode.
    
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- packages/patternfly-4/react-core/src/components/Alert/Alert.d.ts
+++ packages/patternfly-4/react-core/src/components/Alert/Alert.d.ts
@@ -1,5 +1,5 @@
 import { SFC, HTMLProps, ReactNode } from 'react';
-import { OneOf } from '../../typeUtils';
+import { OneOf, Omit } from '../../typeUtils';
 
 export const AlertVariant: {
   success: 'success';
@@ -8,9 +8,10 @@ export const AlertVariant: {
   info: 'info';
 };
 
-export interface AlertProps extends HTMLProps<HTMLDivElement> {
+export interface AlertProps extends Omit<HTMLProps<HTMLDivElement>, 'action'> {
   variant: OneOf<typeof AlertVariant, keyof typeof AlertVariant>;
   children?: ReactNode;
+  action?: ReactNode;
 }
 
 declare const Alert: SFC<AlertProps>;
