40faaff053b1dbb5c1bc79ce666e8762cc66e326
Dan Labrecque <dlabrecq@redhat.com>
2018-10-12 10:41:12 -0400

    fix(react-docs): fix broken image paths for patternfly-4 docs

--- .travis/release.sh
+++ .travis/release.sh
@@ -29,6 +29,7 @@ yarn storybook:build
 yarn build:prdocs
 cp -r .out/ .public/patternfly-3
 cp -r packages/patternfly-4/react-docs/public .public/patternfly-4
+cp -r packages/patternfly-4/react-docs/public/assets .public/assets
 
 if [ "${TRAVIS_REPO_SLUG}" != "${TRIGGER_REPO_SLUG}" -o "${TRAVIS_BRANCH}" != "${TRIGGER_REPO_BRANCH}" ]; then
   echo -e "${RED}Exiting, this is not a production release.${NC}"
