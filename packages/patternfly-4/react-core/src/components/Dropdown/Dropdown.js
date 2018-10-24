b142e6fd33102fac05bbb522e497fd1ee0ec11f7
Titani Labaj <tlabaj@redhat.com>
2018-10-24 12:29:05 -0400

    Remove commented code

--- packages/patternfly-4/react-core/src/components/Dropdown/Dropdown.js
+++ packages/patternfly-4/react-core/src/components/Dropdown/Dropdown.js
@@ -2,7 +2,6 @@ import React, { Children, cloneElement } from 'react';
 import styles from '@patternfly/patternfly-next/components/Dropdown/dropdown.css';
 import { css } from '@patternfly/react-styles';
 import PropTypes from 'prop-types';
-// import FocusTrap from 'focus-trap-react';
 import DropdownMenu from './DropdownMenu';
 
 export const DropdownPosition = {
