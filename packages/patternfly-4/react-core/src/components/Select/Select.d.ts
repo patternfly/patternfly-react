7329bf5c23d08e86b42b48ac6072bef2e7316f3c
Boaz Shuster <boaz.shuster.github@gmail.com>
2018-10-25 19:27:07 +0300

    fix(Select/TextInput): fix components typescript interfaces
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- packages/patternfly-4/react-core/src/components/Select/Select.d.ts
+++ packages/patternfly-4/react-core/src/components/Select/Select.d.ts
@@ -6,9 +6,9 @@ export interface SelectProps extends Omit<HTMLProps<HTMLInputElement>, 'onChange
   value?: any;
   isValid?: boolean;
   isDisabled?: boolean;
-  onBlur(event: React.FormEvent<HTMLSelectElement>): void;
-  onFocus(event: React.FormEvent<HTMLSelectElement>): void;
-  onChange(event: React.FormEvent<HTMLSelectElement>): void;
+  onBlur?(event: React.FormEvent<HTMLSelectElement>): void;
+  onFocus?(event: React.FormEvent<HTMLSelectElement>): void;
+  onChange?(event: React.FormEvent<HTMLSelectElement>): void;
 }
 
 declare const Select: React.SFC<SelectProps>;
