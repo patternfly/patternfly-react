d78653f532470665c296ecf7ee62319500b4bee7
Amineta Lo <amilo@redhat.com>
2018-11-06 15:11:00 -0500

    updated snapshot and use tokens for spacing

--- packages/patternfly-4/react-core/src/components/LoginPage/LoginBoxHeader.d.ts
+++ packages/patternfly-4/react-core/src/components/LoginPage/LoginBoxHeader.d.ts
@@ -3,9 +3,9 @@ import { SFC, HTMLProps, ReactNode } from 'react';
 export interface LoginBoxHeaderProps extends HTMLProps<HTMLDivElement> {
   children?: ReactNode;
   className?: string;
-  dropdown: ReactNode;
-  title: string;
-  subtitle: string;
+  dropdown?: ReactNode;
+  title?: string;
+  subtitle?: string;
 }
 
 declare const LoginBoxHeader: SFC<LoginBoxHeaderProps>;
--- packages/patternfly-4/react-core/src/components/LoginPage/LoginBoxHeader.js
+++ packages/patternfly-4/react-core/src/components/LoginPage/LoginBoxHeader.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { StyleSheet } from '@patternfly/react-styles';
 import { Title } from '../Title';
 import { css } from '@patternfly/react-styles';
 import styles from '@patternfly/patternfly-next/components/LoginBox/login-box.css';
-import spacing from '@patternfly/patternfly-next/utilities/Spacing/spacing.css';
+import { global_spacer_md, global_spacer_sm } from '@patternfly/react-tokens';
 import displayStyles from '@patternfly/patternfly-next/utilities/Display/display.css';
 import flexAlignment from '@patternfly/patternfly-next/utilities/Flex/flex.css';
 
@@ -13,26 +14,38 @@ const propTypes = {
   /** Additional classes added to the LoginBox Header */
   className: PropTypes.string,
   /** Dropdown component for the LoginBox Header */
-  dropdown: PropTypes.node.isRequired,
+  dropdown: PropTypes.node,
   /** Title for the LoginBox Header */
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
   /** Subtitle that contains the Text, URL, and URL Text for the LoginBox Header */
-  subtitle: PropTypes.node.isRequired
+  subtitle: PropTypes.node
 };
 
 const defaultProps = {
   children: null,
-  className: ''
+  className: '',
+  dropdown: null,
+  title: '',
+  subtitle: ''
 };
+
+const tokenStyles = StyleSheet.create({
+  title: {
+    marginBottom: global_spacer_sm.var
+  },
+  text: {
+    marginLeft: global_spacer_md.var
+  }
+});
 
 const LoginBoxHeader = ({ children, className, dropdown, title, subtitle, ...props }) => (
   <div className={css(styles.loginBoxHeader, className)} {...props}>
-    <Title className={css(spacing.mbSm)} size="3xl">
+    <Title className={css(tokenStyles.title)} size="3xl">
       {title}
     </Title>
     <div className={css(displayStyles.displayInlineFlex, flexAlignment.alignItemsCenter)}>
       {dropdown}
-      {subtitle && <p className={css(spacing.mlMd)}>{subtitle}</p>}
+      {subtitle && <p className={css(tokenStyles.subtitle)}>{subtitle}</p>}
     </div>
 
     {children}
