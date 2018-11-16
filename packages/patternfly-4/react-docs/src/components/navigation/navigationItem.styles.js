057a3417e5852c81a9570055d48f404cd8c5a063
Joachim Schuler <jschuler@redhat.com>
2018-11-15 23:13:10 -0500

    chore(pf4-docs): show components as secondary items and package

--- packages/patternfly-4/react-docs/src/components/navigation/navigationItem.styles.js
+++ packages/patternfly-4/react-docs/src/components/navigation/navigationItem.styles.js
@@ -3,8 +3,12 @@ import {
   global_spacer_sm as spacerSm,
   global_spacer_xs as spacerXs,
   global_spacer_xl as spacerXl,
+  global_spacer_3xl as spacer3Xl,
   global_Color_dark_100 as itemColor,
-  global_BackgroundColor_300 as bgColorHover
+  global_BackgroundColor_300 as bgColorHover,
+  global_BackgroundColor_200,
+  global_FontSize_sm,
+  global_FontSize_xs,
 } from '@patternfly/react-tokens';
 
 export default StyleSheet.create({
@@ -18,7 +22,23 @@ export default StyleSheet.create({
       textDecoration: 'none'
     }
   },
+  navigationItemSecondary: {
+    padding: `${spacerXs.var} ${spacerXs.var} ${spacerXs.var} ${spacer3Xl.var}`,
+    display: 'block',
+    color: itemColor.var,
+    textDecoration: 'none',
+    ':hover': {
+      backgroundColor: global_BackgroundColor_200.var,
+      textDecoration: 'none'
+    },
+    fontSize: global_FontSize_xs.var
+  },
   active: {
     backgroundColor: bgColorHover.var
+  },
+  badge: {
+    float: 'right',
+    marginTop: '-30px !important',
+    marginRight: '10px !important'
   }
 });
