c828b0481bd40708ae41c625579f566957fd9282
Ron Lavi <1ronlavi@gmail.com>
2018-11-20 22:00:01 +0200

    fix(LoginPage): improve form's error style and behavior (#942)

--- packages/patternfly-3/patternfly-react/src/components/LoginPage/components/LoginCardComponents/LoginFormError.js
+++ packages/patternfly-3/patternfly-react/src/components/LoginPage/components/LoginCardComponents/LoginFormError.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Collapse from 'react-collapse';
+import { default as cx } from 'classnames';
+import { excludeKeys } from '../../../../common/helpers';
 
-const LoginFormError = ({ children, show, className, ...props }) => (
-  <div className={`login-form-error ${className}`}>
-    <Collapse {...props} isOpened={show}>
+const LoginFormError = ({ children, show, className, ...props }) =>
+  children && (
+    <Collapse {...excludeKeys(props, ['className'])} isOpened={show} className={cx('login-form-error', className)}>
       {children}
     </Collapse>
-  </div>
-);
+  );
 
 LoginFormError.propTypes = {
   className: PropTypes.string,
