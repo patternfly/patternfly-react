b74503be5e88c576744909a670fb276b697d5935
Titani Labaj <39532947+tlabaj@users.noreply.github.com>
2018-11-26 11:17:05 -0500

    chore(generator): Update generator to match new docs structure (#931)
    #924

--- scripts/generators/patternfly-4-component/templates/component/examples/Simple{{componentName}}.js
+++ scripts/generators/patternfly-4-component/templates/component/examples/Simple{{componentName}}.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { {{componentName}} } from '@patternfly/react-core';
 
 class Simple{{componentName}} extends React.Component {
-  static title = 'Simple {{componentName}}';
 
   render() {
     return (
