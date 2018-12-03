6ef59c12ce48b7b83e3df5d28ffdb9e7c0ea7bf7
Donald Labaj <donald.labaj@gmail.com>
2018-12-03 17:19:08 -0500

    reverted avatar

--- packages/patternfly-4/react-core/src/components/Avatar/Avatar.js
+++ packages/patternfly-4/react-core/src/components/Avatar/Avatar.js
@@ -1,7 +1,7 @@
-import * as React from 'react';
+import React from 'react';
 import styles from '@patternfly/patternfly-next/components/Avatar/avatar.css';
 import { css } from '@patternfly/react-styles';
-import * as PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 const propTypes = {
   /** Additional classes added to the Avatar. */
