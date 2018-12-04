c0d834a1127c729b43877166980640cb38d883d4
Mike Ro <mikerodev@gmail.com>
2018-12-04 16:46:29 -0500

    feat(PF4-Modal): Adds keyboard and screen reader focus trapping
    BREAKING CHANGE: The reactRoot prop is required
    
    Fixes #561

--- packages/patternfly-4/react-core/src/components/Modal/examples/SimpleModal.js
+++ packages/patternfly-4/react-core/src/components/Modal/examples/SimpleModal.js
@@ -32,6 +32,7 @@ class SimpleModal extends React.Component {
               Confirm
             </Button>
           ]}
+          reactRoot={document.querySelector('#___gatsby')}
         >
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
           magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
---
+++ packages/patternfly-4/react-core/src/internal/safeHTMLElement.js
@@ -0,0 +1,4 @@
+// https://github.com/reactjs/react-modal/blob/master/src/helpers/safeHTMLElement.js
+import { canUseDOM } from 'exenv';
+
+export default (canUseDOM ? window.HTMLElement : {});
