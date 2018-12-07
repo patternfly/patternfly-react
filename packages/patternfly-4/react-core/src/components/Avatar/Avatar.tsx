50657c46e1744c4668d99ad58dbd6544ddb34c1b
Donald Labaj <donald.labaj@gmail.com>
2018-12-07 11:13:11 -0500

    Typescript updates

--- packages/patternfly-4/react-core/src/components/Avatar/Avatar.tsx
+++ packages/patternfly-4/react-core/src/components/Avatar/Avatar.tsx
@@ -1,16 +1,7 @@
-import React from 'react';
+import React, { DetailedHTMLProps, ImgHTMLAttributes } from 'react';
 import styles from '@patternfly/patternfly-next/components/Avatar/avatar.css';
 import { css } from '@patternfly/react-styles';
 import PropTypes from 'prop-types';
-
-const propTypes = {
-  /** Additional classes added to the Avatar. */
-  className: PropTypes.string,
-  /** Attribute that specifies the URL of the image for the Avatar. */
-  src: PropTypes.string,
-  /** Attribute that specifies the alt text of the image for the Avatar. */
-  alt: PropTypes.string.isRequired
-};
 
 const defaultProps = {
   className: '',
@@ -20,7 +11,7 @@ const defaultProps = {
 /**
  * Column properties.
  */
-export interface IAvatarProps {
+export interface AvatarProps extends DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>{
   /** Additional classes added to the Avatar. */
   className?: string;
   /** Attribute that specifies the URL of the image for the Avatar. */
@@ -29,8 +20,8 @@ export interface IAvatarProps {
   alt: string;
 }
 
-const Avatar:React.SFC<IAvatarProps> = (props) => (
-  <img {...props} className={css(styles.avatar, props.className)} src={props.src} alt={props.alt} />
+const Avatar:React.SFC<AvatarProps> = (props) => (
+  <img {...props} className={css(styles.avatar, props.className)} />
 );
 
 Avatar.defaultProps = defaultProps;
