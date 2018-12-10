874c0c27f7c9f8e7fddb61236059ffa7b613a53b
Donald Labaj <dlabaj@redhat.com>
2018-12-10 12:35:58 -0500

    Typescript support for react-core (#884)
    * [Work in Progess - Do Not Merge] Initial work for typescript support in react core.
    
    * Updated with Babel 7 for react-core
    
    * Removed tsc for building typescript in favor of babel 7
    
    * Yarn.lock update
    
    * Updated babel to use interface to props plugin
    
    * Updated build:typescript to call tsc
    
    * Updates for typescript build.
    
    * Reverted Button to JavaScript.
    
    * reverted avatar
    
    * Start of update Avatar to typescript
    
    * ts docgen changes
    
    * Avatar updates
    
    * Added ts-jest
    
    * Typescript updates
    
    * Update to fix
    
    * Merge
    
    * Added missing Avatar tsx snap
    
    * Added back proptypes to make tsc happy when running unit tests
    
    * Updated to remove package-lock.json and extra @types
    
    * Update package.json

---
+++ packages/patternfly-4/react-core/.babelrc.js
@@ -0,0 +1,48 @@
+const babelENV = process.env.BABEL_ENV || 'development';
+const modules = babelENV !== 'production:esm' ? 'commonjs' : false;
+
+module.exports = {
+  presets: [['@babel/env', { modules }], '@babel/preset-typescript', '@babel/react'],
+  ignore: (() => {
+    const ignore = ['src/**/__snapshots__'];
+    ignore.push('src/**/*.d.ts');
+    if (babelENV.includes('production')) {
+      ignore.push('test.js', '__mocks__');
+    }
+    return ignore;
+  })(),
+  env: {
+    'production:esm': {
+      plugins: [
+        'babel-plugin-typescript-to-proptypes',
+        '@babel/plugin-proposal-export-default-from',
+        '@babel/proposal-class-properties',
+        '@babel/proposal-object-rest-spread',
+        [
+          '@patternfly/react-styles/babel',
+          {
+            srcDir: './src',
+            outDir: './dist/esm',
+            useModules: true
+          }
+        ]
+      ]
+    },
+    'production:cjs': {
+      plugins: [
+        'babel-plugin-typescript-to-proptypes',
+        '@babel/plugin-proposal-export-default-from',
+        '@babel/proposal-class-properties',
+        '@babel/proposal-object-rest-spread',
+        [
+          '@patternfly/react-styles/babel',
+          {
+            srcDir: './src',
+            outDir: './dist/js',
+            useModules: false
+          }
+        ]
+      ]
+    }
+  }
+};
