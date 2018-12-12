6785edcd008f30a28858f15f6b0c1bca81868d98
kmcfaul <45077788+kmcfaul@users.noreply.github.com>
2018-12-12 15:19:55 -0500

    docs(Page): match example color order to core (#1043)
    n

--- packages/patternfly-4/react-core/src/layouts/Page/examples/HorizontalPage.js
+++ packages/patternfly-4/react-core/src/layouts/Page/examples/HorizontalPage.js
@@ -14,9 +14,9 @@ class HorizontalPage extends React.Component {
 
     return (
       <Page header={Header}>
-        <PageSection variant={PageSectionVariants.light}>Section with light background</PageSection>
-        <PageSection variant={PageSectionVariants.dark}>Section with dark background</PageSection>
         <PageSection variant={PageSectionVariants.darker}>Section with darker background</PageSection>
+        <PageSection variant={PageSectionVariants.dark}>Section with dark background</PageSection>
+        <PageSection variant={PageSectionVariants.light}>Section with light background</PageSection>
       </Page>
     );
   }
