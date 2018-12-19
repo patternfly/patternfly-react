5c1f035b9f66bc2d3259a3d5db3e9fbc5c345cf9
Joachim Schuler <jschuler@redhat.com>
2018-12-19 10:07:15 -0500

    missing change in index.js

--- packages/patternfly-4/react-core/src/components/index.js
+++ packages/patternfly-4/react-core/src/components/index.js
@@ -19,7 +19,7 @@ export * from './List';
 export * from './LoginPage';
 export * from './Modal';
 export * from './Nav';
-// export * from './Popover'; // Not ready yet
+export * from './Popover';
 export * from './Page';
 export * from './Progress';
 export * from './Radio';
