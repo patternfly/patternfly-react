c86aa2784f5c83b0278e6cfb450bd1eb25c11dbf
Ian Bolton <ibolton336@gmail.com>
2018-12-20 11:53:09 -0500

    chore(update): Upgrade to 1.0.107 pf-next (#1101)
    * chore(update): Upgrade to 1.0.7 pf-next
    
    * update snapshot

--- packages/patternfly-4/react-core/src/components/AboutModal/AboutModalBoxLogo.d.ts
+++
@@ -1,8 +0,0 @@
-import { SFC, HTMLProps } from 'react';
-
-export interface AboutMoalLogoHeroProps extends HTMLProps<HTMLImageElement> {
-  src: string;
-  alt: string;
-}
-declare const AboutMoalLogoBrand: SFC<AboutMoalLogoHeroProps>;
-export default AboutMoalLogoHeroProps;
