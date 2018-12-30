f4547daf4264c7a135c9b41f5e3748fe127fe17a
Boaz Shuster <boaz.shuster.github@gmail.com>
2018-12-30 17:57:03 +0200

    docs(Select): Add an invalid select example
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- packages/patternfly-4/react-core/src/components/Select/Select.docs.js
+++ packages/patternfly-4/react-core/src/components/Select/Select.docs.js
@@ -2,6 +2,7 @@ import { Select, SelectOption, SelectOptionGroup } from '@patternfly/react-core'
 import SelectInput from './examples/SelectInput';
 import SelectInputDisabled from './examples/SelectInputDisabled';
 import SelectInputGrouped from './examples/SelectInputGrouped';
+import SelectInputInvalid from './examples/SelectInputInvalid';
 
 export default {
   title: 'Select',
@@ -13,6 +14,7 @@ export default {
   examples: [
     { component: SelectInput, title: 'Select Input' },
     { component: SelectInputGrouped, title: 'Select Input with grouping' },
+    { component: SelectInputInvalid, title: 'Select Input Invalid' },
     { component: SelectInputDisabled, title: 'Select Input Disabled' }
   ]
 };
