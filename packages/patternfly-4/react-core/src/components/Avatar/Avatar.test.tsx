2ed9996216ac09cffe2663abdfba30606b3cfe3d
Donald Labaj <donald.labaj@gmail.com>
2019-01-02 14:37:56 -0500

    Fix #1118: Added tslint support to react-core

--- packages/patternfly-4/react-core/src/components/Avatar/Avatar.test.tsx
+++ packages/patternfly-4/react-core/src/components/Avatar/Avatar.test.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+
 import { shallow } from 'enzyme';
+import React from 'react';
 import Avatar from './Avatar';
+
 
 test('simple avatar', () => {
   const view: any = shallow(<Avatar alt="avatar" />);
