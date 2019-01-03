c9aa25453d029fc473c50b6710835d79b93031e3
Joachim <jschuler@redhat.com>
2019-01-03 16:31:42 -0500

    feat(tooltip): add tooltip to PF4 (#1103)
    * feat(tooltip): add tooltip to PF4
    
    * ts fix and docs
    
    * keep tooltip open when hovered over

---
+++ packages/patternfly-4/react-core/src/components/Tooltip/Tooltip.d.ts
@@ -0,0 +1,33 @@
+import { SFC, HTMLProps, ReactNode, ReactElement } from 'react';
+import { Omit } from '../../typeUtils';
+import { Instance, BasicPlacement } from 'tippy.js';
+
+export const TooltipPosition: {
+  top: 'top';
+  bottom: 'bottom';
+  left: 'left';
+  right: 'right';
+};
+
+export interface TooltipProps extends Omit<HTMLProps<HTMLDivElement>, 'children' | 'size'> {
+  /** Tooltip position */
+  position?: BasicPlacement;
+  /** If true, tries to keep the tooltip in view by flipping it if necessary */
+  enableFlip?: boolean;
+  /** Tooltip additional class */
+  className?: string;
+  /** The reference element to which the tooltip is relatively placed to */
+  children: ReactElement<any>;
+  /** Tooltip content */
+  content: ReactNode;
+  /** The element to append the tooltip to, defaults to body */
+  appendTo?: Element | ((ref: Element) => Element);
+  /** z-index of the tooltip */
+  zIndex?: number;
+  /** Size of the tooltip */
+  size: 'small' | 'regular' | 'large';
+}
+
+declare const Tooltip: SFC<TooltipProps>;
+
+export default Tooltip;
---
+++ packages/patternfly-4/react-core/src/components/Tooltip/Tooltip.docs.js
@@ -0,0 +1,19 @@
+import { Tooltip, TooltipPosition } from '@patternfly/react-core';
+import SimpleTooltip from './examples/SimpleTooltip';
+import TooltipPositions from './examples/TooltipPositions';
+
+export default {
+  title: 'Tooltip',
+  components: {
+    Tooltip
+  },
+  enumValues: {
+    'Object.keys(TooltipPosition).map(key => TooltipPosition[key])': Object.keys(TooltipPosition).map(
+      key => TooltipPosition[key]
+    )
+  },
+  examples: [
+    { component: SimpleTooltip, title: 'Simple Tooltip' },
+    { component: TooltipPositions, title: 'Tooltip Positions' }
+  ]
+};
