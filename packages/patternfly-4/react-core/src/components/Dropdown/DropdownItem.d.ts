156f41649f201765cc6837eae5bf9205a55a7c67
Patrick Riley <priley86@gmail.com>
2019-01-10 10:27:32 -0500

    fix(Dropdown typings): fix the DropdownItem typings and add react-tokens dep

--- packages/patternfly-4/react-core/src/components/Dropdown/DropdownItem.d.ts
+++ packages/patternfly-4/react-core/src/components/Dropdown/DropdownItem.d.ts
@@ -2,7 +2,5 @@ import { SFC, HTMLProps, ReactType, ReactNode } from 'react';
 
 export interface DropdownItemProps extends HTMLProps<HTMLAnchorElement> {
   component?: ReactType<DropdownItemProps>;
-  children?: ReactNode;
   isDisabled?: Boolean;
-  onClick?: Function;
 }
