e5c197a2172b9a7aa4e00a62ae8d416c4721be3d
Mike Turley <mturley@redhat.com>
2019-01-16 14:07:02 -0500

    feat(Alert): Add optional Close button to Alert via onClose prop
    Fix #1036

--- packages/patternfly-4/react-core/src/components/Alert/examples/InfoAlert.js
+++ packages/patternfly-4/react-core/src/components/Alert/examples/InfoAlert.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { Alert, Button } from '@patternfly/react-core';
 
 class InfoAlert extends React.Component {
+  state = { alertOneVisible: true, alertTwoVisible: true };
+  hideAlertOne = () => this.setState({ alertOneVisible: false });
+  hideAlertTwo = () => this.setState({ alertTwoVisible: false });
+
   render() {
+    const { alertOneVisible, alertTwoVisible } = this.state;
     return (
       <React.Fragment>
-        <Alert variant="info" title="Info notification title">
-          Info notification description. <a href="#">This is a link.</a>
-        </Alert>
+        {alertOneVisible && (
+          <Alert variant="info" title="Info notification title" onClose={this.hideAlertOne}>
+            Info notification description. <a href="#">This is a link.</a>
+          </Alert>
+        )}
+        {alertTwoVisible && <Alert variant="info" title="Info notification title" onClose={this.hideAlertTwo} />}
         <Alert variant="info" title="Info notification title" action={<Button variant="link">Action Button</Button>} />
         <Alert variant="info" title="Info notification title" />
       </React.Fragment>
--- packages/patternfly-4/react-core/src/components/Alert/examples/WarningAlert.js
+++ packages/patternfly-4/react-core/src/components/Alert/examples/WarningAlert.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { Alert, Button } from '@patternfly/react-core';
 
 class WarningAlert extends React.Component {
+  state = { alertOneVisible: true, alertTwoVisible: true };
+  hideAlertOne = () => this.setState({ alertOneVisible: false });
+  hideAlertTwo = () => this.setState({ alertTwoVisible: false });
+
   render() {
+    const { alertOneVisible, alertTwoVisible } = this.state;
     return (
       <React.Fragment>
-        <Alert variant="warning" title="Warning notification title">
-          Warning notification description. <a href="#">This is a link.</a>
-        </Alert>
+        {alertOneVisible && (
+          <Alert variant="warning" title="Warning notification title" onClose={this.hideAlertOne}>
+            Warning notification description. <a href="#">This is a link.</a>
+          </Alert>
+        )}
+        {alertTwoVisible && <Alert variant="warning" title="Warning notification title" onClose={this.hideAlertTwo} />}
         <Alert
           variant="warning"
           title="Warning notification title"
