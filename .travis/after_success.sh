04d4c707615d29c0fc81198612a2c2000f7ccc39
Patrick Riley <priley86@gmail.com>
2019-01-16 14:05:52 -0500

    fix(lerna-changelog): update lerna changelog

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -9,5 +9,5 @@ fi
 if [ "${TRAVIS_REPO_SLUG}" == "${TRIGGER_REPO_SLUG}" -a "$TRAVIS_BRANCH" == "${TRIGGER_REPO_BRANCH}" ]; then
   # note: yarn travis-deploy-once "yarn lerna:publish" currently breaks NPM_TOKEN auth context
   export GITHUB_AUTH="${GH_TOKEN}"
-  npm run travis-deploy-once "npm run lerna:changelog; npm run lerna:publish"
+  npm run travis-deploy-once "npm run lerna:publish"
 fi
