89927ca72b2af49bebd0f17ac7e9eddb855461a4
Levente Berky <lberky@redhat.com>
2019-01-24 15:49:30 +0100

    Added the link to the npm page of the tree.

--- packages/patternfly-3/react-tree-view/src/components/TreeView/TreeView.stories.js
+++ packages/patternfly-3/react-tree-view/src/components/TreeView/TreeView.stories.js
@@ -11,7 +11,9 @@ const stories = storiesOf(`${storybookPackageName(name)}/Tree View`, module);
 stories.addDecorator(
   defaultTemplate({
     title: 'Catalog Tile',
-    description: 'This is a Tree View component to display trees and hierarchical data using React Wooden Tree.'
+    description:
+      'This is a Tree View component to display trees and hierarchical data using React Wooden Tree.' +
+      'The tree uses the https://www.npmjs.com/package/react-wooden-tree component.'
   })
 );
 
