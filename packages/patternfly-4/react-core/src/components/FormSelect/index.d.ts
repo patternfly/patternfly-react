cfecdb3dd89bdd3439162150a5f3f15e40034557
Katie McFaul <kmcfaul@redhat.com>
2019-01-28 13:50:45 -0500

    fix(FormSelect): update TS exports to new names

--- packages/patternfly-4/react-core/src/components/FormSelect/index.d.ts
+++ packages/patternfly-4/react-core/src/components/FormSelect/index.d.ts
@@ -1,3 +1,3 @@
 export { default as FormSelect, FormSelectProps } from './FormSelect';
-export { default as SelectOption, FormSelectOptionProps } from './FormSelectOption';
-export { default as SelectOptionGroup, FormSelectOptionGroupProps } from './FormSelectOptionGroup';
+export { default as FormSelectOption, FormSelectOptionProps } from './FormSelectOption';
+export { default as FormSelectOptionGroup, FormSelectOptionGroupProps } from './FormSelectOptionGroup';
