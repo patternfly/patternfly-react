a1cdf9c64f3d64592f158d77a4103e5294b1f66f
Titani Labaj <tlabaj@redhat.com>
2019-01-29 11:53:47 -0500

    Add tabs to integration App

--- packages/patternfly-4/react-integration/demo-app-ts/src/App.tsx
+++ packages/patternfly-4/react-integration/demo-app-ts/src/App.tsx
@@ -1,11 +1,11 @@
-import { Avatar, AvatarProps } from '@patternfly/react-core';
+import { Avatar, AvatarProps, Tabs, Tab } from '@patternfly/react-core';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavTest from './Nav';
 
 class myProps implements AvatarProps {
-  alt: string = "avatar";
+  alt: string = 'avatar';
 }
 
 class App extends Component {
@@ -16,6 +16,17 @@ class App extends Component {
           <Avatar src={logo} alt={new myProps().alt} />
           <NavTest />
         </header>
+        <Tabs>
+          <Tab eventKey={0} title="Tab item 1">
+            Tab 1 section
+          </Tab>
+          <Tab eventKey={1} title="Tab item 2">
+            Tab 2 section
+          </Tab>
+          <Tab eventKey={2} title="Tab item 3">
+            Tab 3 section
+          </Tab>
+        </Tabs>
       </div>
     );
   }
