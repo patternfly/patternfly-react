b69d45d40b764bfe089e69fe6b4fd4c4e6840a8c
Titani Labaj <39532947+tlabaj@users.noreply.github.com>
2019-02-05 09:07:30 -0500

    fix(Alert): Fix index.d.ts file (#1320)
    * fix(Alert): Fix index.d.ts file
    
    #1318
    
    * fix Alert.d.ts file

--- packages/patternfly-4/react-integration/demo-app-ts/src/App.tsx
+++ packages/patternfly-4/react-integration/demo-app-ts/src/App.tsx
@@ -1,4 +1,15 @@
-import { Avatar, AvatarProps, Tabs, Tab, Tooltip, Popover } from '@patternfly/react-core';
+import {
+  Avatar,
+  AvatarProps,
+  Tabs,
+  Tab,
+  Tooltip,
+  Popover,
+  Alert,
+  AlertActionCloseButton,
+  AlertActionLink,
+  AlertVariant
+} from '@patternfly/react-core';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -26,9 +37,23 @@ class App extends Component {
             Tab 3 section
           </Tab>
         </Tabs>
-        <Tooltip content={<div>World</div>}><div>Hello</div></Tooltip>
-        <Popover bodyContent={<div>World</div>}><div>Hello</div></Popover>
+        <Tooltip content={<div>World</div>}>
+          <div>Hello</div>
+        </Tooltip>
+        <Popover bodyContent={<div>World</div>}>
+          <div>Hello</div>
+        </Popover>
         <NavTest />
+        <Alert variant={AlertVariant.success} title="Success notification title" action={<AlertActionCloseButton />}>
+          Success notification description. <a href="#">This is a link.</a>
+        </Alert>
+        <Alert variant={AlertVariant.success} title="Success notification title" action={<AlertActionCloseButton />} />
+        <Alert
+          variant={AlertVariant.success}
+          title="Success notification title"
+          action={<AlertActionLink>Action Button</AlertActionLink>}
+        />
+        <Alert variant="success" title="Success notification title" />
       </div>
     );
   }
