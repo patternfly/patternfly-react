f3ebdeeec0be88324d1fecc03a24c081b855bdc0
boaz0 <ripcurld.github@gmail.com>
2019-02-06 16:08:22 +0200

    fix(Nav): do not render the examples page (#1316)
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- packages/patternfly-4/react-core/src/components/Nav/examples/NavHorizontalList.js
+++ packages/patternfly-4/react-core/src/components/Nav/examples/NavHorizontalList.js
@@ -18,13 +18,13 @@ class NavHorizontalList extends React.Component {
       <div style={{ backgroundColor: '#292e34', padding: '1rem' }}>
         <Nav onSelect={this.onSelect} aria-label="Primary Nav Horizontal Example">
           <NavList variant={NavVariants.horizontal}>
-            <NavItem to="#horizontal-link1" itemId={0} isActive={activeItem === 0}>
+            <NavItem preventDefault to="#horizontal-link1" itemId={0} isActive={activeItem === 0}>
               Item 1
             </NavItem>
-            <NavItem to="#horizontal-link2" itemId={1} isActive={activeItem === 1}>
+            <NavItem preventDefault to="#horizontal-link2" itemId={1} isActive={activeItem === 1}>
               Item 2
             </NavItem>
-            <NavItem to="#horizontal-link3" itemId={2} isActive={activeItem === 2}>
+            <NavItem preventDefault to="#horizontal-link3" itemId={2} isActive={activeItem === 2}>
               Item 3
             </NavItem>
           </NavList>
--- packages/patternfly-4/react-core/src/components/Nav/examples/NavTertiaryList.js
+++ packages/patternfly-4/react-core/src/components/Nav/examples/NavTertiaryList.js
@@ -17,16 +17,16 @@ class NavTertiaryList extends React.Component {
     return (
       <Nav onSelect={this.onSelect} aria-label="Tertiary Example">
         <NavList variant={NavVariants.tertiary}>
-          <NavItem to="#tertiary-link1" itemId={0} isActive={activeItem === 0}>
+          <NavItem preventDefault to="#tertiary-link1" itemId={0} isActive={activeItem === 0}>
             Link 1
           </NavItem>
-          <NavItem to="#tertiary-link2" itemId={1} isActive={activeItem === 1}>
+          <NavItem preventDefault to="#tertiary-link2" itemId={1} isActive={activeItem === 1}>
             Link 2
           </NavItem>
-          <NavItem to="#tertiary-link3" itemId={2} isActive={activeItem === 2}>
+          <NavItem preventDefault to="#tertiary-link3" itemId={2} isActive={activeItem === 2}>
             Link 3
           </NavItem>
-          <NavItem to="#tertiary-link4" itemId={3} isActive={activeItem === 3}>
+          <NavItem preventDefault to="#tertiary-link4" itemId={3} isActive={activeItem === 3}>
             Link 4
           </NavItem>
         </NavList>
