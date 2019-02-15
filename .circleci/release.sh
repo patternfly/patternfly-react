0e707363ace1e38bd25dfd32eff14f075c2cbd55
zallen <47335686+redallen@users.noreply.github.com>
2019-02-15 15:06:23 -0500

    Faster CI (#1388)
    * Add CircleCI and faster TravisCI
    
    * fix yarn syntax
    
    * fix PR deploy
    
    * fix surge deploy
    
    * practice surge deploy
    
    * test surge deploy
    
    * untest surge deploy
    
    * fix deploy check script
    
    * more reasonable doc deploy skipping
    
    * give gatsby more RAM

---
+++ .circleci/release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Note: do not do set -x or the passwords will leak!
+set -e
+
+echo "Doing a release..."
+git config credential.helper store
+echo "https://${GH_USERNAME}:${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" > ~/.git-credentials
+
+npm config set "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" -q
+echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
+npm prune
+
+git config --global user.email "patternfly-build@redhat.com"
+git config --global user.name "patternfly-build"
+git config --global push.default simple
+
+# helpful for debugging any lerna EUNCOMMIT errors
+git status --short;
+yarn run lerna changed
+yarn lerna:publish
