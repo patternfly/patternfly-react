a0fda30669544c242cb8bd52541110a351e8162a
zack allen <zallen@redhat.com>
2019-02-18 17:04:38 -0500

    Remove some unused dependencies

--- packages/patternfly-4/react-styled-system/scripts/copyTS.js
+++ packages/patternfly-4/react-styled-system/scripts/copyTS.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const glob = require('glob');
 const fse = require('fs-extra');
-const log = require('npmlog');
 
 const srcDir = path.join('./src');
 const distDir = path.join('./dist/js');
@@ -13,6 +12,5 @@ const files = glob.sync('**/*.d.ts', {
 files.forEach(file => {
   const from = path.join(srcDir, file);
   const to = path.join(distDir, file);
-  log.info('copyTS', `${from} --> ${to}`);
   fse.copySync(from, to);
 });
