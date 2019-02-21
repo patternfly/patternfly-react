3cfd99e425ac7ade647b7e01c8e47c55be27733d
Katie McFaul <kmcfaul@redhat.com>
2019-02-21 12:52:36 -0500

    fix(Select): remove toggle ts

--- packages/patternfly-4/react-core/src/components/Select/index.js
+++ packages/patternfly-4/react-core/src/components/Select/index.js
@@ -1,4 +1,3 @@
 export { default as Select } from './Select';
 export { default as SelectOption } from './SelectOption';
-export { default as SelectToggle } from './SelectToggle';
 export { SelectContext, SelectVariant } from './selectConstants';
