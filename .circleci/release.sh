9c9318659650c4bc87993cd327d93bf76e9945c9
zallen <47335686+redallen@users.noreply.github.com>
2019-02-21 13:25:22 -0500

    build(publish): fix and improve lerna publish (#1422)
    * fix lerna publish
    
    * tweak
    
    * update lockfile
    
    * keep testing
    
    * try from-package
    
    * add --yes flag
    
    * only do git releases on npm publishing
    
    * new release script
    
    * make lerna do what we want
    
    * make it safe again
    
    * comments

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -2,20 +2,25 @@
 # Note: do not do set -x or the passwords will leak!
 set -e
 
-echo "Doing a release..."
-git config credential.helper store
-echo "https://${GH_USERNAME}:${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" > ~/.git-credentials
-
-npm config set "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" -q
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
-npm prune
-
+echo "Preparing release environment..."
 git config --global user.email "patternfly-build@redhat.com"
 git config --global user.name "patternfly-build"
-git config --global push.default simple
-git checkout master
 
-# helpful for debugging any lerna EUNCOMMIT errors
-git rev-parse HEAD
-yarn run lerna changed || true
-yarn lerna:publish
+echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
+
+echo "Doing a release..."
+# Lerna is complicated. Commands: https://github.com/lerna/lerna/tree/master/commands
+git checkout $TRAVIS_BRANCH # Lerna needs to be on a real HEAD
+git rev-parse HEAD # helpful for debugging any lerna EUNCOMMIT errors
+
+# Tag this new commit with each package change. Don't push it yet.
+npx lerna version --conventional-commits --no-commit-hooks --no-git-tag-version --no-push --yes
+
+# Check each package.json and determe if any package version is not present in the registry.
+# Any versions not present in the registry will be published.
+if npx lerna publish from-package --no-changelog --no-git-tag-version --no-push --yes ; then
+    # Undo that last commit locally
+    git reset --hard HEAD~1
+    # Now only if it publishes should we also push this commit to Github and do a Github release.
+    npx lerna version --conventional-commits --github-release --no-commit-hooks --yes
+fi
