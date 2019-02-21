d41ba29c5f34e43ac07366b24d60b7262d323d49
zack allen <zallen@redhat.com>
2019-02-21 09:21:06 -0500

    fix lerna publish

--- .circleci/clean_cache.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This removes these files so Travis won't cache them after building docs.
-# Saves 40s each on doc build job
-rm -rf node_modules/.cache
-rm -rf packages/patternfly-4/react-core/node_modules/.cache
-rm -rf packages/patternfly-4/react-docs/node_modules/.cache
--- .circleci/release.sh
+++ .circleci/release.sh
@@ -3,19 +3,16 @@
 set -e
 
 echo "Doing a release..."
+git config --global user.email "patternfly-build@redhat.com"
+git config --global user.name "patternfly-build"
+git config --global push.default simple
 git config credential.helper store
 echo "https://${GH_USERNAME}:${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" > ~/.git-credentials
 
 npm config set "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" -q
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
-npm prune
 
-git config --global user.email "patternfly-build@redhat.com"
-git config --global user.name "patternfly-build"
-git config --global push.default simple
 git checkout master
-
-# helpful for debugging any lerna EUNCOMMIT errors
-git rev-parse HEAD
-yarn run lerna changed || true
-yarn lerna:publish
+git rev-parse HEAD # helpful for debugging any lerna EUNCOMMIT errors
+npx lerna changed || true
+npm run lerna:publish
