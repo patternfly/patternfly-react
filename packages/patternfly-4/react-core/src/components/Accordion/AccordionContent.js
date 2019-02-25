bdce0b33d39956ffe25c20e5fad5d0f12f70a852
Ian Bolton <ibolton@redhat.com>
2019-02-25 10:47:38 -0500

    feat(accordion): Add accordion to pf

---
+++ packages/patternfly-4/react-core/src/components/Accordion/AccordionContent.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { css } from '@patternfly/react-styles';
+import styles from '@patternfly/patternfly/components/Accordion/accordion.css';
+
+const AccordionContent = ({ className, children, id, isHidden, isFixed, 'aria-label': ariaLabel, ...props }) => (
+  <dd
+    id={id}
+    className={css(styles.accordionExpandedContent, isFixed && styles.modifiers.fixed, className)}
+    hidden={isHidden}
+    aria-label={ariaLabel}
+    {...props}
+  >
+  <div className={css(styles.accordionExpandedContentBody)}>
+    {children}
+  </div>
+  </dd>
+);
+
+AccordionContent.propTypes = {
+  /** Content rendered inside the Accordion */
+  children: PropTypes.node,
+  /** Additional classes added to the Accordion content */
+  className: PropTypes.string,
+  /** Identify the AccordionContent item */
+  id: PropTypes.string,
+  /** Flag to show if the expanded content of the Accordion item is visible */
+  isHidden: PropTypes.bool,
+  /** Flag to indicate Accordion content is fixed */
+  isFixed: PropTypes.bool,
+  /** Adds accessible text to the Accordion content */
+  'aria-label': PropTypes.string.isRequired,
+  /** Additional props are spread to the container <dd> */
+  '': PropTypes.any
+};
+
+AccordionContent.defaultProps = {
+  className: '',
+  id: '',
+  isHidden: false,
+  isFixed: false
+};
+
+export default AccordionContent;
