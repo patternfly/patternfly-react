629880d18ffb5c50efae66debc391abb7eb20062
Karel Hala <ch4rlien@gmail.com>
2019-03-04 17:03:07 +0100

    feat(Icons): Add fa regular icons (#1385)

--- packages/react-icons/build/generateIcons.js
+++ packages/react-icons/build/generateIcons.js
@@ -7,7 +7,8 @@ const pascalCase = plop.getHelper('pascalCase');
 const kebabCase = plop.getHelper('kebabCase');
 
 const allIcons = [
-  ...icons.fontAwesome.solid.map(getFontAwesomeIcon),
+  ...icons.fontAwesome.solid.map((icon) => getFontAwesomeIcon(icon, 'solid')),
+  ...icons.fontAwesome.regular.map((icon) => getFontAwesomeIcon(icon, 'regular', 'outlined')),
   ...Object.keys(icons.pfIcons).map(getPfIcon)
 ];
 
@@ -23,9 +24,9 @@ function getPfIcon(iconName) {
   return generateIcon(currentIcon, `${iconName}`)
 }
 
-function getFontAwesomeIcon(name) {
-  const faIconDef = require(`@fortawesome/free-solid-svg-icons/${name}`); // eslint-disable-line
-  const iconName = kebabCase(name.substr(2)); // remove fa and make name kebab cased
+function getFontAwesomeIcon(name, packageType, prefix = '') {
+  const faIconDef = require(`@fortawesome/free-${packageType}-svg-icons/${name}`); // eslint-disable-line
+  const iconName = kebabCase(`${prefix}${name.substr(2)}`); // remove fa and make name kebab cased
 
   return generateIcon(faIconDef, iconName);
 }
--- packages/react-icons/build/icons.js
+++ packages/react-icons/build/icons.js
@@ -1,8 +1,10 @@
 const { fas } = require('@fortawesome/free-solid-svg-icons');
+const { far } = require('@fortawesome/free-regular-svg-icons');
 const { pfIcons } = require('./pfIcons');
 module.exports = {
   fontAwesome: {
-    solid: Object.keys(fas)
+    solid: Object.keys(fas),
+    regular: Object.keys(far)
   },
 
   pfIcons
