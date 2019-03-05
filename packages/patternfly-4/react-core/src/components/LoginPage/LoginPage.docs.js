91533a990d9a0f61f6586ef00e98df85b2227968
Boaz Shuster <boaz.shuster.github@gmail.com>
2019-03-05 03:32:26 +0200

    chore(LoginPage): a single function to update password and username
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- packages/patternfly-4/react-core/src/components/LoginPage/LoginPage.docs.js
+++ packages/patternfly-4/react-core/src/components/LoginPage/LoginPage.docs.js
@@ -1,5 +1,6 @@
 import { LoginPage, LoginForm } from '@patternfly/react-core';
 import SimpleLoginPage from './examples/SimpleLoginPage';
+import OneHandlerLoginPage from './examples/OneHandlerLoginPage';
 
 export default {
   title: 'LoginPage',
@@ -8,6 +9,9 @@ export default {
     LoginForm
   },
   variablesRoot: 'pf-c-login',
-  examples: [{ component: SimpleLoginPage, title: 'Simple LoginPage' }],
+  examples: [
+    { component: SimpleLoginPage, title: 'Simple LoginPage' },
+    { component: OneHandlerLoginPage, title: 'One Handler for Username and Password' }
+  ],
   fullPageOnly: true
 };
