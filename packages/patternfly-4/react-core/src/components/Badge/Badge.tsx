08ac2094dccf0f5ad6215d4f73d0e0737f2977cc
undefined <undefined>
2019-03-05 09:55:38 -0500

    convert Badge to typescript

---
+++ packages/patternfly-4/react-core/src/components/Badge/Badge.tsx
@@ -0,0 +1,28 @@
+import React, { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react';
+
+import { css } from '@patternfly/react-styles';
+import styles from '@patternfly/patternfly/components/Badge/badge.css';
+
+const defaultProps = {
+  isRead: false,
+  children: '',
+  className: ''
+};
+
+export interface BadgeProps extends DetailedHTMLProps<HTMLAttributes<HTMLSpanElement>, HTMLSpanElement> {
+  /**  Adds styling to the badge to indicate it has been read */
+  isRead: boolean;
+  /** content rendered inside the Badge */
+  children: ReactNode;
+  /** additional classes added to the Badge */
+  className?: string;
+}
+
+const Badge: React.FunctionComponent<BadgeProps> = (props) => (
+  <span {...props} className={css(styles.badge, (props.isRead ? styles.modifiers.read : styles.modifiers.unread) as any, props.className)}>
+    {props.children}
+  </span>
+);
+
+Badge.defaultProps = defaultProps;
+export default Badge;
