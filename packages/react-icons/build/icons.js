07330faf474f29bc06ca6d9fa1ba675b3fd575f7
Karel Hala <ch4rlien@gmail.com>
2019-03-08 17:46:53 +0100

    feat(Icon): Openshift icon (#1508)

--- packages/react-icons/build/icons.js
+++ packages/react-icons/build/icons.js
@@ -1,11 +1,12 @@
 const { fas } = require('@fortawesome/free-solid-svg-icons');
 const { far } = require('@fortawesome/free-regular-svg-icons');
 const { pfIcons } = require('./pfIcons');
+const { custom } = require('./customIcons');
 module.exports = {
   fontAwesome: {
     solid: Object.keys(fas),
     regular: Object.keys(far)
   },
-
+  custom,
   pfIcons
 };
