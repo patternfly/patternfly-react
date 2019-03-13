016377b604bec2c0edfab2c19be10bb7d001ed75
Vojtech Szocs <vszocs@redhat.com>
2019-03-13 17:50:01 +0100

    feat(ModalPattern): Add ModalPattern and StatefulModalPattern components

---
+++ packages/patternfly-3/patternfly-react/src/components/Modal/Stories/index.js
@@ -0,0 +1,3 @@
+export { default as modalExampleWithInfo } from './ModalExampleStory';
+
+export { default as modalPatternExampleAddWithInfo } from './ModalPatternExampleStory';
--- packages/patternfly-3/patternfly-react/src/components/Modal/index.js
+++ packages/patternfly-3/patternfly-react/src/components/Modal/index.js
@@ -1,4 +1,7 @@
 import Modal from './Modal';
 import ModalCloseButton from './ModalCloseButton';
 
-export { Modal, ModalCloseButton };
+import ModalPattern from './Patterns/ModalPattern';
+import StatefulModalPattern from './Patterns/StatefulModalPattern';
+
+export { Modal, ModalCloseButton, ModalPattern, StatefulModalPattern };
