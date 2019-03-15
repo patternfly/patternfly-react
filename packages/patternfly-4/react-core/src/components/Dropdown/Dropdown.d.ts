1f1c07a21b8c72d3e3fc7f5e16531ef0a9e484b0
Titani Labaj <tlabaj@redhat.com>
2019-03-15 19:14:06 -0400

    fix(Dropdown): fix d.ts file to make props optional
    #1584

--- packages/patternfly-4/react-core/src/components/Dropdown/Dropdown.d.ts
+++ packages/patternfly-4/react-core/src/components/Dropdown/Dropdown.d.ts
@@ -4,11 +4,11 @@ import { DropdownPosition, DropdownDirection } from './dropdownConstants';
 
 export interface DropdownProps extends HTMLProps<HTMLDivElement> {
   children?: ReactNode;
-  dropdownItems: ReactNode[];
+  dropdownItems?: ReactNode[];
   isPlain?: boolean;
   isOpen?: boolean;
   isAction?: boolean;
-  onSelect(event: React.SyntheticEvent<HTMLDivElement>): void;
+  onSelect?(event: React.SyntheticEvent<HTMLDivElement>): void;
   position?: OneOf<typeof DropdownPosition, keyof typeof DropdownPosition>;
   direction?: OneOf<typeof DropdownDirection, keyof typeof DropdownDirection>;
   toggle?: ReactNode;
