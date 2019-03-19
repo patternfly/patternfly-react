6c3cfacbfb88aa47db0892822ab34efa63ab2956
Vojtech Szocs <vojtech.szocs@gmail.com>
2019-03-19 16:14:45 +0100

    feat(ModalPattern): Add ModalPattern and StatefulModalPattern components (#343)

--- packages/patternfly-3/patternfly-react/src/components/Modal/Modal.js
+++ packages/patternfly-3/patternfly-react/src/components/Modal/Modal.js
@@ -2,6 +2,9 @@ import { Modal as BsModal } from 'react-bootstrap';
 import CustomModalDialog from './InnerComponents/CustomModalDialog';
 import ModalCloseButton from './ModalCloseButton';
 
+import ModalPattern from './Patterns/ModalPattern';
+import StatefulModalPattern from './Patterns/StatefulModalPattern';
+
 /**
  * Modal Component for Patternfly React
  */
@@ -11,11 +14,18 @@ class Modal extends BsModal {
   }
 }
 
+Modal.propTypes = {
+  ...BsModal.propTypes
+};
+
 Modal.defaultProps = {
   ...BsModal.defaultProps,
   dialogComponentClass: CustomModalDialog
 };
 
 Modal.CloseButton = ModalCloseButton;
+
+Modal.Pattern = ModalPattern;
+Modal.Pattern.Stateful = StatefulModalPattern;
 
 export default Modal;
--- packages/patternfly-3/patternfly-react/src/components/Modal/Modal.stories.js
+++ packages/patternfly-3/patternfly-react/src/components/Modal/Modal.stories.js
@@ -1,48 +1,23 @@
-import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info';
-import { inlineTemplate } from 'storybook/decorators/storyTemplates';
-import { storybookPackageName, DOCUMENTATION_URL, STORYBOOK_CATEGORY } from 'storybook/constants/siteConstants';
+import { storybookPackageName, STORYBOOK_CATEGORY } from 'storybook/constants/siteConstants';
 
-import { MockModalManager, basicExampleSource } from './__mocks__/mockModalManager';
-import { Modal } from '../../index';
 import { name } from '../../../package.json';
-import { boolean, withKnobs } from '@storybook/addon-knobs';
+import { modalExampleWithInfo, modalPatternExampleAddWithInfo } from './Stories';
 
-const stories = storiesOf(
-  `${storybookPackageName(name)}/${STORYBOOK_CATEGORY.FORMS_AND_CONTROLS}/Modal Overlay`,
+/**
+ * Modal Component stories
+ */
+const componentStories = storiesOf(
+  `${storybookPackageName(name)}/${STORYBOOK_CATEGORY.FORMS_AND_CONTROLS}/Modal Overlay/Components`,
   module
 );
+modalExampleWithInfo(componentStories);
 
-stories.addDecorator(withKnobs);
-
-const description = (
-  <div>
-    Adding the class <b>right-side-modal-pf</b> will show the modal on the right side of the window.
-  </div>
+/**
+ * Modal Pattern stories
+ */
+const patternStories = storiesOf(
+  `${storybookPackageName(name)}/${STORYBOOK_CATEGORY.FORMS_AND_CONTROLS}/Modal Overlay/Patterns`,
+  module
 );
-
-stories.add(
-  'Modal',
-  withInfo({
-    source: false,
-    propTables: [Modal, Modal.CloseButton, Modal.Header, Modal.Body, Modal.Footer],
-    propTablesExclude: [MockModalManager],
-    text: (
-      <div>
-        <h1>Story Source</h1>
-        <pre>{basicExampleSource}</pre>
-      </div>
-    )
-  })(() => {
-    const rightSide = boolean('Right Side', false);
-    const story = <MockModalManager rightSide={rightSide} />;
-    return inlineTemplate({
-      title: 'Modal Example',
-      documentationLink: `${DOCUMENTATION_URL.PATTERNFLY_ORG_FORMS}modal-overlay/`,
-      reactBootstrapDocumentationLink: `${DOCUMENTATION_URL.REACT_BOOTSTRAP_COMPONENT}modal/`,
-      description,
-      story
-    });
-  })
-);
+modalPatternExampleAddWithInfo(patternStories);
