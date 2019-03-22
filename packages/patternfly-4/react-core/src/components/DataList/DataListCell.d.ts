1db98c45919751bd977834aa96e9fe003603c42c
Boaz Shuster <boaz.shuster.github@gmail.com>
2019-03-22 12:14:59 +0200

    fix(DataList): set width and isExpanded to optional in d.ts
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- packages/patternfly-4/react-core/src/components/DataList/DataListCell.d.ts
+++ packages/patternfly-4/react-core/src/components/DataList/DataListCell.d.ts
@@ -1,7 +1,8 @@
 import { FunctionComponent, HTMLProps } from 'react';
+import { Omit } from '../../helpers/typeUtils'
 
-export interface DataListCellProps extends HTMLProps<HTMLDivElement> {
-  width: 1 | 2 | 3 | 4 | 5;
+export interface DataListCellProps extends Omit<HTMLProps<HTMLDivElement>, 'width'> {
+  width?: 1 | 2 | 3 | 4 | 5;
 }
 
 declare const DataListCell: FunctionComponent<DataListCellProps>;
