a2f8626fab748295ef537b6c9c56a5031c6f1ce5
Boaz Shuster <boaz.shuster.github@gmail.com>
2019-03-26 17:42:50 +0200

    fix(tooltip): trigger tooltip by mouse click or other events
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>
    Co-authored-by: Joachim Schuler <jschuler@redhat.com>

--- packages/patternfly-4/react-core/src/components/Tooltip/Tooltip.test.js
+++ packages/patternfly-4/react-core/src/components/Tooltip/Tooltip.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { Tooltip } from './index';
+import Tippy from '@tippy.js/react';
 
 test('tooltip renders', () => {
   const view = shallow(
@@ -16,4 +17,17 @@ test('tooltip renders', () => {
     </Tooltip>
   );
   expect(view).toMatchSnapshot();
+});
+
+test('tooltip triggered by click', () => {
+  const view = shallow(
+    <Tooltip
+      position="top"
+      trigger="click"
+      content={<p>my content</p>}
+    >
+      <div>Toggle tooltip</div>
+    </Tooltip>
+  );
+  expect(view.find(Tippy).prop('trigger')).toBe('click');
 });
