273075dca2624669f18990bc116c24d4ad025c22
redallen <zallen@redhat.com>
2019-04-02 10:04:55 -0400

    real nice index

--- packages/patternfly-4/react-docs/src/pages/index.js
+++ packages/patternfly-4/react-docs/src/pages/index.js
@@ -3,21 +3,44 @@ import { graphql } from 'gatsby';
 import SidebarLayout from '../templates/sidebarLayout';
 import { Title, PageSection, PageSectionVariants } from '@patternfly/react-core';
 
+const containerStyle = {
+  display: "flex",
+  justifyContent: "center",
+  flexDirection: "column",
+  height: "100%"
+};
+
+// https://philipwalton.github.io/solved-by-flexbox/demos/vertical-centering/
+const centerStyle = {
+  flexGrow: 1,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center"
+};
+
 const IndexPage = ({ data }) => {
+  const prInfo = data.allEnvVars.edges
+    .filter(({ node }) => node.name === 'PR_INFO')[0].node;
+
   return (
     <SidebarLayout>
-      <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
-        <PageSection style={{ flexGrow: 1, textAlign: "center" }}>
-          <Title size="4xl">PatternFly 4 React Docs</Title>
-          <h1>Hi people!</h1>
-          <p>Welcome to Patternfly 4 React docs.</p>
-          <p>Now go build something great.</p>
+      <div style={containerStyle}>
+        <PageSection style={centerStyle}>
+          <div style={{ flex: "none", textAlign: "center" }}>
+            <Title size="4xl">PatternFly 4 React Docs</Title>
+            <Title size="2xl">
+              {prInfo.num ? <a href={prInfo.url}>PR #{prInfo.num}</a> : "Hi people!"}
+            </Title>
+            <p>Welcome to Patternfly 4 React docs.</p>
+            <p>Now go build something great.</p>
+          </div>
         </PageSection>
         <PageSection style={{ flexGrow: 0 }} variant={PageSectionVariants.dark}>
           Built with:
-          {Object.values(data).map(v => v.nodes[0]).map(node =>
-            <p key={node.name}>{node.name}: {node.version}</p>
-          )}
+          {Object.values(data)
+            .filter(v => v.nodes)
+            .map(v => v.nodes[0])
+            .map(node => <p key={node.name}>{node.name}: {node.version}</p>)}
         </PageSection>
       </div>
     </SidebarLayout >
@@ -66,6 +89,15 @@ export const pageQuery = graphql`
     nodes {
       name
       version
+    }
+  }
+  allEnvVars {
+    edges {
+      node {
+        name
+        num
+        url
+      }
     }
   }
 }`;
