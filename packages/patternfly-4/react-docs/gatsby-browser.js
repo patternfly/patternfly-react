843aa13bcb6a126bfa38c3cbf38709f3084271f3
redallen <zallen@redhat.com>
2019-04-04 09:44:58 -0400

    fix docbuild

--- packages/patternfly-4/react-docs/gatsby-browser.js
+++ packages/patternfly-4/react-docs/gatsby-browser.js
@@ -1,6 +1,4 @@
 // Import global CSS files here. Have no remorse.
-import '@patternfly/patternfly/patternfly.css';
 // https://www.gatsbyjs.org/docs/creating-global-styles
+import '@patternfly/patternfly/patternfly.css';
 import './static/base.css';
-import '@patternfly/patternfly/components/Table/table.css';
-import '@patternfly/patternfly/components/Table/table-grid.css';
