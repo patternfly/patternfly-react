ba197dec96832fd3dc7e8820693bdbf8fb40e033
Boaz Shuster <boaz.shuster.github@gmail.com>
2019-04-09 00:54:16 +0300

    fix(emptyState): add small/large modifiers
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- packages/patternfly-4/react-core/src/components/EmptyState/EmptyState.d.ts
+++ packages/patternfly-4/react-core/src/components/EmptyState/EmptyState.d.ts
@@ -1,8 +1,15 @@
 import { FunctionComponent, HTMLProps, ReactNode } from 'react';
-import { Omit } from '../../helpers/typeUtils';
+import { OneOf, Omit } from '../../helpers/typeUtils';
+
+export const EmptyStateVariant = {
+  large: 'large',
+  small: 'small',
+  full: 'full'
+};
 
 export interface EmptyStateProps extends Omit<HTMLProps<HTMLDivElement>, 'children'> {
   children: ReactNode;
+  variant: OneOf<typeof EmptyStateVariant, keyof typeof EmptyStateVariant>;
 }
 
 declare const EmptyState: FunctionComponent<EmptyStateProps>;
--- packages/patternfly-4/react-core/src/components/EmptyState/EmptyState.js
+++ packages/patternfly-4/react-core/src/components/EmptyState/EmptyState.js
@@ -3,23 +3,41 @@ import PropTypes from 'prop-types';
 import { css } from '@patternfly/react-styles';
 import styles from '@patternfly/patternfly/components/EmptyState/empty-state.css';
 
-const EmptyState = ({ children, className, ...props }) => (
-  <div className={css(styles.emptyState, className)} {...props}>
-    {children}
-  </div>
-);
+export const EmptyStateVariant = {
+  large: 'large',
+  small: 'small',
+  full: 'full',
+};
+
+const maxWidthModifiers = {
+  large: styles.modifiers.lg,
+  small: styles.modifiers.sm,
+  full: null,
+}
+
+const EmptyState = ({ children, className, variant, ...props }) => {
+  const maxWidthModifier = variant && maxWidthModifiers[variant];
+  return (
+    <div className={css(styles.emptyState, maxWidthModifier && maxWidthModifier, className)} {...props}>
+      {children}
+    </div>
+  );
+};
 
 EmptyState.propTypes = {
   /** Additional classes added to the EmptyState */
   className: PropTypes.string,
   /** Content rendered inside the EmptyState */
   children: PropTypes.node.isRequired,
+  /** Modifies EmptyState max-width */
+  variant: PropTypes.oneOf(Object.keys(EmptyStateVariant)),
   /** Additional props are spread to the container <div> */
   '': PropTypes.any
 };
 
 EmptyState.defaultProps = {
-  className: ''
+  className: '',
+  variant: EmptyStateVariant.large,
 };
 
 export default EmptyState;
