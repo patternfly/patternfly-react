67190ab06537ec9f59db1c0774f49f87366ab1dc
zallen <47335686+redallen@users.noreply.github.com>
2019-04-09 10:41:31 -0400

    feat(docs): fix regression to hot reload core changes (#1727)

--- packages/patternfly-4/react-docs/src/helpers/dynamicImports.js
+++ packages/patternfly-4/react-docs/src/helpers/dynamicImports.js
@@ -1,35 +1,32 @@
-// Dynamic imports. Yep, this is as ghetto as it looks.
 // We cannot use `"@patternfly/react-core": "file:../react-core"` in package.json and then
 // use the normal require("@patternfly/react-core") here because we need to use a *locked*
 // version of react-core for other component _inside_ our website (like the nav) in case
 // they break during local development. This is so the website won't fall apart if you break
 // the code for react-core/src/components/Button locally.
-const reactCharts = require("../../../react-charts/dist/esm");
-const reactCore = require("../../../react-core/dist/esm");
-const reactIcons = require("../../../../react-icons/dist/esm");
-const reactStyled = require("../../../react-styled-system/dist/esm");
-const reactStyles = require("../../../react-styles/dist/esm");
-const reactTable = require("../../../react-table/dist/esm");
-const reactTokens = require("../../../react-tokens/dist/esm");
-const reactStyledSystem = require("../../../react-styled-system/dist/esm")
-const reactInlineEdit = require("../../../react-inline-edit-extension/dist/esm")
+import * as reactCharts from "../../../react-charts/dist/esm";
+import * as reactCore from "../../../react-core/dist/esm";
+import * as reactIcons from "../../../../react-icons/dist/esm";
+import * as reactInlineEdit from "../../../react-inline-edit-extension/dist/esm";
+import * as reactStyledSystem from "../../../react-styled-system/dist/esm";
+import * as reactStyles from "../../../react-styles/dist/esm";
+import * as reactTable from "../../../react-table/dist/esm";
+import * as reactTokens from "../../../react-tokens/dist/esm";
 // This is from our gatsby-transformer-react-examples plugin
-const exampleComponents = require("../../.cache/example_index");
+import * as exampleComponents from "../../.cache/example_index";
 
 
 let imports = {
   "@patternfly/react-charts": reactCharts,
   "@patternfly/react-core": reactCore,
   "@patternfly/react-icons": reactIcons,
-  "@patternfly/react-styled-system": reactStyled,
+  "@patternfly/react-styled-system": reactStyledSystem,
   "@patternfly/react-styles": reactStyles,
   "@patternfly/react-table": reactTable,
   "@patternfly/react-tokens": reactTokens,
-  "@patternfly/react-styled-system": reactStyledSystem,
   "@patternfly/react-inline-edit-extension": reactInlineEdit
 };
 
-exports.getScope = (sourceCode, exampleResources) => {
+export const getScope = (sourceCode, exampleResources) => {
   let res = {};
   Object.entries(imports).forEach(([key, value]) => {
     if (sourceCode.indexOf(key) !== -1) {
@@ -46,4 +43,4 @@ exports.getScope = (sourceCode, exampleResources) => {
   return res;
 };
 
-exports.getTokens = () => imports["@patternfly/react-tokens"];
+export const getTokens = () => imports["@patternfly/react-tokens"];
