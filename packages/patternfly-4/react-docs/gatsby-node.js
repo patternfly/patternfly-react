698c1016658fbba095ddecef78caac3df89c0444
zallen <47335686+redallen@users.noreply.github.com>
2019-04-10 10:04:30 -0400

    fix(build): travis docs (#1721)
    * fix ignore globs for gatsby build on travis
    
    * Revert always build docs

--- packages/patternfly-4/react-docs/gatsby-node.js
+++ packages/patternfly-4/react-docs/gatsby-node.js
@@ -19,76 +19,61 @@ exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
       type: `EnvVars`,
     },
   });
-}
+};
 
-exports.createPages = ({ actions, graphql }) => {
-  const templatePath = path.resolve('./src/templates/markdownTemplate.js');
+const templatePath = path.resolve('./src/templates/markdownTemplate.js');
 
-  const markdown = graphql(`
-    {
-      allMarkdownRemark {
-        nodes {
-          fileAbsolutePath
-          htmlAst
-          frontmatter {
-            title
-            section
-            seperatePages
-          }
-        }
-      }
-    }
-  `);
-  return markdown.then(result => {
-    result.data.allMarkdownRemark.nodes.forEach(node => {
-      const componentName = navHelpers.getFileName(node.fileAbsolutePath);
-      const folderName = navHelpers.getParentFolder(node.fileAbsolutePath);
+// Create pages for markdown files
+exports.onCreateNode = ({ node, actions }) => {
+  if (node.internal.type !== 'MarkdownRemark')
+    return;
 
-      let link = '/bad-page/';
-      let context = {};
-      // Create fullscreen example component pages for any links in the *.md
-      if (node.frontmatter.seperatePages) {
-        // Create the templated page to link to them differently
-        link = `/${node.frontmatter.section}/${componentName}/`;
-        context = {
-          title: node.frontmatter.title,
-          fileAbsolutePath: node.fileAbsolutePath,
-          pathRegex: '', // No props
-          examplesRegex: '', // No examples to inject (they're on separate pages)
-        };
+  const componentName = navHelpers.getFileName(node.fileAbsolutePath);
+  const folderName = navHelpers.getParentFolder(node.fileAbsolutePath);
 
-        // Create the separate pages
-        astHelpers.getLinks(node.htmlAst).forEach(mdLink => {
-          const split = mdLink
-            .replace('.', '')
-            .split('/')
-            .filter(s => s);
-          const demoComponent = split[split.length - 1];
-          const basePath = path.dirname(node.fileAbsolutePath);
+  let link = '/bad-page/';
+  let context = {};
+  // Create fullscreen example component pages for any links in the *.md
+  if (node.frontmatter.seperatePages) {
+    // Create the templated page to link to them differently
+    link = `/${node.frontmatter.section}/${componentName}/`;
+    context = {
+      title: node.frontmatter.title,
+      fileAbsolutePath: node.fileAbsolutePath,
+      pathRegex: '', // No props
+      examplesRegex: '', // No examples to inject (they're on separate pages)
+    };
 
-          actions.createPage({
-            path: `${link}${split.join('/')}/`,
-            // Assume [Link](/PageLayoutSimpleNav/) in *.md means there is a ./examples/PageLayoutSimpleNav.js
-            component: path.resolve(`${basePath}/examples/${demoComponent}.js`),
-          });
-        });
-      } else {
-        // Normal templated component pages
-        let section = node.frontmatter.section ? node.frontmatter.section : 'components';
-        link = `/${section}/${componentName}/`;
-        context = {
-          title: node.frontmatter.title,
-          fileAbsolutePath: node.fileAbsolutePath, // Helps us get the markdown
-          pathRegex: `/${folderName}\/.*/`, // Helps us get the docgenned props
-          examplesRegex: `/${folderName}\/examples\/.*/`, // Helps us inject the example files
-        }
-      }
-      // console.log('adding page', link);
+    // Create the separate pages
+    astHelpers.getLinks(node.htmlAst).forEach(mdLink => {
+      const split = mdLink
+        .replace('.', '')
+        .split('/')
+        .filter(s => s);
+      const demoComponent = split[split.length - 1];
+      const basePath = path.dirname(node.fileAbsolutePath);
+
       actions.createPage({
-        path: link,
-        component: templatePath,
-        context: context
+        path: `${link}${split.join('/')}/`,
+        // Assume [Link](/PageLayoutSimpleNav/) in *.md means there is a ./examples/PageLayoutSimpleNav.js
+        component: path.resolve(`${basePath}/examples/${demoComponent}.js`),
       });
     });
-  })
+  } else {
+    // Normal templated component pages
+    let section = node.frontmatter.section ? node.frontmatter.section : 'components';
+    link = `/${section}/${componentName}/`;
+    context = {
+      title: node.frontmatter.title,
+      fileAbsolutePath: node.fileAbsolutePath, // Helps us get the markdown
+      pathRegex: `/${folderName}\/.*/`, // Helps us get the docgenned props
+      examplesRegex: `/${folderName}\/examples\/.*/`, // Helps us inject the example files
+    }
+  }
+  // console.log('adding page', link);
+  actions.createPage({
+    path: link,
+    component: templatePath,
+    context: context
+  });
 };
