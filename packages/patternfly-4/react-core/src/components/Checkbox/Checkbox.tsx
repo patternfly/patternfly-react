76a8b7417ad675d4d20d7c0e0222d78bd34b4aa9
Jessie <jhuff@redhat.com>
2019-04-11 11:59:47 -0400

    feat(Checkbox): Convert Checkbox to TypeScript

---
+++ packages/patternfly-4/react-core/src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import styles from '@patternfly/patternfly/components/Check/check.css';
+import { css, getModifier } from '@patternfly/react-styles';
+import { Omit } from '../../helpers/typeUtils';
+
+export interface CheckboxProps
+  extends Omit<React.HTMLProps<HTMLInputElement>, 'type' | 'onChange' | 'disabled' | 'label'> {
+  className?: string;
+  isValid?: boolean;
+  isDisabled?: boolean;
+  isChecked?: boolean;
+  checked?: boolean;
+  onChange?(checked: boolean, event: React.FormEvent<HTMLInputElement>): void;
+  label?: React.ReactNode;
+  id: string;
+  'aria-label': string;
+}
+
+export const Checkbox: React.FunctionComponent<CheckboxProps> = ({
+  className = '',
+  isValid = true,
+  isDisabled = false,
+  isChecked = null,
+  checked = null,
+  onChange = () => undefined,
+  label = undefined,
+  'aria-label': ariaLabel = '',
+  ...props
+}) => {
+  const handleChange = event => {
+    this.props.onChange(event.currentTarget.checked, event);
+  };
+
+  return (
+    <div className={css(styles.check, className)}>
+      <input
+        {...props}
+        className={css(styles.checkInput)}
+        type="checkbox"
+        onChange={handleChange}
+        aria-invalid={!isValid}
+        disabled={isDisabled}
+        checked={isChecked || checked}
+      />
+      {label && (
+        <label className={css(styles.checkLabel, getModifier(styles, isDisabled && 'disabled'))} htmlFor={props.id}>
+          {label}
+        </label>
+      )}
+    </div>
+  );
+};
