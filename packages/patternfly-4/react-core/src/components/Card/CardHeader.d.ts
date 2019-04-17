a5072b0e260d978bf013d89102d70058b9b6befd
Titani <tlabaj@redaht.com>
2019-04-17 10:06:54 -0400

    fix(Card): change Header to be a div and not h1
    #1783

--- packages/patternfly-4/react-core/src/components/Card/CardHeader.d.ts
+++ packages/patternfly-4/react-core/src/components/Card/CardHeader.d.ts
@@ -2,11 +2,7 @@ import { FunctionComponent, HTMLProps, ReactType } from 'react';
 import { TitleSize, TitleLevel } from '../Title';
 import { OneOf, Omit } from '../../helpers/typeUtils';
 
-export interface CardHeaderProps extends Omit<HTMLProps<HTMLHeadingElement>, 'size'> {
-  component?: ReactType<CardHeaderProps>;
-  size?: OneOf<typeof TitleSize, keyof typeof TitleSize>;
-  headingLevel?: TitleLevel;
-}
+export interface CardHeaderProps extends HTMLProps<HTMLDivElement> {}
 
 declare const CardHeader: FunctionComponent<CardHeaderProps>;
 
--- packages/patternfly-4/react-core/src/components/Card/CardHeader.js
+++ packages/patternfly-4/react-core/src/components/Card/CardHeader.js
@@ -2,30 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { css } from '@patternfly/react-styles';
 import styles from '@patternfly/patternfly/components/Card/card.css';
-import { Title, TitleSize, TitleLevel } from '../Title';
+import titleStyles from '@patternfly/patternfly/components/Title/title.css';
 
 const propTypes = {
   children: PropTypes.any,
   className: PropTypes.string,
-  /** the size of the Title  */
-  size: PropTypes.oneOf(Object.values(TitleSize)),
-  /** the heading level to use */
-  headingLevel: PropTypes.oneOf(Object.values(TitleLevel)),
-  /** Additional props are spread to the container component */
   '': PropTypes.any
 };
 
 const defaultProps = {
   children: null,
-  className: '',
-  size: TitleSize.lg,
-  headingLevel: TitleLevel.h1
+  className: ''
 };
 
-const CardHeader = ({ children, className, size, headingLevel, ...props }) => (
-  <Title className={css(styles.cardHeader, className)} size={size} headingLevel={headingLevel} {...props}>
+const CardHeader = ({ children, className, ...props }) => (
+  <div className={css(styles.cardHeader, titleStyles.title, titleStyles.modifiers.lg, className)} {...props}>
     {children}
-  </Title>
+  </div>
 );
 
 CardHeader.propTypes = propTypes;
