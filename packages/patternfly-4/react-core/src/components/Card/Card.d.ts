db64c8c19cfd4d7c7215416077bb4613361066d1
Dana Gutride <dgutride@redhat.com>
2019-04-18 21:36:12 -0400

    feat(card): adds card is-hoverable modifier prop

--- packages/patternfly-4/react-core/src/components/Card/Card.d.ts
+++ packages/patternfly-4/react-core/src/components/Card/Card.d.ts
@@ -2,6 +2,7 @@ import { FunctionComponent, HTMLProps, ReactType } from 'react';
 
 export interface CardProps extends HTMLProps<HTMLDivElement> {
   component?: ReactType<CardProps>;
+  isHoverable?: boolean;
 }
 
 declare const Card: FunctionComponent<CardProps>;
