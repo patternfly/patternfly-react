00fc43abaa39d59584d4d4177e3a3f269ed6f2ad
redallen <zallen@redhat.com>
2019-04-22 11:07:59 -0400

    pretty filtering

--- scripts/incrementalBuild.js
+++ scripts/incrementalBuild.js
@@ -2,63 +2,71 @@ const fs = require('fs');
 const { hashElement } = require("folder-hash");
 const Project = require('@lerna/project');
 const RunCommand = require('@lerna/run')
-const filterPackages = require('@lerna/filter-packages');
 
+// Load cache
 const cacheFile = '.cache/incrementalCache'
 let cache = {}
 if (fs.existsSync(cacheFile)) {
   cache = JSON.parse(fs.readFileSync(cacheFile, 'utf8'));
 }
 
-const lernaRoot = new Project(__dirname);
+// Package filtering
+const isPf3 = process.argv.length > 2 && process.argv[2] === 'pf3';
+const isPf4 = process.argv.length > 2 && process.argv[2] === 'pf4';
 
-const whitelist = [
-  '@patternfly/react-charts',
-  '@patternfly/react-core',
-  '@patternfly/react-inline-edit-extension',
-  '@patternfly/react-styled-system',
-  '@patternfly/react-styles',
-  '@patternfly/react-table',
-  '@patternfly/react-tokens',
-  '@patternfly/react-icons',
-]
+const commonPackages = [
+  '@patternfly/react-icons'
+];
+// Assume src directory gets made into dist
+const getDir = (packageName) => {
+  switch(packageName) {
+    case '@patternfly/react-icons':
+      return 'build'
+    default:
+      return 'src'
+  }
+};
 
 // These are packages we need to rebuild
 async function getInvalidPackages() {
-  let packages = filterPackages(await lernaRoot.getPackages(), whitelist, [], true)
+  const packages = (await new Project(__dirname).getPackages())
+    .filter(p => p.scripts.build) // Only packages that have a build target
+    .filter(p => isPf3
+      ? p.location.indexOf('patternfly-3') !== -1 || commonPackages.indexOf(p.name) !== -1
+      : true) // Based off argv
+    .filter(p => isPf4
+      ? p.location.indexOf('patternfly-4') !== -1 || commonPackages.indexOf(p.name) !== -1
+      : true) // Based off argv
 
   for (let p of packages) {
-    // Assume src directory gets made into dist
-    const watchDir = p.name === '@patternfly/react-icons' ? 'build' : 'src'
-    p.hash = (await hashElement(`${p.location}/${watchDir}`)).hash
-    p.valid = cache && cache[p.name] === p.hash
+    const watchDir = getDir(p.name);
+    p.hash = (await hashElement(`${p.location}/${watchDir}`)).hash;
+    p.valid = cache && cache[p.name] === p.hash;
+    if (p.valid) {
+      console.info('Skipping', p.name, '(already built).');
+    }
   }
 
-  return packages.filter(p => !p.valid)
+  return packages.filter(p => !p.valid);
 }
 
 async function incrementalBuild() {
-  const packages = await getInvalidPackages()
+  const packages = await getInvalidPackages();
 
   if (packages.length > 0) {
     // Run for all invalid packages
-    // console.log(packages.map(p => p.name))
     await RunCommand({
       cwd: '.',
       script: 'build',
       npmClient: 'yarn',
-      scope: packages.map(p => p.name)})
+      scope: packages.map(p => p.name)});
     // Mark as valid
-    packages.forEach(p => cache[p.name] = p.hash)
-    fs.mkdirSync('.cache')
-    fs.writeFileSync(cacheFile, JSON.stringify(cache, null, 4))
+    packages.forEach(p => cache[p.name] = p.hash);
+    if (!fs.existsSync('.cache')) {
+      fs.mkdirSync('.cache');
+    }
+    fs.writeFileSync(cacheFile, JSON.stringify(cache, null, 4));
   }
 }
 
-incrementalBuild().then()
-// new ListCommand({cwd: '.', _: ['ll']}).then(list => {
-//   console.log('list', list)
-// })
-
-// const packages = helpers.getPfPackages(true).then(packages => console.log('packages', packages));
-
+incrementalBuild().then();
