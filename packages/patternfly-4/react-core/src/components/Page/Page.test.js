9368e1c1aec1ebae839b86a2e19f2500e9e477ef
Donald Labaj <donald.labaj@gmail.com>
2019-04-23 11:40:30 -0400

    Added Page skip to content test

--- packages/patternfly-4/react-core/src/components/Page/Page.test.js
+++ packages/patternfly-4/react-core/src/components/Page/Page.test.js
@@ -5,6 +5,7 @@ import PageHeader from './PageHeader';
 import PageSidebar from './PageSidebar';
 import PageSection from './PageSection';
 import { Breadcrumb, BreadcrumbItem } from '../Breadcrumb';
+import { SkipToContent } from '../SkipToContent';
 
 const props = {
   'aria-label': 'Page layout',
@@ -55,6 +56,38 @@ test('Check page to verify breadcrumb is created', () => {
   );
   const view = mount(
     <Page {...props} layout="horizontal" header={Header} sidebar={Sidebar} breadcrumb={PageBreadcrumb}>
+      <PageSection variant="default">Section with default background</PageSection>
+      <PageSection variant="light">Section with light background</PageSection>
+      <PageSection variant="dark">Section with dark background</PageSection>
+      <PageSection variant="darker">Section with darker background</PageSection>
+    </Page>
+  );
+  expect(view).toMatchSnapshot();
+});
+
+test('Check page to verify skip to content is created', () => {
+  const Header = <PageHeader logo="Logo" toolbar="Toolbar" avatar=" | Avatar" nav="Navigation" />;
+  const Sidebar = <PageSidebar isNavOpen />;
+  const PageBreadcrumb = (
+    <Breadcrumb>
+      <BreadcrumbItem>Section Home</BreadcrumbItem>
+      <BreadcrumbItem to="#">Section Title</BreadcrumbItem>
+      <BreadcrumbItem to="#">Section Title</BreadcrumbItem>
+      <BreadcrumbItem to="#" isActive>
+        Section Landing
+      </BreadcrumbItem>
+    </Breadcrumb>
+  );
+  const PageSkipToContent = <SkipToContent href="#main-content-page-layout-default-nav">Skip to Content</SkipToContent>;
+  const view = mount(
+    <Page
+      {...props}
+      layout="horizontal"
+      header={Header}
+      sidebar={Sidebar}
+      breadcrumb={PageBreadcrumb}
+      skipToContent={PageSkipToContent}
+    >
       <PageSection variant="default">Section with default background</PageSection>
       <PageSection variant="light">Section with light background</PageSection>
       <PageSection variant="dark">Section with dark background</PageSection>
