86b3087fddb1b837180b41c3dab1cb89140f19af
Mike Turley <mturley@redhat.com>
2019-04-25 09:18:13 -0400

    feat(DropdownToggle): Add splitButtonItems prop, DropdownToggleCheckbox and split-button dropdown examples (#1843)
    * feat(DropdownToggle): Add splitButtonItems prop, DropdownToggleCheckbox and split-button dropdown examples
    
    Closes #1287
    
    * Fix handling of aria-label, update snapshots
    
    * Refactor away InnerToggle helper, update snapshots
    
    * Fix d.ts file

--- packages/patternfly-4/react-core/src/components/Dropdown/DropdownToggle.js
+++ packages/patternfly-4/react-core/src/components/Dropdown/DropdownToggle.js
@@ -5,19 +5,38 @@ import Toggle from './Toggle';
 import styles from '@patternfly/patternfly/components/Dropdown/dropdown.css';
 import { css } from '@patternfly/react-styles';
 
-const DropdownToggle = ({ children, iconComponent: IconComponent, ...props }) => (
-  <Toggle {...props}>
-    <span className={IconComponent && css(styles.dropdownToggleText)}>{children}</span>
-    {IconComponent && <IconComponent className={css(styles.dropdownToggleIcon)} />}
-  </Toggle>
-);
+const DropdownToggle = ({ children, iconComponent: IconComponent, splitButtonItems, ...props }) => {
+  const toggle = (
+    <Toggle {...props} {...splitButtonItems && { isSplitButton: true, 'aria-label': props['aria-label'] || 'Select' }}>
+      {children && <span className={IconComponent && css(styles.dropdownToggleText)}>{children}</span>}
+      {IconComponent && <IconComponent className={css(children && styles.dropdownToggleIcon)} />}
+    </Toggle>
+  );
+
+  if (splitButtonItems) {
+    return (
+      <div
+        className={css(
+          styles.dropdownToggle,
+          styles.modifiers.splitButton,
+          props.isDisabled && styles.modifiers.disabled
+        )}
+      >
+        {splitButtonItems}
+        {toggle}
+      </div>
+    );
+  }
+
+  return toggle;
+};
 
 DropdownToggle.propTypes = {
   /** HTML ID of dropdown toggle */
   id: PropTypes.string,
-  /** Anything which can be rendered as dropdown toggle */
-  children: PropTypes.node.isRequired,
-  /** Classes applied to root element of dropdown toggle */
+  /** Anything which can be rendered as dropdown toggle button */
+  children: PropTypes.node,
+  /** Classes applied to root element of dropdown toggle button */
   className: PropTypes.string,
   /** Flag to indicate if menu is opened */
   isOpen: PropTypes.bool,
@@ -35,12 +54,17 @@ DropdownToggle.propTypes = {
   isPlain: PropTypes.bool,
   /** The icon to display for the toggle. Defaults to CaretDownIcon. Set to null to not show an icon. */
   iconComponent: PropTypes.func,
+  /** Elements to display before the toggle button. When included, renders the toggle as a split button. */
+  splitButtonItems: PropTypes.arrayOf(PropTypes.node),
+  /** Accessible label for the dropdown toggle button */
+  'aria-label': PropTypes.any,
   /** Additional props are spread to the container component */
   '': PropTypes.any
 };
 
 DropdownToggle.defaultProps = {
   id: '',
+  children: null,
   className: '',
   isOpen: false,
   parentRef: null,
--- packages/patternfly-4/react-core/src/components/Dropdown/index.d.ts
+++ packages/patternfly-4/react-core/src/components/Dropdown/index.d.ts
@@ -5,3 +5,4 @@ export { default as DropdownItem, ItemProps } from './Item';
 export { default as DropdownSeparator, SeparatorProps } from './Separator';
 export { default as KebabToggle, KebabProps } from './KebabToggle';
 export { default as DropdownToggle, DropdownToggleProps } from './DropdownToggle';
+export { default as DropdownToggleCheckbox, DropdownToggleCheckboxProps } from './DropdownToggleCheckbox';
