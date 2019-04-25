6bd3d7efcd9132a00220996862a70617c2fa3f7f
zallen <47335686+redallen@users.noreply.github.com>
2019-04-25 08:34:48 -0400

    feat(build): incremental build (#1817)
    * initial work
    
    * it works for me
    
    * pretty filtering
    
    * add pf3/pf4 build targets
    
    * add to CI
    
    * pretty targets

---
+++ scripts/incrementalBuild.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+const { hashElement } = require("folder-hash");
+const Project = require('@lerna/project');
+const RunCommand = require('@lerna/run')
+
+// Load cache
+const cacheFile = '.cache/incrementalCache'
+let cache = {}
+if (fs.existsSync(cacheFile)) {
+  cache = JSON.parse(fs.readFileSync(cacheFile, 'utf8'));
+}
+
+// Package filtering
+const isPf3 = process.argv.length > 2 && process.argv[2] === 'pf3';
+const isPf4 = process.argv.length > 2 && process.argv[2] === 'pf4';
+
+const commonPackages = [
+  '@patternfly/react-icons'
+];
+// Assume src directory gets made into dist
+const getDir = (packageName) => {
+  switch(packageName) {
+    case '@patternfly/react-icons':
+      return 'build'
+    default:
+      return 'src'
+  }
+};
+
+// These are packages we need to rebuild
+async function getInvalidPackages() {
+  const packages = (await new Project(__dirname).getPackages())
+    .filter(p => p.scripts.build) // Only packages that have a build target
+    .filter(p => isPf3
+      ? p.location.indexOf('patternfly-3') !== -1 || commonPackages.indexOf(p.name) !== -1
+      : true) // Based off argv
+    .filter(p => isPf4
+      ? p.location.indexOf('patternfly-4') !== -1 || commonPackages.indexOf(p.name) !== -1
+      : true) // Based off argv
+
+  for (let p of packages) {
+    const watchDir = getDir(p.name);
+    p.hash = (await hashElement(`${p.location}/${watchDir}`)).hash;
+    p.valid = cache && cache[p.name] === p.hash;
+    if (p.valid) {
+      console.info('Skipping', p.name, '(already built).');
+    }
+  }
+
+  return packages.filter(p => !p.valid);
+}
+
+async function incrementalBuild() {
+  const packages = await getInvalidPackages();
+
+  if (packages.length > 0) {
+    // Run for all invalid packages
+    await RunCommand({
+      cwd: '.',
+      script: 'build',
+      npmClient: 'yarn',
+      scope: packages.map(p => p.name)});
+    // Mark as valid
+    packages.forEach(p => cache[p.name] = p.hash);
+    if (!fs.existsSync('.cache')) {
+      fs.mkdirSync('.cache');
+    }
+    fs.writeFileSync(cacheFile, JSON.stringify(cache, null, 4));
+  }
+}
+
+incrementalBuild().then();
