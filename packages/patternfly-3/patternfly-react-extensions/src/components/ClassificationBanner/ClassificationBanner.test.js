265f9d8b1643aa54dd46e96220f8a131e08735bc
lesleycl <lesleycahi518@gmail.com>
2019-05-01 21:54:50 -0400

    add test case

--- packages/patternfly-3/patternfly-react-extensions/src/components/ClassificationBanner/ClassificationBanner.test.js
+++ packages/patternfly-3/patternfly-react-extensions/src/components/ClassificationBanner/ClassificationBanner.test.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import ClassificationBanner from './ClassificationBanner';
 
-const props = {};
+const testClassificationBannerSnapshot = () => (
+  <ClassificationBanner
+    // bottomBanner={true}
+    classificationLevel="1"
+    closeButton={false}
+    hostName="Host Name"
+    userName="User Name"
+    closed={false}
+    userNamePosition="left"
+    hostNamePosition="right"
+    bannerColor=""
+  >
+    <br />
+    <p>This is the main body of a web page.</p>
+    <p>Scroll down to see the bottom banner.</p>
+  </ClassificationBanner>
+);
 
-test('replace with useful test name', () => {
-  const view = shallow(<ClassificationBanner {...props} />);
-  // Add a useful assertion here.
-  expect(view).toBe(false);
+test('ClassificationBanner with renders properly', () => {
+  const component = mount(testClassificationBannerSnapshot());
+
+  expect(component.render()).toMatchSnapshot();
+});
+
+test('ClassificationBanner expectedly executes mouse click and is dismissed', () => {
+  const component = mount(testClassificationBannerSnapshot({
+    closed: false
+  }));
+  const closeButton = component.find('.classification-banner-pf-close');
+  closeButton.simulate('click');
+  expect(component.props().closed).toBe(true);
 });
