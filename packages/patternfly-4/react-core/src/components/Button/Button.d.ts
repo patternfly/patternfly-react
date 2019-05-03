79d3784514b199c2a45611c6851d1ea5f2631b5f
Donald Labaj <donald.labaj@gmail.com>
2019-05-03 11:14:42 -0400

    Reverted button d ts updates.

--- packages/patternfly-4/react-core/src/components/Button/Button.d.ts
+++ packages/patternfly-4/react-core/src/components/Button/Button.d.ts
@@ -17,7 +17,7 @@ export const ButtonType: {
 
 export interface ButtonProps extends HTMLProps<HTMLButtonElement> {
   children?: ReactNode;
-  component?: ReactType | string;
+  component?: ReactType<ButtonProps>;
   isActive?: boolean;
   isBlock?: boolean;
   isDisabled?: boolean;
@@ -26,7 +26,6 @@ export interface ButtonProps extends HTMLProps<HTMLButtonElement> {
   isInline?: boolean;
   variant?: OneOf<typeof ButtonVariant, keyof typeof ButtonVariant>;
   type?: OneOf<typeof ButtonType, keyof typeof ButtonType>;
-  to?: string
 }
 
 declare const Button: FunctionComponent<ButtonProps>;
