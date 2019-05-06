f0dcd1eb1539a63dbbee50a7d728578d2655a0e9
Joseph Caiani <jcaiani@redhat.com>
2019-05-06 12:10:11 -0400

    Add lodash merge to imports

--- packages/patternfly-4/react-charts/src/components/ChartZoomContainer/ChartZoomContainer.js
+++ packages/patternfly-4/react-charts/src/components/ChartZoomContainer/ChartZoomContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import hoistNonReactStatics from 'hoist-non-react-statics';
 import { VictoryZoomContainer } from 'victory';
+import { merge } from 'lodash';
 
 class ChartZoomContainer extends VictoryZoomContainer {
   static propTypes = {
@@ -12,7 +13,7 @@ class ChartZoomContainer extends VictoryZoomContainer {
     return children.map((child) => {
       // Some victory components expect to control props like domain for
       // children, some props should be merged rather than overridden
-      const style = _.merge(child.props.style, this.props.style);
+      const style = merge(child.props.style, this.props.style);
       return React.cloneElement(child, Object.assign({}, child.props, this.props, { style }));
     });
   }
