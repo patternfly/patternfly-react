c517ec60376b87c5f8d227d9c2718e169427444a
redallen <zallen@redhat.com>
2019-05-10 16:43:49 -0400

    add back 2 prop imports that mysteriously work

--- packages/patternfly-4/react-integration/demo-app-ts/src/components/demos/NavDemo/NavDemo.tsx
+++ packages/patternfly-4/react-integration/demo-app-ts/src/components/demos/NavDemo/NavDemo.tsx
@@ -1,12 +1,16 @@
 import { Nav, NavList, NavItem, NavProps } from '@patternfly/react-core';
 import React, { Component } from 'react';
 
+const myProps: NavProps = {
+  "aria-label": 'Test Nav Props'
+};
+
 export class NavDemo extends Component {
   render() {
     // Nav onToggle and onSelect should be optional
     // https://github.com/patternfly/patternfly-react/issues/1234
     return (
-      <Nav aria-label={'Test Nav Props'}>
+      <Nav aria-label={myProps["aria-label"]}>
         <NavList>
           <NavItem preventDefault={false}>Link 1</NavItem>
           <NavItem>Link 2</NavItem>
--- packages/patternfly-4/react-integration/demo-app-ts/src/components/demos/TitleDemo/TitleDemo.tsx
+++ packages/patternfly-4/react-integration/demo-app-ts/src/components/demos/TitleDemo/TitleDemo.tsx
@@ -1,11 +1,16 @@
-import { Title,  TitleLevel } from '@patternfly/react-core';
+import { Title,  TitleLevel, TitleProps } from '@patternfly/react-core';
 import React, { Component } from 'react';
+
+const myProps: TitleProps = {
+  size: 'md',
+  headingLevel: TitleLevel.h1
+};
 
 export class TitleDemo extends Component {
   render() {
     return (
     <React.Fragment>
-      <Title size={'md'} headingLevel={TitleLevel.h1}>
+      <Title size={myProps.size} headingLevel={myProps.headingLevel}>
         Integration Demo App
       </Title>
     </React.Fragment>
