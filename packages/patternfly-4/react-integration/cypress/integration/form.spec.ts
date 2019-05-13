24098014ae68b56312187aafd765d3e12d342928
Jessie <jhuff@redhat.com>
2019-05-13 09:51:42 -0400

    Added tests and updated snapshots

--- packages/patternfly-4/react-integration/cypress/integration/form.spec.ts
+++ packages/patternfly-4/react-integration/cypress/integration/form.spec.ts
@@ -4,4 +4,13 @@ describe('Form Demo Test', () => {
     cy.get('#form-demo-nav-item-link').click();
     cy.url().should('eq', 'http://localhost:3000/form-demo-nav-link');
   });
+
+  it('Verify default value content', () => {
+    cy.get('input').should('have.value', 'Five');
+  });
+
+  it('Verify form error', () => {
+    cy.get('.pf-m-error').contains('Age has to be a number'); 
+  }); 
+
 })
