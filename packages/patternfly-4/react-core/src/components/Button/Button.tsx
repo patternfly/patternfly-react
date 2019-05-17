8cf34fa27acf1b0afc6fcdf09e52d840016df349
Jessie <jhuff@redhat.com>
2019-05-17 09:53:31 -0400

    Convert button to tsx

--- packages/patternfly-4/react-core/src/components/Button/Button.tsx
+++ packages/patternfly-4/react-core/src/components/Button/Button.tsx
@@ -1,108 +1,86 @@
-import React from 'react';
-import { css, getModifier } from '@patternfly/react-styles';
-import PropTypes from 'prop-types';
-import { componentShape } from '../../helpers/componentShape';
+import * as React from 'react';
 import styles from '@patternfly/patternfly/components/Button/button.css';
+import { css, getModifier } from '@patternfly/react-styles';
 
-export const ButtonVariant = {
-  primary: 'primary',
-  secondary: 'secondary',
-  tertiary: 'tertiary',
-  danger: 'danger',
-  link: 'link',
-  plain: 'plain'
+export enum ButtonVariant {
+  primary = 'primary',
+  secondary = 'secondary',
+  tertiary = 'tertiary',
+  danger = 'danger',
+  link = 'link',
+  plain = 'plain'
 };
 
-export const ButtonType = {
-  button: 'button',
-  submit: 'submit'
+export enum ButtonType {
+  button = 'button',
+  submit = 'submit'
 };
 
-const propTypes = {
-  /** content rendered inside the button */
-  children: PropTypes.node,
-  /** additional classes added to the button */
-  className: PropTypes.string,
+export interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
+  /** Content rendered inside the button */
+  children?: React.ReactNode;
+  /** Additional classes added to the button */
+  className?: string; 
   /** Sets the base component to render. defaults to button */
-  component: componentShape,
-  /**  Adds active styling to button. */
-  isActive: PropTypes.bool,
+  component?: React.ReactNode;  
+  /** Adds active styling to button. */
+  isActive?: boolean;
   /** Adds block styling to button */
-  isBlock: PropTypes.bool,
+  isBlock?: boolean;
   /** Disables the button and adds disabled styling */
-  isDisabled: PropTypes.bool,
+  isDisabled?: boolean;
   /** Adds focus styling to the button */
-  isFocus: PropTypes.bool,
+  isFocus?: boolean;
   /** Adds hover styling to the button */
-  isHover: PropTypes.bool,
-   /** Adds inline styling to a link button */
-   isInline: PropTypes.bool,
+  isHover?: boolean;
+  /** Adds inline styling to a link button */
+  isInline?: boolean;
   /** Sets button type */
-  type: PropTypes.oneOf(Object.values(ButtonType)),
+  type?: 'button' | 'submit';
   /** Adds button variant styles */
-  variant: PropTypes.oneOf(Object.values(ButtonVariant)),
+  variant?: 'primary' | 'secondary' | 'tertiary' | 'danger' | 'link' | 'plain' ;
   /** Adds accessible text to the button. */
-  'aria-label': PropTypes.string,
-  /** Additional props are spread to the container <button> */
-  '': PropTypes.any
-};
+  'aria-label'?: string; 
+}
 
-const defaultProps = {
-  children: '',
-  className: '',
-  component: 'button',
-  isActive: false,
-  isBlock: false,
-  isDisabled: false,
-  isFocus: false,
-  isHover: false,
-  isInline: false,
-  type: ButtonType.button,
-  variant: ButtonVariant.primary,
-  'aria-label': null
-};
-
-const Button = ({
-  children,
-  className,
-  component: Component,
-  isActive,
-  isBlock,
-  isDisabled,
-  isFocus,
-  isHover,
-  isInline,
-  variant,
-  type,
+export const Button: React.FunctionComponent<ButtonProps> = ({
+  children = null, 
+  className = '', 
+  component = 'button', 
+  isActive = false, 
+  isBlock = false,
+  isDisabled = false,
+  isFocus = false,
+  isHover = false,
+  isInline = false,
+  type = ButtonType.button,
+  variant = ButtonVariant.primary,
+  'aria-label': ariaLabel = null, 
   ...props
-}) => {
+}: ButtonProps) => {
+  const Component = component as any;
   const isButtonElement = Component === 'button';
-
   return (
-    <Component
-      {...props}
-      aria-disabled={isButtonElement ? null : isDisabled}
-      className={css(
-        styles.button,
-        getModifier(styles.modifiers, variant),
-        isBlock && styles.modifiers.block,
-        isDisabled && styles.modifiers.disabled,
-        isActive && styles.modifiers.active,
-        isFocus && styles.modifiers.focus,
-        isHover && styles.modifiers.hover,
-        isInline && variant === ButtonVariant.link && styles.modifiers.inline,
-        className
-      )}
-      disabled={isButtonElement ? isDisabled : null}
-      tabIndex={isDisabled && !isButtonElement ? -1 : null}
-      type={isButtonElement ? type : null}
-    >
-      {children}
-    </Component>
+  <Component
+    {...props}
+    aria-disabled={isButtonElement ? null : isDisabled}
+    aria-label={ariaLabel}
+    className={css(
+      styles.button,
+      getModifier(styles.modifiers, variant),
+      isBlock && styles.modifiers.block,
+      isDisabled && styles.modifiers.disabled,
+      isActive && styles.modifiers.active,
+      isFocus && styles.modifiers.focus,
+      isHover && styles.modifiers.hover,
+      isInline && variant === ButtonVariant.link && styles.modifiers.inline,
+      className
+    )}
+    disabled={isButtonElement ? isDisabled : null}
+    tabIndex={isDisabled && !isButtonElement ? -1 : null}
+    type={isButtonElement ? type : null}
+  >
+    {children}
+  </Component>
   );
-};
-
-Button.propTypes = propTypes;
-Button.defaultProps = defaultProps;
-
-export default Button;
+}
