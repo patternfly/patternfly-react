8c3d484cf91c7bf27678e041026fab4dfbe47685
Jeffrey Phillips <jephilli@redhat.com>
2019-05-22 13:58:30 -0400

    Fix lint errors in react-docs

--- packages/patternfly-4/react-docs/src/components/LinkPreview.js
+++ packages/patternfly-4/react-docs/src/components/LinkPreview.js
@@ -1,14 +1,30 @@
-import React from "react";
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import PropTypes from 'prop-types';
 
 // This component really shouldn't be in react-docs.
 // However, it exists downstream in https://github.com/patternfly/patternfly-org
-export default ({name, path, text}) => {
+const LinkPreview = ({ name, path, text }) => {
   const split = path.split('/');
 
   return (
     <>
       {text && <p>{text}</p>}
-      <a href={split[split.length - 1].toLowerCase()} target="_blank">{name}</a>
+      <a href={split[split.length - 1].toLowerCase()} target="_blank" rel="noopener noreferrer">
+        {name}
+      </a>
     </>
   );
-}
+};
+
+LinkPreview.propTypes = {
+  name: PropTypes.node.isRequired,
+  path: PropTypes.string.isRequired,
+  text: PropTypes.node
+};
+
+LinkPreview.defaultProps = {
+  text: null
+};
+
+export { LinkPreview as default };
