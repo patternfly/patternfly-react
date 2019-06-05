f1e82735ac0180b3327b13cd0dfa88d2876e87f5
Dana Gutride <dgutride@redhat.com>
2019-06-05 08:51:06 -0400

    fix(wrapper): revert bodywrapper conversion to stateless-function

--- packages/patternfly-4/react-table/src/components/Table/BodyWrapper.js
+++ packages/patternfly-4/react-table/src/components/Table/BodyWrapper.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import styles from '@patternfly/patternfly/components/Table/table.css';
 import { css } from '@patternfly/react-styles';
 import { mapOpenedRows } from './utils/headerUtils';
+import PropTypes from 'prop-types';
 
-const BodyWrapper = ({ children, mappedRows, ...props }) => {
-  if (mappedRows.some(row => row.hasOwnProperty('parent'))) {
-    return (
-      <React.Fragment>
-        {mapOpenedRows(mappedRows, children).map((oneRow, key) => (
-          <tbody {...props} className={css(oneRow.isOpen && styles.modifiers.expanded)} key={`tbody-${key}`}>
-            {oneRow.rows}
-          </tbody>
-        ))}
-      </React.Fragment>
-    );
+// eslint-disable-next-line react/prefer-stateless-function
+class BodyWrapper extends Component {
+  render() {
+    const { children, mappedRows, ...props } = this.props;
+    if (mappedRows.some(row => row.hasOwnProperty('parent'))) {
+      return (
+        <React.Fragment>
+          {mapOpenedRows(mappedRows, children).map((oneRow, key) => (
+            <tbody {...props} className={css(oneRow.isOpen && styles.modifiers.expanded)} key={`tbody-${key}`}>
+              {oneRow.rows}
+            </tbody>
+          ))}
+        </React.Fragment>
+      );
+    }
+    return <tbody {...props}>{children}</tbody>;
   }
-  return <tbody {...props}>{children}</tbody>;
-};
+}
 
 BodyWrapper.propTypes = {
   children: PropTypes.node,
