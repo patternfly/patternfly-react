f479fde48e430bf72484e79ffc191b99a312c563
Katie McFaul <kmcfaul@redhat.com>
2019-06-10 10:05:05 -0400

    feat(Select): rename files for git history - TS conversion

--- packages/patternfly-4/react-core/src/components/Select/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as Select, SelectProps, SelectVariant } from './Select';
-export { default as SelectOption, SelectOptionProps } from './SelectOption';
-export { default as CheckboxSelectOption, CheckboxSelectOptionProps } from './CheckboxSelectOption';
-export { default as CheckboxSelectGroup, CheckboxSelectGroupProps } from './CheckboxSelectGroup';
