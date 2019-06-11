e0ac80035a5887f63f023e5c62a9c0a045373023
Jessie <jhuff@redhat.com>
2019-06-11 16:35:10 -0400

    Add integration tests

--- packages/patternfly-4/react-integration/cypress/integration/inputgroup.spec.ts
+++ packages/patternfly-4/react-integration/cypress/integration/inputgroup.spec.ts
@@ -4,4 +4,13 @@ describe('Input Group Demo Test', () => {
     cy.get('#input-group-demo-nav-item-link').click();
     cy.url().should('eq', 'http://localhost:3000/input-group-demo-nav-link');
   });
+  it('Verify text input', () => {
+    cy.get('#textarea1').type('Hi');
+    cy.get('#textarea1').should('have.value', 'Hi');
+  });
+
+  it('Verify number input only allows numbers', () => {
+    cy.get('#textInput5').type('Hi');
+    cy.get('#textInput5').should('have.value', ''); 
+  })
 })
