3dde9337fc7e18e61f70645e0e6493f6ca33051c
jenny-s51 <jshandel12@gmail.com>
2019-06-12 09:41:16 -0400

    fix(pagination): works as required

--- packages/patternfly-4/react-core/src/components/Pagination/OptionsToggle.js
+++ packages/patternfly-4/react-core/src/components/Pagination/OptionsToggle.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styles from '@patternfly/react-styles/css/components/OptionsMenu/options-menu';
 import { css, getModifier } from '@patternfly/react-styles';
@@ -6,56 +6,43 @@ import { css, getModifier } from '@patternfly/react-styles';
 import { fillTemplate } from '../../helpers';
 import { DropdownToggle } from '../Dropdown';
 
-class OptionsToggle extends Component {
-  render() {
-    const {
-      itemsTitle,
-      optionsToggle,
-      firstIndex,
-      lastIndex,
-      itemCount,
-      widgetId,
-      onToggle,
-      isOpen,
-      showToggle,
-      toggleTemplate: ToggleTemplate
-    } = this.props;
+export function OptionsToggle(props) {
+  const {
+    itemsTitle,
+    optionsToggle,
+    firstIndex,
+    lastIndex,
+    itemCount,
+    widgetId,
+    onToggle,
+    isOpen,
+    showToggle,
+    parentRef,
+    toggleTemplate: ToggleTemplate
+  } = props;
 
-    return (
-      <div
-        className={css(styles.optionsMenuToggle, getModifier(styles, 'plain'), getModifier(styles, 'text'))}
-        ref={ref => {
-          this.parentRef = ref;
-        }}
-      >
-        <span className={css(styles.optionsMenuToggleText)}>
-          {typeof ToggleTemplate === 'string' ? (
-            fillTemplate(ToggleTemplate, { firstIndex, lastIndex, itemCount, itemsTitle })
-          ) : (
-            <ToggleTemplate
-              firstIndex={firstIndex}
-              lastIndex={lastIndex}
-              itemCount={itemCount}
-              itemsTitle={itemsTitle}
-            />
-          )}
-        </span>
-
-        {showToggle && (
-          <DropdownToggle
-            aria-label={optionsToggle}
-            onClick={() => onToggle(!isOpen)}
-            onToggle={onToggle}
-            isOpen={isOpen}
-            id={`${widgetId}-toggle`}
-            isSplitButton
-            className={styles.optionsMenuToggleButton}
-            parentRef={this.parentRef}
-          />
+  return (
+    <div className={css(styles.optionsMenuToggle, getModifier(styles, 'plain'), getModifier(styles, 'text'))}>
+      <span className={css(styles.optionsMenuToggleText)}>
+        {typeof ToggleTemplate === 'string' ? (
+          fillTemplate(ToggleTemplate, { firstIndex, lastIndex, itemCount, itemsTitle })
+        ) : (
+          <ToggleTemplate firstIndex={firstIndex} lastIndex={lastIndex} itemCount={itemCount} itemsTitle={itemsTitle} />
         )}
-      </div>
-    );
-  }
+      </span>
+      {showToggle && (
+        <DropdownToggle
+          aria-label={optionsToggle}
+          onToggle={onToggle}
+          isOpen={isOpen}
+          id={`${widgetId}-toggle`}
+          isSplitButton
+          className={styles.optionsMenuToggleButton}
+          parentRef={parentRef}
+        />
+      )}
+    </div>
+  );
 }
 
 OptionsToggle.propTypes = {
@@ -67,6 +54,7 @@ OptionsToggle.propTypes = {
   widgetId: PropTypes.string,
   onToggle: PropTypes.func,
   isOpen: PropTypes.bool,
+  parentRef: PropTypes.any,
   showToggle: PropTypes.bool,
   toggleTemplate: PropTypes.oneOfType([PropTypes.string, PropTypes.func])
 };
@@ -80,6 +68,7 @@ OptionsToggle.defaultProps = {
   widgetId: '',
   onToggle: () => undefined,
   isOpen: false,
+  parentRef: null,
   showToggle: true,
   toggleTemplate: ''
 };
