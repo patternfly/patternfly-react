e756f5598ad9cc2ea5bb2c2b5d7640aac244350c
jenny-s51 <jshandel12@gmail.com>
2019-06-13 15:40:28 -0400

    fix(accordion): adds prop to heading level of parent component

--- packages/patternfly-4/react-core/src/components/Accordion/Accordion.tsx
+++ packages/patternfly-4/react-core/src/components/Accordion/Accordion.tsx
@@ -2,6 +2,25 @@ import * as React from 'react';
 import { css } from '@patternfly/react-styles';
 import styles from '@patternfly/react-styles/css/components/Accordion/accordion';
 
+export const AccordionContext = React.createContext('h3');
+
+export enum HeadingLevel {
+  h1 = 'h1',
+  h2 = 'h2',
+  h3 = 'h3',
+  h4 = 'h4',
+  h5 = 'h5',
+  h6 = 'h6'
+}
+
+// declare global {
+//   namespace JSX {
+//     interface IntrinsicElements {
+//       'HeadingLevel': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+//     }
+//   }
+// }
+
 export interface AccordionProps extends React.HTMLProps<HTMLDListElement> {
   /** Content rendered inside the Accordion  */
   children?: React.ReactNode;
@@ -9,15 +28,18 @@ export interface AccordionProps extends React.HTMLProps<HTMLDListElement> {
   className?: string;
   /** Adds accessible text to the Accordion */
   'aria-label'?: string;
+  /** the heading level to use */
+  headingLevel?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 }
 
 export const Accordion: React.FunctionComponent<AccordionProps> = ({
   children = null,
   className = '',
   'aria-label': ariaLabel = '',
+  headingLevel = 'h3',
   ...props
 }: AccordionProps) => (
   <dl className={css(styles.accordion, className)} aria-label={ariaLabel} {...props}>
-    {children}
+    <AccordionContext.Provider value={ headingLevel }>{children}</AccordionContext.Provider>
   </dl>
 );
