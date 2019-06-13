7b4e1004872d3a0cd5b4d748fa5bbc2a88532ce2
Dan Labrecque <dlabrecq@redhat.com>
2019-06-13 10:24:53 -0400

    fix(charts): labels should use the overpass font (#2232)
    Fixes https://github.com/patternfly/patternfly-react/issues/2160
    Fixes https://github.com/patternfly/patternfly-react/issues/2214

--- packages/patternfly-4/react-charts/src/components/ChartTheme/themes/donut-theme.ts
+++ packages/patternfly-4/react-charts/src/components/ChartTheme/themes/donut-theme.ts
@@ -5,10 +5,10 @@ export const DonutStyles = {
   label: {
     subTitle: {
       fill: '#bbb',
-      fontSize: 16
+      fontSize: 14
     },
     title: {
-      fontSize: 26
+      fontSize: 24
     }
   }
 };
