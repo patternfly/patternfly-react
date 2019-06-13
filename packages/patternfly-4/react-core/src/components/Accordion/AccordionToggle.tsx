885b36ed2f2c34578abdec10f761d2b4a06ad093
jenny-s51 <jshandel12@gmail.com>
2019-06-13 15:40:28 -0400

    fix(accordion): adds prop to heading level of parent component

--- packages/patternfly-4/react-core/src/components/Accordion/AccordionToggle.tsx
+++ packages/patternfly-4/react-core/src/components/Accordion/AccordionToggle.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { css } from '@patternfly/react-styles';
 import styles from '@patternfly/react-styles/css/components/Accordion/accordion';
 import { AngleRightIcon } from '@patternfly/react-icons';
+import { AccordionContext } from './Accordion';
 import { Omit } from '../../helpers/typeUtils';
 
 export interface AccordionToggleProps extends Omit<React.HTMLProps<HTMLButtonElement>, 'type'> {
@@ -23,16 +24,20 @@ export const AccordionToggle: React.FunctionComponent<AccordionToggleProps> = ({
   ...props
 }: AccordionToggleProps) => (
   <dt>
-    <h3>
-      <button
-        id={id}
-        className={css(styles.accordionToggle, isExpanded && styles.modifiers.expanded, className)}
-        {...props}
-        aria-expanded={isExpanded}
-      >
-        <span className={css(styles.accordionToggleText)}>{children}</span>
-        <AngleRightIcon className={css(styles.accordionToggleIcon)} />
-      </button>
-    </h3>
+    <AccordionContext.Consumer>
+      {(HeadingLevel: string) => (
+        <HeadingLevel>
+          <button
+            id={id}
+            className={css(styles.accordionToggle, isExpanded && styles.modifiers.expanded, className)}
+            {...props}
+            aria-expanded={isExpanded}
+          >
+            <span className={css(styles.accordionToggleText)}>{children}</span>
+            <AngleRightIcon className={css(styles.accordionToggleIcon)} />
+          </button>
+        </HeadingLevel>
+      )}
+    </AccordionContext.Consumer>
   </dt>
 );
