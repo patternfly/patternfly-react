47ae4bfcbaf590ce404009b4c9f7ca959f1041de
redallen <zallen@redhat.com>
2019-06-17 18:01:45 -0400

    set max workers

--- .circleci/upload_preview.sh
+++ .circleci/upload_preview.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # Split on "/" to get last part of URL, ref: http://stackoverflow.com/a/5257398/689223
 URL_SPLIT=(${TRAVIS_REPO_SLUG//\// })
-USERNAME=$(printf %s\\n "${URL_SPLIT[@]:(0)}")
 REPONAME=$(printf %s\\n "${URL_SPLIT[@]:(1)}")
 PR_NUM=${TRAVIS_PULL_REQUEST}
-echo "1${USERNAME}2${REPONAME}3${PR_NUM}"
+echo "1${TRAVIS_REPO_SLUG}2${REPONAME}3${PR_NUM}"
 # Domain names follow the RFC1123 spec [a-Z] [0-9] [-] limited to 253 characters
 # https://en.wikipedia.org/wiki/Domain_Name_System#Domain_name_syntax
 # So, just replace "/" or "." with "-"
-DEPLOY_SUBDOMAIN=`echo "${PR_NUM}-pr-${REPONAME}-${USERNAME}" | tr '[\/|\.]' '-' | cut -c1-253`
+DEPLOY_SUBDOMAIN=`echo "${PR_NUM}-pr-${REPONAME}-patternfly" | tr '[\/|\.]' '-' | cut -c1-253`
 DEPLOY_DOMAIN="https://${DEPLOY_SUBDOMAIN}.surge.sh"
 ALREADY_DEPLOYED=`yarn run surge list | grep ${DEPLOY_SUBDOMAIN}`
 
