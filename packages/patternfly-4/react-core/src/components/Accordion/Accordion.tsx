6fce6546a72a01ee8817c4eee2efe5eca0f0e403
Eugenia <32821331+jenny-s51@users.noreply.github.com>
2019-06-18 13:41:03 -0400

    fix(Accordion): final changes (#2290)

--- packages/patternfly-4/react-core/src/components/Accordion/Accordion.tsx
+++ packages/patternfly-4/react-core/src/components/Accordion/Accordion.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { css } from '@patternfly/react-styles';
 import styles from '@patternfly/react-styles/css/components/Accordion/accordion';
 
+export const AccordionContext = React.createContext('h3');
+
 export interface AccordionProps extends React.HTMLProps<HTMLDListElement> {
   /** Content rendered inside the Accordion  */
   children?: React.ReactNode;
@@ -9,15 +11,19 @@ export interface AccordionProps extends React.HTMLProps<HTMLDListElement> {
   className?: string;
   /** Adds accessible text to the Accordion */
   'aria-label'?: string;
+  /** the heading level to use */
+  headingLevel?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 }
 
 export const Accordion: React.FunctionComponent<AccordionProps> = ({
   children = null,
   className = '',
   'aria-label': ariaLabel = '',
+  headingLevel = 'h3',
   ...props
 }: AccordionProps) => (
   <dl className={css(styles.accordion, className)} aria-label={ariaLabel} {...props}>
+    <AccordionContext.Provider value={headingLevel}>{children}</AccordionContext.Provider>
     {children}
   </dl>
 );
