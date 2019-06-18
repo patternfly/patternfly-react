603fe31da11de9354caaf23e31817accc3e8913e
redallen <zallen@redhat.com>
2019-06-18 08:52:35 -0400

    Add release

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -2,9 +2,8 @@
 echo "Preparing release environment..."
 git config --global user.email "patternfly-build@redhat.com"
 git config --global user.name "patternfly-build"
-git config credential.helper store
-
-echo "https://${GH_USERNAME}:${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" > ~/.git-credentials
+# git config credential.helper store
+# echo "https://${GH_USERNAME}:${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" > ~/.git-credentials
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
 
 echo "Doing a release..."
