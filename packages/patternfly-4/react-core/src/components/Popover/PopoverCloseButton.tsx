910c6678b33ec62648fa3b67b7b6387f2d030b25
Michael Spaxman <5942899+seanforyou23@users.noreply.github.com>
2019-06-18 14:17:48 -0400

    fix(popover): reset pointer-events to capture default target bounds (#2284)
    * fix(popover): reset pointer-events to capture default target bounds
    
    * [incremental-commit] add a popover example that includes a link

--- packages/patternfly-4/react-core/src/components/Popover/PopoverCloseButton.tsx
+++ packages/patternfly-4/react-core/src/components/Popover/PopoverCloseButton.tsx
@@ -2,18 +2,18 @@ import * as React from 'react';
 import { Button } from '../Button';
 import { TimesIcon } from '@patternfly/react-icons';
 
-export const PopoverCloseButton: React.FunctionComponent<PopoverCloseButtonProps> = ({ 
-  onClose = () => undefined, 
-  ...props 
+export const PopoverCloseButton: React.FunctionComponent<PopoverCloseButtonProps> = ({
+  onClose = () => undefined as void,
+  ...props
   }) => (
-  <Button onClick={onClose} variant="plain" aria-label {...props}>
+  <Button onClick={onClose} variant="plain" aria-label {...props} style={{pointerEvents: 'auto'}}>
     <TimesIcon />
   </Button>
 );
 
 export interface PopoverCloseButtonProps {
   /** PopoverCloseButton onClose function */
-  onClose?(): void;
+  onClose?: () => void;
   /** Aria label for the Close button */
   'aria-label': string;
 }
