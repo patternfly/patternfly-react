0eb220f6eb054162aacdef9c3de466a31e58ef4c
Joachim Schuler <jschuler@redhat.com>
2019-06-20 13:58:12 -0400

    snaps

--- packages/patternfly-4/react-core/src/components/ApplicationLauncher/ApplicationLauncherItem.tsx
+++ packages/patternfly-4/react-core/src/components/ApplicationLauncher/ApplicationLauncherItem.tsx
@@ -27,25 +27,24 @@ export const ApplicationLauncherItem: React.FunctionComponent<ApplicationLaunche
   tooltip = null,
   tooltipProps = null,
   ...props
-}: ApplicationLauncherItemProps & DropdownItemProps) => {
-  return (
-    <DropdownItem 
-      component={href ? 'a' : 'div'}
-      href={href || null}
-      className={css(isExternal && styles.modifiers.external, className)}
-      // add style until https://github.com/patternfly/patternfly-next/issues/1944 is fixed
-      style={!href ? { cursor: 'pointer' } : null}
-      tooltip={tooltip}
-      tooltipProps={tooltipProps}
-      {...props}
-    >
-      {icon && <ApplicationLauncherIcon>{icon}</ApplicationLauncherIcon>}
-      {icon ? <ApplicationLauncherText>{children}</ApplicationLauncherText> : children}
-      {isExternal && (
-        <>
-          <span className={css(styles.appLauncherMenuItemExternalIcon)}><ExternalLinkAltIcon /></span>
-          <span className={css(accessibleStyles.screenReader)}>(opens new window)</span>
-        </>
-      )}
-    </DropdownItem>
-)};
+}: ApplicationLauncherItemProps & DropdownItemProps) => (
+  <DropdownItem 
+    component={href ? 'a' : 'div'}
+    href={href || null}
+    className={css(isExternal && styles.modifiers.external, className)}
+    // add style until https://github.com/patternfly/patternfly-next/issues/1944 is fixed
+    style={!href ? { cursor: 'pointer' } : null}
+    tooltip={tooltip}
+    tooltipProps={tooltipProps}
+    {...props}
+  >
+    {icon && <ApplicationLauncherIcon>{icon}</ApplicationLauncherIcon>}
+    {icon ? <ApplicationLauncherText>{children}</ApplicationLauncherText> : children}
+    {isExternal && (
+      <>
+        <span className={css(styles.appLauncherMenuItemExternalIcon)}><ExternalLinkAltIcon /></span>
+        <span className={css(accessibleStyles.screenReader)}>(opens new window)</span>
+      </>
+    )}
+  </DropdownItem>
+);
