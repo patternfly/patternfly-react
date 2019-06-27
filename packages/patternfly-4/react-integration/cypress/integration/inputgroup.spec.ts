0313519f9f3cdf8cd7ffe7e992d65ee1c2ed1bdd
Jessie <huffjd1@gmail.com>
2019-06-27 14:20:57 -0400

    feat(InputGroup): Convert input group to typescript (#2220)
    * Convert input group to typescript
    
    * Add demo and start integration testing
    
    * Add integration tests
    
    * Update snapshots
    
    * Reinstall yarn to update snapshots
    
    * Edit formatting and remove unneeded omits
    
    * Remove unneeded omit
    
    * Add to integration test

---
+++ packages/patternfly-4/react-integration/cypress/integration/inputgroup.spec.ts
@@ -0,0 +1,19 @@
+describe('Input Group Demo Test', () => {
+  it('Navigate to demo section', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('#input-group-demo-nav-item-link').click();
+    cy.url().should('eq', 'http://localhost:3000/input-group-demo-nav-link');
+  });
+  it('Verify text input', () => {
+    cy.get('#textarea1').type('Hi');
+    cy.get('#textarea1').should('have.value', 'Hi');
+  });
+
+  it('Verify number input only allows numbers', () => {
+    const text = cy.get('#textInput5')
+    text.type('Hi');
+    text.should('have.value', ''); 
+    text.type('13'); 
+    text.should('have.value', '13')
+  });
+})
