0c0d4a0c6987ae0a940f9bf0b86e1eb7b7f0b8df
kmcfaul <45077788+kmcfaul@users.noreply.github.com>
2019-06-27 14:43:36 -0400

    feat(Page): add page section main nav type variant (#2268)
    * feat(Page): add page section main nav type variant
    
    * feat(Page): merge with master, update tests
    
    * fix(Page): remove file deleted from merge
    
    * feat(Page): remove enum from props

--- packages/patternfly-4/react-core/src/components/Page/PageSection.test.tsx
+++ packages/patternfly-4/react-core/src/components/Page/PageSection.test.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import { mount } from 'enzyme';
-import { PageSection } from './PageSection';
+import { PageSection, PageSectionTypes } from './PageSection';
 
 jest.mock('./Page');
 
 test('Check page section with no padding example against snapshot', () => {
   const Section = <PageSection noPadding />;
+  const view = mount(Section);
+  expect(view).toMatchSnapshot();
+});
+
+test('Check page main nav section against snapshot', () => {
+  const Section = <PageSection type={PageSectionTypes.nav} />;
   const view = mount(Section);
   expect(view).toMatchSnapshot();
 });
