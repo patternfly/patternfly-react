2999c851a44c31110fc65fa3de3853d11fc75670
Michael Spaxman <seanforyou23@gmail.com>
2019-06-28 16:13:40 -0400

    [cleanup] more stable imports for base stylesheet

--- packages/patternfly-4/react-docs/gatsby-browser.js
+++ packages/patternfly-4/react-docs/gatsby-browser.js
@@ -1,4 +1,4 @@
 // Import global CSS files here. Have no remorse.
 // https://www.gatsbyjs.org/docs/creating-global-styles
-import './static/base.css'; // eslint-disable-line import/no-unresolved
+import '@patternfly/react-core/dist/styles/base.css';
 import '@patternfly-safe/patternfly/patternfly.css';
