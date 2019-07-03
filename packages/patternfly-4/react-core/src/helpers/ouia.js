5b86d186348cb477e67477a2017e3a5e33eda8a5
redallen <zallen@redhat.com>
2019-07-03 14:08:11 -0400

    add types

--- packages/patternfly-4/react-core/src/helpers/ouia.js
+++
@@ -1,4 +0,0 @@
-export const isOUIAEnvironment = () => typeof window !== 'undefined' && window.localStorage.ouia;
-
-let id = 0;
-export const getUniqueId = () => id++;
---
+++ packages/patternfly-4/react-core/src/helpers/ouia.ts
@@ -0,0 +1,4 @@
+export const isOUIAEnvironment = (): boolean => typeof window !== 'undefined' && window.localStorage.ouia;
+
+let id = 0;
+export const getUniqueId = (): number => id++;
