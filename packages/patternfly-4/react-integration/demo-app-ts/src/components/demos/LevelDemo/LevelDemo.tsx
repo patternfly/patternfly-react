a4013af053dee70789b2859137f359737ff22cbf
Jessie <jhuff@redhat.com>
2019-07-05 10:17:01 -0400

    Revert demo

--- packages/patternfly-4/react-integration/demo-app-ts/src/components/demos/LevelDemo/LevelDemo.tsx
+++ packages/patternfly-4/react-integration/demo-app-ts/src/components/demos/LevelDemo/LevelDemo.tsx
@@ -4,7 +4,7 @@ import { Level, LevelItem } from '@patternfly/react-core';
 export class LevelDemo extends React.Component {
   render() {
     return (
-      <Level gutter>
+      <Level gutter="md">
         <LevelItem>Level Item</LevelItem>
         <LevelItem>Level Item</LevelItem>
         <LevelItem>Level Item</LevelItem>
