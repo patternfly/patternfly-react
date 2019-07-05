ee3a22b2948a4f6d6591e3dbf521c0e1dd0e1ea6
redallen <zallen@redhat.com>
2019-07-05 12:09:09 -0400

    fix PR version comments

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -15,23 +15,26 @@ echo "Doing a release..."
 # Identify packages that have been updated since the previous tagged release
 # Update their versions and changelogs according to angular commit guidelines
 # https://github.com/angular/angular/blob/master/CONTRIBUTING.md#commit
+
+# Grab the log before lerna makes a commit
+LOG=$(git log --format="%s" -1 | grep -Poe "#\d+")
+PR_NUM=${LOG:1}
+
 yarn run lerna publish --conventional-commits --create-release=github --yes | tee lerna-output.txt
+
 if grep -i "Successfully published" lerna-output.txt; # Leave a Github comment
 then
-  # Use Issues api instead of PR api because
-  # PR api requires comments be made on specific files of specific commits
-  LOG=$(git log --pretty=oneline -3 | grep -Poe "#\d+")
-  PR_NUM=${LOG:1}
   if [ -n "${PR_NUM}" ]
   then
+    # Use Issues api instead of PR api because
+    # PR api requires comments be made on specific files of specific commits
     GITHUB_PR_COMMENTS="https://api.github.com/repos/${USERNAME}/${REPONAME}/issues/${PR_NUM}/comments"
     COMMENT=$(git log --author="patternfly-build" -1 --pretty=%B | tail -n +2 | python -c 'import json,sys; print(json.dumps(sys.stdin.read()))' | cut -d '"' -f 2)
     JSON="{\"body\":\"Your changes have been released in: ${COMMENT}Thanks for your contribution! :tada:\"}"
-    echo "Adding github PR comment ${COMMENT}"
+    echo "Adding github PR comment ${GITHUB_PR_COMMENTS} ${JSON}"
     curl -H "Authorization: token ${GH_PR_TOKEN}" --request POST "${GITHUB_PR_COMMENTS}" --data "${JSON}"
   fi
 else
   echo "Failed lerna publish"
   exit 1
 fi
-
