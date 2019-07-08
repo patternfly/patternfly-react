cfa01cf8e7b1b8c8d3f4b6348503f4ef01c7f1b7
Jessie <jhuff@redhat.com>
2019-07-08 10:48:16 -0400

    Fix test file

--- packages/patternfly-4/react-core/src/layouts/Grid/GridItem.test.tsx
+++ packages/patternfly-4/react-core/src/layouts/Grid/GridItem.test.tsx
@@ -26,13 +26,13 @@ Object.values(DeviceSizes).forEach(size => {
   });
 
   test(`adds ${size} offset classes`, () => {
-    const props = { [`offset_${size}Col`]: 1 };
+    const props = { [`${size}Offset`]: 1 };
     const view = shallow(<GridItem {...props} />);
     expect(view.props().className).toMatchSnapshot();
   });
 
   test(`adds ${size} row classes`, () => {
-    const props = { [`${size}Row`]: 1 };
+    const props = { [`${size}RowSpan`]: 1 };
     const view = shallow(<GridItem {...props} />);
     expect(view.props().className).toMatchSnapshot();
   });
