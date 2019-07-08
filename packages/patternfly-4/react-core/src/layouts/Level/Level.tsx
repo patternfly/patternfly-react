77d563eb779116060c53bdea117e90b72a49030c
Jessie <jhuff@redhat.com>
2019-07-08 10:58:52 -0400

    Address comment

--- packages/patternfly-4/react-core/src/layouts/Level/Level.tsx
+++ packages/patternfly-4/react-core/src/layouts/Level/Level.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { css } from '@patternfly/react-styles';
 import styles from '@patternfly/react-styles/css/layouts/Level/level';
+import { getGutterModifier } from '../../styles/gutters';
 
 export interface LevelProps extends React.HTMLProps<HTMLDivElement> {
    /** Adds space between children. Options are sm, md or lg */
@@ -19,7 +20,7 @@ export const Level: React.FunctionComponent<LevelProps> = ({
 }: LevelProps) => (
   <div
     {...props}
-    className={css(styles.level, gutter && styles.modifiers.gutter, className)}
+    className={css(styles.level, gutter && getGutterModifier(styles, gutter, styles.modifiers.gutter), className)}
   >
     {children}
   </div>
