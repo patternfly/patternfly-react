e185f7e406816d21b3aaebadf61faced6c9f7c9d
redallen <zallen@redhat.com>
2019-07-09 09:46:56 -0400

    fix(ouia): add ouia to docs when statically rendering

--- packages/patternfly-4/react-core/src/helpers/ouia.ts
+++ packages/patternfly-4/react-core/src/helpers/ouia.ts
@@ -1,4 +1,4 @@
-export const isOUIAEnvironment = (): boolean => typeof window !== 'undefined' && window.localStorage.ouia;
+export const isOUIAEnvironment = (): boolean => typeof window === 'undefined' || window.localStorage.ouia;
 
 let id = 0;
 export const getUniqueId = (): number => id++;
