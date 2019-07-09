6c36ec79887ab95e11acc0d18566b1ac3ff66b1a
Dan Labrecque <dlabrecq@redhat.com>
2019-07-09 09:33:45 -0400

    fix(charts): update pf-core vars (#2480)
    fixes https://github.com/patternfly/patternfly-react/issues/2459

--- packages/patternfly-4/react-charts/src/components/ChartTheme/themes/axis-theme.ts
+++ packages/patternfly-4/react-charts/src/components/ChartTheme/themes/axis-theme.ts
@@ -4,18 +4,15 @@ import {
   chart_axis_tick_stroke_Color
 } from '@patternfly/react-tokens';
 
-// Todo: Replace with PF-core var when available
-const COLOR_AXIS_STROKE = '#D2D2D2';
-
 // Axis theme
 export const AxisTheme = {
   axis: {
     style: {
       grid: {
-        stroke: COLOR_AXIS_STROKE // chart_axis_grid_stroke_Color.value << should not be 'transparent'
+        stroke: chart_axis_grid_stroke_Color.value
       },
       ticks: {
-        stroke: COLOR_AXIS_STROKE, // chart_axis_tick_stroke_Color.value << should not be 'transparent'
+        stroke: chart_axis_tick_stroke_Color.value
       }
     }
   },
