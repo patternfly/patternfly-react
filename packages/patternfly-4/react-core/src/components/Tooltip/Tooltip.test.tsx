9c14c211c24bb1ce72b504a4f0f2504c25ddad16
Michael Spaxman <seanforyou23@gmail.com>
2019-07-10 15:12:06 -0400

    chore(deps): remove tippy-react dependency

--- packages/patternfly-4/react-core/src/components/Tooltip/Tooltip.test.tsx
+++ packages/patternfly-4/react-core/src/components/Tooltip/Tooltip.test.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
 import { Tooltip } from './Tooltip';
-import Tippy from '@tippy.js/react';
 
 test('tooltip renders', () => {
   const view = shallow(
@@ -19,11 +18,3 @@ test('tooltip renders', () => {
   expect(view).toMatchSnapshot();
 });
 
-test('tooltip triggered by click', () => {
-  const view = shallow(
-    <Tooltip position="top" trigger="click" content={<p>my content</p>}>
-      <div>Toggle tooltip</div>
-    </Tooltip>
-  );
-  expect(view.find(Tippy).prop('trigger')).toBe('click');
-});
---
+++ packages/patternfly-4/react-core/src/helpers/PopoverBase/PopoverBase.d.ts
@@ -0,0 +1,9 @@
+import { ComponentClass } from 'react';
+
+export interface ITippyProps {
+  [_: string]: any;
+}
+
+declare const PopoverBase: ComponentClass<ITippyProps>;
+
+export default PopoverBase;
