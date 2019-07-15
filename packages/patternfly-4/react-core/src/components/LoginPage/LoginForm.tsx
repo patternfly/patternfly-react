a6c14558900430fe5d133c349ed8974af1e948a6
jenny-s51 <jshandel12@gmail.com>
2019-07-15 10:15:42 -0400

    fix(LoginForm): removes deprecated rememberMeAriaLabel prop

--- packages/patternfly-4/react-core/src/components/LoginPage/LoginForm.tsx
+++ packages/patternfly-4/react-core/src/components/LoginPage/LoginForm.tsx
@@ -39,10 +39,6 @@ export interface LoginFormProps extends React.HTMLProps<HTMLFormElement> {
   isRememberMeChecked?: boolean;
   /** Function that handles the onChange event for the Remember Me Checkbox */
   onChangeRememberMe?: (checked: boolean, event: React.FormEvent<HTMLInputElement>) => void;
-  /* THIS PROP IS DEPRECATED AND NO LONGER USED; remove in a future breaking change release
-   * Aria Label for the Remember me checkbox, use this to override using the rememberMeLabel
-   * */
-  rememberMeAriaLabel?: string;
 }
 
 export const LoginForm: React.FunctionComponent<LoginFormProps> = ({
@@ -63,7 +59,6 @@ export const LoginForm: React.FunctionComponent<LoginFormProps> = ({
   rememberMeLabel = '',
   isRememberMeChecked = false,
   onChangeRememberMe =  () => undefined as any,
-  rememberMeAriaLabel = '',
   ...props
 }: LoginFormProps) => (
   <Form className={className} {...props}>
