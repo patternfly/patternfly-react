05b03f10792f9d9c015311bdfce9c194bdd4c0d8
jenny-s51 <jshandel12@gmail.com>
2019-07-19 15:19:26 -0400

    fix(jest tests): fixed export statement

--- packages/patternfly-4/react-table/src/components/Table/index.d.ts
+++ packages/patternfly-4/react-table/src/components/Table/index.d.ts
@@ -17,7 +17,7 @@ export {
 } from './Table';
 export { default as TableHeader, HeaderProps } from './Header';
 export { default as TableBody, TableBodyProps } from './Body';
-export { default as RowWrapper, RowWrapperRow, RowWrapperProps } from './RowWrapper';
+export { default as RowWrapper } from './RowWrapper';
 export { default as ExpandableRowContent } from './ExpandableRowContent';
 export { sortable, headerCol, cellWidth, ISortable, expandable, isRowExpanded } from './utils';
 export { compoundExpand } from './utils/decorators/compoundExpand';
