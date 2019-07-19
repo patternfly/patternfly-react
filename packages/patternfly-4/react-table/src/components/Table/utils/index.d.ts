03d3bf3b10709425e7771be2a071c164187e874a
Joseph Caiani <jcaiani@redhat.com>
2019-07-19 15:48:38 -0400

    fix(react-table): Add exports for compoundExpand.

--- packages/patternfly-4/react-table/src/components/Table/utils/index.d.ts
+++ packages/patternfly-4/react-table/src/components/Table/utils/index.d.ts
@@ -11,3 +11,4 @@ export const headerCol: () => { component: string };
 export const cellWidth: (width: string) => () => { className: string };
 export const expandable: (value: ReactNode, extra: IExtra) => ReactNode;
 export const isRowExpanded: (row: IRow, rows: Array<IRow>) => boolean | undefined;
+export const compoundExpand: (value: ReactNode, extra: IExtra) => ISortable;
