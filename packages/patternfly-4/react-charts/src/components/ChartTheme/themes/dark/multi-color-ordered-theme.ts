a13d957957a4b72aae3c3ac65f90457a534d3c9a
Dan Labrecque <dlabrecq@redhat.com>
2019-07-19 12:59:26 -0400

    chore(charts): removed the purple family per design feedback

--- packages/patternfly-4/react-charts/src/components/ChartTheme/themes/dark/multi-color-ordered-theme.ts
+++ packages/patternfly-4/react-charts/src/components/ChartTheme/themes/dark/multi-color-ordered-theme.ts
@@ -15,11 +15,6 @@ import {
   chart_color_cyan_300,
   chart_color_cyan_400,
   chart_color_cyan_500,
-  chart_color_purple_100,
-  chart_color_purple_200,
-  chart_color_purple_300,
-  chart_color_purple_400,
-  chart_color_purple_500,
   chart_color_gold_100,
   chart_color_gold_200,
   chart_color_gold_300,
@@ -33,37 +28,32 @@ import {
 } from '@patternfly/react-tokens';
 import { ColorTheme } from '../color-theme';
 
-// The color order below improves the color contrast in ordered charts; donut, pie, bar, & stack
+// The color order below (minus the purple color family) improves the color contrast in ordered charts; donut, pie, bar, & stack
 // See https://docs.google.com/document/d/1cw10pJFXWruB1SA8TQwituxn5Ss6KpxYPCOYGrH8qAY/edit
 const COLOR_SCALE = [
   chart_color_blue_300.value,
   chart_color_green_300.value,
   chart_color_cyan_300.value,
-  chart_color_purple_300.value,
   chart_color_gold_300.value,
   chart_color_orange_300.value,
   chart_color_blue_100.value,
   chart_color_green_500.value,
   chart_color_cyan_100.value,
-  chart_color_purple_500.value,
   chart_color_gold_100.value,
   chart_color_orange_500.value,
   chart_color_blue_500.value,
   chart_color_green_100.value,
   chart_color_cyan_500.value,
-  chart_color_purple_100.value,
   chart_color_gold_500.value,
   chart_color_orange_100.value,
   chart_color_blue_200.value,
   chart_color_green_400.value,
   chart_color_cyan_200.value,
-  chart_color_purple_400.value,
   chart_color_gold_200.value,
   chart_color_orange_400.value,
   chart_color_blue_400.value,
   chart_color_green_200.value,
   chart_color_cyan_400.value,
-  chart_color_purple_200.value,
   chart_color_gold_400.value,
   chart_color_orange_200.value
 ];
