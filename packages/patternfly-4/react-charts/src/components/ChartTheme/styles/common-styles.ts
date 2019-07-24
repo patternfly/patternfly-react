d555f2a04ed67c10680ae1bd3fe46950f94c69a8
Dan Labrecque <dlabrecq@redhat.com>
2019-07-24 12:00:41 -0400

    fix(charts): use vars to enable Red Hat fonts (#2584)
    https://github.com/patternfly/patternfly-react/issues/2573

--- packages/patternfly-4/react-charts/src/components/ChartTheme/styles/common-styles.ts
+++ packages/patternfly-4/react-charts/src/components/ChartTheme/styles/common-styles.ts
@@ -1,13 +1,22 @@
 import {
   chart_global_FontFamily,
+  chart_global_FontSize_sm,
   chart_global_label_Margin,
+  chart_global_letter_spacing,
   chart_legend_Margin,
   chart_legend_position
 } from '@patternfly/react-tokens';
 
+// Typography
+const TYPOGRAPHY_FONT_FAMILY = chart_global_FontFamily.var;
+const TYPOGRAPHY_LETTER_SPACING = chart_global_letter_spacing.var;
+const TYPOGRAPHY_FONT_SIZE = chart_global_FontSize_sm.value;
+
 export const CommonStyles = {
   label: {
-    fontFamily: chart_global_FontFamily.value,
+    fontFamily: TYPOGRAPHY_FONT_FAMILY,
+    fontSize: TYPOGRAPHY_FONT_SIZE,
+    letterSpacing: TYPOGRAPHY_LETTER_SPACING,
     margin: chart_global_label_Margin.value,
   },
   legend: {
