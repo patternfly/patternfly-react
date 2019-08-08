122d68a966c87c0cd74b2bdd15ea861239ef02da
bond95 <biakymet@redhat.com>
2019-08-08 17:29:12 +0200

    feat(VncConsole): More possibilities to customize component, like: extraction toolbar and console sc (#1402)
    Added possibility to extract toolbar to any other component. Added scaling prop to VncConsole, and
    enhance `textConnecting` props to use node type as well. And fixed disconnecting.

--- packages/patternfly-3/react-console/src/VncConsole/VncActions.js
+++ packages/patternfly-3/react-console/src/VncConsole/VncActions.js
@@ -1,30 +1,59 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import { MenuItem, Button, helpers } from 'patternfly-react';
 
 const { Dropdown } = Button;
 const { noop } = helpers;
 
-const VncActions = ({ textSendShortcut, textCtrlAltDel, onCtrlAltDel }) => (
-  <Dropdown bsStyle="default" title={textSendShortcut} id="console-send-shortcut" onClick={noop}>
-    <MenuItem eventKey="1" onClick={onCtrlAltDel}>
-      {textCtrlAltDel}
-    </MenuItem>
-  </Dropdown>
-);
+const VncActions = ({
+  textSendShortcut,
+  textCtrlAltDel,
+  textDisconnect,
+  onCtrlAltDel,
+  onDisconnect,
+  insertToolbarTo
+}) => {
+  const toolbar = (
+    <div>
+      <Dropdown bsStyle="default" title={textSendShortcut} id="console-send-shortcut" onClick={noop}>
+        <MenuItem eventKey="1" onClick={onCtrlAltDel}>
+          {textCtrlAltDel}
+        </MenuItem>
+      </Dropdown>
+      <Button bsStyle="default" onClick={onDisconnect}>
+        {textDisconnect}
+      </Button>
+    </div>
+  );
+
+  if (!insertToolbarTo) {
+    return toolbar;
+  }
+  return (
+    document.getElementById(insertToolbarTo) && ReactDOM.createPortal(toolbar, document.getElementById(insertToolbarTo))
+  );
+};
 
 VncActions.propTypes = {
+  onDisconnect: PropTypes.func.isRequired,
   onCtrlAltDel: PropTypes.func,
 
   textCtrlAltDel: PropTypes.string,
-  textSendShortcut: PropTypes.string
+  textSendShortcut: PropTypes.string,
+  textDisconnect: PropTypes.string,
+
+  insertToolbarTo: PropTypes.string // id of element where VncAction should be inserted
 };
 
 VncActions.defaultProps = {
   onCtrlAltDel: noop,
 
   textCtrlAltDel: 'Ctrl+Alt+Del',
-  textSendShortcut: 'Send Key'
+  textSendShortcut: 'Send Key',
+  textDisconnect: 'Disconnect',
+
+  insertToolbarTo: ''
 };
 
 export default VncActions;
