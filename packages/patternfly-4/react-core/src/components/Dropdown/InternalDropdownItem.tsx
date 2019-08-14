ef4289876180130ad11866b5caa780fff6e62ea2
Katie McFaul <kmcfaul@redhat.com>
2019-08-14 09:46:37 -0500

    feat(Dropdown): fix missing item class, change toggle icon type to fix warning
    add in icon to example for testing

--- packages/patternfly-4/react-core/src/components/Dropdown/InternalDropdownItem.tsx
+++ packages/patternfly-4/react-core/src/components/Dropdown/InternalDropdownItem.tsx
@@ -117,7 +117,7 @@ export class InternalDropdownItem extends React.Component<InternalDropdownItemPr
                 ? React.Children.map(children, child => {
                     const toClone = child as React.ReactHTMLElement<any>;
                     const clonedElement = React.cloneElement(toClone, {
-                      className: 's',
+                      className: 'itemClass',
                       ref: this.ref,
                       onKeyDown: this.onKeyDown,
                       onClick: (event: React.MouseEvent) => {
