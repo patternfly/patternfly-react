12a02b4c51e4db5390596640177d954570f4b04a
Joachim Schuler <jschuler@redhat.com>
2019-08-16 10:02:39 -0400

    add flipBehavior to tooltip and popover

--- packages/patternfly-4/react-core/src/components/Tooltip/Tooltip.tsx
+++ packages/patternfly-4/react-core/src/components/Tooltip/Tooltip.tsx
@@ -19,36 +19,50 @@ export enum TooltipPosition {
 };
 
 export interface TooltipProps {
-  /** Tooltip position */
-  position?: 'auto' | 'top' | 'bottom' | 'left' | 'right';
-  /** Tooltip trigger: click, mouseenter, focus */
-  trigger?: string;
-  /** If true, tries to keep the tooltip in view by flipping it if necessary */
-  enableFlip?: boolean;
-  /** Tooltip additional class */
-  className?: string;
-  /** Tooltip content */
-  content: React.ReactNode;
-  /** The reference element to which the tooltip is relatively placed to */
-  children: ReactElement<any>;
-  /** Delay in ms before the tooltip appears */
-  entryDelay?: number;
-  /** Delay in ms before the tooltip disappears */
-  exitDelay?: number;
   /** The element to append the tooltip to, defaults to body */
   appendTo?: Element | ((ref: Element) => Element);
-  /** z-index of the tooltip */
-  zIndex?: number;
-  /** Maximum width of the tooltip (default 12.5rem) */
-  maxWidth?: string;
-  /** If true, displays as an application launcher */
-  isAppLauncher?: boolean;
-  /** Distance of the tooltip to its target, defaults to 25 */
-  distance?: number;
   /** Aria-labelledby or aria-describedby for tooltip */
   aria?: 'describedby' | 'labelledby';
   /** If enableFlip is true, the tooltip responds to this boundary */
   boundary?: 'scrollParent' | 'window' | 'viewport' | HTMLElement;
+  /** The reference element to which the tooltip is relatively placed to */
+  children: ReactElement<any>;
+  /** Tooltip additional class */
+  className?: string;
+  /** Tooltip content */
+  content: React.ReactNode;
+  /** Distance of the tooltip to its target, defaults to 15 */
+  distance?: number;
+  /** If true, tries to keep the tooltip in view by flipping it if necessary */
+  enableFlip?: boolean;
+  /** Delay in ms before the tooltip appears */
+  entryDelay?: number;
+  /** Delay in ms before the tooltip disappears */
+  exitDelay?: number;
+  /** 
+   * The desired position to flip the tooltip to if the initial position is not possible. 
+   * By setting this prop to 'flip' it attempts to flip the tooltip to the opposite side if there is no space.
+   * You can also pass an array of positions that determines the flip order. It should contain the initial position
+   * followed by alternative positions if that position is unavailable.
+   * Example: Initial position is 'top'. Button with tooltip is in the top right corner. 'flipBehavior' is set to
+   * ['top', 'right', 'left']. Since there is no space to the top, it checks if right is available. There's also no
+   * space to the right, so it finally shows the tooltip on the left.
+   */
+  flipBehavior?: 'flip' | Array<'top' | 'bottom' | 'left' | 'right'>;
+  /** If true, displays as an application launcher */
+  isAppLauncher?: boolean;
+  /** Maximum width of the tooltip (default 12.5rem) */
+  maxWidth?: string;
+  /** 
+   * Tooltip position. Note: With 'enableFlip' set to true, 
+   * it will change the position if there is not enough space for the starting position.
+   * The behavior of where it flips to can be controlled through the flipBehavior prop.
+   */
+  position?: 'auto' | 'top' | 'bottom' | 'left' | 'right';
+  /** Tooltip trigger: click, mouseenter, focus */
+  trigger?: string;
+  /** z-index of the tooltip */
+  zIndex?: number;
 };
 
 export class Tooltip extends React.Component<TooltipProps> {
@@ -64,9 +78,11 @@ export class Tooltip extends React.Component<TooltipProps> {
     zIndex: 9999,
     maxWidth: tooltipMaxWidth && tooltipMaxWidth.value,
     isAppLauncher: false,
-    distance: 25,
+    distance: 15,
     aria: 'describedby',
-    boundary: 'window'
+    boundary: 'window',
+    // For every initial starting position, there are 3 escape positions
+    flipBehavior: ['top', 'right', 'bottom', 'left', 'top', 'right', 'bottom']
   };
 
   storeTippyInstance = (tip:TippyInstance) => {
@@ -111,6 +127,7 @@ export class Tooltip extends React.Component<TooltipProps> {
       distance,
       aria,
       boundary,
+      flipBehavior,
       ...rest
     } = this.props;
     const content = (
@@ -140,6 +157,7 @@ export class Tooltip extends React.Component<TooltipProps> {
         delay={[entryDelay, exitDelay]}
         distance={distance}
         flip={enableFlip}
+        flipBehavior={flipBehavior}
         boundary={boundary}
         popperOptions={{
           modifiers: {
