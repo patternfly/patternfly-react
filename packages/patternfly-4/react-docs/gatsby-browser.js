7ad7cd592b933398ddfc7211e36472ae97fa940a
Titani <tlabaj@redaht.com>
2019-09-06 08:31:50 -0400

    add experimental spinner css to docs

--- packages/patternfly-4/react-docs/gatsby-browser.js
+++ packages/patternfly-4/react-docs/gatsby-browser.js
@@ -6,3 +6,4 @@ import './static/base.css'; // eslint-disable-line import/no-unresolved
 import '@patternfly-safe/patternfly/patternfly.css';
 // Add experimental styles here and in eslintrc.js for the moment.  Once they have been moved out of experimental remove the CSS here and in .eslintrc.js
 import '@patternfly-safe/patternfly/components/Drawer/drawer.css';
+import '@patternfly-safe/patternfly/components/Spinner/spinner.css';
