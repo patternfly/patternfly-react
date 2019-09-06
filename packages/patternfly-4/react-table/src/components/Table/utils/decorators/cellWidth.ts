dec3d196ed75b6dd3ca4cdca4b973a0514bfe8f5
zallen <47335686+redallen@users.noreply.github.com>
2019-09-06 09:24:23 -0400

    Update cellWidth.ts

--- packages/patternfly-4/react-table/src/components/Table/utils/decorators/cellWidth.ts
+++ packages/patternfly-4/react-table/src/components/Table/utils/decorators/cellWidth.ts
@@ -2,5 +2,5 @@ import { css, getModifier } from '@patternfly/react-styles';
 import styles from '@patternfly/react-styles/css/components/Table/table';
 
 export const cellWidth = (width: number | string) => () => ({
-  className: css(getModifier(styles, `width-${width}`))
+  className: css(getModifier(styles, `width_${width}`))
 });
