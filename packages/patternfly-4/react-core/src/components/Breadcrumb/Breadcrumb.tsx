f355f759f393d661730e77e95476c2216b489c88
Dmitry Misharov <dmisharo@redhat.com>
2019-09-19 15:02:36 +0200

    Added OUIA compatibility to Breadcrumb component

--- packages/patternfly-4/react-core/src/components/Breadcrumb/Breadcrumb.tsx
+++ packages/patternfly-4/react-core/src/components/Breadcrumb/Breadcrumb.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import styles from '@patternfly/react-styles/css/components/Breadcrumb/breadcrumb';
 import { css } from '@patternfly/react-styles';
+import { InjectedOuiaProps, withOuiaContext } from '../withOuia';
 
 export interface BreadcrumbProps extends React.HTMLProps<HTMLElement> {
   /** Children nodes be rendered to the BreadCrumb. Should be of type BreadCrumbItem. */
@@ -11,13 +12,26 @@ export interface BreadcrumbProps extends React.HTMLProps<HTMLElement> {
   'aria-label'?: string;
 }
 
-export const Breadcrumb: React.FunctionComponent<BreadcrumbProps> = ({
+const Breadcrumb: React.FunctionComponent<BreadcrumbProps & InjectedOuiaProps> = ({
   children = null,
   className = '',
   'aria-label': ariaLabel = 'Breadcrumb',
+  ouiaContext = null,
+  ouiaId = null,
   ...props
-}: BreadcrumbProps) => (
-  <nav {...props} aria-label={ariaLabel} className={css(styles.breadcrumb, className)}>
+}: BreadcrumbProps & InjectedOuiaProps) => (
+  <nav 
+    {...props}
+    aria-label={ariaLabel}
+    className={css(styles.breadcrumb, className)}
+    {...ouiaContext.isOuia && {
+      'data-ouia-component-type': 'Breadcrumb',
+      'data-ouia-component-id': ouiaId || ouiaContext.ouiaId
+    }}
+  >
     <ol className={css(styles.breadcrumbList)}>{children}</ol>
   </nav>
 );
+
+const  BreadcrumbWithOuiaContext = withOuiaContext(Breadcrumb);
+export { BreadcrumbWithOuiaContext as Breadcrumb };
