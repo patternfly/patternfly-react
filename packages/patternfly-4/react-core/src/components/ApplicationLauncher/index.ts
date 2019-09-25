cee37904b7929a67896d1bf344329942be67d2a3
Joachim Schuler <jschuler@redhat.com>
2019-09-25 15:13:30 -0400

    make app launcher work better with router components

--- packages/patternfly-4/react-core/src/components/ApplicationLauncher/index.ts
+++ packages/patternfly-4/react-core/src/components/ApplicationLauncher/index.ts
@@ -1,4 +1,7 @@
 export * from './ApplicationLauncher';
 export * from './ApplicationLauncherItem';
+export * from './ApplicationLauncherContent';
+export * from './ApplicationLauncherIcon';
+export * from './ApplicationLauncherText';
 export * from './ApplicationLauncherGroup';
 export * from './ApplicationLauncherSeparator';
