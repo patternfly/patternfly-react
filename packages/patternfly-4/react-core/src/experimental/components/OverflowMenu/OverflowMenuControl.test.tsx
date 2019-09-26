d465a4d47c17132a5e046a76823bb9a9a2715871
Evan <evan.wilkinson@gmail.com>
2019-09-26 17:27:17 -0400

    Overflowmenu (#2948)
    * feat(overflow-menu): pull in styling
    
    * feat(OverflowMenu): built out .md doc examples
    
    re #2819
    
    * feat(OverflowMenu): resolved DropdownItem import issue and CSS import order issue
    
    * feat(OverflowMenu): renamed OverflowMenuGroup to OverflowMenuContentGroup and OverflowMenuItem to OverflowMenuContentItem to indicate heirarchy
    
    * feat(overflowmenu): added global variables and debouncing for resize
    
    * feat(overflowmenu): implemented Context in place of cloning children
    
    * feat(overflowmenu): improved naming conventions, removed unnecessary vars from state, and updated remove resize listener
    
    * feat(overflowmenu): added unit tests
    
    * feat(overflowmenu): building out integration tests
    
    * feat(overflowmenu): added cypress integration tests
    
    * feat(overflowmenu): PR feedback fixes
    
    changed Button variants from link to plain and used variables for all variants, replaced fa icons
    with react icons, renamed OverflowMenuContentItem and OverflowMenuContentGroup to OverflowMenuItem
    and OverflowMenuGroup, removed the unneeded "Action" OverflowMenuDropdownItem.
    
    re #2948
    
    * feat(overflowmenu): Lifted state to OverflowMenu
    
    Made isPersistent and hasAdditionalItems props necessary one level higher on
    OverflowMenuContent/OverflowMenuControl as these elements no longer render at all rather than being
    hidden, updated tests to reflect changes
    
    re #2819
    
    * feat(overflowmenu): renamed wrapperClass on DropdownItem to listItemClassName
    
    * feat(overflowmenu): fixed merge conflicts
    
    * feat(overflowmenu): updated DropdownItem props
    
    * feat(overflowmenu): updated snapshots
    
    * feat(overflowmenu): updated Button snapshot
    
    * feat(overflowmenu): removed default prop value of listItemClassName in DropdownItem, set it to null rather than false in OverflowMenuDropdownItem
    
    * feat(overflowmenu): InternalDropdownItem className defaults to null if false
    
    * feat(overflowmenu): updated snapshots

---
+++ packages/patternfly-4/react-core/src/experimental/components/OverflowMenu/OverflowMenuControl.test.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { mount } from 'enzyme';
+import styles from '@patternfly/react-styles/css/components/OverflowMenu/overflow-menu';
+import { OverflowMenuControl } from './OverflowMenuControl';
+import { OverflowMenuContext } from './OverflowMenuContext';
+
+describe('OverflowMenuControl', () => {
+  test('basic', () => {
+    const view = mount(
+      <OverflowMenuContext.Provider value={{ isBelowBreakpoint: true }}>
+        <OverflowMenuControl />
+      </OverflowMenuContext.Provider>
+    );
+    expect(view.find(`.${styles.overflowMenuControl}`).length).toBe(1);
+    expect(view).toMatchSnapshot();
+  });
+
+  test('Additional Options', () => {
+    const view = mount(<OverflowMenuControl hasAdditionalOptions={true} />);
+    expect(view.find(`.${styles.modifiers.additionalOptions}`).length).toBe(1);
+    expect(view).toMatchSnapshot();
+  });
+});
--- packages/patternfly-4/react-docs/gatsby-browser.js
+++ packages/patternfly-4/react-docs/gatsby-browser.js
@@ -7,3 +7,4 @@ import '@patternfly-safe/patternfly/patternfly.css';
 // Add experimental styles here and in eslintrc.js for the moment.  Once they have been moved out of experimental remove the CSS here and in .eslintrc.js
 import '@patternfly-safe/patternfly/components/Drawer/drawer.css';
 import '@patternfly-safe/patternfly/components/DataToolbar/data-toolbar.css';
+import '@patternfly-safe/patternfly/components/OverflowMenu/overflow-menu.css';
