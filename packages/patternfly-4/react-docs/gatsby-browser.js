1f7cdc63188348e0c2104f2e605e93b876bcffb3
redallen <zallen@redhat.com>
2019-10-11 17:00:51 -0400

    fix theme

---
+++ packages/patternfly-4/react-docs/gatsby-browser.js
@@ -0,0 +1,10 @@
+import '@patternfly/patternfly/patternfly.css';
+// Utilities
+import '@patternfly/patternfly/patternfly-addons.css';
+// Experimental components
+import '@patternfly/patternfly/components/DataToolbar/data-toolbar.css';
+import '@patternfly/patternfly/components/Divider/divider.css';
+import '@patternfly/patternfly/components/Drawer/drawer.css';
+import '@patternfly/patternfly/components/OverflowMenu/overflow-menu.css';
+import '@patternfly/patternfly/components/Spinner/spinner.css';
+import 'gatsby-theme-patternfly-org/global.css';
