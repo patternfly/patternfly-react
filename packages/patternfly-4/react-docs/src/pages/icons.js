51eee7220468e96ce1c15638ee83a3fb8a0bfdb2
redallen <zallen@redhat.com>
2019-10-15 14:49:41 -0400

    fix icons page

--- packages/patternfly-4/react-docs/src/pages/icons.js
+++ packages/patternfly-4/react-docs/src/pages/icons.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { Title, Grid, GridItem, Tooltip, Text, PageSection } from '@patternfly/react-core';
 import { global_spacer_md as spacerMd, global_FontSize_sm as labelFontSize } from '@patternfly/react-tokens';
 import * as IconsModule from '@patternfly/react-icons';
+import SideNavLayout from 'gatsby-theme-patternfly-org/layouts/sideNavLayout';
 
-const Icons = () => {
+export default ({ location }) => {
   const allIcons = Object.entries(IconsModule)
     .filter(([name]) => name.endsWith('Icon'))
     .sort(([name1], [name2]) => name1.localeCompare(name2));
@@ -34,23 +35,22 @@ const Icons = () => {
   };
 
   return (
-    <PageSection>
-      <Title size="4xl" headingLevel="h1">
-        Icons
-      </Title>
-      <Text>These are all Patternfly React Icons.</Text>
-      <Grid>
-        {allIcons.map(([id, Icon]) => (
-          <GridItem key={id} style={cellStyle} sm={6} md={4} lg={2}>
-            <Tooltip content={<div>{id}</div>}>
-              <Icon size="xl" title={id} />
-            </Tooltip>
-            <div style={labelStyle}>{getLabel(id)}</div>
-          </GridItem>
-        ))}
-      </Grid>
-    </PageSection>
+    <SideNavLayout location={location}>
+      <PageSection className="ws-section">
+        <Title size="md" className="ws-framework-title">React</Title>
+        <Title size="4xl">Icons</Title>
+        <Text>These are all Patternfly React Icons.</Text>
+        <Grid>
+          {allIcons.map(([id, Icon]) => (
+            <GridItem key={id} style={cellStyle} sm={6} md={4} lg={2}>
+              <Tooltip content={<div>{id}</div>}>
+                <Icon size="xl" title={id} />
+              </Tooltip>
+              <div style={labelStyle}>{getLabel(id)}</div>
+            </GridItem>
+          ))}
+        </Grid>
+      </PageSection>
+    </SideNavLayout>
   );
 };
-
-export default Icons;
