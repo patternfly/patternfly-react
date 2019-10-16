17a51ff69ce6e800775049cd6cf77bc00cb9e4b6
Scott Dickerson <sdickers@redhat.com>
2019-10-16 10:45:01 -0400

    fix(Cards): refactor sub-component references (#2935)
    Due to patternfly/patternfly-react#2931, rearrange the way `Card`
    includes the component's sub-components to match the way the
    `Wizard` (and all other components) does it.  The new style will
    allow the subcomponent references to survive webpack tree shaking.

--- packages/patternfly-3/patternfly-react/src/components/Cards/index.js
+++ packages/patternfly-3/patternfly-react/src/components/Cards/index.js
@@ -1,13 +1,17 @@
 import Card from './Card';
-import CardTitle from './CardTitle';
 import CardBody from './CardBody';
+import CardDropdownButton from './CardDropdownButton';
+import CardFooter from './CardFooter';
 import CardGrid from './CardGrid';
 import CardHeading from './CardHeading';
-import CardFooter from './CardFooter';
-import CardLink from './CardLink';
-import CardDropdownButton from './CardDropdownButton';
 import CardHeightMatching from './CardHeightMatching';
-import { AggregateStatusCount, AggregateStatusNotifications, AggregateStatusNotification } from './AggregateStatusCard';
+import CardLink from './CardLink';
+import CardTitle from './CardTitle';
+import {
+  AggregateStatusCount,
+  AggregateStatusNotification,
+  AggregateStatusNotifications
+} from './AggregateStatusCard';
 import {
   UtilizationCard,
   UtilizationCardDetails,
@@ -17,37 +21,19 @@ import {
   UtilizationCardDetailsLine2
 } from './UtilizationTrendCard';
 
-Card.Title = CardTitle;
-Card.Body = CardBody;
-Card.Heading = CardHeading;
-Card.Footer = CardFooter;
-Card.Link = CardLink;
-Card.Grid = CardGrid;
-Card.DropdownButton = CardDropdownButton;
-Card.HeightMatching = CardHeightMatching;
-Card.UtilizationCard = UtilizationCard;
-Card.UtilizationCardDetails = UtilizationCardDetails;
-Card.UtilizationCardDetailsCount = UtilizationCardDetailsCount;
-Card.UtilizationCardDetailsDesc = UtilizationCardDetailsDesc;
-Card.UtilizationCardDetailsLine1 = UtilizationCardDetailsLine1;
-Card.UtilizationCardDetailsLine2 = UtilizationCardDetailsLine2;
-Card.AggregateStatusCount = AggregateStatusCount;
-Card.AggregateStatusNotifications = AggregateStatusNotifications;
-Card.AggregateStatusNotification = AggregateStatusNotification;
-
 export {
   Card,
   CardTitle,
   CardBody,
-  CardGrid,
   CardHeading,
   CardFooter,
   CardLink,
+  CardGrid,
   CardDropdownButton,
   CardHeightMatching,
   AggregateStatusCount,
-  AggregateStatusNotifications,
   AggregateStatusNotification,
+  AggregateStatusNotifications,
   UtilizationCard,
   UtilizationCardDetails,
   UtilizationCardDetailsCount,
