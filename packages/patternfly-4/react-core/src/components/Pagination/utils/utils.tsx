d6aaddd39717e94992e9adeec7df8fceca87216a
evwilkin <evan.wilkinson@gmail.com>
2019-10-22 16:33:32 -0400

    eat(pagination): added utils to Pagination that paginate the data and handle pagination overflow. Fix for #3136

--- packages/patternfly-4/react-core/src/components/Pagination/utils/utils.tsx
+++ packages/patternfly-4/react-core/src/components/Pagination/utils/utils.tsx
@@ -1,7 +1,7 @@
 // getPageRows used to paginate data and load previous page if user selects 'perPage' above the number of rows remaining
 // used on Pagination component in combination with onSetPage and onPerPageSelect as follows
-// onSetPage={(_evt, value) => this.setState(this.getPageRows(this.state, this.defaultRows, perPage, value))}
-// onPerPageSelect={(_evt, value) => this.setState(this.getPageRows(this.state, this.defaultRows, value, page))}
+// onSetPage={(_evt, value) => this.setState(getPageRows(this.state, this.defaultRows, perPage, value))}
+// onPerPageSelect={(_evt, value) => this.setState(getPageRows(this.state, this.defaultRows, value, page))}
 
 interface PaginationState {
   perPage: number
