236f9233086f6d8a29bcaf80f0e573e4e0d8d786
Evan <evan.wilkinson@gmail.com>
2019-10-25 11:16:48 -0400

    OverflowMenuDropdownItem: added prop description

--- packages/patternfly-4/react-core/src/experimental/components/OverflowMenu/OverflowMenuDropdownItem.tsx
+++ packages/patternfly-4/react-core/src/experimental/components/OverflowMenu/OverflowMenuDropdownItem.tsx
@@ -5,6 +5,7 @@ import { OverflowMenuContext } from './OverflowMenuContext';
 export interface OverflowMenuDropdownItemProps extends React.HTMLProps<HTMLDivElement> {
   /** Any elements that can be rendered in the menu */
   children?: any;
+  /** Indicates when a dropdown item shows and hides the corresponding list item */
   isShared?: boolean;
 }
 
