c83cda0762d14ff93aafb93dfe737d8fe50ff2bd
Evan <evan.wilkinson@gmail.com>
2019-10-25 11:15:50 -0400

    OverflowMenuDropdownItem: added isShared prop back
    DropdownItems read from isShared and from context to conditionally render.

--- packages/patternfly-4/react-core/src/experimental/components/OverflowMenu/OverflowMenuDropdownItem.tsx
+++ packages/patternfly-4/react-core/src/experimental/components/OverflowMenu/OverflowMenuDropdownItem.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
-import { css } from '@patternfly/react-styles';
-import styles from '@patternfly/react-styles/css/components/OverflowMenu/overflow-menu';
 import { DropdownItem } from '../../../components/Dropdown';
+import { OverflowMenuContext } from './OverflowMenuContext';
 
 export interface OverflowMenuDropdownItemProps extends React.HTMLProps<HTMLDivElement> {
   /** Any elements that can be rendered in the menu */
   children?: any;
+  isShared?: boolean;
 }
 
-export const OverflowMenuDropdownItem: React.SFC<OverflowMenuDropdownItemProps> = ({ children}) => (
-  <DropdownItem component="button"> {children} </DropdownItem>
+export const OverflowMenuDropdownItem: React.SFC<OverflowMenuDropdownItemProps> = ({ children, isShared }) => (
+  <OverflowMenuContext.Consumer>
+    {value =>
+      (!isShared || value.isBelowBreakpoint) && (
+        <DropdownItem component="button"> {children} </DropdownItem>
+      )
+    }
+  </OverflowMenuContext.Consumer>
 );
