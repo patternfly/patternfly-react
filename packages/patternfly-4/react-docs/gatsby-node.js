553093ae1f1384c42f26cafeb8460713c7110db7
redallen <zallen@redhat.com>
2019-11-11 14:41:13 -0500

    more reversions

--- packages/patternfly-4/react-docs/gatsby-node.js
+++ packages/patternfly-4/react-docs/gatsby-node.js
@@ -1,3 +1,44 @@
-const { createSchemaCustomization } = require('@patternfly/patternfly/gatsby-node.js');
-// Add types that are present in React
-exports.createSchemaCustomization = createSchemaCustomization;
+// Add types that are present in Core, React, and Org
+exports.createSchemaCustomization = ({ actions }) => {
+  const typeDefs = `
+    type MdxFrontmatter @infer {
+      title: String!
+      section: String
+      cssPrefix: String
+      hideTOC: Boolean
+      optIn: String
+      experimentalStage: String
+      propComponents: [String]
+      hideDarkMode: Boolean
+      reactComponentName: String
+      coreComponentName: String
+      showTitle: Boolean
+    }
+    type Mdx implements Node @infer {
+      frontmatter: MdxFrontmatter
+    }
+    type TypeType @noInfer {
+      name: String
+    }
+    type TsType @noInfer {
+      name: String
+      raw: String
+    }
+    type defaultValue @noInfer {
+      value: String
+    }
+    type PropsType @noInfer {
+      name: String!
+      description: String
+      required: Boolean
+      type: TypeType
+      tsType: TsType
+      defaultValue: defaultValue
+    }
+    type ComponentMetadata implements Node @noInfer {
+      name: String!
+      props: [PropsType]
+    }
+  `;
+  actions.createTypes(typeDefs);
+}
