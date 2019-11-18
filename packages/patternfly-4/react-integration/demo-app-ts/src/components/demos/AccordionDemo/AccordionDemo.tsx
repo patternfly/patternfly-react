23c141dfa03dbaf7f7fc67233a1b3e8fdfa97c17
Titani <tlabaj@redaht.com>
2019-11-18 09:27:27 -0500

    feat(Accordion): Add a prop to allow th box shadow to be removed
    #3195

--- packages/patternfly-4/react-integration/demo-app-ts/src/components/demos/AccordionDemo/AccordionDemo.tsx
+++ packages/patternfly-4/react-integration/demo-app-ts/src/components/demos/AccordionDemo/AccordionDemo.tsx
@@ -22,41 +22,78 @@ export class AccordionDemo extends React.Component<null, AccordionDemoState> {
   render() {
     const { expanded } = this.state;
     return (
-      <Accordion id="accordion-example">
-        <AccordionItem>
-          <AccordionToggle onClick={() => this.onToggle('item-1')} isExpanded={expanded === 'item-1'} id="item-1">
-            Item One
-          </AccordionToggle>
-          <AccordionContent isHidden={expanded !== 'item-1'} id="item-1-content">
-            <p>
-              Item One Content: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua.
-            </p>
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem>
-          <AccordionToggle onClick={() => this.onToggle('item-2')} isExpanded={expanded === 'item-2'} id="item-2">
-            Item Two
-          </AccordionToggle>
-          <AccordionContent isHidden={expanded !== 'item-2'} id="item-2-content">
-            <p>
-              Item Two Content: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua.
-            </p>
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem>
-          <AccordionToggle onClick={() => this.onToggle('item-3')} isExpanded={expanded === 'item-3'} id="item-3">
-            Item Three
-          </AccordionToggle>
-          <AccordionContent isHidden={expanded !== 'item-3'} id="item-3-content">
-            <p>
-              Item Three Content: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua.
-            </p>
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
+      <React.Fragment>
+        <Accordion id="accordion-example">
+          <AccordionItem>
+            <AccordionToggle onClick={() => this.onToggle('item-1')} isExpanded={expanded === 'item-1'} id="item-1">
+              Item One
+            </AccordionToggle>
+            <AccordionContent isHidden={expanded !== 'item-1'} id="item-1-content">
+              <p>
+                Item One Content: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua.
+              </p>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionToggle onClick={() => this.onToggle('item-2')} isExpanded={expanded === 'item-2'} id="item-2">
+              Item Two
+            </AccordionToggle>
+            <AccordionContent isHidden={expanded !== 'item-2'} id="item-2-content">
+              <p>
+                Item Two Content: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua.
+              </p>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionToggle onClick={() => this.onToggle('item-3')} isExpanded={expanded === 'item-3'} id="item-3">
+              Item Three
+            </AccordionToggle>
+            <AccordionContent isHidden={expanded !== 'item-3'} id="item-3-content">
+              <p>
+                Item Three Content: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua.
+              </p>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+        <Accordion noBoxShadow id="accordion-example-no-box-shadow">
+          <AccordionItem>
+            <AccordionToggle onClick={() => this.onToggle('item-1')} isExpanded={expanded === 'item-1'} id="item-1">
+              Item One
+            </AccordionToggle>
+            <AccordionContent isHidden={expanded !== 'item-1'} id="item-1-content">
+              <p>
+                Item One Content: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua.
+              </p>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionToggle onClick={() => this.onToggle('item-2')} isExpanded={expanded === 'item-2'} id="item-2">
+              Item Two
+            </AccordionToggle>
+            <AccordionContent isHidden={expanded !== 'item-2'} id="item-2-content">
+              <p>
+                Item Two Content: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua.
+              </p>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionToggle onClick={() => this.onToggle('item-3')} isExpanded={expanded === 'item-3'} id="item-3">
+              Item Three
+            </AccordionToggle>
+            <AccordionContent isHidden={expanded !== 'item-3'} id="item-3-content">
+              <p>
+                Item Three Content: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua.
+              </p>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </React.Fragment>
     );
   }
 }
