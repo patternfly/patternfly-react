a450b8c8e44fe9196b3c587fe3b375b9e3552e79
Katie McFaul <kmcfaul@redhat.com>
2019-12-05 14:04:38 -0500

    feat(EmptyStateIcon): remove @

--- packages/patternfly-4/react-core/src/components/EmptyState/EmptyStateIcon.tsx
+++ packages/patternfly-4/react-core/src/components/EmptyState/EmptyStateIcon.tsx
@@ -11,11 +11,11 @@ export enum IconSize {
 }
 
 export interface IconProps extends Omit<React.HTMLProps<SVGElement>, 'size'> {
-  /** @deprecated */
+  /** deprecated */
   color?: string;
-  /** @deprecated */
+  /** deprecated */
   size?: 'sm' | 'md' | 'lg' | 'xl';
-  /** @deprecated */
+  /** deprecated */
   title?: string;
 }
 
