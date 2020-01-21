668408d23601c8027b9bba1ab33daf04e5cb4ba2
Titani <tlabaj@redaht.com>
2020-01-21 16:05:46 -0500

    remove css fro react-docs

--- packages/patternfly-4/react-docs/gatsby-browser.js
+++ packages/patternfly-4/react-docs/gatsby-browser.js
@@ -2,11 +2,7 @@ import '@patternfly/patternfly/patternfly.css';
 // Utilities
 import '@patternfly/patternfly/patternfly-addons.css';
 // Experimental components
-import '@patternfly/patternfly/components/DataToolbar/data-toolbar.css';
-import '@patternfly/patternfly/components/Divider/divider.css';
 import '@patternfly/patternfly/components/Drawer/drawer.css';
-import '@patternfly/patternfly/components/OverflowMenu/overflow-menu.css';
-import '@patternfly/patternfly/components/Spinner/spinner.css';
 // React-specific CSS
 import '@patternfly/react-styles/src/css/components/Table/inline-edit.css';
 import '@patternfly/react-styles/src/css/components/Tooltip/tippy.css';
