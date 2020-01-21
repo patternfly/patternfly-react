795df00256dc837105a23ea14a38f2a40b78c2c4
Titani <tlabaj@redaht.com>
2020-01-21 13:35:34 -0500

    fix drawer test

--- packages/patternfly-4/react-integration/demo-app-ts/src/components/demos/DrawerDemo/DrawerDemo.tsx
+++ packages/patternfly-4/react-integration/demo-app-ts/src/components/demos/DrawerDemo/DrawerDemo.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Drawer, DrawerPanelContent, DrawerProps } from '@patternfly/react-core';
-import { Button } from '@patternfly/react-core';
+import { Button, Drawer, DrawerContent, DrawerPanelContent, DrawerProps } from '@patternfly/react-core';
 
 export interface DrawerDemoState {
   isExpanded: boolean;
 }
+
 
 export class DrawerDemo extends React.Component<DrawerProps, DrawerDemoState> {
   constructor(props) {
@@ -25,16 +25,20 @@ export class DrawerDemo extends React.Component<DrawerProps, DrawerDemoState> {
     const { isExpanded } = this.state;
     const panelContent = <DrawerPanelContent> drawer-panel </DrawerPanelContent>;
 
-    const drawerContent =
-       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pretium est a porttitor vehicula. Quisque vel commodo urna. Morbi mattis rutrum ante, id vehicula ex accumsan ut. Morbi viverra, eros vel porttitor facilisis, eros purus aliquet erat,nec lobortis felis elit pulvinar sem. Vivamus vulputate, risus eget commodo eleifend, eros nibh porta quam, vitae lacinia leo libero at magna. Maecenas aliquam sagittis orci, et posuere nisi ultrices sit amet. Aliquam ex odio, malesuada sed posuere quis, pellentesque at mauris. Phasellus venenatis massa ex, eget pulvinar libero auctor pretium. Aliquam erat volutpat. Duis euismod justo in quam ullamcorper, in commodo massa vulputate.';
+    const drawerContent = (
+      <DrawerContent>
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pretium est a porttitor vehicula. Quisque vel commodo urna. Morbi mattis rutrum ante, id vehicula ex accumsan ut. Morbi viverra, eros vel porttitor facilisis, eros purus aliquet erat,nec lobortis felis elit pulvinar sem. Vivamus vulputate, risus eget commodo eleifend, eros nibh porta quam, vitae lacinia leo libero at magna. Maecenas aliquam sagittis orci, et posuere nisi ultrices sit amet. Aliquam ex odio, malesuada sed posuere quis, pellentesque at mauris. Phasellus venenatis massa ex, eget pulvinar libero auctor pretium. Aliquam erat volutpat. Duis euismod justo in quam ullamcorper, in commodo massa vulputate.';
+      </DrawerContent>
+       );
 
     return (
       <React.Fragment>
         <Button id="toggleButton" onClick={this.onClick}>
           Toggle Drawer
         </Button>
-        <Drawer isExpanded={isExpanded} isInline panelContent={panelContent}>
+        <Drawer isExpanded={isExpanded}>
           {drawerContent}
+          {panelContent}
         </Drawer>
       </React.Fragment>
     );
