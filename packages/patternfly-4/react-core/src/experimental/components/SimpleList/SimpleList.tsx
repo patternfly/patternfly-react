73a8be0b2ce133230138397c71dabdf30ccb9cdd
Katie McFaul <kmcfaul@redhat.com>
2020-01-30 12:54:56 -0500

    feat(SimpleList): track current item

--- packages/patternfly-4/react-core/src/experimental/components/SimpleList/SimpleList.tsx
+++ packages/patternfly-4/react-core/src/experimental/components/SimpleList/SimpleList.tsx
@@ -10,11 +10,32 @@ export interface SimpleListProps {
   className?: string;
 }
 
+interface SimpleListContextProps {
+  currentRef: React.RefObject<HTMLButtonElement>;
+  updateCurrentRef: (id: React.RefObject<HTMLButtonElement>) => void;
+}
+
+export const SimpleListContext = React.createContext<Partial<SimpleListContextProps>>({});
+
 export class SimpleList extends React.Component<SimpleListProps> {
+  state = {
+    currentRef: { current: null }
+  };
+
   static defaultProps: SimpleListProps = {
     children: null as React.ReactNode,
     className: ''
   };
+
+  componentDidMount() {
+    //console with debug mode telling user component is in beta
+  }
+
+  handleCurrentUpdate = (newCurrentRef: React.RefObject<HTMLButtonElement>) => {
+    console.log('handling update for ', newCurrentRef);
+    this.setState({ currentRef: newCurrentRef });
+  };
+
   render() {
     const { children, className, ...props } = this.props;
 
@@ -24,10 +45,17 @@ export class SimpleList extends React.Component<SimpleListProps> {
     }
 
     return (
-      <div className={css(styles.simpleList)} {...props}>
-        {isGrouped && children}
-        {!isGrouped && <ul className={css('pf-c-simple-list__list')}>{children}</ul>}
-      </div>
+      <SimpleListContext.Provider
+        value={{
+          currentRef: this.state.currentRef,
+          updateCurrentRef: this.handleCurrentUpdate
+        }}
+      >
+        <div className={css(styles.simpleList)} {...props}>
+          {isGrouped && children}
+          {!isGrouped && <ul className={css('pf-c-simple-list__list')}>{children}</ul>}
+        </div>
+      </SimpleListContext.Provider>
     );
   }
 }
--- packages/patternfly-4/react-core/src/experimental/components/SimpleList/SimpleListItem.tsx
+++ packages/patternfly-4/react-core/src/experimental/components/SimpleList/SimpleListItem.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { css } from '@patternfly/react-styles';
 import styles from '@patternfly/react-styles/css/components/SimpleList/simple-list';
+import { SimpleListContext } from './SimpleList';
 
 export interface SimpleListItemProps {
   /** Content rendered inside the SimpleList item */
@@ -11,23 +12,51 @@ export interface SimpleListItemProps {
   listItemClassName?: string;
   /** Indicates if the link is current/highlighted */
   isCurrent?: boolean;
+  /** Component type of the SimpleList item */
+  component?: 'button' | 'a';
+  /** OnClick callback for the SimpleList item */
+  onClick?: (event: React.MouseEvent | React.ChangeEvent) => void;
 }
 
 export class SimpleListItem extends React.Component<SimpleListItemProps> {
+  ref = React.createRef<HTMLButtonElement & HTMLAnchorElement>();
   static defaultProps: SimpleListItemProps = {
     children: null,
     className: '',
     listItemClassName: '',
-    isCurrent: false
+    isCurrent: false,
+    component: 'button',
+    onClick: () => {}
   };
+
+  componentDidMount() {
+    console.log('mounted ref', this.ref);
+  }
   render() {
-    const { children, isCurrent, className, listItemClassName, ...props } = this.props;
+    const { children, isCurrent, className, listItemClassName, component: Component, onClick, ...props } = this.props;
+    console.log('ref', this.ref);
     return (
-      <li className={css('pf-c-simple-list__item', listItemClassName)} {...props}>
-        <button className={css(styles.simpleListItemLink, isCurrent && styles.modifiers.current, className)}>
-          {children}
-        </button>
-      </li>
+      <SimpleListContext.Consumer>
+        {({ currentRef, updateCurrentRef }) => {
+          const isCurrentItem = this.ref && currentRef ? currentRef.current === this.ref.current : isCurrent;
+
+          return (
+            <li className={css('pf-c-simple-list__item', listItemClassName)}>
+              <Component
+                className={css(styles.simpleListItemLink, isCurrentItem && styles.modifiers.current, className)}
+                onClick={(evt: React.MouseEvent) => {
+                  onClick(evt);
+                  updateCurrentRef(this.ref);
+                }}
+                {...props}
+                ref={this.ref}
+              >
+                {children}
+              </Component>
+            </li>
+          );
+        }}
+      </SimpleListContext.Consumer>
     );
   }
 }
