1c34016eaa3fcc5d0c281b0a5e235a8b43536a10
redallen <zallen@redhat.com>
2020-02-07 13:56:19 -0500

    regex gm

--- .github/workflows/pr-verification.js
+++ .github/workflows/pr-verification.js
@@ -11,7 +11,7 @@ const prBody = process.argv[3];
 // This is a fun one.
 // https://help.github.com/en/github/managing-your-work-on-github/linking-a-pull-request-to-an-issue#linking-a-pull-request-to-an-issue-using-a-keyword
 // https://regex101.com/r/HDx3ZS/1
-const closeRegex = /(close[sd]?|fix(es|ed)?|resolve[sd]?)\s+(#|https?:\/\/(www.)?github\.com\/patternfly\/[^\/]+\/issues\/)\d+/
+const closeRegex = /(close[sd]?|fix(es|ed)?|resolve[sd]?)\s+(#|https?:\/\/(www.)?github\.com\/patternfly\/[^\/]+\/issues\/)\d+/gm;
 let returnCode = 0;
 
 // This should be consistent with lerna. I haven't thoroughly tested it, though.
