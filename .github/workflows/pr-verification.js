a9c11648531c54207d5be518793826a55073ffc6
redallen <zallen@redhat.com>
2020-02-10 17:16:16 -0500

    require things

--- .github/workflows/pr-verification.js
+++ .github/workflows/pr-verification.js
@@ -1,3 +1,5 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
 const load = require('@commitlint/load');
 const lint = require('@commitlint/lint');
 
@@ -5,24 +7,22 @@ const CONFIG = {
   extends: ['@commitlint/config-conventional']
 };
 
-const title =
-  github.context.payload &&
-  github.context.payload.pull_request &&
-  github.context.payload.pull_request.title
-core.info(title)
-const commitMsg = process.argv[2];
-const prBody = process.argv[3];
+const title = github.context.payload.pull_request.title;
+const body = github.context.payload.pull_request.body;
 // This is a fun one.
 // https://help.github.com/en/github/managing-your-work-on-github/linking-a-pull-request-to-an-issue#linking-a-pull-request-to-an-issue-using-a-keyword
 // https://regex101.com/r/HDx3ZS/1
 const closeRegex = /(close[sd]?|fix(es|ed)?|resolve[sd]?)\s+(#|https?:\/\/(www.)?github\.com\/patternfly\/[^\/]+\/issues\/)\d+/gmi;
 let returnCode = 0;
 
+core.info(title);
+core.info(body);
+
 // This should be consistent with lerna. I haven't thoroughly tested it, though.
 // https://commitlint.js.org/#/reference-api?id=lint
 load(CONFIG)
   .then(opts => lint(
-    commitMsg,
+    title,
     opts.rules,
     opts.parserPreset ? { parserOpts: opts.parserPreset.parserOpts } : {}
   ))
@@ -34,7 +34,7 @@ load(CONFIG)
       );
       returnCode -= 1;
     }
-    if (!closeRegex.test(prBody)) {
+    if (!closeRegex.test(body)) {
       core.setFailed('PR description must close an existing issue');
       returnCode -= 1;
     }
