b2f6dedde89d67a6b3ba6be15be483d89b984cd6
evwilkin <evan.wilkinson@gmail.com>
2020-02-10 16:14:36 -0500

    fix copyTS imports

--- packages/patternfly-4/react-charts/scripts/copyTS.js
+++ packages/patternfly-4/react-charts/scripts/copyTS.js
@@ -1,6 +1,6 @@
-import path from 'path';
-import glob from 'glob';
-import fse from 'fs-extra';
+import * as path from 'path';
+import * as glob from 'glob';
+import * as fse from 'fs-extra';
 
 const srcDir = path.join('./src');
 const distDir = path.join('./dist/js');
