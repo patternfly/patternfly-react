175201afbac4dca1b544c91c010b9f7287d05700
redallen <zallen@redhat.com>
2020-03-06 17:12:05 -0500

    fix backgroundImage on chrome

--- packages/react-core/src/components/BackgroundImage/BackgroundImage.tsx
+++ packages/react-core/src/components/BackgroundImage/BackgroundImage.tsx
@@ -1,17 +1,12 @@
 import * as React from 'react';
 import { Omit } from '../../helpers/typeUtils';
-
+import cssVar from '@patternfly/react-tokens/dist/js/c_background_image_BackgroundImage';
+import cssVarName2x from '@patternfly/react-tokens/dist/js/c_background_image_BackgroundImage_2x';
+import cssVarNameSm from '@patternfly/react-tokens/dist/js/c_background_image_BackgroundImage_sm';
+import cssVarNameSm2x from '@patternfly/react-tokens/dist/js/c_background_image_BackgroundImage_sm_2x';
+import cssVarNameLg from '@patternfly/react-tokens/dist/js/c_background_image_BackgroundImage_lg';
 import { css } from '@patternfly/react-styles';
 import styles from '@patternfly/react-styles/css/components/BackgroundImage/background-image';
-
-export enum BackgroundImageSrc {
-  xs = 'xs',
-  xs2x = 'xs2x',
-  sm = 'sm',
-  sm2x = 'sm2x',
-  lg = 'lg',
-  filter = 'filter'
-}
 
 export interface BackgroundImageSrcMap {
   xs: string;
@@ -19,37 +14,59 @@ export interface BackgroundImageSrcMap {
   sm: string;
   sm2x: string;
   lg: string;
-  filter?: string;
 }
+
+const defaultFilter = (
+  <filter className="test123">
+    <feColorMatrix type="matrix" values="1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 0 0 0 1 0"></feColorMatrix>
+    <feComponentTransfer colorInterpolationFilters="sRGB" result="duotone">
+      <feFuncR type="table" tableValues="0.086274509803922 0.43921568627451"></feFuncR>
+      <feFuncG type="table" tableValues="0.086274509803922 0.43921568627451"></feFuncG>
+      <feFuncB type="table" tableValues="0.086274509803922 0.43921568627451"></feFuncB>
+      <feFuncA type="table" tableValues="0 1"></feFuncA>
+    </feComponentTransfer>
+  </filter>
+);
 
 export interface BackgroundImageProps extends Omit<React.HTMLProps<HTMLDivElement>, 'src'> {
   /** Additional classes added to the background. */
   className?: string;
+  /** Override svg filter to use */
+  filter?: React.ReactElement;
   /** Override image styles using a string or BackgroundImageSrc */
   src: string | BackgroundImageSrcMap;
 }
 
-export const BackgroundImage: React.FunctionComponent<BackgroundImageProps> = ({
-  className = '',
+export const BackgroundImage = ({
+  className,
+  src,
+  filter = defaultFilter, 
   ...props
-}: BackgroundImageProps) => (
-  <div className={css(styles.backgroundImage, className)} {...props}>
-    <svg xmlns="http://www.w3.org/2000/svg" className="pf-c-background-image__filter" width="0" height="0">
-      <filter id="image_overlay">
-        <feColorMatrix
-          type="matrix"
-          values="1 0 0 0 0
-          1 0 0 0 0
-          1 0 0 0 0
-          0 0 0 1 0"
-        />
-        <feComponentTransfer colorInterpolationFilters="sRGB" result="duotone">
-          <feFuncR type="table" tableValues="0.086274509803922 0.43921568627451" />
-          <feFuncG type="table" tableValues="0.086274509803922 0.43921568627451" />
-          <feFuncB type="table" tableValues="0.086274509803922 0.43921568627451" />
-          <feFuncA type="table" tableValues="0 1" />
-        </feComponentTransfer>
-      </filter>
-    </svg>
-  </div>
-);
+}: BackgroundImageProps) => {
+  const getUrlValue = (size: keyof BackgroundImageSrcMap) => {
+    if (typeof src === 'string') {
+      return `url(${src})`;
+    }
+    else if (typeof src === 'object') {
+      return `url(${src[size]})`;
+    }
+    
+    return '';
+  }
+
+  const style = {
+    [cssVar.name]: getUrlValue('xs'),
+    [cssVarName2x.name]: getUrlValue('xs2x'),
+    [cssVarNameSm.name]: getUrlValue('sm'),
+    [cssVarNameSm2x.name]: getUrlValue('sm2x'),
+    [cssVarNameLg.name]: getUrlValue('lg'),
+  } as React.CSSProperties;
+
+  return (
+    <div className={css(styles.backgroundImage, className)} style={style} {...props}>
+      <svg xmlns="http://www.w3.org/2000/svg" className="pf-c-background-image__filter" width="0" height="0">
+        {React.cloneElement(filter, { id: 'image-overlay' })}
+      </svg>
+    </div>
+  )
+}
