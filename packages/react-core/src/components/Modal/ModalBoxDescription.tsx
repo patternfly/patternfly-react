0e5b1c8839864c79b027ddf902ef2709f451f059
Katie McFaul <kmcfaul@redhat.com>
2020-03-10 14:28:10 -0400

    feat(Modal): fix property descriptions

--- packages/react-core/src/components/Modal/ModalBoxDescription.tsx
+++ packages/react-core/src/components/Modal/ModalBoxDescription.tsx
@@ -3,9 +3,9 @@ import { css } from '@patternfly/react-styles';
 import styles from '@patternfly/react-styles/css/components/ModalBox/modal-box';
 
 export interface ModalBoxDescriptionProps {
-  /** Content rendered inside the Footer */
+  /** Content rendered inside the description */
   children?: React.ReactNode;
-  /** Additional classes added to the Footer */
+  /** Additional classes added to the description */
   className?: string;
   /** ID of the description */
   id?: string;
