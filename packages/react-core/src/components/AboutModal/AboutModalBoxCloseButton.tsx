f824828fa5b6a0879e9aa419fe8f0ff7b94db348
boaz0 <boaz.shuster.github@gmail.com>
2020-03-17 15:56:32 +0200

    fix(aboutmodal): set a customize aria-label to the close button (#3877)
    * fix(aboutmodal): set a customize aria-label to the close button
    
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>
    
    * Quick sentence-case fix
    
    Co-authored-by: Eugenia <32821331+jenny-s51@users.noreply.github.com>

--- packages/react-core/src/components/AboutModal/AboutModalBoxCloseButton.tsx
+++ packages/react-core/src/components/AboutModal/AboutModalBoxCloseButton.tsx
@@ -9,15 +9,18 @@ export interface AboutModalBoxCloseButtonProps extends React.HTMLProps<HTMLDivEl
   className?: string;
   /** A callback for when the close button is clicked  */
   onClose?: () => void;
+  /** Set close button aria label */
+  'aria-label'?: string;
 }
 
 export const AboutModalBoxCloseButton: React.FunctionComponent<AboutModalBoxCloseButtonProps> = ({
   className = '',
   onClose = () => undefined as any,
+  'aria-label': ariaLabel = 'Close Dialog',
   ...props
 }: AboutModalBoxCloseButtonProps) => (
   <div className={css(styles.aboutModalBoxClose, className)} {...props}>
-    <Button variant="plain" onClick={onClose} aria-label="Close Dialog">
+    <Button variant="plain" onClick={onClose} aria-label={ariaLabel}>
       <TimesIcon />
     </Button>
   </div>
