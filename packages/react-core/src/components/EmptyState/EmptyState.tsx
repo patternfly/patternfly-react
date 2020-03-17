d4ea57f47eab89b8cdedc792ebe078b746176679
Rebecca Alpert <ralpert@redhat.com>
2020-03-17 13:22:08 -0400

    feat(EmptyState): Added support for extra-large empty state (#3844)
    * feat(EmptyState): Added support for extra-large empty state
    
    Added new prop and updated examples
    
    Fixes 3540
    
    * Change prop name
    
    * Adjust title size

--- packages/react-core/src/components/EmptyState/EmptyState.tsx
+++ packages/react-core/src/components/EmptyState/EmptyState.tsx
@@ -3,12 +3,14 @@ import { css, getModifier } from '@patternfly/react-styles';
 import styles from '@patternfly/react-styles/css/components/EmptyState/empty-state';
 
 export enum EmptyStateVariant {
+  'xl' = 'xl',
   large = 'large',
   small = 'small',
   full = 'full'
 }
 
 const maxWidthModifiers: { [variant in keyof typeof EmptyStateVariant]: string } = {
+  xl: 'xl',
   large: 'lg',
   small: 'sm',
   full: ''
@@ -20,7 +22,7 @@ export interface EmptyStateProps extends React.HTMLProps<HTMLDivElement> {
   /** Content rendered inside the EmptyState */
   children: React.ReactNode;
   /** Modifies EmptyState max-width */
-  variant?: 'small' | 'large' | 'full';
+  variant?: 'small' | 'large' | 'full' | 'xl';
 }
 
 export const EmptyState: React.FunctionComponent<EmptyStateProps> = ({
