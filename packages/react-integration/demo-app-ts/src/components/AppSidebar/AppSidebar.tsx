3958f307db342aa4f86e9f3c2aaef5f274824aca
redallen <zallen@redhat.com>
2020-03-24 16:18:03 -0400

    chore(prop-types): remove a few usages

--- packages/react-integration/demo-app-ts/src/components/AppSidebar/AppSidebar.tsx
+++ packages/react-integration/demo-app-ts/src/components/AppSidebar/AppSidebar.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { PageSidebar, PageSidebarProps } from '@patternfly/react-core';
 
 export const AppSidebar: React.FunctionComponent<PageSidebarProps> = props => <PageSidebar nav={props.nav} />;
-
-AppSidebar.propTypes = {
-  nav: PropTypes.node
-};
 
 export default AppSidebar;
