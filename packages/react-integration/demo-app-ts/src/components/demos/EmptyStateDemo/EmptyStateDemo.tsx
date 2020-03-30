3ae3441111ff3b992712d725b1838a017ca3a965
redallen <zallen@redhat.com>
2020-03-30 16:41:56 -0400

    fix empty state demo

--- packages/react-integration/demo-app-ts/src/components/demos/EmptyStateDemo/EmptyStateDemo.tsx
+++ packages/react-integration/demo-app-ts/src/components/demos/EmptyStateDemo/EmptyStateDemo.tsx
@@ -57,7 +57,7 @@ export class EmptyStateDemo extends Component {
         </EmptyStateSecondaryActions>
       </React.Fragment>
     ),
-    variant: EmptyStateVariant.sm
+    variant: EmptyStateVariant.small
   };
   myFullEmptyStateProps: EmptyStateProps = {
     children: (
