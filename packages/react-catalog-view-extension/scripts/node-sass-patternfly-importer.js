5030834bb9e29fed714f35fa4e6754f93c429207
redallen <zallen@redhat.com>
2020-04-03 14:27:05 -0400

    add sass generation

--- packages/react-catalog-view-extension/scripts/node-sass-patternfly-importer.js
+++ packages/react-catalog-view-extension/scripts/node-sass-patternfly-importer.js
@@ -6,14 +6,14 @@ const path = require('path');
 function importer(url) {
   // Assume yarn hoists these.
   if (url.startsWith('bootstrap')) {
-    url = path.join(__dirname, `../../node_modules/bootstrap-sass/assets/stylesheets/${url}`);
+    url = path.join(__dirname, `../../../node_modules/bootstrap-sass/assets/stylesheets/${url}`);
   } else if (url.startsWith('patternfly')) {
-    url = path.join(__dirname, `../../node_modules/patternfly/dist/sass/${url}`);
+    url = path.join(__dirname, `../../../node_modules/patternfly/dist/sass/${url}`);
   } else if (url.startsWith('font-awesome')) {
     // font-awesome is for pf3 docs
-    url = path.join(__dirname, `../../node_modules/font-awesome/scss/${url}`);
+    url = path.join(__dirname, `../../../node_modules/font-awesome/scss/${url}`);
   } else if (url.startsWith('~')) {
-    url = path.join(__dirname, `../../node_modules/${url.substr(1)}`);
+    url = path.join(__dirname, `../../../node_modules/${url.substr(1)}`);
   }
   return { file: url };
 }
