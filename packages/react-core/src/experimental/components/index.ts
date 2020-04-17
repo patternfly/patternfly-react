378d15cc230008639a892270c7c17a532a0684aa
Jessie <jhuff@redhat.com>
2020-04-17 11:42:43 -0400

    Remove promoted components from experimental index file (#4029)

--- packages/react-core/src/experimental/components/index.ts
+++ packages/react-core/src/experimental/components/index.ts
@@ -1,5 +1,2 @@
 export * from '../../components/DataToolbar';
-export * from '../../components/Divider';
 export * from '../../components/Drawer';
-export * from '../../components/OverflowMenu';
-export * from '../../components/Spinner';
