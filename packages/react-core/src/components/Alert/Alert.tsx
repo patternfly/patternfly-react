2bf3b8fd3fec2aa56825d66080b4f62db727b003
Joachim Schuler <jschuler@redhat.com>
2020-05-04 17:42:38 -0400

    Breaking changes: Alert

--- packages/react-core/src/components/Alert/Alert.tsx
+++ packages/react-core/src/components/Alert/Alert.tsx
@@ -5,6 +5,9 @@ import accessibleStyles from '@patternfly/react-styles/css/utilities/Accessibili
 import { AlertIcon } from './AlertIcon';
 import { capitalize, getOUIAProps, OUIAProps } from '../../helpers';
 import { AlertContext } from './AlertContext';
+import { AlertActionCloseButton } from './AlertActionCloseButton';
+import { AlertActionLink } from './AlertActionLink';
+import { ButtonProps } from '../Button';
 
 export enum AlertVariant {
   success = 'success',
@@ -14,6 +17,10 @@ export enum AlertVariant {
   default = 'default'
 }
 
+export interface ActionItem extends ButtonProps {
+  text: string;
+}
+
 export interface AlertProps extends Omit<React.HTMLProps<HTMLDivElement>, 'action' | 'title'> {
   /** Adds Alert variant styles  */
   variant?: 'success' | 'danger' | 'warning' | 'info' | 'default';
@@ -21,8 +28,10 @@ export interface AlertProps extends Omit<React.HTMLProps<HTMLDivElement>, 'actio
   isInline?: boolean;
   /** Title of the Alert  */
   title: React.ReactNode;
-  /** Action button to put in the Alert. Should be <AlertActionLink> or <AlertActionCloseButton> */
-  action?: React.ReactNode;
+  /** Adds a close button to the alert and defines the callback for when it is clicked  */
+  onClose?: () => void;
+  /** Action item links */
+  actionItems?: ActionItem[];
   /** Content rendered inside the Alert */
   children?: React.ReactNode;
   /** Additional classes added to the Alert  */
@@ -41,7 +50,8 @@ export const Alert: React.FunctionComponent<AlertProps & OUIAProps> = ({
   isLiveRegion = false,
   variantLabel = `${capitalize(variant)} alert:`,
   'aria-label': ariaLabel = `${capitalize(variant)} Alert`,
-  action = null,
+  onClose,
+  actionItems,
   title,
   children = '',
   className = '',
@@ -75,12 +85,26 @@ export const Alert: React.FunctionComponent<AlertProps & OUIAProps> = ({
     >
       <AlertIcon variant={variant} />
       <h4 className={css(styles.alertTitle)}>{getHeadingContent}</h4>
+      {onClose && (
+        <AlertContext.Provider value={{ title, variantLabel }}>
+          <div className={css(styles.alertAction)}>
+            <AlertActionCloseButton onClose={onClose} />
+          </div>
+        </AlertContext.Provider>
+      )}
       {children && <div className={css(styles.alertDescription)}>{children}</div>}
-      <AlertContext.Provider value={{ title, variantLabel }}>
-        {action && (typeof action === 'object' || typeof action === 'string') && (
-          <div className={css(styles.alertAction)}>{action}</div>
-        )}
-      </AlertContext.Provider>
+      {actionItems && (
+        <div className={css(styles.alertActionGroup)}>
+          {actionItems.map((item: ActionItem, index: number) => {
+            const { text, ...rest } = item;
+            return (
+              <AlertActionLink key={index} {...rest}>
+                {text}
+              </AlertActionLink>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
