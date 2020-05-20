7ba1ed4ec118656761b6390fcd578531bcf75b02
zallen <47335686+redallen@users.noreply.github.com>
2020-05-20 14:23:25 -0400

    fix(patternfly-react): remove side effects (#4273)

--- packages/patternfly-react/src/components/Cards/UtilizationTrendCard/UtilizationCard.js
+++ packages/patternfly-react/src/components/Cards/UtilizationTrendCard/UtilizationCard.js
@@ -2,6 +2,11 @@ import classNames from 'classnames';
 import React from 'react';
 import PropTypes from 'prop-types';
 import Card from '../Card';
+import UtilizationCardDetails from './UtilizationCardDetails';
+import UtilizationCardDetailsCount from './UtilizationCardDetailsCount';
+import UtilizationCardDetailsDesc from './UtilizationCardDetailsDesc';
+import UtilizationCardDetailsLine1 from './UtilizationCardDetailsLine1';
+import UtilizationCardDetailsLine2 from './UtilizationCardDetailsLine2';
 
 const UtilizationCard = ({ children, className, ...props }) => {
   const classes = classNames('card-pf-utilization', className);
@@ -21,4 +26,11 @@ UtilizationCard.propTypes = {
 UtilizationCard.defaultProps = {
   className: ''
 };
+
+UtilizationCard.Details = UtilizationCardDetails;
+UtilizationCard.DetailsCount = UtilizationCardDetailsCount;
+UtilizationCard.DetailsDesc = UtilizationCardDetailsDesc;
+UtilizationCard.DetailsLine1 = UtilizationCardDetailsLine1;
+UtilizationCard.DetailsLine2 = UtilizationCardDetailsLine2;
+
 export default UtilizationCard;
