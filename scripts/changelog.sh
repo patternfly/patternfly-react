59c9d129eb41aa120bd567ea70613cbc6dee276e
redallen <zallen@redhat.com>
2020-05-28 15:00:23 -0400

    chore(changelog): add bash script for git

---
+++ scripts/changelog.sh
@@ -0,0 +1,7 @@
+if [ -z $1 ]
+then
+  echo 'Usage: changelog.sh @patternfly/react-core@version';
+  exit 1;
+fi;
+
+git log --pretty="format:%ci %s" --perl-regexp --author='^((?!patternfly-build).*)$' $1..HEAD
