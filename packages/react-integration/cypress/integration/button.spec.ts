a6ef2545833f3cf6b98ad0736719975cc0a9c6dd
Michael Spaxman <5942899+seanforyou23@users.noreply.github.com>
2020-06-19 13:22:14 -0400

    feat(button): add isAriaDisabled (#3801)
    > The changes Matt mentioned are in there
    > We pulled in those changes (https://github.com/patternfly/patternfly/pull/3166) with the latest pf-core version

--- packages/react-integration/cypress/integration/button.spec.ts
+++ packages/react-integration/cypress/integration/button.spec.ts
@@ -5,30 +5,178 @@ describe('Button Demo Test', () => {
     cy.url().should('eq', 'http://localhost:3000/button-demo-nav-link');
   });
 
-  it('Verify primary button', () => {
-    cy.get('button').should('have.class', 'pf-m-primary');
+  it('Verify button element variant classes', () => {
+    cy.get('.btn-demo-area').within(() => {
+      cy.get('.pf-c-button[id="normal-btn-1"]').should('have.class', 'pf-m-primary');
+      cy.get('.pf-c-button[id="normal-btn-2"]').should('have.class', 'pf-m-secondary');
+      cy.get('.pf-c-button[id="normal-btn-3"]').should('have.class', 'pf-m-tertiary');
+      cy.get('.pf-c-button[id="normal-btn-4"]').should('have.class', 'pf-m-danger');
+      cy.get('.pf-c-button[id="normal-btn-5"]').should('have.class', 'pf-m-link');
+      cy.get('.pf-c-button[id="normal-btn-6"]').should('have.class', 'pf-m-plain');
+      cy.get('.pf-c-button[id="normal-btn-7"]').should('have.class', 'pf-m-control');
+    });
   });
 
-  it('Verify secondary button', () => {
-    cy.get('button').should('have.class', 'pf-m-secondary');
+  it('Verify disabled button classes and attributes', () => {
+    cy.get('.btn-demo-area').within(() => {
+      cy.get('.pf-c-button[id="normal-btn-8"]')
+        .should('have.class', 'pf-m-disabled')
+        .and('have.attr', 'disabled');
+      cy.get('.pf-c-button[id="normal-btn-8"]').should('not.have.attr', 'tabindex');
+      cy.get('.pf-c-button[id="normal-btn-8"]').should('have.attr', 'aria-disabled', 'true');
+      cy.get('.pf-c-button[id="normal-btn-9"]')
+        .should('have.class', 'pf-m-aria-disabled')
+        .and('not.have.attr', 'disabled');
+    });
   });
 
-  it('Verify tertiary button', () => {
-    cy.get('button').should('have.class', 'pf-m-tertiary');
+  it('Verify isAriaDisabled button classes and attributes', () => {
+    cy.get('.btn-demo-area').within(() => {
+      cy.get('.pf-c-button[id="normal-btn-9"]').should('have.attr', 'aria-disabled', 'true');
+      cy.get('.pf-c-button[id="normal-btn-9"]').should('not.have.attr', 'disabled');
+      cy.get('.pf-c-button[id="normal-btn-9"]').should('not.have.attr', 'tabindex');
+    });
   });
 
-  it('Tertiary button should be disabled', () => {
-    cy.get('.pf-m-tertiary').should('be.disabled');
+  it('Verify button can set explicit tabindex attribute', () => {
+    cy.get('.btn-demo-area').within(() => {
+      cy.get('.pf-c-button[id="normal-btn-10"]').should('have.attr', 'tabindex', '2');
+    });
   });
 
-  it('Link button should be set to tabindex=-1', () => {
-    cy.get('.pf-m-link').should('have.attr', 'tabindex', '-1');
+  it('Verify isAriaDisabled button has tooltip when focused', () => {
+    cy.get('.btn-demo-area').within(() => {
+      cy.get('.pf-c-button[id="normal-btn-11"]')
+        .focus()
+        .should('have.attr', 'aria-describedby', 'tippy-1');
+    });
+    cy.get('.tippy-popper').should('be.visible');
   });
 
-  // do this last since it leaves the page
+  it('Verify isAriaDisabled button has tooltip when hovered', () => {
+    cy.get('.btn-demo-area').within(() => {
+      cy.get('.pf-c-button[id="normal-btn-11"]')
+        .trigger('mouseover')
+        .should('have.attr', 'aria-describedby', 'tippy-1');
+    });
+    cy.get('.tippy-popper').should('be.visible');
+  });
+
+  it('Verify isAriaDisabled button prevents default actions', () => {
+    cy.get('.btn-demo-area').within(() => {
+      cy.get('.pf-c-button[id="normal-btn-11"]')
+        .focus()
+        .click();
+      cy.url().should('eq', 'http://localhost:3000/button-demo-nav-link'); // shouldn't have navigated anywhere
+    });
+  });
+
+  it('Verify isAriaDisabled button click events are permitted if overridden in inoperableEvents', () => {
+    cy.get('.btn-demo-area').within(() => {
+      cy.get('.pf-c-button[id="aria-disabled-btn-1"]').should(
+        'have.text',
+        'Aria-disabled link button with custom list of prevented events 0'
+      );
+
+      cy.get('.pf-c-button[id="aria-disabled-btn-1"]')
+        .focus() // should not decrement to -1 since inoperableEvents={['onFocus']} is set
+        .type('{enter}') // should increment to 1 since this event is not prevented
+        .should('have.text', 'Aria-disabled link button with custom list of prevented events 1');
+
+      cy.get('.pf-c-button[id="aria-disabled-btn-1"]')
+        .focus()
+        .blur() // should decrement back to to 0 since this event is not prevented
+        .should('have.text', 'Aria-disabled link button with custom list of prevented events 0');
+
+      cy.get('.pf-c-button[id="aria-disabled-btn-1"]')
+        .focus()
+        .should('have.text', 'Aria-disabled link button with custom list of prevented events 0')
+        .click() // click event should increment back to 1
+        .should('have.text', 'Aria-disabled link button with custom list of prevented events 1');
+    });
+  });
+
+  it('Verify link button classes and attributes', () => {
+    cy.get('.btn-demo-area').within(() => {
+      cy.get('.pf-c-button[id="link-btn-1"]').should('have.class', 'pf-m-link');
+      cy.get('.pf-c-button[id="link-btn-2"]').should('have.class', 'pf-m-inline');
+
+      cy.get('.pf-c-button[id="link-btn-3"]').should('have.class', 'pf-m-disabled');
+      cy.get('.pf-c-button[id="link-btn-3"]').should('have.class', 'pf-m-link');
+      cy.get('.pf-c-button[id="link-btn-3"]').should('have.attr', 'disabled');
+      cy.get('.pf-c-button[id="link-btn-3"]').should('have.attr', 'aria-disabled', 'true');
+
+      cy.get('.pf-c-button[id="link-btn-4"]').should('have.attr', 'aria-disabled', 'true');
+      cy.get('.pf-c-button[id="link-btn-4"]').should('not.have.attr', 'disabled');
+      cy.get('.pf-c-button[id="link-btn-4"]')
+        .should('have.class', 'pf-m-aria-disabled')
+        .and('have.class', 'pf-m-link');
+
+      cy.get('.pf-c-button[id="link-btn-5"]').should('have.class', 'pf-m-link');
+      cy.get('.pf-c-button[id="link-btn-5"]').should('have.attr', 'tabindex', '-1');
+    });
+  });
+
+  it('Verify link as button classes and attributes', () => {
+    cy.get('.btn-demo-area').within(() => {
+      cy.get('.pf-c-button[id="link-as-btn-1"]')
+        .should('have.class', 'pf-c-button')
+        .and('have.attr', 'href', 'https://github.com/patternfly/patternfly-react');
+
+      cy.get('.pf-c-button[id="link-as-btn-2"]').should('have.class', 'pf-m-disabled');
+      cy.get('.pf-c-button[id="link-as-btn-2"]')
+        .should('have.attr', 'aria-disabled', 'true')
+        .and('not.have.attr', 'disabled');
+      cy.get('.pf-c-button[id="link-as-btn-2"]').should('have.attr', 'tabindex', '-1');
+      cy.get('.pf-c-button[id="link-as-btn-2"]').click();
+      cy.url().should('eq', 'http://localhost:3000/button-demo-nav-link'); // shouldn't have navigated anywhere
+
+      cy.get('.pf-c-button[id="link-as-btn-3"]').should('not.have.class', 'pf-m-disabled');
+      cy.get('.pf-c-button[id="link-as-btn-3"]')
+        .should('have.attr', 'aria-disabled', 'true')
+        .and('not.have.attr', 'disabled');
+      cy.get('.pf-c-button[id="link-as-btn-3"]').should('not.have.attr', 'tabindex');
+      cy.get('.pf-c-button[id="link-as-btn-3"]').click();
+      cy.url().should('eq', 'http://localhost:3000/button-demo-nav-link'); // shouldn't have navigated anywhere
+
+      cy.get('.pf-c-button[id="link-as-btn-4"]').should('have.attr', 'tabindex', '4');
+
+      cy.get('.pf-c-button[id="link-as-btn-5"]').should('have.attr', 'aria-disabled', 'true');
+    });
+  });
+
+  it('Verify isAriaDisabled link as button has tooltip when focused', () => {
+    cy.get('.btn-demo-area').within(() => {
+      cy.get('.pf-c-button[id="link-as-btn-5"]')
+        .focus()
+        .should('have.attr', 'aria-describedby', 'tippy-2');
+    });
+  });
+
+  it('Verify isAriaDisabled link as button has tooltip when hovered', () => {
+    cy.get('.btn-demo-area').within(() => {
+      cy.get('.pf-c-button[id="link-as-btn-5"]')
+        .trigger('mouseover')
+        .should('have.attr', 'aria-describedby', 'tippy-2');
+    });
+  });
+
+  it('Verify isAriaDisabled link as button prevents default actions', () => {
+    cy.get('.btn-demo-area').within(() => {
+      cy.get('.pf-c-button[id="link-as-btn-5"]')
+        .focus()
+        .click();
+      cy.url().should('eq', 'http://localhost:3000/button-demo-nav-link'); // shouldn't have navigated anywhere
+    });
+  });
+
   it('Verify primary button clicks', () => {
-    cy.get('button.pf-m-primary')
-      .parent()
-      .should('have.attr', 'href', 'https://www.google.com');
+    cy.get('.btn-demo-area').within(() => {
+      cy.get('.pf-c-button[id="link-as-btn-4"]').should(
+        'have.attr',
+        'href',
+        'https://github.com/patternfly/patternfly-react'
+      );
+    });
   });
 });
--- packages/react-integration/demo-app-ts/src/components/demos/ButtonDemo/ButtonDemo.tsx
+++ packages/react-integration/demo-app-ts/src/components/demos/ButtonDemo/ButtonDemo.tsx
@@ -1,14 +1,65 @@
 import React from 'react';
-import { Button, ButtonProps } from '@patternfly/react-core';
+import { Button, ButtonProps, Tooltip } from '@patternfly/react-core';
+import PlusCircleIcon from '@patternfly/react-icons/dist/js/icons/plus-circle-icon';
+import ExternalLinkAltIcon from '@patternfly/react-icons/dist/js/icons/external-link-alt-icon';
+import '@patternfly/react-styles/css/utilities/Spacing/spacing.css';
 
 const href = 'https://www.google.com';
 
-export class ButtonDemo extends React.Component {
+interface ButtonDemoState {
+  counter: number;
+}
+export class ButtonDemo extends React.Component<ButtonProps, ButtonDemoState> {
   static displayName = 'ButtonDemo';
+  constructor(props: ButtonProps) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
+
+  normalButton: ButtonProps = {
+    className: 'pf-u-m-sm',
+    component: 'button',
+    onClick: () => {
+      window.location.href = 'https://github.com/patternfly/patternfly-react';
+    }
+  };
+
+  linkButton: ButtonProps = {
+    className: 'pf-u-m-sm',
+    component: 'button',
+    icon: <PlusCircleIcon />,
+    onKeyPress: () => {
+      window.location.href = 'https://github.com/patternfly/patternfly-react';
+    },
+    variant: 'link'
+  };
+
+  linkAsButton: ButtonProps = {
+    className: 'pf-u-m-sm',
+    component: 'a',
+    href: 'https://github.com/patternfly/patternfly-react',
+    icon: <ExternalLinkAltIcon />,
+    target: '_blank'
+  };
+
   myButtonProps: ButtonProps = {
     component: 'button',
     href,
     target: '_blank'
+  };
+
+  incrementCounter = () => {
+    this.setState(prevState => ({
+      counter: prevState.counter + 1
+    }));
+  };
+
+  decrementCounter = () => {
+    this.setState(prevState => ({
+      counter: prevState.counter - 1
+    }));
   };
 
   componentDidMount() {
@@ -17,48 +68,98 @@ export class ButtonDemo extends React.Component {
 
   render() {
     return (
-      <React.Fragment>
-        <a href={href}>
+      <div className="btn-demo-area">
+        <Button {...this.normalButton} id="normal-btn-1" variant="primary">
+          Primary
+        </Button>
+        <Button {...this.normalButton} id="normal-btn-2" variant="secondary">
+          Secondary
+        </Button>
+        <Button {...this.normalButton} id="normal-btn-3" variant="tertiary">
+          Tertiary
+        </Button>
+        <Button {...this.normalButton} id="normal-btn-4" variant="danger">
+          Danger
+        </Button>
+        <Button {...this.normalButton} id="normal-btn-5" variant="link">
+          Link
+        </Button>
+        <Button {...this.normalButton} id="normal-btn-6" variant="plain">
+          Plain
+        </Button>
+        <Button {...this.normalButton} id="normal-btn-7" variant="control">
+          Control
+        </Button>
+        <Button {...this.normalButton} id="normal-btn-8" isDisabled>
+          Disabled button
+        </Button>
+        <Button {...this.normalButton} id="normal-btn-9" isAriaDisabled>
+          Aria-disabled button
+        </Button>
+        <Button {...this.normalButton} id="normal-btn-10" tabIndex={2}>
+          Button with tabindex set to 2
+        </Button>
+        <Tooltip content="This tooltip content is available to the aria-disabled button">
           <Button
-            component={this.myButtonProps.component}
-            href={this.myButtonProps.href}
-            target={this.myButtonProps.target}
-            variant="primary"
+            {...this.normalButton}
+            id="normal-btn-11"
+            isAriaDisabled
+            onKeyPress={() => {
+              window.location.href = 'https://github.com/patternfly/patternfly-react';
+            }}
           >
-            Link to Core Docs
-          </Button>{' '}
-        </a>
-        <a href={href}>
-          <Button
-            component={this.myButtonProps.component}
-            href={this.myButtonProps.href}
-            target={this.myButtonProps.target}
-            variant="secondary"
-          >
-            Secondary Link to Core Docs
-          </Button>{' '}
-        </a>
+            Aria-disabled with tooltip
+          </Button>
+        </Tooltip>
+
         <Button
-          component={this.myButtonProps.component}
-          isDisabled
-          href={this.myButtonProps.href}
-          target={this.myButtonProps.target}
-          variant="tertiary"
+          id="aria-disabled-btn-1"
+          onBlur={this.decrementCounter}
+          onFocus={this.decrementCounter}
+          onClick={this.incrementCounter}
+          onKeyPress={this.incrementCounter}
+          isAriaDisabled
+          inoperableEvents={['onFocus']}
         >
-          Tertiary Link to Core Docs
+          Aria-disabled link button with custom list of prevented events {this.state.counter}
         </Button>
-        <Button
-          component={this.myButtonProps.component}
-          href={this.myButtonProps.href}
-          target={this.myButtonProps.target}
-          variant="control"
-        >
-          Control Button
+
+        <hr className="pf-u-m-md" />
+        <Button {...this.linkButton} id="link-btn-1">
+          Link button
         </Button>
-        <Button variant="link" tabIndex={-1}>
-          Button with tab index set to -1
+        <Button {...this.linkButton} id="link-btn-2" isInline>
+          Inline Link Button
         </Button>
-      </React.Fragment>
+        <Button {...this.linkButton} id="link-btn-3" isDisabled>
+          Disabled link button
+        </Button>
+        <Button {...this.linkButton} id="link-btn-4" isAriaDisabled>
+          Aria-disabled link button
+        </Button>
+        <Button {...this.linkButton} id="link-btn-5" tabIndex={-1}>
+          Button with tabindex set to -1
+        </Button>
+
+        <hr className="pf-u-m-md" />
+        <Button {...this.linkAsButton} id="link-as-btn-1">
+          Link as button
+        </Button>
+        <Button {...this.linkAsButton} id="link-as-btn-2" isDisabled>
+          Disabled link as button
+        </Button>
+        <Button {...this.linkAsButton} id="link-as-btn-3" isAriaDisabled>
+          Aria-disabled link as button
+        </Button>
+        <Button {...this.linkAsButton} id="link-as-btn-4" tabIndex={4}>
+          Link as button with tabindex set to 4
+        </Button>
+        <Tooltip content="This tooltip content is available to the aria-disabled link as button">
+          <Button {...this.linkAsButton} id="link-as-btn-5" isAriaDisabled>
+            Aria-disabled link as button with tooltip
+          </Button>
+        </Tooltip>
+      </div>
     );
   }
 }
