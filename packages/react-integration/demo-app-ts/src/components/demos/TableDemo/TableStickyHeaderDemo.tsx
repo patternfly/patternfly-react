6a58f1f91a6c4cde53be562b6e740e6fe33a8b33
Eugenia <32821331+jenny-s51@users.noreply.github.com>
2020-06-25 11:58:45 -0400

    feat(Table): adds sticky header prop and demo (#4417)
    * adds sticky header prop and table demo
    
    * adding integration demo and cypress test, bump version
    
    * update snapshots
    
    * fix conflict
    
    * fix linting errors
    
    * add displayName
    
    * yarn.locl
    
    * update snapshots
    
    * update yarn.lock

---
+++ packages/react-integration/demo-app-ts/src/components/demos/TableDemo/TableStickyHeaderDemo.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { Table, TableHeader, TableBody, TableProps } from '@patternfly/react-table';
+
+interface TableState {
+  page: number;
+  perPage: number;
+  res: any;
+}
+
+export class TableStickyHeaderDemo extends React.Component<TableProps, TableState> {
+  static displayName = 'TableStickyHeaderDemo';
+  constructor(props: TableProps) {
+    super(props);
+    this.state = {
+      res: [],
+      perPage: 0,
+      page: 0
+    };
+  }
+
+  fetch(page: number, perPage: number) {
+    fetch(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${perPage}`)
+      .then(resp => resp.json())
+      .then(resp => this.setState({ res: resp, perPage, page }))
+      .catch(err => this.setState({ perPage: 0, page: 0 }));
+  }
+
+  componentDidMount() {
+    this.fetch(this.state.page || 1, this.state.perPage || 20);
+  }
+
+  render() {
+    const { res } = this.state;
+
+    return (
+      <React.Fragment>
+        {
+          <Table
+            caption="Sticky Header Table"
+            isStickyHeader
+            cells={['Title', 'Body']}
+            rows={res.map((post: { title: any; body: any }) => [post.title, post.body])}
+            aria-label="Sticky Header Table Demo"
+          >
+            <TableHeader />
+            <TableBody />
+          </Table>
+        }
+      </React.Fragment>
+    );
+  }
+}
