2e2b6f877330d878fede9f64cb3ce7779c0fb805
redallen <zallen@redhat.com>
2020-07-08 17:02:07 -0400

    fix token path

--- packages/react-core/src/components/Drawer/Drawer.tsx
+++ packages/react-core/src/components/Drawer/Drawer.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import styles from '@patternfly/react-styles/css/components/Drawer/drawer';
 import { css } from '@patternfly/react-styles';
-import transitionDuration from '@patternfly/react-tokens/c_drawer__panel_TransitionDuration/c_drawer__panel_TransitionDuration/dist/js/c_drawer__panel_TransitionDuration';
+import transitionDuration from '@patternfly/react-tokens/dist/js/c_drawer__panel_TransitionDuration';
 
 export interface DrawerProps extends React.HTMLProps<HTMLDivElement> {
   /** Additional classes added to the Drawer. */
