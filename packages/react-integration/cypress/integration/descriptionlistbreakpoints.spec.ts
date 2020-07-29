b3ded89075848266b10ddd579ce819a2eaee4b28
jenny-s51 <jshandel12@gmail.com>
2020-07-29 13:21:45 -0400

    clean up breakpoints test file

--- packages/react-integration/cypress/integration/descriptionlistbreakpoints.spec.ts
+++ packages/react-integration/cypress/integration/descriptionlistbreakpoints.spec.ts
@@ -28,25 +28,4 @@ describe('Description List Breakpoints Demo Test', () => {
     cy.get('#3-col-description-list.pf-m-3-col-on-xl').should('exist');
     cy.get('#3-col-description-list.pf-m-3-col-on-2xl').should('exist');
   });
-
-//   it('Verify 1 Col Description List', () => {
-//     cy.get('.pf-c-description-list[id="1-col-description-list"]').should(
-//       'have.class',
-//       'pf-m-1-col pf-m-1-col-on-md pf-m-1-col-on-lg pf-m-1-col-on-xl pf-m-1-col-on-2xl'
-//     );
-//   });
-
-//   it('Verify 2 Col Description List', () => {
-//     cy.get('.pf-c-description-list[id="2-col-description-list"]').should(
-//       'have.class',
-//       'pf-m-2-col pf-m-2-col-on-md pf-m-2-col-on-lg pf-m-2-col-on-xl pf-m-2-col-on-2xl'
-//     );
-//   });
-
-//   it('Verify 3 Col Description List', () => {
-//     cy.get('.pf-c-description-list[id="3-col-description-list"]').should(
-//       'have.class',
-//       'pf-m-3-col pf-m-3-col-on-md pf-m-3-col-on-lg pf-m-3-col-on-xl pf-m-3-col-on-2xl'
-//     );
-//   });
 });
