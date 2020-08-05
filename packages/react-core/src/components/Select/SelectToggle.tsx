3635f26520af063a926c836c028d8dc896813415
kmcfaul <45077788+kmcfaul@users.noreply.github.com>
2020-08-05 13:44:50 -0400

    feat(Select): fix space/enter closing typeahead variants incorrectly (#4663)
    * feat(Select): prevent typeahead variants from reaching other variant open/close logic in global key
    
    * feat(Select): fix integration
    
    * feat(Select): fix integration ids

--- packages/react-core/src/components/Select/SelectToggle.tsx
+++ packages/react-core/src/components/Select/SelectToggle.tsx
@@ -135,6 +135,8 @@ export class SelectToggle extends React.Component<SelectToggleProps> {
     }
 
     if (
+      variant === SelectVariant.typeahead ||
+      variant === SelectVariant.typeaheadMulti ||
       (event.key === KeyTypes.Tab && variant === SelectVariant.checkbox) ||
       (event.key === KeyTypes.Tab && !isOpen) ||
       (event.key !== KeyTypes.Enter && event.key !== KeyTypes.Space)
