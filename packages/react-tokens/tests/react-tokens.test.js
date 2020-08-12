7955a8a32245443918c7bf7a43a03b551988bc0e
redallen <zallen@redhat.com>
2020-08-12 14:59:22 -0400

    fix lint

--- packages/react-tokens/tests/react-tokens.test.js
+++ packages/react-tokens/tests/react-tokens.test.js
@@ -2,6 +2,6 @@ const reactTokens = require('@patternfly/react-tokens');
 
 // Test importing CJS tokens
 test('CJS token', () => {
-  const { global_palette_green_500 } = reactTokens;
-  expect(global_palette_green_500.value).toBeTruthy();
+  const { global_palette_green_500: green } = reactTokens;
+  expect(green.value).toBeTruthy();
 });
