6d85b07b5eb0098f54fe3e0d544df9b83de23f02
Jessie <jhuff@redhat.com>
2020-09-16 10:26:03 -0400

    Update skip to content to support component prop

--- packages/react-core/src/components/SkipToContent/SkipToContent.tsx
+++ packages/react-core/src/components/SkipToContent/SkipToContent.tsx
@@ -6,21 +6,24 @@ import { PickOptional } from '../../helpers/typeUtils';
 
 export interface SkipToContentProps extends React.HTMLProps<HTMLAnchorElement> {
   /** The skip to content link. */
-  href: string;
+  href?: string;
   /** Content to display within the skip to content component, typically a string. */
   children?: React.ReactNode;
   /** Additional styles to apply to the skip to content component. */
   className?: string;
   /** Forces the skip to content to display. This is primarily for demonstration purposes and would not normally be used. */
   show?: boolean;
+  /** Component used to render skip to content link */
+  component?: React.ReactNode;
 }
 
 export class SkipToContent extends React.Component<SkipToContentProps> {
   static displayName = 'SkipToContent';
   static defaultProps: PickOptional<SkipToContentProps> = {
-    show: false
+    show: false,
+    component: 'a'
   };
-  componentRef = React.createRef<HTMLAnchorElement>();
+  componentRef = React.createRef<any>();
 
   componentDidMount() {
     if (this.props.show && this.componentRef.current) {
@@ -30,16 +33,26 @@ export class SkipToContent extends React.Component<SkipToContentProps> {
 
   render() {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { children, className, href, show, type, ...rest } = this.props;
-    return (
-      <a
+    const { children, className, component, href, show, type, ...rest } = this.props;
+    const Component = component as any;
+
+    const renderDefaultLink = (): React.ReactNode => (
+      <Component
         {...rest}
         className={css(buttonStyles.button, buttonStyles.modifiers.primary, styles.skipToContent, className)}
         ref={this.componentRef}
         href={href}
       >
         {children}
-      </a>
+      </Component>
     );
+
+    const renderClonedChild = (child: React.ReactElement): React.ReactNode =>
+      React.cloneElement(child, {
+        className: css(buttonStyles.button, buttonStyles.modifiers.primary, styles.skipToContent, className),
+        ref: this.componentRef
+      });
+
+    return React.isValidElement(children) ? renderClonedChild(children as React.ReactElement) : renderDefaultLink();
   }
 }
