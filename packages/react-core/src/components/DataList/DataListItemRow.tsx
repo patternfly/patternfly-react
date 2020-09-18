e80f08e2d06ab2d2fe5f400748c442629af5f5db
Katerina Koukiou <kkoukiou@redhat.com>
2020-09-18 14:21:21 +0200

    feat(DataList): add support for text modifiers
    Follow the pattern of the Table component for exposing the text
    modifiers.
    
    Closes #4822

--- packages/react-core/src/components/DataList/DataListItemRow.tsx
+++ packages/react-core/src/components/DataList/DataListItemRow.tsx
@@ -1,20 +1,23 @@
 import * as React from 'react';
 import { css } from '@patternfly/react-styles';
 import styles from '@patternfly/react-styles/css/components/DataList/data-list';
+import { WrapModifier } from './DataList';
 
 export interface DataListItemRowProps extends Omit<React.HTMLProps<HTMLDivElement>, 'children'> {
   children: React.ReactNode;
   className?: string;
   rowid?: string;
+  wrapModifier?: WrapModifier | 'nowrap' | 'truncate' | 'breakWord';
 }
 
 export const DataListItemRow: React.FunctionComponent<DataListItemRowProps> = ({
   children,
   className = '',
   rowid = '',
+  wrapModifier = null,
   ...props
 }: DataListItemRowProps) => (
-  <div className={css(styles.dataListItemRow, className)} {...props}>
+  <div className={css(styles.dataListItemRow, className, wrapModifier && styles.modifiers[wrapModifier])} {...props}>
     {React.Children.map(
       children,
       child =>
