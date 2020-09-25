59dbde8a5b70ebd6f64dcafe73754581f31ead6a
nicolethoen <nthoen@redhat.com>
2020-09-25 11:44:12 -0400

    fix build

--- packages/react-date-time/src/components/DatePicker/__tests__/DatePicker.test.tsx
+++ packages/react-date-time/src/components/DatePicker/__tests__/DatePicker.test.tsx
@@ -3,6 +3,6 @@ import { DatePicker } from '../DatePicker';
 import React from 'react';
 
 test('disabled date picker', () => {
-  const view = mount(<DatePicker isDisabled aria-label="disabled text input" />);
+  const view = mount(<DatePicker isDisabled aria-label="disabled date picker" />);
   expect(view.find('input')).toMatchSnapshot();
 });
