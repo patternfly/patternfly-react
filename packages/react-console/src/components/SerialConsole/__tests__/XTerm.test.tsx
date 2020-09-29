a60e61e9e541ca7693e1a774c3c753f944ee4a9a
Katerina Koukiou <kkoukiou@redhat.com>
2020-09-29 13:58:03 +0200

    Add jest SerialConsole snapshot tests and generated snapshots

---
+++ packages/react-console/src/components/SerialConsole/__tests__/XTerm.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'enzyme';
+import { XTerm } from '../XTerm';
+
+test('Render empty XTerm component', () => {
+  const view = render(<XTerm cols={15} rows={60} onTitleChanged={jest.fn()} onData={jest.fn()} onResize={jest.fn()} />);
+  expect(view).toMatchSnapshot();
+});
