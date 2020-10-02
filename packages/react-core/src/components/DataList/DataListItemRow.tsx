55a88d7db52726cef64a4823d224b0821e0eaa99
Katerina Koukiou <kkoukiou@redhat.com>
2020-10-02 15:44:56 +0200

    Add missing property documentation on DataListItemRow

--- packages/react-core/src/components/DataList/DataListItemRow.tsx
+++ packages/react-core/src/components/DataList/DataListItemRow.tsx
@@ -4,9 +4,13 @@ import styles from '@patternfly/react-styles/css/components/DataList/data-list';
 import { DataListWrapModifier } from './DataList';
 
 export interface DataListItemRowProps extends Omit<React.HTMLProps<HTMLDivElement>, 'children'> {
+  /** Content rendered inside the DataListItemRow  */
   children: React.ReactNode;
+  /** Additional classes added to the DataListItemRow */
   className?: string;
+  /** Id for the row item */
   rowid?: string;
+  /** Determines which wrapping modifier to apply to the DataListItemRow */
   wrapModifier?: DataListWrapModifier | 'nowrap' | 'truncate' | 'breakWord';
 }
 
