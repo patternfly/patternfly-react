2b0b204321aa5c82fb8442d1341ad169c826698b
Katerina Koukiou <kkoukiou@redhat.com>
2020-10-09 20:47:31 +0200

    Fix typescript complaining about missing default export when importing React (#4938)
    All the remaining places where we use `import React from 'react'` are
    either example code which is not typescript or tests code.
    
    Fixes #4936

--- packages/react-charts/src/components/ChartCursorTooltip/ChartCursorFlyout.tsx
+++ packages/react-charts/src/components/ChartCursorTooltip/ChartCursorFlyout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import PropTypes from 'prop-types';
 import { Helpers, CommonProps, Path } from 'victory-core';
 import isPlainObject from 'lodash/isPlainObject';
