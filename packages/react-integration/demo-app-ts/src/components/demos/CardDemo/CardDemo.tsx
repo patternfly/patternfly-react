f942a29c8a152d06164ee6601f6d301077698ec6
Michael Spaxman <seanforyou23@gmail.com>
2020-10-29 23:33:30 -0400

    chore(card): add keyboard interaction a11y test, clean up docs

--- packages/react-integration/demo-app-ts/src/components/demos/CardDemo/CardDemo.tsx
+++ packages/react-integration/demo-app-ts/src/components/demos/CardDemo/CardDemo.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import { Card, CardTitle, CardBody, CardFooter } from '@patternfly/react-core';
+interface CardDemoState {
+  selected: string;
+}
 
 export class CardDemo extends React.Component {
   static displayName = 'CardDemo';
+
+  state: CardDemoState = {
+    selected: null
+  };
+
+  onKeyDown = (event: any) => {
+    if (event.target !== event.currentTarget) {
+      return;
+    }
+    if ([13, 32].includes(event.keyCode)) {
+      const newSelected = event.currentTarget.id === this.state.selected ? null : event.currentTarget.id;
+      this.setState({
+        selected: newSelected
+      });
+    }
+  };
+
   componentDidMount() {
     window.scrollTo(0, 0);
   }
@@ -36,7 +56,18 @@ export class CardDemo extends React.Component {
           <CardFooter>Footer</CardFooter>
         </Card>
         <br></br>
-        <Card isFlat>
+        <Card id="flatCard" isFlat>
+          <CardTitle>Header</CardTitle>
+          <CardBody>Body</CardBody>
+          <CardFooter>Footer</CardFooter>
+        </Card>
+
+        <Card
+          id="selectableCard"
+          isSelectable
+          isSelected={this.state.selected === 'selectableCard'}
+          onKeyDown={this.onKeyDown}
+        >
           <CardTitle>Header</CardTitle>
           <CardBody>Body</CardBody>
           <CardFooter>Footer</CardFooter>
