bf46baddcb1d46da43e2f76ae3838908f6cbaaf7
redallen <zallen@redhat.com>
2020-11-11 16:18:25 -0500

    add integration tests

--- packages/react-integration/cypress/integration/jumplinkdemo.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('Jump Link Demo Test', () => {
-  it('Navigate to demo section', () => {
-    cy.visit('http://localhost:3000/');
-    cy.get('#jump-links-demo-nav-item-link').click();
-    cy.url().should('eq', 'http://localhost:3000/jump-links-demo-nav-link');
-  });
-});
