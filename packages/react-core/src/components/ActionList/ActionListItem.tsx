8408dd10b67233c94eaed34c364ef32f90f194f9
redallen <zallen@redhat.com>
2020-11-13 10:38:49 -0500

    bump core, fix react-console version

--- packages/react-core/src/components/ActionList/ActionListItem.tsx
+++ packages/react-core/src/components/ActionList/ActionListItem.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import { css } from '@patternfly/react-styles';
-import styles from '@patternfly/react-styles/css/components/ActionList/action-list';
 
 export interface ActionListItemProps extends React.HTMLProps<HTMLDivElement> {
   /** Children of the action list item */
@@ -10,9 +8,5 @@ export interface ActionListItemProps extends React.HTMLProps<HTMLDivElement> {
 export const ActionListItem: React.FunctionComponent<ActionListItemProps> = ({
   children,
   ...props
-}: ActionListItemProps) => (
-  <div className={css(styles.actionListItem)} {...props}>
-    {children}
-  </div>
-);
+}: ActionListItemProps) => <div {...props}>{children}</div>;
 ActionListItem.displayName = 'ActionListItem';
