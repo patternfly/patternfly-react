30d714cd786aaadcbcf191a3e4678197810c8e33
Joel <joeldmyers@gmail.com>
2021-01-11 20:34:08 +0100

    Adding demo and integration test for Chip Group with custom overflow chip event handler

---
+++ packages/react-integration/demo-app-ts/src/components/demos/ChipGroupDemo/ChipGroupWithOverflowChipEventHandler.tsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { Chip, ChipGroup } from '@patternfly/react-core';
+
+interface ChipGroupWithOverflowChipEventHandlerState {
+  chipArray: {
+    name: string;
+  }[];
+  shouldShowAdditionalText: boolean;
+}
+
+export class ChipGroupWithOverflowChipEventHandler extends Component<{}, ChipGroupWithOverflowChipEventHandlerState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      chipArray: [
+        {
+          name: 'Lemons'
+        },
+        {
+          name: 'Limes'
+        },
+        {
+          name: 'Peaches'
+        },
+        {
+          name: 'Pears'
+        },
+        {
+          name: 'Tangerines'
+        }
+      ],
+      shouldShowAdditionalText: false
+    };
+    this.handleOverflowChipClick = this.handleOverflowChipClick.bind(this);
+  }
+
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+
+  handleOverflowChipClick() {
+    this.setState({ shouldShowAdditionalText: !this.state.shouldShowAdditionalText });
+  }
+
+  render() {
+    const { chipArray } = this.state;
+    return (
+      <>
+        <ChipGroup onOverflowChipClick={() => this.handleOverflowChipClick()}>
+          {chipArray.map(chip => (
+            <Chip key={chip.name}>{chip.name}</Chip>
+          ))}
+        </ChipGroup>
+        {this.state.shouldShowAdditionalText && (
+          <h1 className="pf-c-title pf-m-2xl pf-u-p-lg">Full results are currently expanded.</h1>
+        )}
+      </>
+    );
+  }
+}
