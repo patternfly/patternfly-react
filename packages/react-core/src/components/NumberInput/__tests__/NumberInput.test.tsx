57ff7d76bc8d9070165e048caa05e8c2590991f8
Titani <tlabaj@redaht.com>
2021-01-18 11:53:46 -0500

    (Number input): Renamed Touchspin to NumberInput

--- packages/react-core/src/components/NumberInput/__tests__/NumberInput.test.tsx
+++ packages/react-core/src/components/NumberInput/__tests__/NumberInput.test.tsx
@@ -1,56 +1,56 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import { Touchspin } from '../Touchspin';
+import { NumberInput } from '../NumberInput';
 
-describe('touchspin', () => {
+describe('numberInput', () => {
   test('renders defaults & extra props', () => {
-    const view = mount(<Touchspin className="custom" id="touchspin1" />);
+    const view = mount(<NumberInput className="custom" id="numberInput1" />);
     expect(view).toMatchSnapshot();
   });
 
   test('renders value', () => {
-    const view = mount(<Touchspin value={90} />);
+    const view = mount(<NumberInput value={90} />);
     expect(view).toMatchSnapshot();
   });
 
   test('renders disabled', () => {
-    const view = mount(<Touchspin value={90} isDisabled />);
+    const view = mount(<NumberInput value={90} isDisabled />);
     expect(view).toMatchSnapshot();
   });
 
   test('disables lower threshold', () => {
-    const view = mount(<Touchspin value={0} min={0} />);
+    const view = mount(<NumberInput value={0} min={0} />);
     expect(view).toMatchSnapshot();
   });
 
   test('disables upper threshold', () => {
-    const view = mount(<Touchspin value={100} max={100} />);
+    const view = mount(<NumberInput value={100} max={100} />);
     expect(view).toMatchSnapshot();
   });
 
   test('renders unit', () => {
-    const view = mount(<Touchspin value={5} unit="%" unitPosition="after" />);
+    const view = mount(<NumberInput value={5} unit="%" unitPosition="after" />);
     expect(view).toMatchSnapshot();
   });
 
   test('renders unit & position', () => {
-    const view = mount(<Touchspin value={5} unit="$" unitPosition="before" />);
+    const view = mount(<NumberInput value={5} unit="$" unitPosition="before" />);
     expect(view).toMatchSnapshot();
   });
 
   test('renders custom width', () => {
-    const view = mount(<Touchspin value={5} widthChars="10" />);
+    const view = mount(<NumberInput value={5} widthChars="10" />);
     expect(view).toMatchSnapshot();
   });
 
   test('passes input props successfully', () => {
-    const view = mount(<Touchspin value={5} onChange={jest.fn()} inputName="test-name" />);
+    const view = mount(<NumberInput value={5} onChange={jest.fn()} inputName="test-name" />);
     expect(view).toMatchSnapshot();
   });
 
   test('passes button props successfully', () => {
     const view = mount(
-      <Touchspin
+      <NumberInput
         value={5}
         onMinus={jest.fn()}
         minusBtnProps={{ id: 'minus-id' }}
