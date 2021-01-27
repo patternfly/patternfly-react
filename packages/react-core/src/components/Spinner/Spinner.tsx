95798f91bba78939d7f50f755737cb39f8c268dc
redallen <zallen@redhat.com>
2021-01-27 16:07:57 -0500

    fix lint

--- packages/react-core/src/components/Spinner/Spinner.tsx
+++ packages/react-core/src/components/Spinner/Spinner.tsx
@@ -31,25 +31,27 @@ export const Spinner: React.FunctionComponent<SpinnerProps> = ({
   diameter,
   ...props
 }: SpinnerProps) => {
-  const Component = isSVG ? 'svg' : 'span' as any;
+  const Component = isSVG ? 'svg' : ('span' as any);
 
   return (
     <Component
       className={css(styles.spinner, styles.modifiers[size], className)}
       role="progressbar"
       aria-valuetext={ariaValueText}
-      {...isSVG && { viewBox: "0 0 100 100" }}
-      {...isSVG && diameter && { style: { '--pf-c-spinner--diameter': diameter } }}
+      {...(isSVG && { viewBox: '0 0 100 100' })}
+      {...(isSVG && diameter && { style: { '--pf-c-spinner--diameter': diameter } })}
       {...props}
     >
-      {isSVG ?
-      <circle className={styles.spinnerPath} cx="50" cy="50" r="45" fill="none" />
-      : <React.Fragment>
-      <span className={css(styles.spinnerClipper)} />
-      <span className={css(styles.spinnerLeadBall)} />
-      <span className={css(styles.spinnerTailBall)} />
-      </React.Fragment>}
+      {isSVG ? (
+        <circle className={styles.spinnerPath} cx="50" cy="50" r="45" fill="none" />
+      ) : (
+        <React.Fragment>
+          <span className={css(styles.spinnerClipper)} />
+          <span className={css(styles.spinnerLeadBall)} />
+          <span className={css(styles.spinnerTailBall)} />
+        </React.Fragment>
+      )}
     </Component>
   );
-}
+};
 Spinner.displayName = 'Spinner';
