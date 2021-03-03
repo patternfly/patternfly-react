c6bc19549f98f830069686a5158c4df1e3704b06
nicolethoen <nthoen@redhat.com>
2021-03-03 17:44:04 -0500

    add integration test

--- packages/react-integration/cypress/integration/searchinput.spec.ts
+++ packages/react-integration/cypress/integration/searchinput.spec.ts
@@ -13,74 +13,84 @@ describe('Search Input Demo Test', () => {
     cy.focused().blur();
   });
 
+  it('Disabled search input is disabled', () => {
+    cy.get('#disabled-search .pf-c-search-input__text-input').should('be.disabled');
+    cy.get('#disabled-search.pf-c-search-input button')
+      .eq(0)
+      .should('be.disabled');
+    cy.get('#disabled-search.pf-c-search-input button')
+      .eq(1)
+      .should('be.disabled');
+  });
+
   it('Verify search input and its handlers work', () => {
-    cy.get('.pf-c-search-input__count').should('not.exist');
-    cy.get('.pf-c-search-input__clear').should('not.exist');
-    cy.get('.pf-c-search-input__nav').should('not.exist');
+    cy.get('#enabled-search .pf-c-search-input__count').should('not.exist');
+    cy.get('#enabled-search .pf-c-search-input__clear').should('not.exist');
+    cy.get('#enabled-search .pf-c-search-input__nav').should('not.exist');
 
-    cy.get('.pf-c-search-input__text-input').type('Hello world');
-    cy.get('.pf-c-search-input__text-input').should('have.value', 'Hello world');
+    cy.get('#enabled-search .pf-c-search-input__text-input').type('Hello world');
+    cy.get('#enabled-search .pf-c-search-input__text-input').should('have.value', 'Hello world');
 
-    cy.get('.pf-c-search-input__count').should('be.visible');
-    cy.get('.pf-c-search-input__clear').should('be.visible');
-    cy.get('.pf-c-search-input__nav').should('be.visible');
+    cy.get('#enabled-search .pf-c-search-input__count').should('be.visible');
+    cy.get('#enabled-search .pf-c-search-input__clear').should('be.visible');
+    cy.get('#enabled-search .pf-c-search-input__nav').should('be.visible');
 
-    cy.get('.pf-c-badge').should('have.text', '1 / 3');
-    cy.get('.pf-c-search-input__nav button')
+    cy.get('#enabled-search .pf-c-badge').should('have.text', '1 / 3');
+    cy.get('#enabled-search .pf-c-search-input__nav button')
       .last()
       .click();
-    cy.get('.pf-c-badge').should('have.text', '2 / 3');
-    cy.get('.pf-c-search-input__nav button')
+    cy.get('#enabled-search .pf-c-badge').should('have.text', '2 / 3');
+    cy.get('#enabled-search .pf-c-search-input__nav button')
       .first()
       .click();
-    cy.get('.pf-c-badge').should('have.text', '1 / 3');
+    cy.get('#enabled-search .pf-c-badge').should('have.text', '1 / 3');
 
-    cy.get('.pf-c-search-input__clear').click();
-    cy.get('.pf-c-search-input__text-input').should('not.have.value', 'Hello world');
+    cy.get('#enabled-search .pf-c-search-input__clear').click();
+    cy.get('#enabled-search .pf-c-search-input__text-input').should('not.have.value', 'Hello world');
 
-    cy.get('.pf-c-search-input__count').should('not.exist');
-    cy.get('.pf-c-search-input__clear').should('not.exist');
-    cy.get('.pf-c-search-input__nav').should('not.exist');
+    cy.get('#enabled-search .pf-c-search-input__count').should('not.exist');
+    cy.get('#enabled-search .pf-c-search-input__clear').should('not.exist');
+    cy.get('#enabled-search .pf-c-search-input__nav').should('not.exist');
   });
 
   it('Verify advanced search and its handlers work', () => {
-    cy.get('.pf-c-search-input__menu').should('not.exist');
-    cy.get('.pf-c-search-input button')
+    cy.get('#enabled-search .pf-c-search-input__menu').should('not.exist');
+    cy.get('#enabled-search.pf-c-search-input button')
       .eq(0)
       .click();
-    cy.get('.pf-c-search-input__menu').should('be.visible');
+    cy.get('#enabled-search .pf-c-search-input__menu').should('be.visible');
 
-    cy.get('.pf-c-search-input__menu input')
+    cy.get('#enabled-search .pf-c-search-input__menu input')
       .eq(0)
       .type('test');
-    cy.get('.pf-c-search-input__text-input').should('have.value', 'username:test');
+    cy.get('#enabled-search .pf-c-search-input__text-input').should('have.value', 'username:test');
 
-    cy.get('.pf-c-search-input__text-input').type(' firstname:hi another test');
-    cy.get('.pf-c-search-input__menu input')
+    cy.get('#enabled-search .pf-c-search-input__text-input').type(' firstname:hi another test');
+    cy.get('#enabled-search .pf-c-search-input__menu input')
       .eq(1)
       .should('have.value', 'hi');
-    cy.get('.pf-c-search-input__menu input')
+    cy.get('#enabled-search .pf-c-search-input__menu input')
       .eq(2)
       .should('have.value', 'another test');
 
-    cy.get('.pf-c-form__actions button')
+    cy.get('#enabled-search .pf-c-form__actions button')
       .eq(1)
       .click();
-    cy.get('.pf-c-search-input__text-input').should('have.value', '');
-    cy.get('.pf-c-search-input__menu input')
+    cy.get('#enabled-search .pf-c-search-input__text-input').should('have.value', '');
+    cy.get('#enabled-search .pf-c-search-input__menu input')
       .eq(1)
       .should('have.value', '');
-    cy.get('.pf-c-search-input__menu input')
+    cy.get('#enabled-search .pf-c-search-input__menu input')
       .eq(2)
       .should('have.value', '');
 
-    cy.get('.pf-c-search-input__menu input')
+    cy.get('#enabled-search .pf-c-search-input__menu input')
       .eq(0)
       .type('test');
-    cy.get('.pf-c-form__actions button')
+    cy.get('#enabled-search .pf-c-form__actions button')
       .eq(0)
       .click();
-    cy.get('.pf-c-search-input__menu').should('not.be.visible');
-    cy.get('.pf-c-search-input__text-input').should('have.value', 'username:test');
+    cy.get('#enabled-search .pf-c-search-input__menu').should('not.be.visible');
+    cy.get('#enabled-search .pf-c-search-input__text-input').should('have.value', 'username:test');
   });
 });
