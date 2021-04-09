a97fc269373832b1dc6ff033d0cf7858fe01337e
Jessie <jhuff@redhat.com>
2021-04-09 15:45:52 -0400

    fix(Wizard): disable back button on first step for keyboard users (#5623)
    * Add disabled attribute to back button on first step
    
    * Update snapshots
    
    * Simplify logic
    
    * Remove unnecessary classname

--- packages/react-core/src/components/Wizard/WizardFooterInternal.tsx
+++ packages/react-core/src/components/Wizard/WizardFooterInternal.tsx
@@ -32,7 +32,7 @@ export const WizardFooterInternal: React.FunctionComponent<WizardFooterInternalP
       {nextButtonText}
     </Button>
     {!activeStep.hideBackButton && (
-      <Button variant={ButtonVariant.secondary} onClick={onBack} className={css(firstStep && 'pf-m-disabled')}>
+      <Button variant={ButtonVariant.secondary} onClick={onBack} isDisabled={firstStep}>
         {backButtonText}
       </Button>
     )}
