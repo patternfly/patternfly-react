b02ad91df49eb35cdcf9bb17276a7965bbddabcb
Titani <tlabaj@redaht.com>
2021-04-22 16:21:07 -0400

    chore(integration test): added delay to table sticky header inegration test

--- packages/react-integration/cypress/integration/tablestickyheader.spec.ts
+++ packages/react-integration/cypress/integration/tablestickyheader.spec.ts
@@ -10,6 +10,7 @@ describe('Table Simple Test', () => {
   });
 
   it('Check number of rows', () => {
+    cy.wait(1000);
     cy.get('.pf-c-table')
       .find('tr')
       .should('have.length', 21);
