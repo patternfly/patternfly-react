a73d6d6a18761529afc22f50cff7490f368c178b
Katie McFaul <kmcfaul@redhat.com>
2021-04-27 14:53:53 -0400

    update tests/snaps from forwardref

--- packages/react-core/src/components/MenuToggle/__tests__/MenuToggle.test.tsx
+++ packages/react-core/src/components/MenuToggle/__tests__/MenuToggle.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { MenuToggle } from '../MenuToggle';
 import { Badge } from '../../Badge';
 import CogIcon from '@patternfly/react-icons/dist/js/icons/cog-icon';
@@ -7,32 +7,32 @@ import EllipsisVIcon from '@patternfly/react-icons/dist/js/icons/ellipsis-v-icon
 
 describe('menu toggle', () => {
   test('renders successfully', () => {
-    const view = shallow(<MenuToggle>Toggle</MenuToggle>);
+    const view = mount(<MenuToggle>Toggle</MenuToggle>);
     expect(view).toMatchSnapshot();
   });
 
   test('passes additional classes', () => {
-    const view = shallow(<MenuToggle className="test-class">Toggle</MenuToggle>);
+    const view = mount(<MenuToggle className="test-class">Toggle</MenuToggle>);
     expect(view).toMatchSnapshot();
   });
 
   test('shows isExpanded', () => {
-    const view = shallow(<MenuToggle isExpanded>Toggle</MenuToggle>);
+    const view = mount(<MenuToggle isExpanded>Toggle</MenuToggle>);
     expect(view).toMatchSnapshot();
   });
 
   test('shows isDisabled', () => {
-    const view = shallow(<MenuToggle isDisabled>Toggle</MenuToggle>);
+    const view = mount(<MenuToggle isDisabled>Toggle</MenuToggle>);
     expect(view).toMatchSnapshot();
   });
 
   test('shows isPrimary', () => {
-    const view = shallow(<MenuToggle isPrimary>Toggle</MenuToggle>);
+    const view = mount(<MenuToggle isPrimary>Toggle</MenuToggle>);
     expect(view).toMatchSnapshot();
   });
 
   test('shows isPlain', () => {
-    const view = shallow(
+    const view = mount(
       <MenuToggle isPlain>
         <EllipsisVIcon />
       </MenuToggle>
@@ -41,12 +41,12 @@ describe('menu toggle', () => {
   });
 
   test('shows icon', () => {
-    const view = shallow(<MenuToggle icon={<CogIcon />}>Toggle</MenuToggle>);
+    const view = mount(<MenuToggle icon={<CogIcon />}>Toggle</MenuToggle>);
     expect(view).toMatchSnapshot();
   });
 
   test('shows badge', () => {
-    const view = shallow(<MenuToggle badge={<Badge>badge</Badge>}>Toggle</MenuToggle>);
+    const view = mount(<MenuToggle badge={<Badge>badge</Badge>}>Toggle</MenuToggle>);
     expect(view).toMatchSnapshot();
   });
 });
