336c447dd95f9759dd791032f52669ea2e97881f
Evan <evan.wilkinson@gmail.com>
2021-05-07 10:51:17 -0400

    feat(Card): add isToggleRightAligned prop (#5723)

--- packages/react-core/src/components/Card/CardHeader.tsx
+++ packages/react-core/src/components/Card/CardHeader.tsx
@@ -16,6 +16,8 @@ export interface CardHeaderProps extends React.HTMLProps<HTMLDivElement> {
   onExpand?: (event: React.MouseEvent, id: string) => void;
   /** Additional props for expandable toggle button */
   toggleButtonProps?: any;
+  /** Whether to right-align expandable toggle button */
+  isToggleRightAligned?: boolean;
 }
 
 export const CardHeader: React.FunctionComponent<CardHeaderProps> = ({
@@ -24,30 +26,40 @@ export const CardHeader: React.FunctionComponent<CardHeaderProps> = ({
   id,
   onExpand,
   toggleButtonProps,
+  isToggleRightAligned,
   ...props
 }: CardHeaderProps) => (
   <CardContext.Consumer>
-    {({ cardId }) => (
-      <div className={css(styles.cardHeader, className)} id={id} {...props}>
-        {onExpand && (
-          <div className={css(styles.cardHeaderToggle)}>
-            <Button
-              variant="plain"
-              type="button"
-              onClick={evt => {
-                onExpand(evt, cardId);
-              }}
-              {...toggleButtonProps}
-            >
-              <span className={css(styles.cardHeaderToggleIcon)}>
-                <AngleRightIcon aria-hidden="true" />
-              </span>
-            </Button>
-          </div>
-        )}
-        {children}
-      </div>
-    )}
+    {({ cardId }) => {
+      const cardHeaderToggle = (
+        <div className={css(styles.cardHeaderToggle)}>
+          <Button
+            variant="plain"
+            type="button"
+            onClick={evt => {
+              onExpand(evt, cardId);
+            }}
+            {...toggleButtonProps}
+          >
+            <span className={css(styles.cardHeaderToggleIcon)}>
+              <AngleRightIcon aria-hidden="true" />
+            </span>
+          </Button>
+        </div>
+      );
+
+      return (
+        <div
+          className={css(styles.cardHeader, isToggleRightAligned && styles.modifiers.toggleRight, className)}
+          id={id}
+          {...props}
+        >
+          {onExpand && !isToggleRightAligned && cardHeaderToggle}
+          {children}
+          {onExpand && isToggleRightAligned && cardHeaderToggle}
+        </div>
+      );
+    }}
   </CardContext.Consumer>
 );
 CardHeader.displayName = 'CardHeader';
