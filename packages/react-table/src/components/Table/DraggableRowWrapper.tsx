731119b1cd2ded6ce382cb79b322aff989473084
nicolethoen <nthoen@redhat.com>
2021-06-14 15:44:07 -0400

    most of it is there

---
+++ packages/react-table/src/components/Table/DraggableRowWrapper.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { css } from '@patternfly/react-styles';
+import styles from '@patternfly/react-styles/css/components/Table/table';
+import stylesTreeView from '@patternfly/react-styles/css/components/Table/table-tree-view';
+import { RowWrapperProps } from './RowWrapper';
+import { Tr } from '../TableComposable';
+
+export const DraggableRowWrapper: React.FunctionComponent<RowWrapperProps> = ({
+  className,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  rowProps,
+  row,
+  ...props
+}: RowWrapperProps) => {
+  const {
+    'aria-level': level,
+    'aria-posinset': posinset,
+    'aria-setsize': setsize,
+    isExpanded,
+    isDetailsExpanded,
+    isHidden
+  } = row.props;
+  return (
+    <Tr
+      aria-level={level}
+      aria-posinset={posinset}
+      aria-setsize={setsize}
+      aria-expanded={!!isExpanded}
+      isHidden={isHidden}
+      className={css(
+        className,
+        isExpanded && styles.modifiers.expanded,
+        isDetailsExpanded && stylesTreeView.modifiers.treeViewDetailsExpanded
+      )}
+      {...props}
+    />
+  );
+};
+DraggableRowWrapper.displayName = 'DraggableRowWrapper';
