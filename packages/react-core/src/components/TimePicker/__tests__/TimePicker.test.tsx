a5fcf76785772b11959be7cbb42cf02520d8a623
Luiz João Motta <luizjoaomotta@gmail.com>
2021-07-09 12:11:20 -0300

    fix(TimePicker): onInputChange is always set to valid (#5982)
    * fix(TimePicker): Use isValid function on onInputChange
    
    * fix(TimePicker): Add tests
    
    * fix(TimePicker): Fix tests

--- packages/react-core/src/components/TimePicker/__tests__/TimePicker.test.tsx
+++ packages/react-core/src/components/TimePicker/__tests__/TimePicker.test.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { mount } from 'enzyme';
 import { act } from 'react-dom/test-utils';
 import { TimePicker } from '../TimePicker'
@@ -40,5 +40,53 @@ describe('test timepicker onChange method with valid values', () => {
 
   test('should return the correct value using the 24h pattern - last minute of the day', () => {
     testOnChange({ input: { value: "23:59", is24Hour: true }, expects: { hour: 23, minutes: 59 }})
+  })
+})
+
+describe('test isInvalid', () => {
+  test('should be valid by default', () => {
+    const validateTime = (time: string) => {
+      return true;
+    }
+
+    const view = mount(<TimePicker value={"00:00"} validateTime={validateTime} aria-label="time picker" />);
+
+    expect(view.find('.pf-m-error')).toHaveLength(0);
+  })
+
+  test('should stay valid after onChange', () => {
+    const validateTime = (time: string) => {
+      return true;
+    }
+
+    const event: any = {
+      currentTarget: { value: "01:00" }
+    };
+    const view = mount(<TimePicker value={"00:00"} validateTime={validateTime} aria-label="time picker" />);
+
+    act(() => {
+      view.find('input').prop('onChange')(event);
+    })
+    view.update();
+
+    expect(view.find('.pf-m-error')).toHaveLength(0);
+  })
+
+  test('should be invalid after onChange', () => {
+    const validateTime = (time: string) => {
+      return false;
+    }
+
+    const event: any = {
+      currentTarget: { value: "01:00" }
+    };
+    const view = mount(<TimePicker value={"00:00"} validateTime={validateTime} aria-label="time picker" />);
+
+    act(() => {
+      view.find('input').prop('onChange')(event);
+    })
+    view.update();
+
+    expect(view.find('.pf-m-error')).toHaveLength(1);
   })
 })
