65d7cddfb498562dde586e24df50f1030532eb4e
zallen <47335686+redallen@users.noreply.github.com>
2021-07-20 15:09:10 -0400

    feat(avatar): add border prop (#6040)
    * feat(avatar): add border prop
    
    * update snapshot test
    
    * add integration test

--- packages/react-core/src/components/Avatar/__tests__/Avatar.test.tsx
+++ packages/react-core/src/components/Avatar/__tests__/Avatar.test.tsx
@@ -3,6 +3,6 @@ import { shallow } from 'enzyme';
 import { Avatar } from '../Avatar';
 
 test('simple avatar', () => {
-  const view: any = shallow(<Avatar alt="avatar" />);
+  const view: any = shallow(<Avatar alt="avatar" src="test.png" border="light" />);
   expect(view).toMatchSnapshot();
 });
