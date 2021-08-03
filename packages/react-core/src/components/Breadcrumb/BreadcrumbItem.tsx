1264fead066155f251369d7d80dae0884aa06b9f
Jon Koops <jonkoops@gmail.com>
2021-08-03 11:12:23 +0200

    feat(Breadcrumb): Spread props over custom component passed to BreadcrumbItem

--- packages/react-core/src/components/Breadcrumb/BreadcrumbItem.tsx
+++ packages/react-core/src/components/Breadcrumb/BreadcrumbItem.tsx
@@ -59,7 +59,7 @@ export const BreadcrumbItem: React.FunctionComponent<BreadcrumbItemProps> = ({
       {isDropdown && <span className={css(styles.breadcrumbDropdown)}>{children}</span>}
       {render && render({ className, ariaCurrent })}
       {to && !render && (
-        <Component href={to} target={target} className={className} aria-current={ariaCurrent}>
+        <Component {...props} href={to} target={target} className={className} aria-current={ariaCurrent}>
           {children}
         </Component>
       )}
