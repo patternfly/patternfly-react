37b2f86cb295a86ba570a7eb51b9df853f545af5
Cooper Trowbridge <ctrowbri@redhat.com>
2021-08-19 14:28:28 -0400

    chore(TreeView): update demo

--- packages/react-integration/demo-app-ts/src/components/demos/TreeViewDemo/TreeViewDemo.tsx
+++ packages/react-integration/demo-app-ts/src/components/demos/TreeViewDemo/TreeViewDemo.tsx
@@ -74,6 +74,66 @@ export class TreeViewDemo extends Component {
     }
   ];
 
+  compactOptions: TreeViewDataItem[] = [
+    {
+      name:
+        'APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value and may reject unrecognized values.',
+      title: 'apiVersion',
+      id: 'apiVersion'
+    },
+    {
+      name:
+        'Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated is CamelCase. More info:',
+      title: 'kind',
+      id: 'kind'
+    },
+    {
+      name: 'Standard metadata object',
+      title: 'metadata',
+      id: 'metadata'
+    },
+    {
+      name: 'Standard metadata object',
+      title: 'spec',
+      id: 'spec',
+      children: [
+        {
+          name:
+            'Minimum number of seconds for which a newly created pod should be ready without any of its container crashing, for it to be considered available. Default to 0 (pod will be considered available as soon as it is ready).',
+          title: 'minReadySeconds',
+          id: 'minReadySeconds'
+        },
+        {
+          name: 'Indicates that the deployment is paused',
+          title: 'paused',
+          id: 'paused'
+        },
+        {
+          name:
+            'The maximum time in seconds for a deployment to make progress before it is considered to be failed. The deployment controller will continue to process failed deployments and a condition with a ProgressDeadlineExceeded reason will be surfaced in the deployment status. Note that the progress will not de estimated during the time a deployment is paused. Defaults to 600s.',
+          title: 'progressDeadlineSeconds',
+          id: 'progressDeadlineSeconds',
+          children: [
+            {
+              name:
+                'The number of old ReplicaSets to retain to allow rollback. This is a pointer to distinguish between explicit zero and not specified. Defaults to 10.',
+              title: 'revisionHistoryLimit',
+              id: 'revisionHistoryLimit',
+              children: [
+                {
+                  name:
+                    'Map of {key.value} pairs. A single {key.value} in the matchLabels map is equivalent to an element of matchExpressions, whose key field is "key", the operator is "In" and the values array contains only "value". The requirements are ANDed.',
+                  title: 'matchLabels',
+                  id: 'matchLabels'
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ];
+
   state = {
     allExpanded: null as boolean,
     activeItems: [] as TreeViewDataItem[],
@@ -230,9 +290,26 @@ export class TreeViewDemo extends Component {
           activeItems={activeItems}
           onSelect={this.onClick}
           toolbar={toolbar}
+          hasGuides
         />
         <br />
         <TreeView id="mixed" data={flagOptions} activeItems={activeItems2} onSelect={this.onClick2} />
+        <br />
+        <TreeView
+          id="compact"
+          data={this.compactOptions}
+          activeItems={activeItems2}
+          onSelect={this.onClick2}
+          variant="compact"
+        />
+        <br />
+        <TreeView
+          id="compactNoBackground"
+          data={this.compactOptions}
+          activeItems={activeItems2}
+          onSelect={this.onClick2}
+          variant="compactNoBackground"
+        />
       </React.Fragment>
     );
   }
