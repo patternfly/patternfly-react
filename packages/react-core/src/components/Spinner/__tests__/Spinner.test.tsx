03468df047412f0125e7c8a9064512263018c05b
Katerina Koukiou <kkoukiou@redhat.com>
2021-09-06 14:44:32 +0200

    Spinner: fix spinner tests to check all component sizes
    Fixes #6144

--- packages/react-core/src/components/Spinner/__tests__/Spinner.test.tsx
+++ packages/react-core/src/components/Spinner/__tests__/Spinner.test.tsx
@@ -8,21 +8,21 @@ test('simple spinner', () => {
 });
 
 test('small spinner', () => {
-  const view = shallow(<Spinner />);
+  const view = shallow(<Spinner size="sm" />);
   expect(view).toMatchSnapshot();
 });
 
 test('medium spinner', () => {
-  const view = shallow(<Spinner />);
+  const view = shallow(<Spinner size="md" />);
   expect(view).toMatchSnapshot();
 });
 
 test('large spinner', () => {
-  const view = shallow(<Spinner />);
+  const view = shallow(<Spinner size="lg" />);
   expect(view).toMatchSnapshot();
 });
 
 test('extra large spinner', () => {
-  const view = shallow(<Spinner />);
+  const view = shallow(<Spinner size="xl" />);
   expect(view).toMatchSnapshot();
 });
