4e05b21966c0ddbe1be716e1231c353dc507dfea
Cooper Trowbridge <ctrowbri@redhat.com>
2021-09-13 11:01:37 -0400

    chore(Tabs): disabled demo, integration tests

---
+++ packages/react-integration/cypress/integration/tabdisbleddemo.spec.ts
@@ -0,0 +1,47 @@
+describe('Disabled Tab Demo Test', () => {
+  it('Navigate to demo section', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('#tabs-disabled-demo-nav-item-link').click();
+    cy.url().should('eq', 'http://localhost:3000/tabs-disabled-demo-nav-link');
+  });
+
+  const tabIds = {
+    notDisabled: {
+      button: '#pf-tab-0-not-disabled',
+      content: '#pf-tab-section-0-not-disabled'
+    },
+    disabled: {
+      button: '#pf-tab-1-is-disabled',
+      content: '#pf-tab-section-1-is-disabled'
+    },
+    ariaDisabled: {
+      button: '#pf-tab-2-is-aria-disabled',
+      content: '#pf-tab-section-2-is-aria-disabled'
+    },
+    withTooltip: {
+      button: '#pf-tab-3-with-tooltip',
+      content: '#pf-tab-section-3-with-tooltip'
+    }
+  };
+  const { notDisabled, disabled, ariaDisabled, withTooltip } = tabIds;
+
+  it('Verify disabled tabs', () => {
+    cy.get('#disabledTabs').within(() => {
+      cy.get(notDisabled.content).should('not.have.a.property', 'hidden');
+      cy.get(disabled.button).click({ force: true });
+      cy.get(disabled.content).should('not.exist');
+      cy.get(notDisabled.content).should('not.have.a.property', 'hidden');
+      cy.get(ariaDisabled.button).click({ force: true });
+      cy.get(ariaDisabled.content).should('not.exist');
+      cy.get(notDisabled.content).should('not.have.a.property', 'hidden');
+      cy.get(withTooltip.button).click({ force: true });
+      cy.get(withTooltip.content).should('not.exist');
+      cy.get(notDisabled.content).should('not.have.a.property', 'hidden');
+    });
+  });
+
+  it('Verify aria-disabled with tooltip', () => {
+    cy.get(withTooltip.button).trigger('mouseover');
+    cy.get('.pf-c-tooltip').should('be.visible');
+  });
+});
