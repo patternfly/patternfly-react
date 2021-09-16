6f5cc82540bdff8f7949c4e96d42f9cca0e3a77e
Mike Turley <mike.turley@alum.cs.umass.edu>
2021-09-16 15:49:54 -0400

    Simplify ComposableTableControllingText

--- packages/react-table/src/components/TableComposable/examples/ComposableTableControllingText.tsx
+++ packages/react-table/src/components/TableComposable/examples/ComposableTableControllingText.tsx
@@ -1,69 +1,52 @@
 import React from 'react';
 import { TableComposable, Thead, Tr, Th, Tbody, Td } from '@patternfly/react-table';
 
-interface ExampleItem {
-  one: string;
-  two: React.ReactNode;
-  three: React.ReactNode;
-  four: string;
-  five: React.ReactNode;
-}
+// This example has been simplified to focus on the text modifier props.
+// In real usage, you may want a type for your underlying data and a map of column names by key.
+// See other examples.
 
-const columnNames: Record<keyof ExampleItem, string> = {
-  one: 'Truncate (width 20%)',
-  two: 'Break word',
-  three: 'Wrapping table header text. This th text will wrap instead of truncate.',
-  four: 'Fit content',
-  five: 'No wrap'
-};
+const columnNames = [
+  'Truncate (width 20%)',
+  'Break word',
+  'Wrapping table header text. This th text will wrap instead of truncate.',
+  'Fit content',
+  'No wrap'
+];
 
-export const ComposableTableCellWidth: React.FunctionComponent = () => {
-  // In real usage, this data would come from some external source like an API via props.
-  const exampleItems: ExampleItem[] = [
-    {
-      one: 'This text will truncate instead of wrap.',
-      two: <a>http://thisisaverylongurlthatneedstobreakusethebreakwordmodifier.org</a>,
-      three: (
-        <p>
-          By default,
-          <code>thead</code> cells will truncate and
-          <code>tbody</code> cells will wrap. Use
-          <code>.pf-m-wrap</code> on a<code>th</code> to change its behavior.
-        </p>
-      ),
-      four: "This cell's content will adjust itself to the parent th width. This modifier only affects table layouts.",
-      five: <a href="#">No wrap</a>
-    }
-  ];
-
-  return (
-    <TableComposable aria-label="Simple table">
-      <Thead>
-        <Tr>
-          <Th width={20}>{columnNames.one}</Th>
-          <Th>{columnNames.two}</Th>
-          <Th modifier="wrap">{columnNames.three}</Th>
-          <Th modifier="fitContent">{columnNames.four}</Th>
-          <Th>{columnNames.five}</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {exampleItems.map(item => (
-          <Tr key={item.one}>
-            <Td dataLabel={columnNames.one} modifier="truncate">
-              {item.one}
-            </Td>
-            <Td dataLabel={columnNames.two} modifier="breakWord">
-              {item.two}
-            </Td>
-            <Td dataLabel={columnNames.three}>{item.three}</Td>
-            <Td dataLabel={columnNames.four}>{item.four}</Td>
-            <Td dataLabel={columnNames.five} modifier="nowrap">
-              {item.five}
-            </Td>
-          </Tr>
-        ))}
-      </Tbody>
-    </TableComposable>
-  );
-};
+export const ComposableTableCellWidth: React.FunctionComponent = () => (
+  <TableComposable aria-label="Simple table">
+    <Thead>
+      <Tr>
+        <Th width={20}>{columnNames[0]}</Th>
+        <Th>{columnNames[1]}</Th>
+        <Th modifier="wrap">{columnNames[2]}</Th>
+        <Th modifier="fitContent">{columnNames[3]}</Th>
+        <Th>{columnNames[4]}</Th>
+      </Tr>
+    </Thead>
+    <Tbody>
+      <Tr>
+        <Td dataLabel={columnNames[0]} modifier="truncate">
+          This text will truncate instead of wrap.
+        </Td>
+        <Td dataLabel={columnNames[1]} modifier="breakWord">
+          <a>http://thisisaverylongurlthatneedstobreakusethebreakwordmodifier.org</a>
+        </Td>
+        <Td dataLabel={columnNames[2]}>
+          <p>
+            By default,
+            <code>thead</code> cells will truncate and
+            <code>tbody</code> cells will wrap. Use
+            <code>.pf-m-wrap</code> on a<code>th</code> to change its behavior.
+          </p>
+        </Td>
+        <Td dataLabel={columnNames[3]}>
+          This cell's content will adjust itself to the parent th width. This modifier only affects table layouts.
+        </Td>
+        <Td dataLabel={columnNames[4]} modifier="nowrap">
+          <a href="#">No wrap</a>
+        </Td>
+      </Tr>
+    </Tbody>
+  </TableComposable>
+);
