98fbbe8677d35a38abe7c2c597b690afd5d942a0
nicolethoen <nthoen@redhat.com>
2021-10-06 13:07:01 -0400

    fix button focus trapping

--- packages/react-core/src/components/DualListSelector/DualListSelectorListItem.tsx
+++ packages/react-core/src/components/DualListSelector/DualListSelectorListItem.tsx
@@ -66,8 +66,8 @@ export const DualListSelectorListItemBase: React.FunctionComponent<DualListSelec
       <div className={css(styles.dualListSelectorListItemRow, isSelected && styles.modifiers.selected)}>
         {isDraggable && (
           <div className={css(styles.dualListSelectorDraggable)}>
-            <Button variant={ButtonVariant.plain} aria-label={draggableButtonAriaLabel} tabIndex={-1}>
-              <GripVerticalIcon />
+            <Button variant={ButtonVariant.plain} aria-label={draggableButtonAriaLabel} component="span">
+              <GripVerticalIcon style={{ verticalAlign: '-0.3em' }} />
             </Button>
           </div>
         )}
