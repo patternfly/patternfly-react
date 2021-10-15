7726c3ab6bbcd5d090664c7063462f328816e4e3
Michael Coker <mcoker@redhat.com>
2021-10-15 12:10:17 -0500

    chore(labelgroup): fix jest test

--- packages/react-core/src/components/LabelGroup/__tests__/LabelGroup.test.tsx
+++ packages/react-core/src/components/LabelGroup/__tests__/LabelGroup.test.tsx
@@ -64,14 +64,12 @@ describe('LabelGroup', () => {
   test('label group compact', () => {
     const view = shallow(
       <LabelGroup isCompact>
-        <Label>1</Label>
-        <Label>2</Label>
-        <Label>3</Label>
-        <Label>4</Label>
+        <Label isCompact>1</Label>
+        <Label isCompact>2</Label>
+        <Label isCompact>3</Label>
+        <Label isCompact>4</Label>
       </LabelGroup>
     );
-    const overflowButton = view.find('.pf-m-overflow');
-    expect(overflowButton).hasClass('pf-m-compact');
     expect(view).toMatchSnapshot();
   });
 });
