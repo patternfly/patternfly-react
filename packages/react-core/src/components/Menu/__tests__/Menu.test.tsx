7be7cb61e5251d41160000eec7fcb2b18010ab47
Gabi Podolnikova <gpodolni@redhat.com>
2021-10-18 12:04:12 +0200

    feat(Menu): adds snapshot tests

---
+++ packages/react-core/src/components/Menu/__tests__/Menu.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Menu } from '../Menu';
+
+test('should render Menu successfully', () => {
+    const view = mount(
+        <Menu activeItemId={0} onSelect={jest.fn()}>
+            content
+        </Menu>
+    );
+    expect(view).toMatchSnapshot();
+});
+
+describe('with isPlain', () => {
+    test('should render Menu with plain styles applied', () => {
+        const view = mount(
+            <Menu activeItemId={0} onSelect={jest.fn()} isPlain>
+                content
+            </Menu>
+        );
+        expect(view.find('div.pf-m-plain')).toMatchSnapshot();
+    });
+});
+
+describe('with isScrollable', () => {
+    test('should render Menu with scrollable styles applied', () => {
+        const view = mount(
+            <Menu activeItemId={0} onSelect={jest.fn()} isScrollable>
+                content
+            </Menu>
+        );
+        expect(view.find('div.pf-m-scrollable')).toMatchSnapshot();
+    });
+});
