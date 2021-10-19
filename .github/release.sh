a37b1d3500e220240fa72501bd4ea9113c00a06b
redallen <zallen@redhat.com>
2021-10-19 16:10:59 -0400

    fix(github-actions): release 14

--- .github/release.sh
+++ .github/release.sh
@@ -5,7 +5,7 @@ REPO="github.com/${GH_REPO}"
 echo "Preparing release environment..."
 git config user.email "patternfly-build@redhat.com"
 git config user.name ${GIT_USERNAME}
-git remote set-url origin "https://${GIT_USERNAME}:${GH_TOKEN_ADMIN}@${REPO}"
+git remote set-url origin "https://${GIT_USERNAME}:${GH_TOKEN}@${REPO}"
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
 
 echo "Doing a release..."
