094920a3405d1e17ab2a1bfe62da26ed455fd1d9
redallen <zallen@redhat.com>
2021-10-19 15:27:21 -0400

    fix(github-actions): release 10

--- .github/release.sh
+++ .github/release.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-USERNAME=${CIRCLE_PROJECT_USERNAME}
-REPONAME=${CIRCLE_PROJECT_REPONAME}
 GIT_USERNAME="patternfly-build"
-REPO="github.com:${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}.git"
+GH_REPO=${GITHUB_REPOSITORY}
+REPO="github.com:${GH_REPO}.git"
 echo "Preparing release environment..."
 git config user.email "patternfly-build@redhat.com"
 git config user.name ${GIT_USERNAME}
 git config credential.helper store
-echo "https://${GIT_USERNAME}:${GH_TOKEN}@${REPO}" > ~/.git-credentials
+echo "https://${GIT_USERNAME}:${GH_TOKEN_ADMIN}@${REPO}" > ~/.git-credentials
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
 
 echo "Doing a release..."
@@ -20,7 +19,7 @@ echo "Doing a release..."
 LOG=$(git log --format="%s" -1 | grep -Poe "#\d+")
 PR_NUM=${LOG:1}
 
-yarn run lerna publish --conventional-commits --create-release=github --no-verify-access --yes | tee lerna-output.txt
+yarn run lerna publish --conventional-commits --create-release=github --no-verify-access --yes 2>&1 | tee lerna-output.txt
 
 if grep -i "Successfully published" lerna-output.txt; # Leave a Github comment
 then
@@ -28,12 +27,15 @@ then
   then
     # Use Issues api instead of PR api because
     # PR api requires comments be made on specific files of specific commits
-    GITHUB_PR_COMMENTS="https://api.github.com/repos/${USERNAME}/${REPONAME}/issues/${PR_NUM}/comments"
+    GH_PR_COMMENTS="https://api.github.com/repos/${GH_REPO}/issues/${PR_NUM}/comments"
     COMMENT=$(git log --author="patternfly-build" -1 --pretty=%B | tail -n +2 | python -c 'import json,sys; print(json.dumps(sys.stdin.read()))' | cut -d '"' -f 2)
     JSON="{\"body\":\"Your changes have been released in: ${COMMENT}Thanks for your contribution! :tada:\"}"
-    echo "Adding github PR comment ${GITHUB_PR_COMMENTS} ${JSON}"
-    curl -H "Authorization: token ${GH_PR_TOKEN}" --request POST "${GITHUB_PR_COMMENTS}" --data "${JSON}"
+    echo "Adding github PR comment ${GH_PR_COMMENTS} ${JSON}"
+    curl -H "Authorization: token ${GH_PR_TOKEN}" --request POST "${GH_PR_COMMENTS}" --data "${JSON}"
   fi
+elif grep -i "No changed packages to publish" lerna-output.txt;
+then
+  echo "No changed packages to publish"
 else
   echo "Failed lerna publish"
   exit 1
