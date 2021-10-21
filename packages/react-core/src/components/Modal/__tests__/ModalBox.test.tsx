1a7b92131f45fecf5b745de07649f4cde68a2b55
Gabi Podolnikova <gpodolni@redhat.com>
2021-10-21 15:42:51 +0200

    fix(Modal): test clean up

--- packages/react-core/src/components/Modal/__tests__/ModalBox.test.tsx
+++ packages/react-core/src/components/Modal/__tests__/ModalBox.test.tsx
@@ -2,29 +2,37 @@ import * as React from 'react';
 import { shallow } from 'enzyme';
 
 import { ModalBox } from '../ModalBox';
-import BullhornIcon from '@patternfly/react-icons/dist/esm/icons/bullhorn-icon';
 
 test('ModalBox Test', () => {
   const view = shallow(
-    <ModalBox title="Test Modal Box" id="boxId">
+    <ModalBox aria-describedby="Test Modal Box" id="boxId">
       This is a ModalBox
     </ModalBox>
   );
   expect(view).toMatchSnapshot();
 });
 
-test('ModalBox Test isLarge', () => {
+test('ModalBox Test large', () => {
   const view = shallow(
-    <ModalBox title="Test Modal Box" id="boxId" isLarge>
+    <ModalBox aria-describedby="Test Modal Box" id="boxId" variant="large">
       This is a ModalBox
     </ModalBox>
   );
   expect(view).toMatchSnapshot();
 });
 
-test('ModalBox Test isSmall', () => {
+test('ModalBox Test medium', () => {
   const view = shallow(
-    <ModalBox title="Test Modal Box" id="boxId" isSmall>
+    <ModalBox aria-describedby="Test Modal Box" id="boxId" variant="medium">
+      This is a ModalBox
+    </ModalBox>
+  );
+  expect(view).toMatchSnapshot();
+});
+
+test('ModalBox Test small', () => {
+  const view = shallow(
+    <ModalBox aria-describedby="Test Modal Box" id="boxId" variant="small">
       This is a ModalBox
     </ModalBox>
   );
@@ -33,7 +41,7 @@ test('ModalBox Test isSmall', () => {
 
 test('ModalBox Test top aligned', () => {
   const view = shallow(
-    <ModalBox title="Test Modal Box" id="boxId" position="top">
+    <ModalBox aria-describedby="Test Modal Box" id="boxId" position="top">
       This is a ModalBox
     </ModalBox>
   );
@@ -42,25 +50,7 @@ test('ModalBox Test top aligned', () => {
 
 test('ModalBox Test top aligned distance', () => {
   const view = shallow(
-    <ModalBox title="Test Modal Box" id="boxId" position="top" positionOffset="50px">
-      This is a ModalBox
-    </ModalBox>
-  );
-  expect(view).toMatchSnapshot();
-});
-
-test('ModalBox Test alert variant', () => {
-  const view = shallow(
-    <ModalBox title="Test Modal Box" id="boxId" titleIconVariant="warning">
-      This is a ModalBox
-    </ModalBox>
-  );
-  expect(view).toMatchSnapshot();
-});
-
-test('ModalBox Test custom icon variant', () => {
-  const view = shallow(
-    <ModalBox title="Test Modal Box" id="boxId" titleIconVariant={BullhornIcon}>
+    <ModalBox aria-describedby="Test Modal Box" id="boxId" position="top" positionOffset="50px">
       This is a ModalBox
     </ModalBox>
   );
