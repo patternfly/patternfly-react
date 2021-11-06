3a86354bb927d15e2ea9d19cf72d741abe70c2bf
Austin Sullivan <ausulliv@redhat.com>
2021-11-06 15:49:03 -0400

    feat(textinputgroup): add additional props

--- packages/react-core/src/components/TextInputGroup/TextInputGroup.tsx
+++ packages/react-core/src/components/TextInputGroup/TextInputGroup.tsx
@@ -7,16 +7,25 @@ export interface TextInputGroupProps extends React.HTMLProps<HTMLDivElement> {
   children?: React.ReactNode;
   /** Additional classes applied to the text input group container */
   className?: string;
+  /** Adds disabled styling and a disabled context value which text input group main hooks into for the input itself */
+  isDisabled?: boolean;
 }
+
+export const TextInputGroupContext = React.createContext<Partial<TextInputGroupProps>>({
+  isDisabled: false
+});
 
 export const TextInputGroup: React.FunctionComponent<TextInputGroupProps> = ({
   children,
   className,
+  isDisabled,
   ...props
 }: TextInputGroupProps) => (
-  <div className={css(styles.textInputGroup, className)} {...props}>
-    {children}
-  </div>
+  <TextInputGroupContext.Provider value={{ isDisabled }}>
+    <div className={css(styles.textInputGroup, isDisabled && styles.modifiers.disabled, className)} {...props}>
+      {children}
+    </div>
+  </TextInputGroupContext.Provider>
 );
 
 TextInputGroup.displayName = 'TextInputGroup';
--- packages/react-core/src/components/TextInputGroup/TextInputGroupMain.tsx
+++ packages/react-core/src/components/TextInputGroup/TextInputGroupMain.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import styles from '@patternfly/react-styles/css/components/TextInputGroup/text-input-group';
 import { css } from '@patternfly/react-styles';
+import { TextInputGroupContext } from './TextInputGroup';
 
 export interface TextInputGroupMainProps extends React.HTMLProps<HTMLDivElement> {
   /** Content rendered inside the text input group main div */
@@ -9,8 +10,25 @@ export interface TextInputGroupMainProps extends React.HTMLProps<HTMLDivElement>
   className?: string;
   /** Icon to be shown on the left side of the text input group main container */
   icon?: React.ReactNode;
+  /** Type that the input accepts. */
+  type?:
+    | 'text'
+    | 'date'
+    | 'datetime-local'
+    | 'email'
+    | 'month'
+    | 'number'
+    | 'password'
+    | 'search'
+    | 'tel'
+    | 'time'
+    | 'url';
   /** Callback for when there is a change in the input field*/
   onChange?: () => void;
+  /** Callback for when the input field is focused*/
+  onFocus?: () => void;
+  /** Callback for when focus is lost on the input field*/
+  onBlur?: () => void;
   /** Accessibility label for the input */
   'aria-label'?: string;
 }
@@ -19,25 +37,35 @@ export const TextInputGroupMain: React.FunctionComponent<TextInputGroupMainProps
   children,
   className,
   icon,
+  type,
   onChange,
+  onFocus,
+  onBlur,
   'aria-label': ariaLabel,
   value: inputValue,
   ...props
-}: TextInputGroupMainProps) => (
-  <div className={css(styles.textInputGroupMain, icon && styles.modifiers.icon, className)} {...props}>
-    {children}
-    <span className={css(styles.textInputGroupText)}>
-      {icon && <span className={css(styles.textInputGroupIcon)}>{icon}</span>}
-      <input
-        type="text"
-        className={css(styles.textInputGroupTextInput)}
-        aria-label={ariaLabel}
-        onChange={onChange}
-        value={inputValue}
-      />
-    </span>
-  </div>
-);
+}: TextInputGroupMainProps) => {
+  const { isDisabled } = React.useContext(TextInputGroupContext);
 
-TextInputGroupMain.defaultProps = { 'aria-label': 'Type to filter' };
+  return (
+    <div className={css(styles.textInputGroupMain, icon && styles.modifiers.icon, className)} {...props}>
+      {children}
+      <span className={css(styles.textInputGroupText)}>
+        {icon && <span className={css(styles.textInputGroupIcon)}>{icon}</span>}
+        <input
+          type={type}
+          className={css(styles.textInputGroupTextInput)}
+          aria-label={ariaLabel}
+          disabled={isDisabled}
+          onChange={onChange}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          value={inputValue}
+        />
+      </span>
+    </div>
+  );
+};
+
+TextInputGroupMain.defaultProps = { 'aria-label': 'Type to filter', type: 'text' };
 TextInputGroupMain.displayName = 'TextInputGroupMain';
