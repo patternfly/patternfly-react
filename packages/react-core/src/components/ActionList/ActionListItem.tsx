9b930c9104fe514e946564abe9871df5fd0d3334
Michael Coker <mcoker@redhat.com>
2021-11-24 10:58:58 -0600

    fix(action-list): added support for classname

--- packages/react-core/src/components/ActionList/ActionListItem.tsx
+++ packages/react-core/src/components/ActionList/ActionListItem.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react';
+import { css } from '@patternfly/react-styles';
 
 export interface ActionListItemProps extends React.HTMLProps<HTMLDivElement> {
   /** Children of the action list item */
   children?: React.ReactNode;
+  /** Additional classes added to the action list item */
+  className?: string;
 }
 
 export const ActionListItem: React.FunctionComponent<ActionListItemProps> = ({
   children,
+  className = '',
   ...props
 }: ActionListItemProps) => (
-  <div className="pf-c-action-list__item" {...props}>
+  <div className={css('pf-c-action-list__item', className)} {...props}>
     {children}
   </div>
 );
