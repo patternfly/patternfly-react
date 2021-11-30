dd3d2ce32b4d3e7562faf465abdc508e17f097d9
Michael Coker <35148959+mcoker@users.noreply.github.com>
2021-11-30 08:56:30 -0600

    fix(action-list): added support for classname (#6647)

--- packages/react-core/src/components/ActionList/ActionListGroup.tsx
+++ packages/react-core/src/components/ActionList/ActionListGroup.tsx
@@ -5,13 +5,16 @@ import styles from '@patternfly/react-styles/css/components/ActionList/action-li
 export interface ActionListGroupProps extends React.HTMLProps<HTMLDivElement> {
   /** Children of the action list group */
   children?: React.ReactNode;
+  /** Additional classes added to the action list group */
+  className?: string;
 }
 
 export const ActionListGroup: React.FunctionComponent<ActionListGroupProps> = ({
   children,
+  className = '',
   ...props
 }: ActionListGroupProps) => (
-  <div className={css(styles.actionListGroup)} {...props}>
+  <div className={css(styles.actionListGroup, className)} {...props}>
     {children}
   </div>
 );
