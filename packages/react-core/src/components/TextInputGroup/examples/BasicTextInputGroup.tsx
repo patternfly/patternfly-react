b49903ae201653f171fa3b231ad36b7bdb54a50c
Austin Sullivan <ausulliv@redhat.com>
2021-12-14 16:45:33 -0500

    chore(TextInputGroup): convert examples and demos to typescript=

---
+++ packages/react-core/src/components/TextInputGroup/examples/BasicTextInputGroup.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { TextInputGroup, TextInputGroupMain } from '@patternfly/react-core';
+
+export const BasicTextInputGroup: React.FunctionComponent = () => (
+  <TextInputGroup>
+    <TextInputGroupMain />
+  </TextInputGroup>
+);
