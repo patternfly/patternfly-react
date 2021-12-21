abd41baa37ad1fe2500b835b44d05ba9d322866c
Boaz Shuster <boaz.shuster.github@gmail.com>
2021-12-21 08:27:34 +0200

    fixup! feat(ToggleGroup): support disabling all items under the parent
    Add description above disableAll

--- packages/react-core/src/components/ToggleGroup/ToggleGroup.tsx
+++ packages/react-core/src/components/ToggleGroup/ToggleGroup.tsx
@@ -10,6 +10,7 @@ export interface ToggleGroupProps extends React.HTMLProps<HTMLDivElement> {
   className?: string;
   /** Modifies the toggle group to include compact styling. */
   isCompact?: boolean;
+  /** Disable all ToggleGroupItems under this component. */
   disableAll?: boolean;
   /** Accessible label for the toggle group */
   'aria-label'?: string;
