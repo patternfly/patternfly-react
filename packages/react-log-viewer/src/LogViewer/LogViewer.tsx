4aeb763577179a3bb84572197f373a4d70cc6b63
Juntao Wang <37624318+DaoDaoNoCode@users.noreply.github.com>
2022-01-10 10:26:36 -0500

    fix(log-viewer): update text selection behavior (#6746)
    * fix(log-viewer): update text selection behavior
    
    * apply footer style

--- packages/react-log-viewer/src/LogViewer/LogViewer.tsx
+++ packages/react-log-viewer/src/LogViewer/LogViewer.tsx
@@ -275,7 +275,7 @@ const LogViewerBase: React.FunctionComponent<LogViewerProps> = memo(
           <div className={css(styles.logViewerMain)} style={{ height, width }} ref={containerRef}>
             {loading ? <div>{loadingContent}</div> : createList(parsedData)}
           </div>
-          {footer}
+          <div className={css(styles.logViewerFooter)}>{footer}</div>
         </div>
       </LogViewerContext.Provider>
     );
