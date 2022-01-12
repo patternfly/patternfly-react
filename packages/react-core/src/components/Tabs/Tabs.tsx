8fb7613d76991d09f2db5021cc4aeae41a951ad3
Jon Koops <jonkoops@gmail.com>
2022-01-12 16:19:59 +0100

    Clarify the allowed children

--- packages/react-core/src/components/Tabs/Tabs.tsx
+++ packages/react-core/src/components/Tabs/Tabs.tsx
@@ -18,12 +18,12 @@ export enum TabsComponent {
   nav = 'nav'
 }
 
-type ChildElement = React.ReactElement<TabProps, React.JSXElementConstructor<TabProps>>;
-type Child = ChildElement | boolean | null | undefined;
+type TabElement = React.ReactElement<TabProps, React.JSXElementConstructor<TabProps>>;
+type TabsChild = TabElement | boolean | null | undefined;
 
 export interface TabsProps extends Omit<React.HTMLProps<HTMLElement | HTMLDivElement>, 'onSelect'>, OUIAProps {
-  /** Content rendered inside the tabs component. */
-  children: Child | Child[];
+  /** Content rendered inside the tabs component. Only `Tab` components or expressions resulting in a falsy value are allowed here. */
+  children: TabsChild | TabsChild[];
   /** Additional classes added to the tabs */
   className?: string;
   /** Tabs background color variant */
@@ -166,7 +166,7 @@ export class Tabs extends React.Component<TabsProps, TabsState> {
     // process any tab content sections outside of the component
     if (tabContentRef) {
       React.Children.toArray(this.props.children)
-        .filter((child): child is ChildElement => React.isValidElement(child))
+        .filter((child): child is TabElement => React.isValidElement(child))
         .filter(child => child.props && child.props.tabContentRef && child.props.tabContentRef.current)
         .forEach(child => (child.props.tabContentRef.current.hidden = true));
       // most recently selected tabContent
@@ -312,7 +312,7 @@ export class Tabs extends React.Component<TabsProps, TabsState> {
       uncontrolledIsExpandedLocal
     } = this.state;
     const filteredChildren = React.Children.toArray(children)
-      .filter((child): child is ChildElement => React.isValidElement(child))
+      .filter((child): child is TabElement => React.isValidElement(child))
       .filter(child => !child.props.isHidden);
 
     const uniqueId = id || getUniqueId();
