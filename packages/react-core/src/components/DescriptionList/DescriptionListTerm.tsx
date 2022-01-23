31e037333f9423459ede4d509b249b4771868be7
Boaz Shuster <boaz.shuster.github@gmail.com>
2022-01-23 17:50:19 +0200

    feat(DescriptionList): add icon variant
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- packages/react-core/src/components/DescriptionList/DescriptionListTerm.tsx
+++ packages/react-core/src/components/DescriptionList/DescriptionListTerm.tsx
@@ -5,6 +5,8 @@ import { css } from '@patternfly/react-styles';
 export interface DescriptionListTermProps extends React.HTMLProps<HTMLElement> {
   /** Anything that can be rendered inside of list term */
   children: React.ReactNode;
+  /** icon that is rendered inside of list term */
+  icon?: React.ReactNode;
   /** Additional classes added to the DescriptionListTerm */
   className?: string;
 }
@@ -12,9 +14,11 @@ export interface DescriptionListTermProps extends React.HTMLProps<HTMLElement> {
 export const DescriptionListTerm: React.FunctionComponent<DescriptionListTermProps> = ({
   children,
   className,
+  icon,
   ...props
 }: DescriptionListTermProps) => (
   <dt className={css(styles.descriptionListTerm, className)} {...props}>
+    {icon ? <span className={css(styles.descriptionListTermIcon)}>{icon}</span> : null}
     <span className={css(styles.descriptionListText)}>{children}</span>
   </dt>
 );
