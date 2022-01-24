646e385abbb7e719b20cb7216ec387f4bead16dc
dallas <dallas.nicol@gmail.com>
2022-01-24 13:44:19 -0600

    fix: windows issue listening on md changes to refresh

--- packages/react-docs/patternfly-docs.source.js
+++ packages/react-docs/patternfly-docs.source.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
-module.exports = (sourceMD, sourceProps) => {
+module.exports = (baseSourceMD, sourceProps) => {
+  const sourceMD = (basePath, ...props) => baseSourceMD(basePath.split(path.sep).join(path.posix.sep), ...props);
+
   // Theme pages
   const themePagesPath = require.resolve('theme-patternfly-org/package.json').replace('package.json', 'pages');
   sourceMD(path.join(themePagesPath, '*.md'), 'pages-overview');
