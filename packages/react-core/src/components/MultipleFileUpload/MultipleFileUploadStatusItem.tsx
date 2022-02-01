3b6cd56ef7420ceb99706ac3f2b0ba78e75b2821
Austin Sullivan <ausulliv@redhat.com>
2022-02-01 17:59:49 -0500

    cleans up MultiFileUploadStatusItem

--- packages/react-core/src/components/MultipleFileUpload/MultipleFileUploadStatusItem.tsx
+++ packages/react-core/src/components/MultipleFileUpload/MultipleFileUploadStatusItem.tsx
@@ -3,13 +3,16 @@ import styles from '@patternfly/react-styles/css/components/MultipleFileUpload/m
 import { css } from '@patternfly/react-styles';
 import { Progress } from '../Progress';
 import { Button } from '../Button';
+import FileIcon from '@patternfly/react-icons/dist/esm/icons/file-icon';
+import TimesCircleIcon from '@patternfly/react-icons/dist/esm/icons/times-circle-icon';
 
 interface MultipleFileUploadStatusItemProps extends React.HTMLProps<HTMLLIElement> {
   /** Class to add to outer div */
   className?: string;
-
+  /** The file object being represented by the status item */
   file?: File;
-
+  /** A custom icon to show in place of the generic file icon */
+  fileIcon?: React.ReactNode;
   /** A callback for when a selected file starts loading */
   onReadStarted?: (fileHandle: File) => void;
   /** A callback for when a selected file finishes loading */
@@ -18,88 +21,103 @@ interface MultipleFileUploadStatusItemProps extends React.HTMLProps<HTMLLIElemen
   onReadFailed?: (error: DOMException, fileHandle: File) => void;
   /** Clear button was clicked */
   onClearClick?: React.MouseEventHandler<HTMLButtonElement>;
-  /** Text area text changed */
-  onTextChange?: (text: string) => void;
-  /** On data changed - if type='text' or type='dataURL' and file was loaded it will call this method */
+  /** A callback for when the FileReader successfully reads the file */
   onDataChange?: (data: string) => void;
 
-  loadPercentage?: number;
+  // Props to bypass built in behavior
+
+  /** A flag to disable having the status item read the file itself */
+  useCustomFileHandling?: boolean;
+  /** A custom name to display for the file rather than using built in functionality to auto-fill it */
+  fileName?: string;
+  /** A custom file size to display for the file rather than using built in functionality to auto-fill it */
+  fileSize?: number;
+  /** A custom value to display for the progress component rather than using built in functionality to auto-fill it */
+  progressValue?: number;
+  /** A custom variant to apply to the progress component rather than using built in functionality to auto-fill it */
+  progressVariant?: 'danger' | 'success' | 'warning';
 }
 
 export const MultipleFileUploadStatusItem: React.FunctionComponent<MultipleFileUploadStatusItemProps> = ({
   className,
   file,
+  fileIcon,
   onReadStarted = () => {},
   onReadFinished = () => {},
   onReadFailed = () => {},
   onClearClick = () => {},
   onDataChange = () => {},
-  loadPercentage,
+  useCustomFileHandling,
+  fileName,
+  fileSize,
+  progressValue,
+  progressVariant,
   ...props
 }) => {
-  const [loadStatus, setLoadStatus] = React.useState(0);
+  const [loadPercentage, setLoadPercentage] = React.useState(0);
   const [loadResult, setLoadResult] = React.useState<undefined | 'danger' | 'success'>();
 
-  function readFile(fileHandle: File) {
+  function readFile(file: File) {
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
       reader.onload = () => resolve(reader.result);
       reader.onerror = () => reject(reader.error);
-      reader.readAsDataURL(fileHandle);
+      reader.readAsDataURL(file);
     });
   }
 
   React.useEffect(() => {
-    onReadStarted(file);
-    setLoadStatus(1);
-    readFile(file)
-      .then(data => {
-        setLoadResult('success');
-        setLoadStatus(100);
-        onReadFinished(file);
-        onDataChange(data as string);
-      })
-      .catch((error: DOMException) => {
-        onReadFailed(error, file);
-        onReadFinished(file);
-        setLoadResult('danger');
-      });
+    if (!useCustomFileHandling) {
+      onReadStarted(file);
+      setLoadPercentage(1);
+      readFile(file)
+        .then(data => {
+          setLoadResult('success');
+          setLoadPercentage(100);
+          onReadFinished(file);
+          onDataChange(data as string);
+        })
+        .catch((error: DOMException) => {
+          onReadFinished(file);
+          onReadFailed(error, file);
+          setLoadResult('danger');
+        });
+    }
   }, []);
 
-  const fileSizeString = (size: number) => {
-    if (size < 1000) {
-      return `${size}B`;
-    } else if (size < 1000000) {
-      return `${size / 1000} KB`;
-    } else if (size < 1000000000) {
-      return `${size / 1000000} MB`;
-    } else if (size < 1000000000000) {
-      return `${size / 1000000000} GB`;
-    } else if (size < 1000000000000000) {
-      return `${size / 1000000000000} TB`;
-    } else {
-      return 'File too large';
+  const getHumanReadableFileSize = (size: number) => {
+    const prefixes = ['', 'K', 'M', 'G', 'T'];
+    let prefixUnit = 0;
+    while (size >= 1000) {
+      prefixUnit += 1;
+      size = size / 1000;
     }
+
+    if (prefixUnit >= prefixes.length) {
+      return 'File size too large';
+    }
+
+    return `${Math.round(size)}${prefixes[prefixUnit]}B`;
   };
 
   const title = (
     <span className={styles.multipleFileUploadStatusItemProgress}>
-      <span className={styles.multipleFileUploadStatusItemProgressText}>{file.name}</span>
-      <span className={styles.multipleFileUploadStatusItemProgressSize}>{fileSizeString(file.size)}</span>
+      <span className={styles.multipleFileUploadStatusItemProgressText}>{fileName || file.name}</span>
+      <span className={styles.multipleFileUploadStatusItemProgressSize}>
+        {fileSize || getHumanReadableFileSize(file.size)}
+      </span>
     </span>
   );
 
   return (
     <li className={css(styles.multipleFileUploadStatusItem, className)} {...props}>
-      <div className={styles.multipleFileUploadStatusItemIcon}>
-        <i className="fas fa-file" aria-hidden="true"></i>
-      </div>
+      <div className={styles.multipleFileUploadStatusItemIcon}>{fileIcon || <FileIcon />}</div>
       <div className={styles.multipleFileUploadStatusItemMain}>
-        <Progress title={title} value={loadPercentage || loadStatus} variant={loadResult} />
+        <Progress title={title} value={progressValue || loadPercentage} variant={progressVariant || loadResult} />
       </div>
       <div className={styles.multipleFileUploadStatusItemClose}>
         <Button variant="plain" aria-label="Remove from list" onClick={onClearClick}>
-          <i className="fas fa-times-circle" aria-hidden="true"></i>
+          <TimesCircleIcon />
         </Button>
       </div>
     </li>
