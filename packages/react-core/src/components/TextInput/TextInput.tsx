44c0ffa403052ee8c0bfb1cd636dbef500459570
eliran <eliran@redhat.com>
2022-02-13 18:02:26 +0200

    fix(components): fix newline interpolation of text input values
    newline characters can be omitted from the input value, fix this by always replacing incoming newlines with whitespace characters.
    
    fixes: #5324

--- packages/react-core/src/components/TextInput/TextInput.tsx
+++ packages/react-core/src/components/TextInput/TextInput.tsx
@@ -201,7 +201,7 @@ export class TextInputBase extends React.Component<TextInputProps, TextInputStat
         )}
         onChange={this.handleChange}
         type={type}
-        value={value}
+        value={this.sanitizeInputValue(value)}
         aria-invalid={props['aria-invalid'] ? props['aria-invalid'] : validated === ValidatedOptions.error}
         required={isRequired}
         disabled={isDisabled}
@@ -212,6 +212,9 @@ export class TextInputBase extends React.Component<TextInputProps, TextInputStat
       />
     );
   }
+
+  private sanitizeInputValue = (value: string | number) =>
+    typeof value === 'string' ? value.replace(/\n/g, ' ') : value;
 }
 
 export const TextInput = React.forwardRef((props: TextInputProps, ref: React.Ref<HTMLInputElement>) => (
