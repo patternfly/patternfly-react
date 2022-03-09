d041ca66ca56f2bd97a32c971dc3152071314c75
Eric Olkowski <thatblindgeye@gmail.com>
2022-03-09 15:39:52 -0500

    Utilize reference for delete methods

--- packages/react-core/src/components/ChipGroup/examples/ChipGroupRemovableCategories.tsx
+++ packages/react-core/src/components/ChipGroup/examples/ChipGroupRemovableCategories.tsx
@@ -5,20 +5,19 @@ export const ChipGroupRemovableCategories: React.FunctionComponent = () => {
   const [chipGroup1, setChipGroup1] = React.useState(['Chip one', 'Chip two', 'Chip three']);
   const [chipGroup2, setChipGroup2] = React.useState(['Chip one', 'Chip two', 'Chip three', 'Chip four']);
 
-  const deleteItem = (id: string, group: string) => {
-    const isGroupOne = group === 'chipGroup1';
-    const copyOfChips = isGroupOne ? [...chipGroup1] : [...chipGroup2];
+  const deleteItem = (id: string, group: string[]) => {
+    const copyOfChips = [...group];
     const filteredCopy = copyOfChips.filter(chip => chip !== id);
 
-    if (isGroupOne) {
+    if (group === chipGroup1) {
       setChipGroup1(filteredCopy);
     } else {
       setChipGroup2(filteredCopy);
     }
   };
 
-  const deleteCategory = (id: string) => {
-    if (id === 'Category one') {
+  const deleteCategory = (group: string[]) => {
+    if (group === chipGroup1) {
       setChipGroup1([]);
     } else {
       setChipGroup2([]);
@@ -27,21 +26,17 @@ export const ChipGroupRemovableCategories: React.FunctionComponent = () => {
 
   return (
     <React.Fragment>
-      <ChipGroup categoryName="Category one" isClosable onClick={() => deleteCategory('Category one')}>
+      <ChipGroup categoryName="Category one" isClosable onClick={() => deleteCategory(chipGroup1)}>
         {chipGroup1.map(currentChip => (
-          <Chip key={currentChip} onClick={() => deleteItem(currentChip, 'chipGroup1')}>
+          <Chip key={currentChip} onClick={() => deleteItem(currentChip, chipGroup1)}>
             {currentChip}
           </Chip>
         ))}
       </ChipGroup>
       <br /> <br />
-      <ChipGroup
-        categoryName="Category two has a very long name"
-        isClosable
-        onClick={() => deleteCategory('Category two')}
-      >
+      <ChipGroup categoryName="Category two has a very long name" isClosable onClick={() => deleteCategory(chipGroup2)}>
         {chipGroup2.map(currentChip => (
-          <Chip key={currentChip} onClick={() => deleteItem(currentChip, 'chipGroup2')}>
+          <Chip key={currentChip} onClick={() => deleteItem(currentChip, chipGroup2)}>
             {currentChip}
           </Chip>
         ))}
