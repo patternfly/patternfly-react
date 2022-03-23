183a47ae981aebf438a3dc894465673dffb13a49
Eric Olkowski <thatblindgeye@gmail.com>
2022-03-23 09:48:09 -0400

    Update rangeStart date in example

--- packages/react-core/src/components/CalendarMonth/examples/CalendarMonthDateRange.tsx
+++ packages/react-core/src/components/CalendarMonth/examples/CalendarMonthDateRange.tsx
@@ -4,5 +4,5 @@ import { CalendarMonth } from '@patternfly/react-core';
 export const CalendarMonthDateRange: React.FunctionComponent = () => {
   const [date, setDate] = React.useState(new Date(2020, 10, 24));
 
-  return <CalendarMonth date={date} rangeStart={date} onChange={setDate} />;
+  return <CalendarMonth date={date} rangeStart={new Date(2020, 10, 24)} onChange={setDate} />;
 };
