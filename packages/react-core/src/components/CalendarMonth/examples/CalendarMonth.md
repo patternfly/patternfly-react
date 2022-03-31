30b39d4972570568ff5d8fab75533e3e1e09ef21
Eric Olkowski <thatblindgeye@gmail.com>
2022-03-31 09:44:00 -0400

    Add validator in example to disable pre-start dates

--- packages/react-core/src/components/CalendarMonth/examples/CalendarMonth.md
+++ packages/react-core/src/components/CalendarMonth/examples/CalendarMonth.md
@@ -22,5 +22,7 @@ beta: true
 
 In this example, there are two dates selected: the first is a range start date (via the `rangeStart` prop) and the second is a range end date (via the `date` prop). For the purposes of this example, these dates are static and cannot be updated.
 
+Additionally, any dates prior to the range start date are disabled by passing in an array of functions to the `validators` prop. In this case a single function is passed in, which checks whether a date is greater than or equal to the range start date.
+
 ```ts file="./CalendarMonthDateRange.tsx"
 ```
--- packages/react-core/src/components/CalendarMonth/examples/CalendarMonthDateRange.tsx
+++ packages/react-core/src/components/CalendarMonth/examples/CalendarMonthDateRange.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { CalendarMonth } from '@patternfly/react-core';
 
-export const CalendarMonthDateRange: React.FunctionComponent = () => (
-  <CalendarMonth date={new Date(2020, 10, 24)} rangeStart={new Date(2020, 10, 11)} />
-);
+export const CalendarMonthDateRange: React.FunctionComponent = () => {
+  const startDate = new Date(2020, 10, 11);
+  const endDate = new Date(2020, 10, 24);
+  const disablePreStartDates = (date: Date) => date >= startDate;
+
+  return <CalendarMonth validators={[disablePreStartDates]} date={endDate} rangeStart={startDate} />;
+};
