6c71cb916305af0fb4b367b077a6f44041f4d300
Katie McFaul <kmcfaul@redhat.com>
2022-04-04 15:50:13 -0400

    feat(Brand): allow picture with child elements and breakpoints, add example and test

--- packages/react-core/src/components/Brand/__tests__/Brand.test.tsx
+++ packages/react-core/src/components/Brand/__tests__/Brand.test.tsx
@@ -1,8 +1,19 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Brand } from '../Brand';
+import '@testing-library/jest-dom';
 
-test('simple brand', () => {
-  const view = render(<Brand alt="brand" />);
-  expect(view.container).toMatchSnapshot();
+describe('Brand', () => {
+  test('simple brand', () => {
+    const view = render(<Brand alt="brand" />);
+    expect(view.container).toMatchSnapshot();
+  });
+  test('passing children creates picture brand', () => {
+    const view = render(
+      <Brand>
+        <div>test</div>
+      </Brand>
+    );
+    expect(screen.getByText('test')).toBeInTheDocument();
+  });
 });
