9c6324534821b0d69f136917c5b6d8c5b5f704aa
Titani <tlabaj@redaht.com>
2022-04-07 11:59:20 -0400

    updated unit test

--- packages/react-core/src/components/ProgressStepper/__tests__/ProgressStepper.test.tsx
+++ packages/react-core/src/components/ProgressStepper/__tests__/ProgressStepper.test.tsx
@@ -87,7 +87,7 @@ describe('ProgressStep', () => {
   });
 
   test('renders help text styling', () => {
-    const view = render(<ProgressStep popover={<div></div>}>Title</ProgressStep>);
+    const view = render(<ProgressStep render={({ stepRef }) => (<div></div>)}>Title</ProgressStep>);
     expect(view.container).toMatchSnapshot();
   });
 
