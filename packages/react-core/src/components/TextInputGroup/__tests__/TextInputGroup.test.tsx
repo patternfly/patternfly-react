ba6cf3a0dcc2a100001911b7020ebc8edb9b3a65
Austin Sullivan <ausulliv@redhat.com>
2022-04-13 14:26:03 -0700

    chore(TextInputGroup): update testing suite to new standards (#7169)
    * chore(TextInputGroup): update testing suite to new standards
    
    * update failing snapshot

--- packages/react-core/src/components/TextInputGroup/__tests__/TextInputGroup.test.tsx
+++ packages/react-core/src/components/TextInputGroup/__tests__/TextInputGroup.test.tsx
@@ -1,49 +1,59 @@
 import React from 'react';
-
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
 
 import { TextInputGroup, TextInputGroupContext } from '../TextInputGroup';
-import { TextInputGroupMain } from '../TextInputGroupMain';
-import { TextInputGroupUtilities } from '../TextInputGroupUtilities';
-import SearchIcon from '@patternfly/react-icons/dist/esm/icons/search-icon';
 
 describe('TextInputGroup', () => {
-  test('renders content', () => {
-    const { asFragment } = render(
-      <TextInputGroup>
-        <TextInputGroupMain>Foo</TextInputGroupMain>
-        <TextInputGroupUtilities />
-      </TextInputGroup>
-    );
-    expect(asFragment()).toMatchSnapshot();
+  it('renders without children', () => {
+    render(<TextInputGroup data-testid="TextInputGroup" />);
+
+    expect(screen.getByTestId('TextInputGroup')).toBeVisible();
   });
 
-  test('gets custom class and id', () => {
-    const { asFragment } = render(
-      <TextInputGroup className="custom-class" id="test-id">
-        <TextInputGroupMain>Foo</TextInputGroupMain>
-        <TextInputGroupUtilities />
-      </TextInputGroup>
-    );
-    expect(asFragment()).toMatchSnapshot();
+  it('renders children', () => {
+    render(<TextInputGroup>Test</TextInputGroup>);
+
+    expect(screen.getByText('Test')).toBeVisible();
   });
 
-  test('renders with the proper stying when disabled', () => {
-    const { asFragment } = render(
-      <TextInputGroup isDisabled>
-        <TextInputGroupMain>Foo</TextInputGroupMain>
-        <TextInputGroupUtilities />
-      </TextInputGroup>
-    );
-    expect(asFragment()).toMatchSnapshot();
+  it('renders with class pf-c-text-input-group', () => {
+    render(<TextInputGroup>Test</TextInputGroup>);
+
+    const inputGroup = screen.getByText('Test');
+
+    expect(inputGroup).toHaveClass('pf-c-text-input-group');
   });
 
-  test('passes isDisabled=false to children via a context when isDisabled prop is not passed', () => {
+  it('renders with custom class names provided via prop', () => {
+    render(<TextInputGroup className="custom-class">Test</TextInputGroup>);
+
+    const inputGroup = screen.getByText('Test');
+
+    expect(inputGroup).toHaveClass('custom-class');
+  });
+
+  it('does not render with the pf-m-disabled class when not disabled', () => {
+    render(<TextInputGroup>Test</TextInputGroup>);
+
+    const inputGroup = screen.getByText('Test');
+
+    expect(inputGroup).not.toHaveClass('pf-m-disabled');
+  });
+
+  it('renders with the pf-m-disabled class when disabled', () => {
+    render(<TextInputGroup isDisabled>Test</TextInputGroup>);
+
+    const inputGroup = screen.getByText('Test');
+
+    expect(inputGroup).toHaveClass('pf-m-disabled');
+  });
+
+  it('passes isDisabled=false to children via a context when isDisabled prop is not passed', () => {
     const TestComponent: React.FunctionComponent = () => {
       const context = React.useContext(TextInputGroupContext);
 
-      return <div>{context.isDisabled ? 'is disabled' : 'not disabled'}</div>;
+      return <button disabled={context.isDisabled} />;
     };
 
     render(
@@ -52,14 +62,16 @@ describe('TextInputGroup', () => {
       </TextInputGroup>
     );
 
-    expect(screen.queryByText('is disabled')).toBeNull();
+    const testComponent = screen.getByRole('button');
+
+    expect(testComponent).not.toBeDisabled();
   });
 
-  test('passes isDisabled=true to children via a context when isDisabled prop is passed', () => {
+  it('passes isDisabled=true to children via a context when isDisabled prop is passed', () => {
     const TestComponent: React.FunctionComponent = () => {
       const context = React.useContext(TextInputGroupContext);
 
-      return <div>{context.isDisabled ? 'is disabled' : 'not disabled'}</div>;
+      return <button disabled={context.isDisabled} />;
     };
 
     render(
@@ -68,125 +80,14 @@ describe('TextInputGroup', () => {
       </TextInputGroup>
     );
 
-    expect(screen.queryByText('not disabled')).toBeNull();
-  });
-});
+    const testComponent = screen.getByRole('button');
 
-describe('TextInputGroupMain', () => {
-  test('renders content', () => {
-    const { asFragment } = render(<TextInputGroupMain>Foo</TextInputGroupMain>);
-    expect(asFragment()).toMatchSnapshot();
+    expect(testComponent).toBeDisabled();
   });
 
-  test('renders given input icon props', () => {
-    const { asFragment } = render(<TextInputGroupMain icon={<SearchIcon />}>Foo</TextInputGroupMain>);
-    expect(asFragment()).toMatchSnapshot();
-  });
+  it('matches the snapshot', () => {
+    const { asFragment } = render(<TextInputGroup>Test</TextInputGroup>);
 
-  test('renders the input with custom aria label when given', () => {
-    const { asFragment } = render(<TextInputGroupMain aria-label="Foo">Foo</TextInputGroupMain>);
-    expect(asFragment()).toMatchSnapshot();
-  });
-
-  test('does not call onChange callback when the input does not change', () => {
-    const onChangeMock = jest.fn();
-
-    render(<TextInputGroupMain onChange={onChangeMock}>Foo</TextInputGroupMain>);
-    expect(onChangeMock).not.toHaveBeenCalled();
-  });
-
-  test('calls the onChange callback when the input changes', () => {
-    const onChangeMock = jest.fn();
-
-    render(<TextInputGroupMain onChange={onChangeMock}>Foo</TextInputGroupMain>);
-
-    userEvent.type(screen.getByText('Foo').querySelector('input'), 'Foo');
-    expect(onChangeMock).toHaveBeenCalled();
-  });
-
-  test('does not call onFocus callback when the input does not get focus', () => {
-    const onFocusMock = jest.fn();
-
-    render(<TextInputGroupMain onFocus={onFocusMock}>Foo</TextInputGroupMain>);
-    expect(onFocusMock).not.toHaveBeenCalled();
-  });
-
-  test('calls the onFocus callback when the input is focused', () => {
-    const onFocusMock = jest.fn();
-
-    render(<TextInputGroupMain onFocus={onFocusMock}>Foo</TextInputGroupMain>);
-    userEvent.click(screen.getByText('Foo').querySelector('input'));
-    expect(onFocusMock).toHaveBeenCalledTimes(1);
-  });
-
-  test('does not call onBlur callback when the input does not lose focus', () => {
-    const onBlurMock = jest.fn();
-
-    render(<TextInputGroupMain onBlur={onBlurMock}>Foo</TextInputGroupMain>);
-
-    userEvent.type(screen.getByText('Foo').querySelector('input'), 'Foo');
-    expect(onBlurMock).not.toHaveBeenCalled();
-  });
-
-  test('calls the onFocus callback when the input loses focus', () => {
-    const onBlurMock = jest.fn();
-
-    render(
-      <>
-        <div>Other element</div>
-        <TextInputGroupMain onBlur={onBlurMock}>Foo</TextInputGroupMain>
-      </>
-    );
-
-    userEvent.type(screen.getByText('Foo').querySelector('input'), 'Foo');
-    userEvent.click(screen.getByText('Other element'));
-
-    expect(onBlurMock).toHaveBeenCalled();
-  });
-
-  test('input type defaults to text', () => {
-    render(<TextInputGroupMain>Foo</TextInputGroupMain>);
-    expect(
-      screen
-        .getByText('Foo')
-        .querySelector('input')
-        .getAttribute('type')
-    ).toEqual('text');
-  });
-
-  test('input type is updated when a different type is passed', () => {
-    render(<TextInputGroupMain type="search">Foo</TextInputGroupMain>);
-    expect(
-      screen
-        .getByText('Foo')
-        .querySelector('input')
-        .getAttribute('type')
-    ).toEqual('search');
-  });
-
-  test('input is not disabled when TextInputGroupContext isDisabled is false', () => {
-    const { asFragment } = render(
-      <TextInputGroupContext.Provider value={{ isDisabled: false }}>
-        <TextInputGroupMain />
-      </TextInputGroupContext.Provider>
-    );
-
-    expect(asFragment()).toMatchSnapshot();
-  });
-
-  test('input is disabled when TextInputGroupContext isDisabled is true', () => {
-    const { asFragment } = render(
-      <TextInputGroupContext.Provider value={{ isDisabled: true }}>
-        <TextInputGroupMain />
-      </TextInputGroupContext.Provider>
-    );
-    expect(asFragment()).toMatchSnapshot();
-  });
-});
-
-describe('TextInputGroupUtilities', () => {
-  test('renders the content', () => {
-    const { asFragment } = render(<TextInputGroupUtilities>{<button>Foo</button>}</TextInputGroupUtilities>);
     expect(asFragment()).toMatchSnapshot();
   });
 });
