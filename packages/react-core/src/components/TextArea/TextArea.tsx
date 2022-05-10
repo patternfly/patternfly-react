f69f9840f8f326b1369822584d9fc43113d2a1c8
Jon Koops <jonkoops@gmail.com>
2022-05-10 17:01:15 +0200

    Use correct type for TextArea ref

--- packages/react-core/src/components/TextArea/TextArea.tsx
+++ packages/react-core/src/components/TextArea/TextArea.tsx
@@ -125,7 +125,7 @@ export class TextAreaBase extends React.Component<TextAreaProps> {
   }
 }
 
-export const TextArea = React.forwardRef((props: TextAreaProps, ref: React.Ref<HTMLInputElement>) => (
+export const TextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>((props, ref) => (
   <TextAreaBase {...props} innerRef={ref as React.MutableRefObject<any>} />
 ));
 TextArea.displayName = 'TextArea';
