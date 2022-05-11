fafdb18f12fe8564b3098bd795bac9f19cb3b745
Katie McFaul <kmcfaul@redhat.com>
2022-05-11 17:23:50 -0400

    fix(Tabs): make close handler optional

--- packages/react-core/src/components/Tabs/TabsContext.ts
+++ packages/react-core/src/components/Tabs/TabsContext.ts
@@ -11,7 +11,7 @@ export interface TabsContextProps {
     eventKey: number | string,
     tabContentRef: React.RefObject<any>
   ) => void;
-  handleTabClose: (
+  handleTabClose?: (
     event: React.MouseEvent<HTMLElement, MouseEvent>,
     eventKey: number | string,
     tabContentRef?: React.RefObject<any>
