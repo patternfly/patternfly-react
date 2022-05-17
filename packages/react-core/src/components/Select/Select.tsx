693a0eb4574fe1f2fdfcada8a89d54e47c4766c6
Eric Olkowski <thatblindgeye@gmail.com>
2022-05-17 12:11:26 -0400

    fix(Select): popper menu flips correctly

--- packages/react-core/src/components/Select/Select.tsx
+++ packages/react-core/src/components/Select/Select.tsx
@@ -173,6 +173,11 @@ export interface SelectProps
    * menuAppendTo={document.getElementById('target')}
    */
   menuAppendTo?: HTMLElement | (() => HTMLElement) | 'inline' | 'parent';
+  /** Flag for indicating that the select menu should automatically flip vertically when
+   * it reaches the boundary. This prop can only be used when the select component is not
+   * appended inline, e.g. `menuAppendTo="parent"`
+   */
+  isFlippable?: boolean;
 }
 
 export interface SelectState {
@@ -245,7 +250,8 @@ export class Select extends React.Component<SelectProps & OUIAProps, SelectState
     isInputValuePersisted: false,
     isInputFilterPersisted: false,
     isCreateSelectOptionObject: false,
-    shouldResetOnSelect: true
+    shouldResetOnSelect: true,
+    isFlippable: false
   };
 
   state: SelectState = {
@@ -978,6 +984,7 @@ export class Select extends React.Component<SelectProps & OUIAProps, SelectState
       loadingVariant,
       isCreateSelectOptionObject,
       shouldResetOnSelect,
+      isFlippable,
       ...props
     } = this.props;
     const {
@@ -1204,6 +1211,10 @@ export class Select extends React.Component<SelectProps & OUIAProps, SelectState
 
     const innerMenu = (
       <SelectMenu
+        // This removes the `position: absolute` styling from the `.pf-c-select__menu` element,
+        // which was causing incorrect positioning of the popper and making it seem like the
+        // menu wasn't flipping.
+        {...(isFlippable && { style: { position: 'revert' } })}
         {...props}
         isGrouped={isGrouped}
         selected={selections}
