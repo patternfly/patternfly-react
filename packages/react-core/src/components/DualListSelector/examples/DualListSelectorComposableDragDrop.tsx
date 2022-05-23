8c90aaf8e97e97b214898fa090ce175b128a919d
Eric Olkowski <thatblindgeye@gmail.com>
2022-05-23 12:10:28 -0400

    Convert drag and drop example

--- packages/react-core/src/components/DualListSelector/examples/DualListSelectorComposableDragDrop.tsx
+++ packages/react-core/src/components/DualListSelector/examples/DualListSelectorComposableDragDrop.tsx
@@ -1,36 +1,41 @@
 import React from 'react';
 import {
-  Button,
-  ButtonVariant,
+  DragDrop,
+  Droppable,
+  Draggable,
   DualListSelector,
   DualListSelectorPane,
   DualListSelectorList,
   DualListSelectorListItem,
   DualListSelectorControlsWrapper,
-  DualListSelectorControl,
-  SearchInput
+  DualListSelectorControl
 } from '@patternfly/react-core';
 import AngleDoubleLeftIcon from '@patternfly/react-icons/dist/esm/icons/angle-double-left-icon';
 import AngleLeftIcon from '@patternfly/react-icons/dist/esm/icons/angle-left-icon';
 import AngleDoubleRightIcon from '@patternfly/react-icons/dist/esm/icons/angle-double-right-icon';
 import AngleRightIcon from '@patternfly/react-icons/dist/esm/icons/angle-right-icon';
-import PficonSortCommonAscIcon from '@patternfly/react-icons/dist/esm/icons/pficon-sort-common-asc-icon';
 
-export const DualListSelectorComposable: React.FunctionComponent = () => {
+interface SourceType {
+  droppableId: string;
+  index: number;
+}
+
+interface DestinationType extends SourceType {}
+
+export const DualListSelectorComposableDragDrop: React.FunctionComponent = () => {
+  const [ignoreNextOptionSelect, setIgnoreNextOptionSelect] = React.useState(false);
   const [availableOptions, setAvailableOptions] = React.useState([
     { text: 'Apple', selected: false, isVisible: true },
     { text: 'Banana', selected: false, isVisible: true },
-    { text: 'Pineapple', selected: false, isVisible: true },
+    { text: 'Pineapple', selected: false, isVisible: true }
+  ]);
+  const [chosenOptions, setChosenOptions] = React.useState([
     { text: 'Orange', selected: false, isVisible: true },
     { text: 'Grape', selected: false, isVisible: true },
     { text: 'Peach', selected: false, isVisible: true },
     { text: 'Strawberry', selected: false, isVisible: true }
   ]);
-  const [chosenOptions, setChosenOptions] = React.useState([]);
-  const [availableFilter, setAvailableFilter] = React.useState('');
-  const [chosenFilter, setChosenFilter] = React.useState('');
 
-  // callback for moving selected options between lists
   const moveSelected = (fromAvailable: boolean) => {
     const sourceOptions = fromAvailable ? availableOptions : chosenOptions;
     const destinationOptions = fromAvailable ? chosenOptions : availableOptions;
@@ -52,7 +57,6 @@ export const DualListSelectorComposable: React.FunctionComponent = () => {
     }
   };
 
-  // callback for moving all options between lists
   const moveAll = (fromAvailable: boolean) => {
     if (fromAvailable) {
       setChosenOptions([...availableOptions.filter(option => option.isVisible), ...chosenOptions]);
@@ -63,12 +67,15 @@ export const DualListSelectorComposable: React.FunctionComponent = () => {
     }
   };
 
-  // callback when option is selected
   const onOptionSelect = (
     event: React.MouseEvent | React.ChangeEvent | React.KeyboardEvent,
     index: number,
     isChosen: boolean
   ) => {
+    if (ignoreNextOptionSelect) {
+      setIgnoreNextOptionSelect(false);
+      return;
+    }
     if (isChosen) {
       const newChosen = [...chosenOptions];
       newChosen[index].selected = !chosenOptions[index].selected;
@@ -80,50 +87,15 @@ export const DualListSelectorComposable: React.FunctionComponent = () => {
     }
   };
 
-  // builds a search input - used in each dual list selector pane
-  const buildSearchInput = (isAvailable: boolean) => {
-    const onChange = (value: string) => {
-      isAvailable ? setAvailableFilter(value) : setChosenFilter(value);
-      const toFilter = isAvailable ? [...availableOptions] : [...chosenOptions];
-      toFilter.forEach(option => {
-        option.isVisible = value === '' || option.text.toLowerCase().includes(value.toLowerCase());
-      });
-    };
-
-    return (
-      <SearchInput
-        value={isAvailable ? availableFilter : chosenFilter}
-        onChange={onChange}
-        onClear={() => onChange('')}
-      />
-    );
-  };
-
-  // builds a sort control - passed to both dual list selector panes
-  const buildSort = (isAvailable: boolean) => {
-    const onSort = () => {
-      const toSort = isAvailable ? [...availableOptions] : [...chosenOptions];
-      toSort.sort((a, b) => {
-        if (a.text > b.text) {
-          return 1;
-        }
-        if (a.text < b.text) {
-          return -1;
-        }
-        return 0;
-      });
-      if (isAvailable) {
-        setAvailableOptions(toSort);
-      } else {
-        setChosenOptions(toSort);
-      }
-    };
-
-    return (
-      <Button variant={ButtonVariant.plain} onClick={onSort} aria-label="Sort" key="sortButton">
-        <PficonSortCommonAscIcon />
-      </Button>
-    );
+  const onDrop = (source: SourceType, dest: DestinationType) => {
+    if (dest) {
+      const newList = [...chosenOptions];
+      const [removed] = newList.splice(source.index, 1);
+      newList.splice(dest.index, 0, removed);
+      setChosenOptions(newList);
+      return true;
+    }
+    return false;
   };
 
   return (
@@ -133,8 +105,6 @@ export const DualListSelectorComposable: React.FunctionComponent = () => {
         status={`${availableOptions.filter(option => option.selected && option.isVisible).length} of ${
           availableOptions.filter(option => option.isVisible).length
         } options selected`}
-        searchInput={buildSearchInput(true)}
-        actions={[buildSort(true)]}
       >
         <DualListSelectorList>
           {availableOptions.map((option, index) =>
@@ -142,7 +112,7 @@ export const DualListSelectorComposable: React.FunctionComponent = () => {
               <DualListSelectorListItem
                 key={index}
                 isSelected={option.selected}
-                id={`composable-available-option-${index}`}
+                id={`composable-drag-drop-available-option-${index}`}
                 onOptionSelect={e => onOptionSelect(e, index, false)}
               >
                 {option.text}
@@ -156,7 +126,6 @@ export const DualListSelectorComposable: React.FunctionComponent = () => {
           isDisabled={!availableOptions.some(option => option.selected)}
           onClick={() => moveSelected(true)}
           aria-label="Add selected"
-          tooltipContent="Add selected"
         >
           <AngleRightIcon />
         </DualListSelectorControl>
@@ -164,7 +133,6 @@ export const DualListSelectorComposable: React.FunctionComponent = () => {
           isDisabled={availableOptions.length === 0}
           onClick={() => moveAll(true)}
           aria-label="Add all"
-          tooltipContent="Add all"
         >
           <AngleDoubleRightIcon />
         </DualListSelectorControl>
@@ -172,7 +140,6 @@ export const DualListSelectorComposable: React.FunctionComponent = () => {
           isDisabled={chosenOptions.length === 0}
           onClick={() => moveAll(false)}
           aria-label="Remove all"
-          tooltipContent="Remove all"
         >
           <AngleDoubleLeftIcon />
         </DualListSelectorControl>
@@ -180,35 +147,44 @@ export const DualListSelectorComposable: React.FunctionComponent = () => {
           onClick={() => moveSelected(false)}
           isDisabled={!chosenOptions.some(option => option.selected)}
           aria-label="Remove selected"
-          tooltipContent="Remove selected"
         >
           <AngleLeftIcon />
         </DualListSelectorControl>
       </DualListSelectorControlsWrapper>
-      <DualListSelectorPane
-        title="Chosen"
-        status={`${chosenOptions.filter(option => option.selected && option.isVisible).length} of ${
-          chosenOptions.filter(option => option.isVisible).length
-        } options selected`}
-        searchInput={buildSearchInput(false)}
-        actions={[buildSort(false)]}
-        isChosen
+      <DragDrop
+        onDrag={() => {
+          setIgnoreNextOptionSelect(true);
+          return true;
+        }}
+        onDrop={onDrop}
       >
-        <DualListSelectorList>
-          {chosenOptions.map((option, index) =>
-            option.isVisible ? (
-              <DualListSelectorListItem
-                key={index}
-                isSelected={option.selected}
-                id={`composable-chosen-option-${index}`}
-                onOptionSelect={e => onOptionSelect(e, index, true)}
-              >
-                {option.text}
-              </DualListSelectorListItem>
-            ) : null
-          )}
-        </DualListSelectorList>
-      </DualListSelectorPane>
+        <DualListSelectorPane
+          title="Chosen"
+          status={`${chosenOptions.filter(option => option.selected && option.isVisible).length} of ${
+            chosenOptions.filter(option => option.isVisible).length
+          } options selected`}
+          isChosen
+        >
+          <Droppable hasNoWrapper>
+            <DualListSelectorList>
+              {chosenOptions.map((option, index) =>
+                option.isVisible ? (
+                  <Draggable key={index} hasNoWrapper>
+                    <DualListSelectorListItem
+                      isSelected={option.selected}
+                      id={`composable-drag-drop-chosen-option-${index}`}
+                      onOptionSelect={e => onOptionSelect(e, index, true)}
+                      isDraggable
+                    >
+                      {option.text}
+                    </DualListSelectorListItem>
+                  </Draggable>
+                ) : null
+              )}
+            </DualListSelectorList>
+          </Droppable>
+        </DualListSelectorPane>
+      </DragDrop>
     </DualListSelector>
   );
 };
