47f8d12e348397336287c7e7efc79665bce61ae2
Eric Olkowski <70952936+thatblindgeye@users.noreply.github.com>
2022-05-26 16:36:13 -0400

    feat(Popper): add mouse events for popper content (#7399)

--- packages/react-core/src/helpers/Popper/Popper.tsx
+++ packages/react-core/src/helpers/Popper/Popper.tsx
@@ -95,6 +95,10 @@ export interface PopperProps {
   onTriggerEnter?: (event?: KeyboardEvent) => void;
   /** Callback function when popper is clicked */
   onPopperClick?: (event?: MouseEvent) => void;
+  /** Callback function when mouse enters popper content */
+  onPopperMouseEnter?: (event?: MouseEvent) => void;
+  /** Callback function when mouse leaves popper content */
+  onPopperMouseLeave?: (event?: MouseEvent) => void;
   /** Callback function when document is clicked */
   onDocumentClick?: (event?: MouseEvent, triggerElement?: HTMLElement, popperElement?: HTMLElement) => void;
   /** Callback function when keydown event occurs on document */
@@ -140,6 +144,8 @@ export const Popper: React.FunctionComponent<PopperProps> = ({
   onTriggerClick,
   onTriggerEnter,
   onPopperClick,
+  onPopperMouseEnter,
+  onPopperMouseLeave,
   onDocumentKeyDown,
   enableFlip = true,
   flipBehavior = 'flip',
@@ -186,6 +192,8 @@ export const Popper: React.FunctionComponent<PopperProps> = ({
     addEventListener(onTriggerClick, refOrTrigger, 'click');
     addEventListener(onTriggerEnter, refOrTrigger, 'keydown');
     addEventListener(onPopperClick, popperElement, 'click');
+    addEventListener(onPopperMouseEnter, popperElement, 'mouseenter');
+    addEventListener(onPopperMouseLeave, popperElement, 'mouseleave');
     onDocumentClick && addEventListener(onDocumentClickCallback, document, 'click');
     addEventListener(onDocumentKeyDown, document, 'keydown');
 
@@ -203,6 +211,8 @@ export const Popper: React.FunctionComponent<PopperProps> = ({
       removeEventListener(onTriggerClick, refOrTrigger, 'click');
       removeEventListener(onTriggerEnter, refOrTrigger, 'keydown');
       removeEventListener(onPopperClick, popperElement, 'click');
+      removeEventListener(onPopperMouseEnter, popperElement, 'mouseenter');
+      removeEventListener(onPopperMouseLeave, popperElement, 'mouseleave');
       onDocumentClick && removeEventListener(onDocumentClickCallback, document, 'click');
       removeEventListener(onDocumentKeyDown, document, 'keydown');
       observer.disconnect();
@@ -217,6 +227,8 @@ export const Popper: React.FunctionComponent<PopperProps> = ({
     onTriggerClick,
     onTriggerEnter,
     onPopperClick,
+    onPopperMouseEnter,
+    onPopperMouseLeave,
     onDocumentClick,
     onDocumentKeyDown,
     refElement
