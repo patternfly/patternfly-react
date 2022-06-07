500a6152f3e150afcc3d5fd09e81a8c5fb2972bd
Austin Sullivan <ausulliv@redhat.com>
2022-06-07 11:27:40 -0400

    expand demo text length

--- packages/react-core/src/demos/examples/JumpLinks/JumpLinksWithDrawer.js
+++ packages/react-core/src/demos/examples/JumpLinks/JumpLinksWithDrawer.js
@@ -98,6 +98,18 @@ export const JumpLinksWithDrawer = () => {
                           augue vehicula eros pulvinar vehicula eget rutrum neque. Duis sit amet interdum lorem. Vivamus
                           porttitor nec quam a accumsan. Nam pretium vitae leo vitae rhoncus.
                         </p>
+                        <p>
+                          At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
+                          voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati
+                          cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est
+                          laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero
+                          tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime
+                          placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus
+                          autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et
+                          voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a
+                          sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis
+                          doloribus asperiores repellat.
+                        </p>
                       </div>
                     ))}
                   </TextContent>
