6c1eda812588fff3b24cef68fb656db89efe5e61
Eric Olkowski <thatblindgeye@gmail.com>
2022-06-07 09:24:18 -0400

    Add group role to form section and field group components

--- packages/react-core/src/components/Form/__tests__/FormFieldGroup.test.tsx
+++ packages/react-core/src/components/Form/__tests__/FormFieldGroup.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import { FormFieldGroup } from '../FormFieldGroup';
 import { FormFieldGroupExpandable } from '../FormFieldGroupExpandable';
@@ -52,4 +52,26 @@ test('Verify console error logged when there is no aria-label or title', () => {
   );
   const { asFragment } = render(FieldGroup);
   expect(consoleErrorMock).toHaveBeenCalled();
+});
+
+test('Verify field group has accessible name when header is passed in', () => {
+  render(
+    <FormFieldGroup
+      header={
+        <FormFieldGroupHeader
+          titleText={{ text: 'Field group 4 (non-expandable)', id: 'title-text-id1' }}
+          titleDescription="Field group 4 description text."
+          actions={<Button />}
+        />
+      }
+    />
+  );
+
+  expect(screen.getByRole('group')).toHaveAccessibleName('Field group 4 (non-expandable)');
+});
+
+test('Verify field group does not have accessible name when header is not passed in', () => {
+  render(<FormFieldGroup data-testid="field-group-test-id" />);
+
+  expect(screen.getByRole('group')).not.toHaveAccessibleName();
 });
--- packages/react-core/src/components/Form/__tests__/FormSection.test.tsx
+++ packages/react-core/src/components/Form/__tests__/FormSection.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { FormSection } from '../FormSection';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 test('Check form section example against snapshot', () => {
   const Section = <FormSection />;
@@ -12,4 +12,16 @@ test('Check form section example with title', () => {
   const Section = <FormSection title="Title" titleElement="h4" />;
   const { asFragment } = render(Section);
   expect(asFragment()).toMatchSnapshot();
+});
+
+test('Verify form section has accessible name when title is passed in', () => {
+  render(<FormSection title="Form title" />);
+
+  expect(screen.getByRole('group')).toHaveAccessibleName('Form title');
+});
+
+test('Verify form section does not have accessible name when title is not passed in', () => {
+  render(<FormSection />);
+
+  expect(screen.getByRole('group')).not.toHaveAccessibleName();
 });
