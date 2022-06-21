cf2fa18e60f54903827736666ccd1c4c6adc4cb4
Eric Olkowski <thatblindgeye@gmail.com>
2022-06-21 14:12:20 -0400

    Complete remaining conversions

---
+++ packages/react-core/src/components/Nav/examples/NavLegacyTertiary.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Nav, NavItem, NavList } from '@patternfly/react-core';
+
+export const NavLegacyTertiary: React.FunctionComponent = () => {
+  const [activeItem, setActiveItem] = React.useState(0);
+
+  const onSelect = (result: { itemId: number | string }) => {
+    setActiveItem(result.itemId as number);
+  };
+
+  return (
+    <Nav onSelect={onSelect} variant="tertiary">
+      <NavList>
+        {Array.apply(0, Array(10)).map(function(_item, index: number) {
+          const num = index + 1;
+          return (
+            <NavItem
+              preventDefault
+              key={num}
+              itemId={num}
+              isActive={activeItem === num}
+              id={`legacy-tertiary-${num}`}
+              to={`#legacy-tertiary-${num}`}
+            >
+              Horizontal nav item {num}
+            </NavItem>
+          );
+        })}
+      </NavList>
+    </Nav>
+  );
+};
