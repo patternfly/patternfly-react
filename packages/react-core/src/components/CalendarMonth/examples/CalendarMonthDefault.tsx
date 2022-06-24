6985dbc9b652e9f2fcc33dad4366ca0f64935d5b
nicolethoen <nthoen@redhat.com>
2022-06-24 14:03:17 -0400

    make console log statement clearer

--- packages/react-core/src/components/CalendarMonth/examples/CalendarMonthDefault.tsx
+++ packages/react-core/src/components/CalendarMonth/examples/CalendarMonthDefault.tsx
@@ -5,7 +5,7 @@ export const CalendarMonthDefault: React.FunctionComponent = () => {
   const [date, setDate] = React.useState(null);
   const onMonthChange = (newDate: Date) => {
     // eslint-disable-next-line no-console
-    console.log(newDate);
+    console.log(`updated month: ${newDate.getMonth()}, updated year: ${newDate.getFullYear()}`);
   };
 
   return <CalendarMonth date={date} onChange={setDate} onMonthChange={onMonthChange} />;
