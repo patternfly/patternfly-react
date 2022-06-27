740ca8bc9859adaf9d1bc578ab55421fbb54cf3b
jenny-s51 <jshandel@redhat.com>
2022-06-27 15:17:49 -0400

    rebase and fix conflicts

--- packages/react-core/src/components/Page/PageSidebar.tsx
+++ packages/react-core/src/components/Page/PageSidebar.tsx
@@ -51,7 +51,7 @@ export const PageSidebar: React.FunctionComponent<PageSidebarProps> = ({
           aria-hidden={!navOpen}
           {...props}
         >
-          <div className={styles.pageSidebarBody}>
+          <div className={css(styles.pageSidebarBody, 'pf-topology-side-bar__body')}>
             <PageSidebarContext.Provider value={{ isNavOpen: navOpen }}>{nav}</PageSidebarContext.Provider>
           </div>
         </div>
