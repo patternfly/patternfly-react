f4d9a56660b2234ab4466158b346f87a79a84d2c
nicolethoen <nthoen@redhat.com>
2022-07-08 21:01:55 -0400

    revert default example

--- packages/react-core/src/components/CalendarMonth/examples/CalendarMonthDefault.tsx
+++ packages/react-core/src/components/CalendarMonth/examples/CalendarMonthDefault.tsx
@@ -1,12 +1,4 @@
 import React from 'react';
 import { CalendarMonth } from '@patternfly/react-core';
 
-export const CalendarMonthDefault: React.FunctionComponent = () => {
-  const [date, setDate] = React.useState(null);
-  const onMonthChange = (newDate: Date) => {
-    // eslint-disable-next-line no-console
-    console.log(`updated month: ${newDate.getMonth()}, updated year: ${newDate.getFullYear()}`);
-  };
-
-  return <CalendarMonth date={date} onChange={setDate} onMonthChange={onMonthChange} />;
-};
+export const CalendarMonthDefault: React.FunctionComponent = () => <CalendarMonth date={new Date()} />;
--- packages/react-core/src/components/CalendarMonth/examples/CalendarMonthSelectableDate.tsx
+++ packages/react-core/src/components/CalendarMonth/examples/CalendarMonthSelectableDate.tsx
@@ -4,10 +4,15 @@ import { CalendarMonth } from '@patternfly/react-core';
 export const CalendarMonthSelectableDate: React.FunctionComponent = () => {
   const [date, setDate] = React.useState(new Date(2020, 10, 24));
 
+  const onMonthChange = (newDate: Date) => {
+    // eslint-disable-next-line no-console
+    console.log(`updated month: ${newDate.getMonth()}, updated year: ${newDate.getFullYear()}`);
+  };
+
   return (
     <React.Fragment>
       <pre>Selected date: {date.toString()}</pre>
-      <CalendarMonth date={date} onChange={setDate} />
+      <CalendarMonth date={date} onChange={setDate} onMonthChange={onMonthChange} />
     </React.Fragment>
   );
 };
