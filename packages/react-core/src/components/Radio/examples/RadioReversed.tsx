596bfb9d8f458832ef669b323df379b39506a715
Dominik Petřík <dominik.petrik@tiscali.cz>
2022-07-18 15:40:23 +0200

    chore(Radio): convert examples to TypeScript

---
+++ packages/react-core/src/components/Radio/examples/RadioReversed.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Radio } from '@patternfly/react-core';
+
+export const RadioReversed: React.FunctionComponent = () => (
+  <Radio isLabelBeforeButton label="Reversed radio example" id="radio-reversed" name="radio-3" />
+);
