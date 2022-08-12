18d2cbe1d4840a807ca93e8875e6d361ade764ab
Dominik Petřík <dominik.petrik@tiscali.cz>
2022-08-12 12:52:21 +0200

    chore(Avatar) update tests to new standarts

--- packages/react-core/src/components/Avatar/__tests__/Avatar.test.tsx
+++ packages/react-core/src/components/Avatar/__tests__/Avatar.test.tsx
@@ -1,8 +1,20 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import {render, screen } from '@testing-library/react';
 import { Avatar } from '../Avatar';
 
-test('simple avatar', () => {
-  const { asFragment } = render(<Avatar alt="avatar" src="test.png" border="light" />);
-  expect(asFragment()).toMatchSnapshot();
+test('renders simple avatar', () => {
+  render(<Avatar alt="avatar" data-testid="avatar" src="test.png" border="light" />);
+  expect(screen.getByTestId('avatar')).toBeVisible();
 });
+
+test('renders with class name pf-m-light when "light" is passed as border prop', () => {
+  render(<Avatar alt="avatar" data-testid="avatar" src="test.png" border="light" />);
+  expect(screen.getByTestId('avatar')).toHaveClass('pf-m-light');
+});
+
+test('renders with class name pf-m-lg when "lg" is passed as size prop', () => {
+  render(<Avatar alt="avatar" data-testid="avatar" src="test.png" size='lg' />);
+  expect(screen.getByTestId('avatar')).toHaveClass('pf-m-lg');
+});
+
+
