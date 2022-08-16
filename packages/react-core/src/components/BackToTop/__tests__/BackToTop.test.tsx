3b6600467d3df0f949b5631f5121a7e6a0138e25
Dominik Petřík <dominik.petrik@tiscali.cz>
2022-08-16 11:44:02 +0200

    chore(BackToTop) update tests to new standarts

--- packages/react-core/src/components/BackToTop/__tests__/BackToTop.test.tsx
+++ packages/react-core/src/components/BackToTop/__tests__/BackToTop.test.tsx
@@ -1,22 +1,39 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { BackToTop } from '../BackToTop';
+import userEvent from '@testing-library/user-event'
 
-describe('BackToTop', () => {
-  test('verify basic', () => {
-    const { asFragment } = render(<BackToTop />);
-    expect(asFragment()).toMatchSnapshot();
-  });
 
-  test('verify custom class', () => {
-    const { asFragment } = render(<BackToTop className="custom-css">test</BackToTop>);
+test('BackToTop button is not yet visible', () => {
+  render(<BackToTop data-testid="backtotop"/>)
+  
+  expect(screen.getByTestId("backtotop")).toHaveClass("pf-m-hidden")
+})
 
-    expect(asFragment()).toMatchSnapshot();
-  });
+test('BackToTop button is visible after scrolling', () => {
+  render(<BackToTop data-testid="backtotop"/>)
 
-  test('verify always show', () => {
-    const { asFragment } = render(<BackToTop isAlwaysVisible>test</BackToTop>);
+  fireEvent.scroll(window,{target: {scrollY: 401}})
+  expect(screen.getByTestId("backtotop")).not.toHaveClass("pf-m-hidden")
+})
 
-    expect(asFragment()).toMatchSnapshot();
-  });
-});
+test('ScrollTo event is fired after clicking BackToTop button', () => {
+  render(<BackToTop data-testid="backtotop"/>)
+
+  fireEvent.scroll(window,{target: {scrollY: 401}})
+  global.scrollTo = jest.fn()
+
+  userEvent.click(screen.getByTestId("backtotop"))
+  expect(global.scrollTo).toHaveBeenCalled()
+})
+
+test('Renders backToTop button when isAlwaysVisible is set', () => {
+  render(<BackToTop data-testid="backtotop" isAlwaysVisible/>)
+
+  expect(screen.getByTestId("backtotop")).toBeVisible()
+  expect(screen.getByTestId("backtotop")).not.toHaveClass("pf-m-hidden")
+})
+
+
+
+
