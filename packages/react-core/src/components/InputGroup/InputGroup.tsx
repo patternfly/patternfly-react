48baf76d2190e6f97cd5b3c0b3b9d51c67036651
gitdallas <dallas.nicol@gmail.com>
2022-08-16 14:46:02 -0500

    preventing inputgroup from overriding form control aria-describedby

--- packages/react-core/src/components/InputGroup/InputGroup.tsx
+++ packages/react-core/src/components/InputGroup/InputGroup.tsx
@@ -4,7 +4,6 @@ import { css } from '@patternfly/react-styles';
 import { FormSelect } from '../FormSelect';
 import { TextArea } from '../TextArea';
 import { TextInput } from '../TextInput';
-import { Button } from '../Button';
 
 export interface InputGroupProps extends React.HTMLProps<HTMLDivElement> {
   /** Additional classes added to the input group. */
@@ -21,7 +20,7 @@ export const InputGroup: React.FunctionComponent<InputGroupProps> = ({
   innerRef,
   ...props
 }: InputGroupProps) => {
-  const formCtrls = [FormSelect, TextArea, TextInput, Button].map(comp => comp.displayName);
+  const formCtrls = [FormSelect, TextArea, TextInput].map(comp => comp.displayName);
   const idItem = React.Children.toArray(children).find(
     (child: any) => !formCtrls.includes(child.type.displayName) && child.props.id
   ) as React.ReactElement<{ id: string }>;
@@ -32,9 +31,11 @@ export const InputGroup: React.FunctionComponent<InputGroupProps> = ({
     <div ref={inputGroupRef} className={css(styles.inputGroup, className)} {...props}>
       {idItem
         ? React.Children.map(children, (child: any) =>
-            formCtrls.includes(child.type.displayName)
-              ? React.cloneElement(child, { 'aria-describedby': idItem.props.id })
-              : child
+            !formCtrls.includes(child.type.displayName) || child.props['aria-describedby']
+              ? child
+              : React.cloneElement(child, {
+                  'aria-describedby': child.props['aria-describedby'] === '' ? undefined : idItem.props.id
+                })
           )
         : children}
     </div>
--- packages/react-core/src/components/InputGroup/__tests__/InputGroup.test.tsx
+++ packages/react-core/src/components/InputGroup/__tests__/InputGroup.test.tsx
@@ -4,20 +4,50 @@ import { render, screen } from '@testing-library/react';
 
 import { InputGroup } from '../InputGroup';
 import { TextInput } from '../../TextInput';
-import { Label } from '@patternfly/react-core';
+import { Button } from '@patternfly/react-core';
 
 describe('InputGroup', () => {
   test('add aria-describedby to form-control if one of the non form-controls has id', () => {
-    // In this test, TextInput is a form-control component and Label is not.
-    // If Label has an id props, this should be used in aria-describedby.
+    // In this test, TextInput is a form-control component and Button is not.
+    // If Button has an id props, this should be used in aria-describedby.
     render(
       <InputGroup>
         <TextInput value="some data" aria-label="some text" />
-        <Label id="label-id">
+        <Button id="button-id">
           hello
-        </Label>
+        </Button>
       </InputGroup>
     );
-    expect(screen.getByLabelText('some text')).toHaveAttribute('aria-describedby', 'label-id');
+    expect(screen.getByLabelText('some text')).toHaveAttribute('aria-describedby', 'button-id');
+  });
+
+  test('wont add aria-describedby to form-control if describedby is empty string', () => {
+    // In this test, TextInput is a form-control component and Button is not.
+    // If Button has an id props, this should be used in aria-describedby, but this
+    // example has an empty aria-describedby to prevent that from happening.
+    render(
+      <InputGroup>
+        <TextInput value="some data" aria-describedby="" aria-label="some text" />
+        <Button id="button-id">
+          hello
+        </Button>
+      </InputGroup>
+    );
+    expect(screen.getByLabelText('some text')).not.toHaveAttribute('aria-describedby');
+  });
+
+  test('wont override aria-describedby in form-control if describedby has value', () => {
+    // In this test, TextInput is a form-control component and Button is not.
+    // If Button has an id props, this should be used in aria-describedby, but this
+    // example has a predefined aria-describedby to prevent that from happening
+    render(
+      <InputGroup>
+        <TextInput value="some data" aria-describedby="myself" aria-label="some text" />
+        <Button id="button-id">
+          hello
+        </Button>
+      </InputGroup>
+    );
+    expect(screen.getByLabelText('some text')).toHaveAttribute('aria-describedby', 'myself');
   });
 });
