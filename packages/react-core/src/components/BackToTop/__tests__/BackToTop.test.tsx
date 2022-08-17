cfbd193e1675172704bfe5a925e1a299b543da8f
Dominik Petřík <dominik.petrik@tiscali.cz>
2022-08-17 15:27:46 +0200

    add more tests

--- packages/react-core/src/components/BackToTop/__tests__/BackToTop.test.tsx
+++ packages/react-core/src/components/BackToTop/__tests__/BackToTop.test.tsx
@@ -1,39 +1,93 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import { BackToTop } from '../BackToTop';
-import userEvent from '@testing-library/user-event'
+import userEvent from '@testing-library/user-event';
 
+test('Renders with the default class', () => {
+  render(
+    <div data-testid="backtotop">
+      <BackToTop />
+    </div>
+  );
 
-test('BackToTop button is not yet visible', () => {
-  render(<BackToTop data-testid="backtotop"/>)
-  
-  expect(screen.getByTestId("backtotop")).toHaveClass("pf-m-hidden")
-})
+  expect(screen.getByTestId('backtotop').firstChild).toHaveClass('pf-c-back-to-top');
+});
 
-test('BackToTop button is visible after scrolling', () => {
-  render(<BackToTop data-testid="backtotop"/>)
+test('BackToTop is not yet visible', () => {
+  render(
+    <div data-testid="backtotop">
+      <BackToTop />
+    </div>
+  );
 
-  fireEvent.scroll(window,{target: {scrollY: 401}})
-  expect(screen.getByTestId("backtotop")).not.toHaveClass("pf-m-hidden")
-})
+  expect(screen.getByTestId('backtotop').firstChild).toHaveClass('pf-m-hidden');
+});
 
-test('ScrollTo event is fired after clicking BackToTop button', () => {
-  render(<BackToTop data-testid="backtotop"/>)
+test('BackToTop is visible after scrolling', () => {
+  render(
+    <div data-testid="backtotop">
+      <BackToTop />
+    </div>
+  );
 
-  fireEvent.scroll(window,{target: {scrollY: 401}})
-  global.scrollTo = jest.fn()
+  fireEvent.scroll(window, { target: { scrollY: 401 } });
+  expect(screen.getByTestId('backtotop').firstChild).not.toHaveClass('pf-m-hidden');
+});
 
-  userEvent.click(screen.getByTestId("backtotop"))
-  expect(global.scrollTo).toHaveBeenCalled()
-})
+test('ScrollTo event is fired after clicking BackToTop', () => {
+  render(
+    <div data-testid="backtotop">
+      <BackToTop />
+    </div>
+  );
 
-test('Renders backToTop button when isAlwaysVisible is set', () => {
-  render(<BackToTop data-testid="backtotop" isAlwaysVisible/>)
+  fireEvent.scroll(window, { target: { scrollY: 401 } });
+  global.scrollTo = jest.fn();
 
-  expect(screen.getByTestId("backtotop")).toBeVisible()
-  expect(screen.getByTestId("backtotop")).not.toHaveClass("pf-m-hidden")
-})
+  userEvent.click(screen.getByTestId('backtotop').firstChild as Element);
+  expect(global.scrollTo).toHaveBeenCalled();
+});
 
+test('Renders backToTop when isAlwaysVisible prop is set', () => {
+  render(
+    <div data-testid="backtotop">
+      <BackToTop isAlwaysVisible />
+    </div>
+  );
 
+  expect(screen.getByTestId('backtotop').firstChild).toBeVisible();
+  expect(screen.getByTestId('backtotop').firstChild).not.toHaveClass('pf-m-hidden');
+});
 
+test('Renders with custom className prop passed', () => {
+  render(
+    <div data-testid="backtotop">
+      <BackToTop className="test-class" />
+    </div>
+  );
+  expect(screen.getByTestId('backtotop').firstChild).toHaveClass('test-class');
+});
 
+test('Renders with custom title prop passed', () => {
+  render(
+    <div data-testid="backtotop">
+      <BackToTop title="Back to the Future" />
+    </div>
+  );
+  expect(screen.getByTestId('backtotop').firstChild).toHaveTextContent('Back to the Future');
+});
+
+test('Renders with passed aria-label', () => {
+  render(
+    <div data-testid="backtotop">
+      <BackToTop aria-label="Back to top test" />
+    </div>
+  );
+  expect(screen.getByTestId('backtotop').firstChild).toHaveAccessibleName('Back to top test');
+});
+
+test('Matches the snapshot', () => {
+  const { asFragment } = render(<BackToTop />);
+
+  expect(asFragment()).toMatchSnapshot();
+});
