2a2690ffed9d40afcc81d22c97756592d278221d
gitdallas <dallas.nicol@gmail.com>
2022-08-17 10:36:03 -0500

    using validatedOptions type in numberinput example

--- packages/react-core/src/components/NumberInput/examples/WithStatus.tsx
+++ packages/react-core/src/components/NumberInput/examples/WithStatus.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { NumberInput } from '@patternfly/react-core';
+import { ValidatedOptions } from '../../../helpers';
 
 export const WithStatus: React.FunctionComponent = () => {
   const max = 10;
   const min = 0;
 
-  const [validated, setValidated] = React.useState<'success' | 'error' | 'warning'>('success');
+  const [validated, setValidated] = React.useState<ValidatedOptions>(ValidatedOptions.success);
   const [value, setValue] = React.useReducer((state, newVal) => Math.max(min, Math.min(max, Number(newVal))), 5);
 
   const onPlus = () => {
@@ -29,11 +30,11 @@ export const WithStatus: React.FunctionComponent = () => {
   const validate = newVal => {
     const diff = Math.abs(5 - newVal);
     if (diff > 3) {
-      setValidated('error');
+      setValidated(ValidatedOptions.error);
     } else if (diff > 1) {
-      setValidated('warning');
+      setValidated(ValidatedOptions.warning);
     } else {
-      setValidated('success');
+      setValidated(ValidatedOptions.success);
     }
   };
   return (
