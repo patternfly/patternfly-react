21905a3b40f45ce108c77d10d1e62d51082836a5
Dominik Petřík <dominik.petrik@tiscali.cz>
2022-08-23 14:38:33 +0200

    fix querying components

--- packages/react-core/src/components/Avatar/__tests__/Avatar.test.tsx
+++ packages/react-core/src/components/Avatar/__tests__/Avatar.test.tsx
@@ -12,107 +12,63 @@ test('Renders simple avatar', () => {
 });
 
 test('Renders without any modifier class when border and size props are not passed', () => {
-  render(
-    <div data-testid="avatar">
-      <Avatar alt="avatar" />
-    </div>
-  );
-  expect(screen.getByTestId('avatar').firstChild).toHaveClass('pf-c-avatar', { exact: true });
+  render(<Avatar alt="avatar" />);
+  expect(screen.getByRole('img')).toHaveClass('pf-c-avatar', { exact: true });
 });
 
 test('Renders with class name pf-m-light when "light" is passed as border prop', () => {
-  render(
-    <div data-testid="avatar">
-      <Avatar alt="avatar" border="light" />
-    </div>
-  );
-  expect(screen.getByTestId('avatar').firstChild).toHaveClass('pf-m-light');
+  render(<Avatar alt="avatar" border="light" />);
+  expect(screen.getByRole('img')).toHaveClass('pf-m-light');
 });
 
 test('Renders with class name pf-m-dark when "dark" is passed as border prop', () => {
-  render(
-    <div data-testid="avatar">
-      <Avatar alt="avatar" border="dark" />
-    </div>
-  );
-  expect(screen.getByTestId('avatar').firstChild).toHaveClass('pf-m-dark');
+  render(<Avatar alt="avatar" border="dark" />);
+  expect(screen.getByRole('img')).toHaveClass('pf-m-dark');
 });
 
 test('Renders with class name pf-m-xl when "xl" is passed as size prop', () => {
-  render(
-    <div data-testid="avatar">
-      <Avatar alt="avatar" size="xl" />
-    </div>
-  );
-  expect(screen.getByTestId('avatar').firstChild).toHaveClass('pf-m-xl');
+  render(<Avatar alt="avatar" size="xl" />);
+  expect(screen.getByRole('img')).toHaveClass('pf-m-xl');
 });
 
 test('Renders with class name pf-m-lg when "lg" is passed as size prop', () => {
-  render(
-    <div data-testid="avatar">
-      <Avatar alt="avatar" size="lg" />
-    </div>
-  );
-  expect(screen.getByTestId('avatar').firstChild).toHaveClass('pf-m-lg');
+  render(<Avatar alt="avatar" size="lg" />);
+  expect(screen.getByRole('img')).toHaveClass('pf-m-lg');
 });
 
 test('Renders with class name pf-m-md when "md" is passed as size prop', () => {
-  render(
-    <div data-testid="avatar">
-      <Avatar alt="avatar" size="md" />
-    </div>
-  );
-  expect(screen.getByTestId('avatar').firstChild).toHaveClass('pf-m-md');
+  render(<Avatar alt="avatar" size="md" />);
+  expect(screen.getByRole('img')).toHaveClass('pf-m-md');
 });
 
 test('Renders with class name pf-m-sm when "sm" is passed as size prop', () => {
-  render(
-    <div data-testid="avatar">
-      <Avatar alt="avatar" size="sm" />
-    </div>
-  );
-  expect(screen.getByTestId('avatar').firstChild).toHaveClass('pf-m-sm');
+  render(<Avatar alt="avatar" size="sm" />);
+  expect(screen.getByRole('img')).toHaveClass('pf-m-sm');
 });
 
 test('Renders with custom class name when className prop is passed', () => {
-  render(
-    <div data-testid="avatar">
-      <Avatar alt="avatar" className="test-class" />
-    </div>
-  );
-  expect(screen.getByTestId('avatar').firstChild).toHaveClass('test-class');
+  render(<Avatar alt="avatar" className="test-class" />);
+  expect(screen.getByRole('img')).toHaveClass('test-class');
 });
 
 test('Renders with passed src prop', () => {
-  render(
-    <div data-testid="avatar">
-      <Avatar alt="avatar" src="test.png" />
-    </div>
-  );
+  render(<Avatar alt="avatar" src="test.png" />);
   const image = screen.getByRole('img') as HTMLImageElement;
   expect(image.src).toMatch('test.png');
 });
 
 test('Renders with passed alt prop', () => {
-  render(
-    <div data-testid="avatar">
-      <Avatar alt="avatar" />
-    </div>
-  );
-  expect(screen.getByTestId('avatar').firstChild).toHaveProperty('alt', 'avatar');
+  render(<Avatar alt="avatar" />);
+  expect(screen.getByRole('img')).toHaveProperty('alt', 'avatar');
 });
 
 test('Renders with passed aria-label prop', () => {
-  render(
-    <div data-testid="avatar">
-      <Avatar alt="avatar" aria-label="Avatar test" />
-    </div>
-  );
-  expect(screen.getByTestId('avatar').firstChild).toHaveAccessibleName('Avatar test');
+  render(<Avatar alt="avatar" aria-label="Avatar test" />);
+  expect(screen.getByRole('img')).toHaveAccessibleName('Avatar test');
 });
 
 test('Matches the snapshot', () => {
-  const { asFragment } = render(<Avatar alt="avatar" />);
+  const { asFragment } = render(<Avatar alt="avatar" aria-label="Avatar test" src="test.png" className="test-class" />);
 
   expect(asFragment()).toMatchSnapshot();
 });
