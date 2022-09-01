1637b27e771c1f30afb1d91889a27e20a0a41096
Austin Sullivan <ausulliv@redhat.com>
2022-09-01 15:44:34 -0400

    fix(AlertGroup): update AlertGroupInline to not use child index as a key (#7878)
    * fix(AlertGroup): update AlertGroupInline to not use child index as a key
    
    * add index as backup key in case an alert is missing a uniqueId
    
    * add tests
    
    * change prop name uniqueId -> id

--- packages/react-core/src/components/AlertGroup/AlertGroupInline.tsx
+++ packages/react-core/src/components/AlertGroup/AlertGroupInline.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { css } from '@patternfly/react-styles';
 import styles from '@patternfly/react-styles/css/components/AlertGroup/alert-group';
 import { AlertGroupProps } from './AlertGroup';
+import { AlertProps } from '../Alert';
 
 export const AlertGroupInline: React.FunctionComponent<AlertGroupProps> = ({
   className,
@@ -18,8 +19,8 @@ export const AlertGroupInline: React.FunctionComponent<AlertGroupProps> = ({
     className={css(styles.alertGroup, className, isToast ? styles.modifiers.toast : '')}
     {...rest}
   >
-    {React.Children.toArray(children).map((Alert: React.ReactNode, index: number) => (
-      <li key={index}>{Alert}</li>
+    {React.Children.toArray(children).map((alert, index) => (
+      <li key={(alert as React.ReactElement<AlertProps>).props?.id || index}>{alert}</li>
     ))}
     {overflowMessage && (
       <li>
