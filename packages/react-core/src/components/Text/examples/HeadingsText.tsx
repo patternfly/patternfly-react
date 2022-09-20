20b0433bb33e98d40e7273da27c73d01a34af909
XiangyuShen <xshen2006@yahoo.com>
2022-09-19 20:57:42 -0400

    chore(Text): Convert examples to typescript

---
+++ packages/react-core/src/components/Text/examples/HeadingsText.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { TextContent, Text, TextVariants } from '@patternfly/react-core';
+
+<TextContent>
+  <Text component={TextVariants.h1}>Hello World</Text>
+  <Text component={TextVariants.h2}>Second level</Text>
+  <Text component={TextVariants.h3}>Third level</Text>
+  <Text component={TextVariants.h4}>Fourth level</Text>
+  <Text component={TextVariants.h5}>Fifth level</Text>
+  <Text component={TextVariants.h6}>Sixth level</Text>
+</TextContent>;
---
+++ packages/react-core/src/components/Text/examples/VisitedText.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { TextContent, Text, TextVariants } from '@patternfly/react-core';
+
+<>
+  <TextContent>
+    <Text component={TextVariants.h3}>Visited link example</Text>
+    <Text component={TextVariants.p}>
+      <Text component={TextVariants.a} isVisitedLink href="#">
+        Visited link
+      </Text>
+    </Text>
+  </TextContent>
+  <br />
+  <TextContent isVisited>
+    <Text component={TextVariants.h3}>Visited content example</Text>
+    <Text component={TextVariants.p}>
+      <Text component={TextVariants.a} href="#">
+        content link 1
+      </Text>
+    </Text>
+    <Text component={TextVariants.p}>
+      <Text component={TextVariants.a} href="#">
+        ß content link 2
+      </Text>
+    </Text>
+    <Text component={TextVariants.p}>
+      <Text component={TextVariants.a} href="#">
+        content link 3
+      </Text>
+    </Text>
+  </TextContent>
+</>;
