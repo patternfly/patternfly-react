c4c5e8ac2206faeb095e1cbcaba39afc8893e356
XiangyuShen <xshen2006@yahoo.com>
2022-09-20 14:20:26 -0400

    Fix export as component

--- packages/react-core/src/components/Text/examples/TextVisited.tsx
+++ packages/react-core/src/components/Text/examples/TextVisited.tsx
@@ -1,32 +1,34 @@
 import React from 'react';
 import { TextContent, Text, TextVariants } from '@patternfly/react-core';
 
-<>
-  <TextContent>
-    <Text component={TextVariants.h3}>Visited link example</Text>
-    <Text component={TextVariants.p}>
-      <Text component={TextVariants.a} isVisitedLink href="#">
-        Visited link
+export const TextVisited: React.FunctionComponent = () => (
+  <>
+    <TextContent>
+      <Text component={TextVariants.h3}>Visited link example</Text>
+      <Text component={TextVariants.p}>
+        <Text component={TextVariants.a} isVisitedLink href="#">
+          Visited link
+        </Text>
       </Text>
-    </Text>
-  </TextContent>
-  <br />
-  <TextContent isVisited>
-    <Text component={TextVariants.h3}>Visited content example</Text>
-    <Text component={TextVariants.p}>
-      <Text component={TextVariants.a} href="#">
-        content link 1
+    </TextContent>
+    <br />
+    <TextContent isVisited>
+      <Text component={TextVariants.h3}>Visited content example</Text>
+      <Text component={TextVariants.p}>
+        <Text component={TextVariants.a} href="#">
+          content link 1
+        </Text>
       </Text>
-    </Text>
-    <Text component={TextVariants.p}>
-      <Text component={TextVariants.a} href="#">
-        ß content link 2
+      <Text component={TextVariants.p}>
+        <Text component={TextVariants.a} href="#">
+          content link 2
+        </Text>
       </Text>
-    </Text>
-    <Text component={TextVariants.p}>
-      <Text component={TextVariants.a} href="#">
-        content link 3
+      <Text component={TextVariants.p}>
+        <Text component={TextVariants.a} href="#">
+          content link 3
+        </Text>
       </Text>
-    </Text>
-  </TextContent>
-</>;
+    </TextContent>
+  </>
+);
