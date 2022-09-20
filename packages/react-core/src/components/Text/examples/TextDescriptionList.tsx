0e10e3704cb6b842fd6cca4a4e9e69daac2e3226
XiangyuShen <xshen2006@yahoo.com>
2022-09-20 15:28:10 -0400

    Add export as component to all Text examples

--- packages/react-core/src/components/Text/examples/TextDescriptionList.tsx
+++ packages/react-core/src/components/Text/examples/TextDescriptionList.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { TextContent, TextList, TextListVariants, TextListItem, TextListItemVariants } from '@patternfly/react-core';
 
-<TextContent>
-  <TextList component={TextListVariants.dl}>
-    <TextListItem component={TextListItemVariants.dt}>Web</TextListItem>
-    <TextListItem component={TextListItemVariants.dd}>
-      The part of the Internet that contains websites and web pages
-    </TextListItem>
-    <TextListItem component={TextListItemVariants.dt}>HTML</TextListItem>
-    <TextListItem component={TextListItemVariants.dd}>A markup language for creating web pages</TextListItem>
-    <TextListItem component={TextListItemVariants.dt}>CSS</TextListItem>
-    <TextListItem component={TextListItemVariants.dd}>A technology to make HTML look better</TextListItem>
-  </TextList>
-</TextContent>;
+export const TextDescriptionList: React.FunctionComponent = () => (
+  <TextContent>
+    <TextList component={TextListVariants.dl}>
+      <TextListItem component={TextListItemVariants.dt}>Web</TextListItem>
+      <TextListItem component={TextListItemVariants.dd}>
+        The part of the Internet that contains websites and web pages
+      </TextListItem>
+      <TextListItem component={TextListItemVariants.dt}>HTML</TextListItem>
+      <TextListItem component={TextListItemVariants.dd}>A markup language for creating web pages</TextListItem>
+      <TextListItem component={TextListItemVariants.dt}>CSS</TextListItem>
+      <TextListItem component={TextListItemVariants.dd}>A technology to make HTML look better</TextListItem>
+    </TextList>
+  </TextContent>
+);
