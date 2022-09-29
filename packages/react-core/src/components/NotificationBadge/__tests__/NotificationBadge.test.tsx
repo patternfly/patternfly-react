8aa4beaf081b09bb808f2666c4b6021223d6dc16
Eric Olkowski <70952936+thatblindgeye@users.noreply.github.com>
2022-09-29 09:55:59 -0400

    feat(NotificationBadge): added expanded state (#8010)
    * feat(NotificationBadge): added expanded state
    
    * Updated badges in notification drawer demos
    
    * Updated tests
    
    * Added example descriptions
    
    * Updated basic example description

--- packages/react-core/src/components/NotificationBadge/__tests__/NotificationBadge.test.tsx
+++ packages/react-core/src/components/NotificationBadge/__tests__/NotificationBadge.test.tsx
@@ -1,6 +1,6 @@
 import { NotificationBadge } from '../NotificationBadge';
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 Object.values([true, false]).forEach(isRead => {
   test(`${isRead} NotificationBadge`, () => {
@@ -23,4 +23,28 @@ Object.values([true, false]).forEach(attentionVariant => {
 test(`NotificationBadge count`, () => {
   const { asFragment } = render(<NotificationBadge variant="read" count={3} />);
   expect(asFragment()).toMatchSnapshot();
+});
+
+test('Renders with aria-expanded="false" when isExpanded is not passed in.', () => {
+  render(<NotificationBadge />);
+
+  expect(screen.getByRole('button')).toHaveAttribute('aria-expanded', 'false');
+});
+
+test('Renders with aria-expanded="true" when isExpanded is passed in.', () => {
+  render(<NotificationBadge isExpanded />);
+
+  expect(screen.getByRole('button')).toHaveAttribute('aria-expanded', 'true');
+});
+
+test('Does not render with .pf-m-expanded when isExpanded is not passed in.', () => {
+  render(<NotificationBadge />);
+
+  expect(screen.getByRole('button').firstChild).not.toHaveClass('pf-m-expanded');
+});
+
+test('Renders with .pf-m-expanded when isExpanded is passed in.', () => {
+  render(<NotificationBadge isExpanded />);
+
+  expect(screen.getByRole('button').firstChild).toHaveClass('pf-m-expanded');
 });
