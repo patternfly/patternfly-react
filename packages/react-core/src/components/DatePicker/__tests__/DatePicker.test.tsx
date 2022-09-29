62e04e735b56ac4f43416192b164c2c3e92909ca
Dominik Petřík <77832970+Dominik-Petrik@users.noreply.github.com>
2022-09-29 14:13:35 +0200

    fix(DatePicker) Invalid input with multiple validators (#7992)
    * fix(DatePicker) Invalid input with multiple validators
    
    * add unit test
    
    * fix unit test

--- packages/react-core/src/components/DatePicker/__tests__/DatePicker.test.tsx
+++ packages/react-core/src/components/DatePicker/__tests__/DatePicker.test.tsx
@@ -1,8 +1,29 @@
-import { render } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 import { DatePicker } from '../DatePicker';
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 
 test('disabled date picker', () => {
   const { asFragment } = render(<DatePicker value="2020-11-20" isDisabled aria-label="disabled date picker" />);
   expect(asFragment()).toMatchSnapshot();
+});
+
+test('Date picker with multiple validators does not show invalid icon on valid input', async () => {
+  const user = userEvent.setup();
+
+  const rangeValidator = (date: Date) => {
+    return '';
+  };
+
+  const rangeValidatorB = (date: Date) => {
+    return '';
+  };
+
+  render(<DatePicker value="2020-03-17" validators={[rangeValidator, rangeValidatorB]} />);
+
+  await user.click(screen.getByRole('textbox'));
+
+  await user.click(document.body);
+
+  expect(screen.getByRole('textbox')).not.toBeInvalid();
 });
