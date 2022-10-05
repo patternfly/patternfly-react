80c84943a15698c3392154977538579f54b20418
Jan Wright <jawright@redhat.com>
2022-10-05 17:17:39 -0400

    chore(SimpleList):convert to typescript

---
+++ packages/react-core/src/components/SimpleList/examples/SimpleListUncontrolled.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { SimpleList, SimpleListItem } from '@patternfly/react-core';
+
+export const SimpleListUncontrolledDemo: React.FunctionComponent = () => {
+  const [activeItem, setActiveItem] = React.useState(0);
+
+  const onSelect = (selectedItem: any, selectedItemProps: any) => {
+    setActiveItem(selectedItemProps.itemId);
+  };
+
+  const items = [
+    <SimpleListItem key="item1" itemId={0} isActive={activeItem === 0}>
+      List item 1
+    </SimpleListItem>,
+    <SimpleListItem key="item2" itemId={1} isActive={activeItem === 1}>
+      List item 2
+    </SimpleListItem>,
+    <SimpleListItem key="item3" itemId={2} isActive={activeItem === 2}>
+      List item 3
+    </SimpleListItem>
+  ];
+
+  return (
+    <SimpleList onSelect={onSelect} isControlled={false} aria-label="Uncontrolled Simple List Example">
+      {items}
+    </SimpleList>
+  );
+};
