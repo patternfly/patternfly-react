0028daf046fe1bf4308bcda7e63a16aa0155b1ea
Dallas <dallas.nicol@gmail.com>
2022-10-07 13:13:32 -0500

    chore: remove keycodes definition (#8174)

--- packages/react-core/src/helpers/constants.ts
+++ packages/react-core/src/helpers/constants.ts
@@ -10,8 +10,6 @@ import globalHeightBreakpointLg from '@patternfly/react-tokens/dist/esm/global_h
 import globalHeightBreakpointXl from '@patternfly/react-tokens/dist/esm/global_height_breakpoint_xl';
 import globalHeightBreakpoint2xl from '@patternfly/react-tokens/dist/esm/global_height_breakpoint_2xl';
 
-export const KEY_CODES = { ARROW_UP: 38, ARROW_DOWN: 40, ESCAPE_KEY: 27, TAB: 9, ENTER: 13, SPACE: 32 };
-
 export const SIDE = { RIGHT: 'right', LEFT: 'left', BOTH: 'both', NONE: 'none' };
 
 export const KEYHANDLER_DIRECTION = { UP: 'up', DOWN: 'down', RIGHT: 'right', LEFT: 'left' };
