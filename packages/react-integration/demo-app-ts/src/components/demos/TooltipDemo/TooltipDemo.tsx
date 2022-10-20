e5242a85bf6d0c352913d63d9d38aef88dd9bda9
Dominik Petřík <77832970+Dominik-Petrik@users.noreply.github.com>
2022-10-20 21:42:42 +0200

    chore(Tooltip) remove deprecated props (#8231)
    * chore(Tooltip) remove deprecated props
    
    * fix demo
    
    * remove deprecated test from demo

--- packages/react-integration/demo-app-ts/src/components/demos/TooltipDemo/TooltipDemo.tsx
+++ packages/react-integration/demo-app-ts/src/components/demos/TooltipDemo/TooltipDemo.tsx
@@ -21,9 +21,6 @@ export class TooltipDemo extends Component {
     return (
       <div>
         <Tooltip content={this.myTooltipProps.content}>{this.myTooltipProps.children}</Tooltip>
-        <Tooltip content="test deprecated props" tippyProps={{ duration: 0, delay: 0 }} isAppLauncher>
-          <button>Trigger</button>
-        </Tooltip>
         <div>
           <button ref={this.tooltipRef} id="tooltip-ref">
             Tooltip attached via react ref
