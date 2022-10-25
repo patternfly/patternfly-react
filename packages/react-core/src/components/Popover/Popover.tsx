108503f167b4cfabab88abf2a52bf4c6a8bd504b
kmcfaul <45077788+kmcfaul@users.noreply.github.com>
2022-10-25 15:13:40 -0400

    feat(Popover): update default flip behavior and width (#8191)
    * feat(Popover): update default flip behavior and width
    
    * fix flip positions for diagonals
    
    * revert default for autoWidth for now
    
    * remove old comment

--- packages/react-core/src/components/Popover/Popover.tsx
+++ packages/react-core/src/components/Popover/Popover.tsx
@@ -260,8 +260,20 @@ export const Popover: React.FunctionComponent<PopoverProps> = ({
   closeBtnAriaLabel = 'Close',
   showClose = true,
   distance = 25,
-  // For every initial starting position, there are 3 escape positions
-  flipBehavior = ['top', 'right', 'bottom', 'left', 'top', 'right', 'bottom'],
+  flipBehavior = [
+    'top',
+    'bottom',
+    'left',
+    'right',
+    'top-start',
+    'top-end',
+    'bottom-start',
+    'bottom-end',
+    'left-start',
+    'left-end',
+    'right-start',
+    'right-end'
+  ],
   animationDuration = 300,
   id,
   withFocusTrap: propWithFocusTrap,
