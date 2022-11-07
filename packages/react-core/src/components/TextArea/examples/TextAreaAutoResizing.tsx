159e91ab321eb6ddb3ce395b6d85359e5bf2a7f0
Kevin Kim <kkim020902@gmail.com>
2022-11-07 01:10:26 -0500

    fix(TextArea): redo

---
+++ packages/react-core/src/components/TextArea/examples/TextAreaAutoResizing.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { TextArea } from '@patternfly/react-core';
+
+export const TextAreaAutoResizing: React.FunctionComponent = () => (
+  <TextArea aria-label="auto resizing text area example" autoResize />
+);
