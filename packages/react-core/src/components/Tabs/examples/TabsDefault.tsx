f86b6ae81a61d83ccfc3ed5c6f20a1ea3299dd2c
Kevin Kim <kkim020902@gmail.com>
2022-11-08 18:39:47 -0500

    fix(Tabs): typed function arguments

--- packages/react-core/src/components/Tabs/examples/TabsDefault.tsx
+++ packages/react-core/src/components/Tabs/examples/TabsDefault.tsx
@@ -12,7 +12,7 @@ export const TabsDefault: React.FunctionComponent = () => {
     setActiveTabKey(tabIndex);
   };
 
-  const toggleBox = checked => {
+  const toggleBox = (checked: boolean) => {
     setIsBox(checked);
   };
 
