dd70e084dc755e6d264cb200a295ff72af33f907
erkanercan <erkanercandev@gmail.com>
2022-11-12 11:50:24 +0300

    fix: badge screen reader prop, delete static class

--- packages/react-core/src/components/Badge/Badge.tsx
+++ packages/react-core/src/components/Badge/Badge.tsx
@@ -3,6 +3,8 @@ import { css } from '@patternfly/react-styles';
 import styles from '@patternfly/react-styles/css/components/Badge/badge';
 
 export interface BadgeProps extends React.HTMLProps<HTMLSpanElement> {
+  /** Text announced by screen readers to indicate the current content/status of the badge. */
+  screenReaderText?: string;
   /**  Adds styling to the badge to indicate it has been read */
   isRead?: boolean;
   /** content rendered inside the Badge */
@@ -15,17 +17,14 @@ export const Badge: React.FunctionComponent<BadgeProps> = ({
   isRead = false,
   className = '',
   children = '',
+  screenReaderText,
   ...props
 }: BadgeProps) => (
   <span
     {...props}
-    className={css(
-      styles.badge,
-      (isRead ? styles.modifiers.read : styles.modifiers.unread) as any,
-      'pf-screen-reader',
-      className
-    )}
+    className={css(styles.badge, (isRead ? styles.modifiers.read : styles.modifiers.unread) as any, className)}
   >
+    {screenReaderText && <span className="pf-u-screen-reader">{screenReaderText}</span>}
     {children}
   </span>
 );
