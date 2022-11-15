cb6eac9516e27cd97bcbe488ec8e232102922617
erkanercan <erkanercandev@gmail.com>
2022-11-15 22:14:59 +0300

    fix: correct example typo
    Signed-off-by: erkanercan <erkanercandev@gmail.com>

--- packages/react-core/src/components/Badge/examples/BadgeUnread.tsx
+++ packages/react-core/src/components/Badge/examples/BadgeUnread.tsx
@@ -3,16 +3,16 @@ import { Badge } from '@patternfly/react-core';
 
 export const BadgeUnread: React.FunctionComponent = () => (
   <React.Fragment>
-    <Badge key={1} screenReaderText="7 Unread Messages">
+    <Badge key={1} screenReaderText="Unread Messages">
       7
     </Badge>
-    <Badge key={2} screenReaderText="24 Unread Messages">
+    <Badge key={2} screenReaderText="Unread Messages">
       24
     </Badge>
-    <Badge key={3} screenReaderText="240 Unread Messages">
+    <Badge key={3} screenReaderText="Unread Messages">
       240
     </Badge>
-    <Badge key={4} screenReaderText="More Than 999 Unread Messages">
+    <Badge key={4} screenReaderText="Unread Messages">
       999+
     </Badge>
   </React.Fragment>
