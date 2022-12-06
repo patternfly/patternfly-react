6797a1e4fa12fdc04232b6953f987fed7631fb87
jenny-s51 <jshandel@redhat.com>
2022-12-06 13:47:38 -0500

    add docs and demo for baseline topology
    add type
    
    preliminary updates based on Jeff feedback
    
    Jeff's implementation for baseline demo
    
    update README with Jeff's implementation
    
    add topology paths to patternfly-docs.source.js; update Topology.md to include new docs for now, WIP on the examples
    
    styles; WIP status decorators
    
    Add custom node demo
    
    add type
    
    add docs and demo for baseline topology
    
    add type
    
    preliminary updates based on Jeff feedback
    
    Jeff's implementation for baseline demo
    
    update README with Jeff's implementation
    
    add topology paths to patternfly-docs.source.js; update Topology.md to include new docs for now, WIP on the examples
    
    styles; WIP status decorators
    
    Add custom node demo
    
    add type
    
    preliminary updates based on Jeff feedback
    
    Jeff's implementation for baseline demo
    
    update README with Jeff's implementation
    
    add topology paths to patternfly-docs.source.js; update Topology.md to include new docs for now, WIP on the examples
    
    styles; WIP status decorators
    
    Add topology baseline demo
    
    update dagre to working version, move/display custom demo in react-topology md
    
    move text into baseline demo, add custom node docs
    
    remove examples from demo app
    
    revert TopologyDemo.tsx to main version
    
    remove consumer-facing TODO

---
+++ packages/react-topology/src/components/TopologyView/examples/TopologyBaselineDemo.tsx
@@ -0,0 +1,160 @@
+import * as React from 'react';
+import {
+  ColaLayout,
+  ComponentFactory,
+  DefaultEdge,
+  DefaultGroup,
+  DefaultNode,
+  EdgeStyle,
+  Graph,
+  GraphComponent,
+  Layout,
+  LayoutFactory,
+  Model,
+  ModelKind,
+  NodeShape,
+  SELECTION_EVENT,
+  Visualization,
+  VisualizationProvider,
+  VisualizationSurface
+} from '@patternfly/react-topology';
+
+const baselineLayoutFactory: LayoutFactory = (type: string, graph: Graph): Layout | undefined => {
+  switch (type) {
+    case 'Cola':
+      return new ColaLayout(graph);
+    default:
+      return new ColaLayout(graph, { layoutOnDrag: false });
+  }
+};
+
+const baselineComponentFactory: ComponentFactory = (kind: ModelKind, type: string) => {
+  switch (type) {
+    case 'group':
+      return DefaultGroup;
+    default:
+      switch (kind) {
+        case ModelKind.graph:
+          return GraphComponent;
+        case ModelKind.node:
+          return DefaultNode;
+        case ModelKind.edge:
+          return DefaultEdge;
+        default:
+          return undefined;
+      }
+  }
+};
+
+const NODE_SHAPE = NodeShape.ellipse;
+const NODE_DIAMETER = 75;
+
+const NODES = [
+  {
+    id: 'node-0',
+    type: 'node',
+    label: 'Node 0',
+    width: NODE_DIAMETER,
+    height: NODE_DIAMETER,
+    shape: NODE_SHAPE
+  },
+  {
+    id: 'node-1',
+    type: 'node',
+    label: 'Node 1',
+    width: NODE_DIAMETER,
+    height: NODE_DIAMETER,
+    shape: NODE_SHAPE
+  },
+  {
+    id: 'node-2',
+    type: 'node',
+    label: 'Node 2',
+    width: NODE_DIAMETER,
+    height: NODE_DIAMETER,
+    shape: NODE_SHAPE
+  },
+  {
+    id: 'node-3',
+    type: 'node',
+    label: 'Node 3',
+    width: NODE_DIAMETER,
+    height: NODE_DIAMETER,
+    shape: NODE_SHAPE
+  },
+  {
+    id: 'node-4',
+    type: 'node',
+    label: 'Node 4',
+    width: NODE_DIAMETER,
+    height: NODE_DIAMETER,
+    shape: NODE_SHAPE
+  },
+  {
+    id: 'node-5',
+    type: 'node',
+    label: 'Node 5',
+    width: NODE_DIAMETER,
+    height: NODE_DIAMETER,
+    shape: NODE_SHAPE
+  },
+  {
+    id: 'Group-1',
+    children: ['node-0', 'node-1', 'node-2'],
+    type: 'group',
+    group: true,
+    label: 'Group-1',
+    style: {
+      padding: 40
+    }
+  }
+];
+
+const EDGES = [
+  {
+    id: 'edge-node-4-node-5',
+    type: 'edge',
+    source: 'node-4',
+    target: 'node-5',
+    edgeStyle: EdgeStyle.default
+  },
+  {
+    id: 'edge-node-0-node-2',
+    type: 'edge',
+    source: 'node-0',
+    target: 'node-2',
+    edgeStyle: EdgeStyle.default
+  }
+];
+
+export const TopologyBaselineDemo: React.FC = () => {
+  const [selectedIds, setSelectedIds] = React.useState<string[]>([]);
+
+  const controller = React.useMemo(() => {
+    const model: Model = {
+      nodes: NODES,
+      edges: EDGES,
+      graph: {
+        id: 'g1',
+        type: 'graph',
+        layout: 'Cola'
+      }
+    };
+
+    const newController = new Visualization();
+    newController.registerLayoutFactory(baselineLayoutFactory);
+    newController.registerComponentFactory(baselineComponentFactory);
+
+    newController.addEventListener(SELECTION_EVENT, setSelectedIds);
+
+    newController.fromModel(model, false);
+
+    return newController;
+  }, []);
+
+  return (
+    <VisualizationProvider controller={controller}>
+      <VisualizationSurface state={{ selectedIds }} />
+    </VisualizationProvider>
+  );
+};
