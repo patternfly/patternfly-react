192a15877619e0645b29d42d9bea67acf4f2ee6f
nicolethoen <nthoen@redhat.com>
2023-01-18 10:31:30 -0500

    more clean up

--- packages/react-docs/patternfly-docs/patternfly-docs.source.js
+++ packages/react-docs/patternfly-docs/patternfly-docs.source.js
@@ -16,9 +16,6 @@ module.exports = (baseSourceMD, sourceProps) => {
   const reactCodeEditorPath = require
     .resolve('@patternfly/react-code-editor/package.json')
     .replace('package.json', 'src');
-  const reactCatalogViewPath = require
-    .resolve('@patternfly/react-catalog-view-extension/package.json')
-    .replace('package.json', 'src');
   const reactVirtualizedPath = require
     .resolve('@patternfly/react-virtualized-extension/package.json')
     .replace('package.json', 'src');
@@ -29,7 +26,6 @@ module.exports = (baseSourceMD, sourceProps) => {
   sourceProps(path.join(reactTablePath, '/**/*.tsx'), reactPropsIgnore);
   sourceProps(path.join(reactChartsPath, '/**/*.tsx'), reactPropsIgnore);
   sourceProps(path.join(reactCodeEditorPath, '/**/*.tsx'), reactPropsIgnore);
-  sourceProps(path.join(reactCatalogViewPath, '/**/*.tsx'), reactPropsIgnore);
   sourceProps(path.join(reactVirtualizedPath, '/**/*.tsx'), reactPropsIgnore);
   sourceProps(path.join(reactTopologyPath, '/**/*.tsx'), reactPropsIgnore);
 
@@ -49,9 +45,6 @@ module.exports = (baseSourceMD, sourceProps) => {
 
   // Charts MD (no demos yet)
   sourceMD(path.join(reactChartsPath, '/**/examples/*.md'), 'react');
-
-  // Catalog view MD
-  sourceMD(path.join(reactCatalogViewPath, '/**/examples/*.md'), 'react');
 
   // Virtualized MD
   sourceMD(path.join(reactVirtualizedPath, '/**/examples/*.md'), 'react');
