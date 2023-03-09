f40d27ff8e21d414508650553a78dc69a4044ee9
adamviktora <viktora.adam@gmail.com>
2023-03-09 08:45:09 +0100

    fix(EmptyStateHeader): props structure changed so the EmptyStateIcon is passed directly as an element

--- packages/react-core/src/components/EmptyState/EmptyStateHeader.tsx
+++ packages/react-core/src/components/EmptyState/EmptyStateHeader.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { css } from '@patternfly/react-styles';
 import styles from '@patternfly/react-styles/css/components/EmptyState/empty-state';
-import { EmptyStateIcon } from './EmptyStateIcon';
+import { EmptyStateIconProps } from './EmptyStateIcon';
 
 export interface EmptyStateHeaderProps extends React.HTMLProps<HTMLDivElement> {
   /** Content rendered inside the empty state header */
@@ -10,14 +10,10 @@ export interface EmptyStateHeaderProps extends React.HTMLProps<HTMLDivElement> {
   className?: string;
   /** Additional classes added to the title inside empty state header */
   titleClassName?: string;
-  /** Additional classes added to the icon inside empty state header */
-  iconClassName?: string;
   /** Text of the title inside empty state header */
   titleText?: string;
-  /** Icon component to be rendered. Can also be a spinner component */
-  icon?: React.ComponentType<any>;
-  /** Color of the icon rendered in the empty state header */
-  iconColor?: string;
+  /** Empty state icon element to be rendered */
+  icon?: React.ReactElement<EmptyStateIconProps>;
   /** The heading level to use */
   headingLevel?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 }
@@ -26,18 +22,18 @@ export const EmptyStateHeader: React.FunctionComponent<EmptyStateHeaderProps> = 
   children,
   className,
   titleClassName,
-  iconClassName,
   titleText,
-  iconColor,
   headingLevel: HeadingLevel = 'h1',
   icon,
   ...props
 }: EmptyStateHeaderProps) => (
   <div className={css('pf-c-empty-state__header', className)} {...props}>
-    {icon && <EmptyStateIcon className={iconClassName} icon={icon} color={iconColor}/>}
+    {icon}
     {(titleText || children) && (
       <div className={css('pf-c-empty-state__title')}>
-        {titleText && <HeadingLevel className={css(styles.emptyStateTitleText, titleClassName)}>{titleText}</HeadingLevel>}
+        {titleText && (
+          <HeadingLevel className={css(styles.emptyStateTitleText, titleClassName)}>{titleText}</HeadingLevel>
+        )}
         {children}
       </div>
     )}
