5f87dc62d65a9a02c9bd3c25594eef46b7788554
adamviktora <viktora.adam@gmail.com>
2023-03-13 14:01:33 +0100

    docs(EmptyStateHeader): updated props documentation

--- packages/react-core/src/components/EmptyState/EmptyStateHeader.tsx
+++ packages/react-core/src/components/EmptyState/EmptyStateHeader.tsx
@@ -4,17 +4,17 @@ import styles from '@patternfly/react-styles/css/components/EmptyState/empty-sta
 import { EmptyStateIconProps } from './EmptyStateIcon';
 
 export interface EmptyStateHeaderProps extends React.HTMLProps<HTMLDivElement> {
-  /** Content rendered inside the empty state header */
+  /** Content rendered inside the empty state header, either in addition to or instead of the titleText prop */
   children?: React.ReactNode;
   /** Additional classes added to the empty state header */
   className?: string;
   /** Additional classes added to the title inside empty state header */
   titleClassName?: string;
-  /** Text of the title inside empty state header */
+  /** Text of the title inside empty state header, will be wrapped in headingLevel */
   titleText?: React.ReactNode;
   /** Empty state icon element to be rendered */
   icon?: React.ReactElement<EmptyStateIconProps>;
-  /** The heading level to use */
+  /** The heading level to use, default is h1 */
   headingLevel?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 }
 
