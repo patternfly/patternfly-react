0a3507a74cc1fde51da49c26f9dad9b3b582d777
Eric Olkowski <thatblindgeye@gmail.com>
2023-03-24 12:11:24 -0400

    Removed stray comma in example

--- packages/react-core/src/components/Dropdown/examples/DropdownWithKebabToggle.tsx
+++ packages/react-core/src/components/Dropdown/examples/DropdownWithKebabToggle.tsx
@@ -45,7 +45,7 @@ export const DropdownWithKebab: React.FunctionComponent = () => {
         <DropdownItem itemId={3} isDisabled key="disabled action" to="#default-link4">
           Disabled action
         </DropdownItem>
-        <Divider key="separator" />,
+        <Divider key="separator" />
         <DropdownItem itemId={4} key="separated link">
           Separated link
         </DropdownItem>
