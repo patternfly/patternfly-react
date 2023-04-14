45d2c7a160a6089241e54508086e541ee81455be
Jeff Puzzo <96431149+jpuzz0@users.noreply.github.com>
2023-04-14 13:47:34 -0400

    feat(FormContext): Add context for Form to manage input state (#8716)
    * feat(FormContext): Add context for Form to manage input state
    
    * fix issues with form state example after rebase - due to new form input promotions

---
+++ packages/react-core/src/components/Form/__tests__/FormContext.test.tsx
@@ -0,0 +1,238 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { FormContextConsumer, FormContextProvider } from '../FormContext';
+
+const customRender = (ui, initialValues?: Record<string, any>) => {
+  return render(<FormContextProvider initialValues={initialValues}>{ui}</FormContextProvider>);
+};
+
+test('consumers can show all values from "values" provided by "initialValues"', () => {
+  const initialValues = { fieldOneId: 'some value', fieldTwoId: 'some other value' };
+
+  customRender(
+    <FormContextConsumer>
+      {({ values }) => Object.values(values).map((value) => <span key={value}>{value}</span>)}
+    </FormContextConsumer>,
+    initialValues
+  );
+
+  Object.values(initialValues).forEach((initialValue) => expect(screen.getByText(initialValue)).toBeVisible());
+});
+
+test('consumer results in "isValid" as "true" without errors and "false" with errors', async () => {
+  const user = userEvent.setup();
+  const initialValues = { someId: 'some value' };
+  const onChange = (setValue, setError) => (event) => {
+    const { value } = event.target;
+    setValue('someId', value);
+
+    if (!value) {
+      setError('someId', 'Some error!');
+    }
+  };
+
+  customRender(
+    <FormContextConsumer>
+      {({ values, setValue, setError, isValid }) => (
+        <>
+          <input onChange={onChange(setValue, setError)} value={values.someId} />
+          {`isValid: ${isValid}`}
+        </>
+      )}
+    </FormContextConsumer>,
+    initialValues
+  );
+
+  expect(screen.getByText(/^isValid:/)).toHaveTextContent('isValid: true');
+
+  await user.clear(screen.getByRole('textbox'));
+  expect(screen.getByText(/^isValid:/)).toHaveTextContent('isValid: false');
+});
+
+test('using getValue, the consumer can show a value passed from initialValues', () => {
+  customRender(<FormContextConsumer>{({ getValue }) => <>{getValue('someId')}</>}</FormContextConsumer>, {
+    someId: 'some value'
+  });
+
+  expect(screen.getByText('some value')).toBeVisible();
+});
+
+test('sets a new value for a single field using setValue', async () => {
+  const user = userEvent.setup();
+
+  customRender(
+    <FormContextConsumer>
+      {({ getValue, setValue }) => {
+        return <input onChange={(event) => setValue('someId', event.target.value)} value={getValue('someId')} />;
+      }}
+    </FormContextConsumer>,
+    {
+      someId: 'some value'
+    }
+  );
+
+  const consumerInput = screen.getByRole('textbox');
+
+  await user.clear(consumerInput);
+  await user.type(consumerInput, 'some updated value');
+
+  expect(consumerInput).toHaveValue('some updated value');
+});
+
+test('using setValues, consumers can set multiple values at once', async () => {
+  const user = userEvent.setup();
+  const newValues = { someId: 'updated value', someNewId: 'new value' };
+
+  customRender(
+    <FormContextConsumer>
+      {({ values, setValues }) => {
+        return (
+          <>
+            <input type="checkbox" onClick={() => setValues(newValues)} />
+            {Object.entries(values).map(([fieldId, value]) => (
+              <span key={fieldId}>{`${fieldId}: ${value}`}</span>
+            ))}
+          </>
+        );
+      }}
+    </FormContextConsumer>,
+    {
+      someId: 'some value'
+    }
+  );
+
+  await user.click(screen.getByRole('checkbox'));
+  Object.entries(newValues).map(([fieldId, value]) => expect(screen.getByText(`${fieldId}: ${value}`)).toBeVisible());
+});
+
+test('using getError, the consumer returns an error', async () => {
+  const user = userEvent.setup();
+
+  customRender(
+    <FormContextConsumer>
+      {({ getError, setError }) => (
+        <>
+          <input type="checkbox" onClick={() => setError('someId', 'some error!')} />
+          {getError('someId')}
+        </>
+      )}
+    </FormContextConsumer>
+  );
+
+  await user.click(screen.getByRole('checkbox'));
+  expect(screen.getByText('some error!')).toBeVisible();
+});
+
+test('sets a new error for a single field using setError', async () => {
+  const user = userEvent.setup();
+
+  customRender(
+    <FormContextConsumer>
+      {({ errors, setError }) => {
+        return (
+          <>
+            <input type="checkbox" onClick={() => setError('someId', 'some error!')} />
+            {`Error: ${errors.someId}`}
+          </>
+        );
+      }}
+    </FormContextConsumer>
+  );
+
+  await user.click(screen.getByRole('checkbox'));
+  expect(screen.getByText(/^Error:/)).toHaveTextContent('Error: some error!');
+});
+
+test('using setErrors, consumers can set multiple errors at once', async () => {
+  const user = userEvent.setup();
+  const newErrors = { someId: 'some error!', someNewId: 'new field error!' };
+
+  customRender(
+    <FormContextConsumer>
+      {({ errors, setErrors }) => {
+        return (
+          <>
+            <input type="checkbox" onClick={() => setErrors(newErrors)} />
+            {Object.entries(errors).map(([fieldId, error]) => (
+              <span key={fieldId}>{`${fieldId}: ${error}`}</span>
+            ))}
+          </>
+        );
+      }}
+    </FormContextConsumer>,
+    {
+      someId: 'some value'
+    }
+  );
+
+  await user.click(screen.getByRole('checkbox'));
+  Object.entries(newErrors).map(([fieldId, error]) => expect(screen.getByText(`${fieldId}: ${error}`)).toBeVisible());
+});
+
+test('sets a new touched state for a single field using setTouched', async () => {
+  const user = userEvent.setup();
+
+  customRender(
+    <FormContextConsumer>
+      {({ touched, setTouched }) => {
+        return (
+          <>
+            <input type="checkbox" onClick={() => setTouched('someId', true)} />
+            {`isTouched: ${touched.someId}`}
+          </>
+        );
+      }}
+    </FormContextConsumer>
+  );
+
+  await user.click(screen.getByRole('checkbox'));
+  expect(screen.getByText(/^isTouched:/)).toHaveTextContent('isTouched: true');
+});
+
+test("can get a single field's touched state by using isTouched", async () => {
+  const user = userEvent.setup();
+
+  customRender(
+    <FormContextConsumer>
+      {({ touched, setTouched, isTouched }) => {
+        return (
+          <>
+            <input type="checkbox" onClick={() => setTouched('someId', !touched.someId)} />
+            {`isTouched: ${isTouched('someId')}`}
+          </>
+        );
+      }}
+    </FormContextConsumer>
+  );
+
+  const consumerCheckbox = screen.getByRole('checkbox');
+
+  await user.click(consumerCheckbox);
+  expect(screen.getByText(/^isTouched:/)).toHaveTextContent('isTouched: true');
+
+  await user.click(consumerCheckbox);
+  expect(screen.getByText(/^isTouched:/)).toHaveTextContent('isTouched: false');
+});
+
+test('validate returns errors set by the "setValidator" function', async () => {
+  const user = userEvent.setup();
+
+  customRender(
+    <FormContextConsumer>
+      {({ validate, setValidator }) => {
+        const errors = validate();
+
+        return (
+          <>
+            <input type="checkbox" onClick={() => setValidator('someId', (value) => (!value ? 'some error!' : null))} />
+            {`Error: ${errors.someId}`}
+          </>
+        );
+      }}
+    </FormContextConsumer>
+  );
+
+  await user.click(screen.getByRole('checkbox'));
+  expect(screen.getByText(/^Error:/)).toHaveTextContent('Error: some error!');
+});
---
+++ packages/react-core/src/components/Form/examples/FormState.tsx
@@ -0,0 +1,120 @@
+import React from 'react';
+import {
+  ActionGroup,
+  Button,
+  ButtonType,
+  ButtonVariant,
+  Divider,
+  Form,
+  FormContextProvider,
+  FormGroup,
+  FormHelperText,
+  HelperText,
+  HelperTextItem,
+  MenuToggle,
+  Select,
+  SelectList,
+  SelectOption,
+  TextArea,
+  TextInput
+} from '@patternfly/react-core';
+import ExclamationCircleIcon from '@patternfly/react-icons/dist/esm/icons/exclamation-circle-icon';
+
+export const FormState = () => {
+  const [isSelectOpen, setIsSelectOpen] = React.useState(false);
+  const [formStateExpanded, setFormStateExpanded] = React.useState(false);
+
+  return (
+    <FormContextProvider initialValues={{ 'select-id': 'Option 1' }}>
+      {({ setValue, getValue, setError, values, errors }) => (
+        <Form>
+          <FormGroup label="Input value" fieldId="input-id" isRequired>
+            <TextInput
+              id="input-id"
+              onChange={(value) => {
+                setValue('input-id', value);
+                setError('input-id', undefined);
+              }}
+              value={getValue('input-id')}
+              validated={errors['input-id'] ? 'error' : 'default'}
+            />
+            <FormHelperText>
+              <HelperText>
+                <HelperTextItem
+                  variant={errors['input-id'] ? 'error' : 'default'}
+                  {...(errors['input-id'] && { icon: <ExclamationCircleIcon /> })}
+                >
+                  {errors['input-id']}
+                </HelperTextItem>
+              </HelperText>
+            </FormHelperText>
+          </FormGroup>
+
+          <TextArea
+            id="textarea-id"
+            aria-label="Form state TextArea"
+            onChange={(value) => setValue('textarea-id', value)}
+            value={getValue('textarea-id')}
+          />
+
+          <Select
+            id="select-id"
+            selected={getValue('select-id')}
+            isOpen={isSelectOpen}
+            toggle={(toggleRef) => (
+              <MenuToggle
+                ref={toggleRef}
+                onClick={(isOpen) => setIsSelectOpen(isOpen)}
+                isExpanded={isSelectOpen}
+                style={
+                  {
+                    width: '200px'
+                  } as React.CSSProperties
+                }
+              >
+                {getValue('select-id')}
+              </MenuToggle>
+            )}
+            onOpenChange={(isOpen) => setIsSelectOpen(isOpen)}
+            onSelect={(_, itemId) => {
+              setValue('select-id', itemId as string);
+              setIsSelectOpen(false);
+            }}
+          >
+            <SelectList>
+              <SelectOption itemId="Option 1">Option 1</SelectOption>
+              <SelectOption itemId="Option 2">Option 2</SelectOption>
+              <SelectOption itemId="Option 3">Option 3</SelectOption>
+            </SelectList>
+          </Select>
+
+          <ActionGroup>
+            <Button
+              type={ButtonType.submit}
+              onClick={(e) => {
+                e.preventDefault();
+
+                if (!values['input-id']) {
+                  setError('input-id', 'Input value is required.');
+                } else {
+                  alert(`Form submitted with: \n ${JSON.stringify(values)}`);
+                }
+              }}
+            >
+              Submit
+            </Button>
+            <Button variant={ButtonVariant.link} onClick={() => setFormStateExpanded((prevExpanded) => !prevExpanded)}>
+              {`${formStateExpanded ? 'Hide' : 'Show'} form state`}
+            </Button>
+          </ActionGroup>
+          {formStateExpanded && (
+            <>
+              <Divider />
+              <pre>{JSON.stringify({ values, errors }, null, 2)}</pre>
+            </>
+          )}
+        </Form>
+      )}
+    </FormContextProvider>
+  );
+};
