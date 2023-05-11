0f5d5210ea66fffb6824ce637232502946b5ec55
Titani Labaj <39532947+tlabaj@users.noreply.github.com>
2023-05-11 16:09:59 -0400

    chore(deps): Removed @testing-library/react-hooks dep (#9105)
    Co-authored-by: Titani <tlabaj@redaht.com>

--- packages/react-core/src/components/Wizard/hooks/__tests__/useWizardFooter.test.tsx
+++ packages/react-core/src/components/Wizard/hooks/__tests__/useWizardFooter.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react';
 
 import { useWizardFooter } from '../useWizardFooter';
 import * as WizardContext from '../../WizardContext';
