1f8a35b1eeefeffa1cf137db9342bbe99da13341
Jeff Puzzo <96431149+jpuzz0@users.noreply.github.com>
2023-06-01 08:18:42 -0400

    fix(BackgroundImage): Added src URL back, improved unit tests, updated screenshot (#9199)

--- packages/react-core/src/components/BackgroundImage/BackgroundImage.tsx
+++ packages/react-core/src/components/BackgroundImage/BackgroundImage.tsx
@@ -1,30 +1,29 @@
-import * as React from 'react';
+import React from 'react';
+
 import { css } from '@patternfly/react-styles';
 import styles from '@patternfly/react-styles/css/components/BackgroundImage/background-image';
-// eslint-disable-next-line camelcase
-// import c_background_image_BackgroundImage from '@patternfly/react-tokens/dist/esm/c_background_image_BackgroundImage';
 
-export interface BackgroundImageProps extends Omit<React.HTMLProps<HTMLDivElement>, 'src'> {
-  /** Additional classes added to the background image. */
-  className?: string;
+export interface BackgroundImageProps extends React.HTMLProps<HTMLDivElement> {
   /** The URL or file path of the image for the background */
   src: string;
+  /** Additional classes added to the background image. */
+  className?: string;
 }
 
 export const BackgroundImage: React.FunctionComponent<BackgroundImageProps> = ({
   className,
-  // src,
+  src,
   ...props
 }: BackgroundImageProps) => (
   <div
     className={css(styles.backgroundImage, className)}
-    /* eslint-disable camelcase */
-    style={{
-      // [c_background_image_BackgroundImage.name as string]: `url(${src})`
-    }}
-    /* eslint-enable camelcase */
+    style={
+      {
+        '--pf-v5-c-background-image--BackgroundImage': `url(${src})`
+      } as React.CSSProperties
+    }
     {...props}
-  ></div>
+  />
 );
 
 BackgroundImage.displayName = 'BackgroundImage';
