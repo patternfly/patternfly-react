d3a36169412774cd73a9b1041ddb62f36bb21650
nicolethoen <nthoen@redhat.com>
2023-06-05 16:04:24 -0400

    fix(release): dryrun publishing prereleases

--- .github/release.sh
+++ .github/release.sh
@@ -17,8 +17,8 @@ echo "Doing a release..."
 LOG=$(git log --format="%s" -1 | grep -Poe "#\d+")
 PR_NUM=${LOG:1}
 
-# yarn run lerna publish --conventional-commits --conventional-prerelease --create-release=github --no-verify-access --dist-tag=alpha --yes 2>&1 | tee lerna-output.txt
-yarn run lerna version --conventional-commits --conventional-prerelease --no-private --yes --no-git-tag-version --no-push
+# yarn run lerna publish --conventional-commits --conventional-prerelease --create-release=github --no-verify-access --dist-tag=prerelease --yes 2>&1 | tee lerna-output.txt
+yarn run lerna version --conventional-commits --conventional-prerelease --dist-tag=prerelease --no-private --yes --no-git-tag-version --no-push
 
 if grep -i "Successfully published" lerna-output.txt; # Leave a Github comment
 then
