770e8fcf20976c2967d4a374031a1eeda028a00e
nicolethoen <nthoen@redhat.com>
2023-06-05 16:10:10 -0400

    stop being clever with dry runs

--- .github/release.sh
+++ .github/release.sh
@@ -18,7 +18,7 @@ LOG=$(git log --format="%s" -1 | grep -Poe "#\d+")
 PR_NUM=${LOG:1}
 
 # yarn run lerna publish --conventional-commits --conventional-prerelease --create-release=github --no-verify-access --dist-tag=prerelease --yes 2>&1 | tee lerna-output.txt
-yarn run lerna version --conventional-commits --conventional-prerelease --dist-tag=prerelease --no-private --yes --no-git-tag-version --no-push
+yarn run lerna version --conventional-commits --conventional-prerelease --no-private --yes --no-git-tag-version --no-push
 
 if grep -i "Successfully published" lerna-output.txt; # Leave a Github comment
 then
