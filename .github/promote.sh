13a29324a630ff8be9edb722af90064c1933e151
nicolethoen <nthoen@redhat.com>
2023-06-07 15:11:08 -0400

    alter logic

--- .github/promote.sh
+++ .github/promote.sh
@@ -12,7 +12,7 @@ echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
 # Update their versions and changelogs according to angular commit guidelines
 # https://github.com/angular/angular/blob/master/CONTRIBUTING.md#commit
 
-if [ -z "${CORE_VERSION}"]; then
+if [[ ! -z "${CORE_VERSION}" ]]; then
   echo "Updating to @patternfly/patternfly: ${CORE_VERSION}"
   npm pkg set dependencies.@patternfly/patternfly=${CORE_VERSION} --workspace @patternfly/react-core --workspace @patternfly/react-docs --workspace @patternfly/react-icons --workspace @patternfly/react-tokens --workspace @patternfly/react-styles
 fi
