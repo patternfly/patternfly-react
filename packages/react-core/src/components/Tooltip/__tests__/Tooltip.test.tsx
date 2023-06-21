8003b1da7adbb79ab9ba5a6752a66e6e6b1d3cc4
Eric Olkowski <thatblindgeye@gmail.com>
2023-06-21 15:20:19 -0400

    chore(Tooltip): updated unit tests

--- packages/react-core/src/components/Tooltip/__tests__/Tooltip.test.tsx
+++ packages/react-core/src/components/Tooltip/__tests__/Tooltip.test.tsx
@@ -1,43 +1,68 @@
-import * as React from 'react';
-import { render } from '@testing-library/react';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 import { Tooltip } from '../Tooltip';
 
-test('tooltip renders', () => {
-  const ref = React.createRef<HTMLDivElement>();
-  const { asFragment } = render(
-    <Tooltip
-      position="top"
-      triggerRef={ref}
-      content={
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id feugiat augue, nec fringilla turpis.
-        </div>
-      }
-    >
-      <div ref={ref}>Toggle tooltip</div>
-    </Tooltip>
-  );
-  expect(asFragment()).toMatchSnapshot();
+test('Does not render by default', () => {
+  render(<Tooltip content="Test content" />);
+
+  expect(screen.queryByText('Test content')).not.toBeInTheDocument();
 });
 
-test('tooltip renders in strict mode', () => {
-  const consoleError = jest.spyOn(console, 'error');
-  const ref = React.createRef<HTMLDivElement>();
-  const { asFragment } = render(
-    <React.StrictMode>
-      <Tooltip
-        position="top"
-        triggerRef={ref}
-        content={
-          <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id feugiat augue, nec fringilla turpis.
-          </div>
-        }
-      >
-        <div ref={ref}>Toggle tooltip</div>
-      </Tooltip>
-    </React.StrictMode>
-  );
-  expect(consoleError).not.toHaveBeenCalled();
-  expect(asFragment()).toMatchSnapshot();
+// Need to pass isVisible and make test callbacks async in order to render tooltip
+
+test('Renders with content', async () => {
+  render(<Tooltip isVisible content="Test content" />);
+
+  const tooltip = await screen.findByText('Test content');
+  expect(tooltip.parentElement).toBeVisible();
+});
+
+test('Renders with class name pf-v5-c-tooltip by default', async () => {
+  render(<Tooltip isVisible content="Test content" />);
+
+  const tooltip = await screen.findByText('Test content');
+  expect(tooltip.parentElement).toHaveClass('pf-v5-c-tooltip');
+});
+
+test('Renders with custom class names provided via prop', async () => {
+  render(<Tooltip isVisible className="test-class" content="Test content" />);
+
+  const tooltip = await screen.findByText('Test content');
+  expect(tooltip.parentElement).toHaveClass('test-class');
+});
+
+test('Renders with role of tooltip', async () => {
+  render(<Tooltip isVisible content="Test content" />);
+
+  const tooltip = await screen.findByText('Test content');
+  expect(tooltip.parentElement).toHaveAttribute('role', 'tooltip');
+});
+
+test('Renders with aria-live of off by default when triggerRef is not passed', async () => {
+  render(<Tooltip isVisible content="Test content" />);
+
+  const tooltip = await screen.findByText('Test content');
+  expect(tooltip.parentElement).toHaveAttribute('aria-live', 'off');
+});
+
+test('Renders with aria-live of polite by default when triggerRef is passed', async () => {
+  const triggerRef = React.createRef();
+  render(<Tooltip triggerRef={triggerRef} isVisible content="Test content" />);
+
+  const tooltip = await screen.findByText('Test content');
+  expect(tooltip.parentElement).toHaveAttribute('aria-live', 'polite');
+});
+
+test('Renders with value passed to aria-live prop', async () => {
+  render(<Tooltip aria-live="polite" isVisible content="Test content" />);
+
+  const tooltip = await screen.findByText('Test content');
+  expect(tooltip.parentElement).toHaveAttribute('aria-live', 'polite');
+});
+
+test('Renders with value passed to id prop', async () => {
+  render(<Tooltip id="custom-id" isVisible content="Test content" />);
+
+  const tooltip = await screen.findByText('Test content');
+  expect(tooltip.parentElement).toHaveAttribute('id', 'custom-id');
 });
