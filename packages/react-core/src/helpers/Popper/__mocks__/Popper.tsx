20e3c8ff6ddcd9cd27799f7c88bf7aa84ae9d890
Eric Olkowski <70952936+thatblindgeye@users.noreply.github.com>
2023-07-19 16:32:55 -0400

    chore(Tooltip): updated unit tests (#9295)
    * chore(Tooltip): updated unit tests
    
    * Updated mock and tests
    
    * Updated based on Austin feedback
    
    * Updated integration tests
    
    * Removed unused imports
    
    * Updated remaining tests using Popper mock
    
    * Removed extraenous snapshot test
    
    * Removed test
    
    * Split out onTooltipHidden test

--- packages/react-core/src/helpers/Popper/__mocks__/Popper.tsx
+++ packages/react-core/src/helpers/Popper/__mocks__/Popper.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import { PopperProps } from '../Popper';
 
-export const Popper = ({ popper, zIndex, isVisible, trigger }: PopperProps) => (
-  <>
-    <div>{popper}</div>
+export const Popper = ({
+  popper,
+  zIndex,
+  placement,
+  trigger,
+  enableFlip,
+  appendTo,
+  distance,
+  flipBehavior,
+  isVisible,
+  minWidth
+}: PopperProps) => (
+  <div data-testid="mock-wrapper">
+    <div data-testid="popper">{isVisible && popper}</div>
     <p>{`zIndex: ${zIndex}`}</p>
-    <p>{`isOpen: ${isVisible}`}</p>
-    <div>{trigger}</div>
-  </>
+    <p>{`isVisible: ${isVisible}`}</p>
+    <p>{`enableFlip: ${enableFlip}`}</p>
+    <p>{`placement: ${placement}`}</p>
+    <p>{`appendTo: ${appendTo}`}</p>
+    <p>{`distance: ${distance}`}</p>
+    <p>{`flipBehavior: ${flipBehavior}`}</p>
+    <p>{`minWidth: ${minWidth}`}</p>
+    {trigger}
+  </div>
 );
+
+export const getOpacityTransition = () => {};
