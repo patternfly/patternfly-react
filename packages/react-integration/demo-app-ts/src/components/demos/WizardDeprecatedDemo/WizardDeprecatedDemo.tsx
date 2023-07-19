77369cdf5125f60d80a555914d40fe611e5bbc59
Titani Labaj <39532947+tlabaj@users.noreply.github.com>
2023-07-19 16:39:21 -0400

    fix(chore): Fix deprecated wizard integration tests (#9312)
    * fix(chore): Fix deprecated wizard integration tests
    
    * updated non deprecated test as well
    
    ---------
    
    Co-authored-by: Titani <tlabaj@redaht.com>

--- packages/react-integration/demo-app-ts/src/components/demos/WizardDeprecatedDemo/WizardDeprecatedDemo.tsx
+++ packages/react-integration/demo-app-ts/src/components/demos/WizardDeprecatedDemo/WizardDeprecatedDemo.tsx
@@ -75,8 +75,96 @@ export class WizardDeprecatedDemo extends React.Component<React.HTMLProps<HTMLDi
       {
         name: 'Step with overflow',
         component: (
-          <div style={{ height: '800px' }}>
-            <p>Step 2</p>
+          <div>
+            <>
+              <p>
+                The content of this step overflows and creates a scrollbar, which causes a tabindex of "0", a role of
+                "region", and an aria-label or aria-labelledby to be applied.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum in neque nec pharetra. Duis
+                lacinia vel sapien ut imperdiet. Nunc ultrices mollis dictum. Duis tempus, massa nec tincidunt tempor,
+                enim ex porttitor odio, eu facilisis dolor tortor id sapien. Etiam sit amet molestie lacus. Nulla
+                facilisi. Duis eget finibus ipsum. Quisque dictum enim sed sodales porta. Curabitur eget orci eu risus
+                posuere pulvinar id nec turpis. Morbi mattis orci vel posuere tincidunt. Fusce bibendum et libero a
+                auctor.
+              </p>
+              <p>
+                Proin elementum commodo sodales. Quisque eget libero mattis, ornare augue at, egestas nisi. Mauris
+                ultrices orci fringilla pretium mattis. Aliquam erat volutpat. Sed pharetra condimentum dui, nec
+                bibendum ante. Vestibulum sollicitudin, sem accumsan pharetra molestie, purus turpis lacinia lorem,
+                commodo sodales quam lectus a urna. Nam gravida, felis a lacinia varius, ex ipsum ultrices orci, non
+                egestas diam velit in mi. Ut sit amet commodo orci. Duis sed diam odio. Duis mi metus, dignissim in odio
+                nec, ornare aliquet libero. Sed luctus elit nibh. Quisque et felis diam. Integer ac metus dolor.
+              </p>
+            </>
+            <>
+              <p>
+                The content of this step overflows and creates a scrollbar, which causes a tabindex of "0", a role of
+                "region", and an aria-label or aria-labelledby to be applied.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum in neque nec pharetra. Duis
+                lacinia vel sapien ut imperdiet. Nunc ultrices mollis dictum. Duis tempus, massa nec tincidunt tempor,
+                enim ex porttitor odio, eu facilisis dolor tortor id sapien. Etiam sit amet molestie lacus. Nulla
+                facilisi. Duis eget finibus ipsum. Quisque dictum enim sed sodales porta. Curabitur eget orci eu risus
+                posuere pulvinar id nec turpis. Morbi mattis orci vel posuere tincidunt. Fusce bibendum et libero a
+                auctor.
+              </p>
+              <p>
+                Proin elementum commodo sodales. Quisque eget libero mattis, ornare augue at, egestas nisi. Mauris
+                ultrices orci fringilla pretium mattis. Aliquam erat volutpat. Sed pharetra condimentum dui, nec
+                bibendum ante. Vestibulum sollicitudin, sem accumsan pharetra molestie, purus turpis lacinia lorem,
+                commodo sodales quam lectus a urna. Nam gravida, felis a lacinia varius, ex ipsum ultrices orci, non
+                egestas diam velit in mi. Ut sit amet commodo orci. Duis sed diam odio. Duis mi metus, dignissim in odio
+                nec, ornare aliquet libero. Sed luctus elit nibh. Quisque et felis diam. Integer ac metus dolor.
+              </p>
+            </>
+            <>
+              <p>
+                The content of this step overflows and creates a scrollbar, which causes a tabindex of "0", a role of
+                "region", and an aria-label or aria-labelledby to be applied.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum in neque nec pharetra. Duis
+                lacinia vel sapien ut imperdiet. Nunc ultrices mollis dictum. Duis tempus, massa nec tincidunt tempor,
+                enim ex porttitor odio, eu facilisis dolor tortor id sapien. Etiam sit amet molestie lacus. Nulla
+                facilisi. Duis eget finibus ipsum. Quisque dictum enim sed sodales porta. Curabitur eget orci eu risus
+                posuere pulvinar id nec turpis. Morbi mattis orci vel posuere tincidunt. Fusce bibendum et libero a
+                auctor.
+              </p>
+              <p>
+                Proin elementum commodo sodales. Quisque eget libero mattis, ornare augue at, egestas nisi. Mauris
+                ultrices orci fringilla pretium mattis. Aliquam erat volutpat. Sed pharetra condimentum dui, nec
+                bibendum ante. Vestibulum sollicitudin, sem accumsan pharetra molestie, purus turpis lacinia lorem,
+                commodo sodales quam lectus a urna. Nam gravida, felis a lacinia varius, ex ipsum ultrices orci, non
+                egestas diam velit in mi. Ut sit amet commodo orci. Duis sed diam odio. Duis mi metus, dignissim in odio
+                nec, ornare aliquet libero. Sed luctus elit nibh. Quisque et felis diam. Integer ac metus dolor.
+              </p>
+            </>
+            <>
+              <p>
+                The content of this step overflows and creates a scrollbar, which causes a tabindex of "0", a role of
+                "region", and an aria-label or aria-labelledby to be applied.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum in neque nec pharetra. Duis
+                lacinia vel sapien ut imperdiet. Nunc ultrices mollis dictum. Duis tempus, massa nec tincidunt tempor,
+                enim ex porttitor odio, eu facilisis dolor tortor id sapien. Etiam sit amet molestie lacus. Nulla
+                facilisi. Duis eget finibus ipsum. Quisque dictum enim sed sodales porta. Curabitur eget orci eu risus
+                posuere pulvinar id nec turpis. Morbi mattis orci vel posuere tincidunt. Fusce bibendum et libero a
+                auctor.
+              </p>
+              <p>
+                Proin elementum commodo sodales. Quisque eget libero mattis, ornare augue at, egestas nisi. Mauris
+                ultrices orci fringilla pretium mattis. Aliquam erat volutpat. Sed pharetra condimentum dui, nec
+                bibendum ante. Vestibulum sollicitudin, sem accumsan pharetra molestie, purus turpis lacinia lorem,
+                commodo sodales quam lectus a urna. Nam gravida, felis a lacinia varius, ex ipsum ultrices orci, non
+                egestas diam velit in mi. Ut sit amet commodo orci. Duis sed diam odio. Duis mi metus, dignissim in odio
+                nec, ornare aliquet libero. Sed luctus elit nibh. Quisque et felis diam. Integer ac metus dolor.
+              </p>
+            </>
+
             <button onClick={this.handleRoleWizardToggle}>Open wizard in modal</button>
           </div>
         )
@@ -122,7 +210,7 @@ export class WizardDeprecatedDemo extends React.Component<React.HTMLProps<HTMLDi
           id="wizard-focusable-overflow"
           steps={stepsOnOverflow}
           startAtStep={1}
-          height={500}
+          height={200}
           mainAriaLabel="Step content"
         />
         <Wizard
