5e6ac94633abb5fc30fb32dff3f68decc58d9984
Dana Gutride <dgutride@gmail.com>
2023-07-27 10:33:39 -0400

    chore(dryrun): enable dry run for preminor

--- .github/promote.sh
+++ .github/promote.sh
@@ -24,4 +24,4 @@ fi
 yarn run lerna version --conventional-commits --conventional-graduate --no-private --yes --no-git-tag-version --no-push
 
 # immediately after promote - set up repo for next prerelease
-yarn run lerna version preminor --force-publish --conventional-commits --no-private --yes --preid prerelease
+# yarn run lerna version preminor --force-publish --conventional-commits --no-private --yes --preid prerelease
