3f11b91dc7ba938373a8fe62eff6d3bc4c4da39b
David Díaz <1691872+dgdavid@users.noreply.github.com>
2023-07-31 20:58:10 +0100

    fix(Modal): remove `aria-hidden` from siblings when unmounting the component (v5) (#9110)
    * fix(Modal): remove aria-hidden when unmount
    
    The component adds the aria-hidden attribute to its siblings when open
    and removes it when closed. However, if the component is mounted as open
    but then directly unmounted, its siblings will remain hidden from the
    accessibility API forever.
    
    Thus, the logic for removing these attributes is now also triggered when
    the component is unmounted.
    
    * fix(Modal): remove trailing spaces in test file

--- packages/react-core/src/components/Modal/Modal.tsx
+++ packages/react-core/src/components/Modal/Modal.tsx
@@ -213,6 +213,7 @@ export class Modal extends React.Component<ModalProps, ModalState> {
     }
     target.removeEventListener('keydown', this.handleEscKeyClick, false);
     target.classList.remove(css(styles.backdropOpen));
+    this.toggleSiblingsFromScreenReaders(false);
   }
 
   render() {
