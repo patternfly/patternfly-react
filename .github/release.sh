230613a0ebe70ffc8b91a7670987b92051d072b1
Austin Sullivan <ausulliv@redhat.com>
2023-08-16 09:16:07 -0400

    chore(CI): updated lerna to not publish private packages (#9501)

--- .github/release.sh
+++ .github/release.sh
@@ -17,7 +17,7 @@ echo "Doing a release..."
 LOG=$(git log --format="%s" -1 | grep -Poe "#\d+")
 PR_NUM=${LOG:1}
 
-yarn run lerna publish --conventional-commits --conventional-prerelease --dist-tag=prerelease --yes 2>&1 | tee lerna-output.txt
+yarn run lerna publish --conventional-commits --conventional-prerelease --dist-tag=prerelease --yes --no-private 2>&1 | tee lerna-output.txt
 
 # use lerna command below for dry run
 # yarn run lerna version --conventional-commits --conventional-prerelease --yes --no-git-tag-version --no-push
