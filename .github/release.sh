585932486b262723e8c7090ce3a72aeae77569ec
nicolethoen <nthoen@redhat.com>
2023-08-23 09:08:05 -0400

    fix: bump lerna version

--- .github/release.sh
+++ .github/release.sh
@@ -17,10 +17,10 @@ echo "Doing a release..."
 LOG=$(git log --format="%s" -1 | grep -Poe "#\d+")
 PR_NUM=${LOG:1}
 
-yarn run lerna publish prerelease --conventional-commits --conventional-prerelease --preid=prerelease --dist-tag=prerelease --yes --no-private 2>&1 | tee lerna-output.txt
+yarn run lerna publish prerelease --preid=prerelease --dist-tag=prerelease --yes --no-private 2>&1 --dryRun | tee lerna-output.txt
 
 # use lerna command below for dry run
-# yarn run lerna version prerelease --conventional-prerelease --preid=prerelease --no-private --yes --no-git-tag-version --no-push | tee lerna-output.txt
+# yarn run lerna publish prerelease --conventional-prerelease --preid=prerelease --no-private --yes --no-git-tag-version --no-push | tee lerna-output.txt
 
 if grep -i "Successfully published" lerna-output.txt; # Leave a Github comment
 then
