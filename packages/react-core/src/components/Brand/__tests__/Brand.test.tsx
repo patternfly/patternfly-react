af52b18a21074703cb54a3c18e97ccb3c278a6cc
Katie McFaul <kmcfaul@redhat.com>
2023-08-28 10:22:43 -0400

    chore(Brand): update tests

--- packages/react-core/src/components/Brand/__tests__/Brand.test.tsx
+++ packages/react-core/src/components/Brand/__tests__/Brand.test.tsx
@@ -3,34 +3,30 @@ import { render, screen } from '@testing-library/react';
 import { Brand } from '../Brand';
 import '@testing-library/jest-dom';
 
-describe('Brand', () => {
-  test('simple brand', () => {
-    const { asFragment } = render(<Brand alt="brand" />);
-    expect(asFragment()).toMatchSnapshot();
-  });
+test('simple brand', () => {
+  render(<Brand alt="brand" />);
+  expect(screen.getByAltText('brand')).toBeInTheDocument();
+});
 
-  test('passing children creates picture brand', () => {
-    render(
-      <Brand alt="brand">
-        <div>test</div>
-      </Brand>
-    );
-    expect(screen.getByText('test')).toBeInTheDocument();
-  });
+test('passing children creates picture brand', () => {
+  render(
+    <Brand alt="brand">
+      <div>test</div>
+    </Brand>
+  );
+  expect(screen.getByText('test')).toBeInTheDocument();
+});
 
-  test('styles get spread when there are children', () => {
-    render(
-      <Brand alt="brand" data-testid="brand" style={{color: "blue"}}>
-        <div>test width</div>
-      </Brand>
-    );
-    expect(screen.getByTestId('brand')).toHaveStyle(`color: blue`);
-  });
+test('styles get spread when there are children', () => {
+  render(
+    <Brand alt="brand" data-testid="brand" style={{ color: 'blue' }}>
+      <div>test width</div>
+    </Brand>
+  );
+  expect(screen.getByTestId('brand')).toHaveStyle(`color: blue`);
+});
 
-  test('styles get spread when there are no children', () => {
-    render(
-      <Brand alt="brand no children" style={{width: "30px"}} />
-    );
-    expect(screen.getByAltText('brand no children')).toHaveStyle(`width: 30px`);
-  });
+test('styles get spread when there are no children', () => {
+  render(<Brand alt="brand no children" style={{ width: '30px' }} />);
+  expect(screen.getByAltText('brand no children')).toHaveStyle(`width: 30px`);
 });
