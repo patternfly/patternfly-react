d1c9973d783e9cbec951936eec0b4e0c95f21191
nicolethoen <nthoen@redhat.com>
2023-09-05 14:06:47 -0400

    fix: dry run patch release with new core versions [skip-a11y]

--- .github/release.sh
+++ .github/release.sh
@@ -17,10 +17,10 @@ echo "Doing a release..."
 LOG=$(git log --format="%s" -1 | grep -Poe "#\d+")
 PR_NUM=${LOG:1}
 
-yarn run lerna publish --conventional-commits --conventional-prerelease --dist-tag=prerelease --yes 2>&1 | tee lerna-output.txt
+# yarn run lerna publish prerelease --preid=prerelease --dist-tag=prerelease --yes 2>&1 | tee lerna-output.txt
 
 # use lerna command below for dry run
-# yarn run lerna version --conventional-commits --conventional-prerelease --yes --no-git-tag-version --no-push
+yarn run lerna version patch --yes --no-git-tag-version --no-push | tee lerna-output.txt
 
 if grep -i "Successfully published" lerna-output.txt; # Leave a Github comment
 then
