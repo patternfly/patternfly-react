97db9440a2d631b7d36c26626235eea32797575c
Katie McFaul <kmcfaul@redhat.com>
2023-09-11 13:09:37 -0400

    add more tests

--- packages/react-core/src/components/Brand/__tests__/Brand.test.tsx
+++ packages/react-core/src/components/Brand/__tests__/Brand.test.tsx
@@ -8,13 +8,38 @@ test('simple brand', () => {
   expect(screen.getByAltText('brand')).toBeInTheDocument();
 });
 
-test('passing children creates picture brand', () => {
+test('Renders with custom class name when className prop is provided', () => {
+  render(<Brand alt="brand" className="custom-class" />);
+  expect(screen.getByAltText('brand')).toHaveClass('custom-class');
+});
+
+test('Renders passed children', () => {
   render(
     <Brand alt="brand">
       <div>test</div>
     </Brand>
   );
   expect(screen.getByText('test')).toBeInTheDocument();
+});
+
+test('Renders as picture when children are present', () => {
+  render(
+    <Brand alt="brand">
+      <div>test</div>
+    </Brand>
+  );
+  expect(screen.getByText('test')?.parentElement?.tagName).toBe('PICTURE');
+});
+
+test('Renders as img when no children are present', () => {
+  render(<Brand alt="brand" />);
+  expect(screen.getByAltText('brand')?.tagName).toBe('IMG');
+});
+
+test('Renders with passed src prop', () => {
+  render(<Brand alt="brand" src="test.png" />);
+  const image = screen.getByRole('img') as HTMLImageElement;
+  expect(image.src).toMatch('test.png');
 });
 
 test('styles get spread when there are children', () => {
