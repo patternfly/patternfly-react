ce3c5cb73ae625c5799b4140a0b60c99cff2682a
Katie McFaul <kmcfaul@redhat.com>
2023-09-11 13:38:01 -0400

    chore(BackgroundImage): update tests

--- packages/react-core/src/components/BackgroundImage/__tests__/BackgroundImage.test.tsx
+++ packages/react-core/src/components/BackgroundImage/__tests__/BackgroundImage.test.tsx
@@ -2,19 +2,27 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { BackgroundImage } from '../BackgroundImage';
 
-test('has default className and src URL applied to style', () => {
+test('renders with default className pf-v5-c-background-image', () => {
+  render(<BackgroundImage src="/image/url.png" data-testid="test-id" />);
+  expect(screen.getByTestId('test-id')).toHaveClass('pf-v5-c-background-image', { exact: true });
+});
+
+test('has src URL applied to style', () => {
   render(<BackgroundImage src="/image/url.png" data-testid="test-id" />);
 
-  const backgroundImage = screen.getByTestId('test-id');
-  const backgroundImageStyle = backgroundImage.getAttribute('style');
+  const backgroundImageStyle = screen.getByTestId('test-id').getAttribute('style');
 
-  expect(backgroundImage).toHaveClass('pf-v5-c-background-image');
   expect(backgroundImageStyle).toContain('--pf-v5-c-background-image--BackgroundImage');
   expect(backgroundImageStyle).toContain('/image/url.png');
 });
 
-test('has additional className when one is provided', () => {
+test('renders with custom className when one is provided', () => {
   render(<BackgroundImage src="/image/url.png" className="another-class" data-testid="test-id" />);
 
   expect(screen.getByTestId('test-id')).toHaveClass('another-class');
+});
+
+test('Matches the snapshot', () => {
+  const { asFragment } = render(<BackgroundImage src="/image/url.png" data-testid="test-id" />);
+  expect(asFragment()).toMatchSnapshot();
 });
