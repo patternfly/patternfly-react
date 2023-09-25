4d4c3f2945b3fc56b82c89edd20fd98b8edf06db
mfrances <mfrances@redhat.com>
2023-09-25 17:01:50 -0400

    chore(TextInputGroup): allow input in basic example

--- packages/react-core/src/components/TextInputGroup/examples/TextInputGroupBasic.tsx
+++ packages/react-core/src/components/TextInputGroup/examples/TextInputGroupBasic.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { TextInputGroup, TextInputGroupMain } from '@patternfly/react-core';
 
-export const TextInputGroupBasic: React.FunctionComponent = () => (
-  <TextInputGroup>
-    <TextInputGroupMain />
-  </TextInputGroup>
-);
+export const TextInputGroupBasic: React.FunctionComponent = () => {
+  const [value, setValue] = React.useState('');
+  return (
+    <TextInputGroup>
+      <TextInputGroupMain value={value} onChange={(_event, value) => setValue(value)} />
+    </TextInputGroup>
+  );
+};
