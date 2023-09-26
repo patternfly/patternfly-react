1fb7c9ccff755bdf2d9b16b58a61e4ff917da8ee
Jenny <32821331+jenny-s51@users.noreply.github.com>
2023-09-26 15:18:01 -0400

    PR feedback from Eric and Titani

--- packages/react-core/src/components/CodeBlock/__tests__/CodeBlock.test.tsx
+++ packages/react-core/src/components/CodeBlock/__tests__/CodeBlock.test.tsx
@@ -1,32 +1,35 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { CodeBlock } from '../CodeBlock';
-import { CodeBlockAction } from '../CodeBlockAction';
-import { CodeBlockCode } from '../CodeBlockCode';
+import styles from '@patternfly/react-styles/css/components/CodeBlock/code-block';
 
 test('CodeBlock renders', () => {
-    render(<CodeBlock>test text</CodeBlock>);
-    expect(screen.getByText('test text')).toBeVisible();
-  });
+  render(<CodeBlock>test text</CodeBlock>);
+  expect(screen.getByText('test text')).toBeVisible();
+});
 
-test('CodeBlock content renders with class pf-v5-c-code-block__content by default', () => {
+test(`CodeBlock content renders with class ${styles.codeBlockContent} by default`, () => {
   render(<CodeBlock>Test</CodeBlock>);
 
-  expect(screen.getByText('Test')).toHaveClass('pf-v5-c-code-block__content');
+  expect(screen.getByText('Test')).toHaveClass(styles.codeBlockContent);
 });
 
 test('CodeBlock renders with custom class', () => {
-  render(<CodeBlock data-testid="code-block" className="tester">Test</CodeBlock>);
+  render(
+    <CodeBlock data-testid="code-block" className="tester">
+      Test
+    </CodeBlock>
+  );
 
   expect(screen.getByTestId('code-block')).toHaveClass('tester');
 });
 
-  test('Renders code when child is of type CodeBlockCode', () => {
-    render(
-      <CodeBlock actions={<CodeBlockAction>button</CodeBlockAction>}>
-        <CodeBlockCode>inside pre/code tags</CodeBlockCode>
-        test outer text
-      </CodeBlock>
-    );
-    expect(screen.getByText('inside pre/code tags')).toBeVisible();
-  });
+test('Renders when actions are passed to CodeBlock', () => {
+  render(<CodeBlock actions={<div>actions</div>} />);
+  expect(screen.getByText('actions')).toBeVisible();
+});
+
+test('Matches the snapshot', () => {
+  const { asFragment } = render(<CodeBlock actions={<div>actions</div>}>children</CodeBlock>);
+  expect(asFragment()).toMatchSnapshot();
+});
--- packages/react-core/src/components/CodeBlock/__tests__/CodeBlockAction.test.tsx
+++ packages/react-core/src/components/CodeBlock/__tests__/CodeBlockAction.test.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { CodeBlockAction } from '../CodeBlockAction';
+import styles from '@patternfly/react-styles/css/components/CodeBlock/code-block';
 
 test('CodeBlockAction renders', () => {
-  render(<CodeBlockAction data-testid="code-block-action">action</CodeBlockAction>);
+  render(<CodeBlockAction>action</CodeBlockAction>);
   expect(screen.getByText('action')).toBeVisible();
 });
 
-test('CodeBlockAction renders with class pf-v5-c-code-block__actions-item by default', () => {
-  render(<CodeBlockAction data-testid="code-block-action">Test</CodeBlockAction>);
+test(`CodeBlockAction renders with class ${styles.codeBlockActions}-item by default`, () => {
+  render(<CodeBlockAction>Test</CodeBlockAction>);
 
-  expect(screen.getByText('Test')).toHaveClass('pf-v5-c-code-block__actions-item');
+  expect(screen.getByText('Test')).toHaveClass(`${styles.codeBlockActions}-item`);
 });
 
 test('CodeBlockAction renders with custom class', () => {
-  render(
-    <CodeBlockAction className="tester">
-      Test
-    </CodeBlockAction>
-  );
+  render(<CodeBlockAction className="tester">Test</CodeBlockAction>);
 
   expect(screen.getByText('Test')).toHaveClass('tester');
+});
+
+test('Matches the snapshot', () => {
+  const { asFragment } = render(<CodeBlockAction>children</CodeBlockAction>);
+  expect(asFragment()).toMatchSnapshot();
 });
