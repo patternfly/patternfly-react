af15759337958bee14e65cc9729f804f63d9e117
gitdallas <dallas.nicol@gmail.com>
2023-10-05 13:16:02 -0500

    add some descriptive text in the example

--- packages/react-core/src/components/ClipboardCopy/examples/ClipboardCopyExpandedWithAltTitle.tsx
+++ packages/react-core/src/components/ClipboardCopy/examples/ClipboardCopyExpandedWithAltTitle.tsx
@@ -8,7 +8,9 @@ export const ClipboardCopyExpanded: React.FunctionComponent = () => (
     clickTip="Copied"
     variant={ClipboardCopyVariant.expansion}
   >
-    Got a lot of text here, need to see all of it? Click that arrow on the left side and check out the resulting
-    expansion.
+    This could be a lot of information that you'd want to summarize with a title as above. The title above will be
+    readonly as it was defined as a title rather than just the first part of this text. However, a user can still modify
+    this content which gets copied into the clipboard. You can use the isReadOnly flag to prevent them from changing
+    this.
   </ClipboardCopy>
 );
