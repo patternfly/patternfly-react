388ec1200fc1f645f7223d025a0e058d20121d6d
David Díaz <1691872+dgdavid@users.noreply.github.com>
2023-10-16 18:35:43 +0100

    [v4] fix(InputGroup): don't add aria-describedby automatically (#9671)
    * fix(InputGroup): don't add aria-describedby automatically
    
    For fixing https://github.com/patternfly/patternfly-react/issues/9667
    
    * test(NumberInput): drop aria-describedby workaround
    
    Test was setting the `aria-describedby=''` attribute to a NumericInput
    inside an InputGroup, most probably to avoid the autogenerated value
    (see https://github.com/patternfly/patternfly-react/issues/9667).
    
    However, after cleaning up the InputGroup component,
    `aria-describedby=''` is not a workaround anymore for unset the
    attribute, breaking the snapshot associated to the test.

--- packages/react-core/src/components/InputGroup/InputGroup.tsx
+++ packages/react-core/src/components/InputGroup/InputGroup.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import styles from '@patternfly/react-styles/css/components/InputGroup/input-group';
 import { css } from '@patternfly/react-styles';
-import { FormSelect } from '../FormSelect';
-import { TextArea } from '../TextArea';
-import { TextInput } from '../TextInput';
 
 export interface InputGroupProps extends React.HTMLProps<HTMLDivElement> {
   /** Additional classes added to the input group. */
@@ -20,25 +17,12 @@ export const InputGroup: React.FunctionComponent<InputGroupProps> = ({
   innerRef,
   ...props
 }: InputGroupProps) => {
-  const formCtrls = [FormSelect, TextArea, TextInput].map(comp => comp.displayName);
-  const idItem = React.Children.toArray(children).find(
-    (child: any) => !formCtrls.includes(child.type.displayName) && child.props.id
-  ) as React.ReactElement<{ id: string }>;
-
   const ref = React.useRef(null);
   const inputGroupRef = innerRef || ref;
 
   return (
     <div ref={inputGroupRef} className={css(styles.inputGroup, className)} {...props}>
-      {idItem
-        ? React.Children.map(children, (child: any) =>
-            !formCtrls.includes(child.type.displayName) || child.props['aria-describedby']
-              ? child
-              : React.cloneElement(child, {
-                  'aria-describedby': child.props['aria-describedby'] === '' ? undefined : idItem.props.id
-                })
-          )
-        : children}
+      {children}
     </div>
   );
 };
