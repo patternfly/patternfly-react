933ff36a112e8d28acdd1f2ad81485b34a853906
Katie McFaul <kmcfaul@redhat.com>
2023-10-18 10:53:00 -0400

    chore(HelperText): update tests

--- packages/react-core/src/components/HelperText/__tests__/HelperText.test.tsx
+++ packages/react-core/src/components/HelperText/__tests__/HelperText.test.tsx
@@ -1,66 +1,75 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { HelperText } from '../HelperText';
 import { HelperTextItem } from '../HelperTextItem';
-import CheckIcon from '@patternfly/react-icons/dist/esm/icons/check-icon';
 
-describe('HelperText', () => {
-  test('simple helper text renders successfully', () => {
-    const { asFragment } = render(
-      <HelperText>
-        <HelperTextItem>help test text</HelperTextItem>
-      </HelperText>
+import styles from '@patternfly/react-styles/css/components/HelperText/helper-text';
+
+test('Renders to match snapshot', () => {
+  const { asFragment } = render(
+    <HelperText>
+      <HelperTextItem>help test text</HelperTextItem>
+      <HelperTextItem>help test text 2</HelperTextItem>
+      <HelperTextItem>help test text 3</HelperTextItem>
+    </HelperText>
+  );
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test('HelperText renders custom className', () => {
+  render(
+    <HelperText className="custom" data-testid="helper">
+      <HelperTextItem>help test text 1</HelperTextItem>
+    </HelperText>
+  );
+  expect(screen.getByTestId('helper')).toHaveClass('custom');
+});
+
+Object.values(['indeterminate', 'warning', 'success', 'error']).forEach((variant) => {
+  test(`Renders ${styles.modifiers[variant]} when variant = ${variant}`, () => {
+    render(
+      <HelperTextItem variant={variant as 'default' | 'indeterminate' | 'warning' | 'success' | 'error'}>
+        text
+      </HelperTextItem>
     );
-    expect(asFragment()).toMatchSnapshot();
+    expect(screen.getByText('text').parentElement).toHaveClass(styles.modifiers[variant]);
   });
+});
 
-  Object.values(['default', 'indeterminate', 'warning', 'success', 'invalid']).forEach((variant) => {
-    test(`${variant} helper text variant applies successfully`, () => {
-      const { asFragment } = render(
-        <HelperTextItem variant={variant as 'default' | 'indeterminate' | 'warning' | 'success' | 'error'}>
-          {variant} help test text
-        </HelperTextItem>
-      );
-      expect(asFragment()).toMatchSnapshot();
-    });
-  });
+test('HelperTextItem renders custom className', () => {
+  render(
+    <HelperText>
+      <HelperTextItem className="custom">help test text 1</HelperTextItem>
+    </HelperText>
+  );
+  expect(screen.getByText('help test text 1').parentElement).toHaveClass('custom');
+});
 
-  test('variant comonent helper text renders properly', () => {
-    const { asFragment } = render(
-      <HelperText component="ul">
-        <HelperTextItem component="li">help test text 1</HelperTextItem>
-        <HelperTextItem component="li">help test text 2</HelperTextItem>
-      </HelperText>
-    );
-    expect(asFragment()).toMatchSnapshot();
-  });
+test('Renders as ul when component = ul', () => {
+  render(
+    <HelperText component="ul" aria-label="helper">
+      <HelperTextItem component="li">help test text 1</HelperTextItem>
+      <HelperTextItem component="li">help test text 2</HelperTextItem>
+    </HelperText>
+  );
+  expect(screen.getByLabelText('helper').tagName).toBe('UL');
+  expect(screen.getByText('help test text 1').parentElement?.tagName).toBe('LI');
+});
 
-  test('icon helper text renders properly', () => {
-    const { asFragment } = render(
-      <HelperText>
-        <HelperTextItem icon={<CheckIcon />}>help test text</HelperTextItem>
-      </HelperText>
-    );
-    expect(asFragment()).toMatchSnapshot();
-  });
+test('Renders custom icon', () => {
+  render(
+    <HelperText>
+      <HelperTextItem icon={<div>test</div>}>help test text</HelperTextItem>
+    </HelperText>
+  );
+  expect(screen.getByText('test').parentElement).toHaveClass(styles.helperTextItemIcon);
+});
 
-  test('dynamic helper text renders successfully', () => {
-    const { asFragment } = render(
-      <HelperText>
-        <HelperTextItem isDynamic>help test text</HelperTextItem>
-      </HelperText>
-    );
-    expect(asFragment()).toMatchSnapshot();
-  });
-
-  test('helper text block renders successfully', () => {
-    const { asFragment } = render(
-      <HelperText>
-        <HelperTextItem>help test text 1</HelperTextItem>
-        <HelperTextItem>help test text 2</HelperTextItem>
-        <HelperTextItem>help test text 3</HelperTextItem>
-      </HelperText>
-    );
-    expect(asFragment()).toMatchSnapshot();
-  });
+test('Renders dynamic helper text', () => {
+  render(
+    <HelperText>
+      <HelperTextItem isDynamic>help test text</HelperTextItem>
+    </HelperText>
+  );
+  expect(screen.getByText('help test text').parentElement).toHaveClass(styles.modifiers.dynamic);
 });
