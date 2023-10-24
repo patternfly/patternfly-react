5085ba6919373e7bd41f5d18b255d31441ef5af5
Katie McFaul <kmcfaul@redhat.com>
2023-10-24 10:08:58 -0400

    break out helpertextitem tests, update tests

--- packages/react-core/src/components/HelperText/__tests__/HelperText.test.tsx
+++ packages/react-core/src/components/HelperText/__tests__/HelperText.test.tsx
@@ -1,75 +1,66 @@
 import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 import { HelperText } from '../HelperText';
-import { HelperTextItem } from '../HelperTextItem';
 
 import styles from '@patternfly/react-styles/css/components/HelperText/helper-text';
 
 test('Renders to match snapshot', () => {
-  const { asFragment } = render(
-    <HelperText>
-      <HelperTextItem>help test text</HelperTextItem>
-      <HelperTextItem>help test text 2</HelperTextItem>
-      <HelperTextItem>help test text 3</HelperTextItem>
-    </HelperText>
-  );
+  const { asFragment } = render(<HelperText />);
   expect(asFragment()).toMatchSnapshot();
 });
 
-test('HelperText renders custom className', () => {
+test('Renders without children', () => {
   render(
-    <HelperText className="custom" data-testid="helper">
-      <HelperTextItem>help test text 1</HelperTextItem>
-    </HelperText>
+    <div data-testid="container">
+      <HelperText />
+    </div>
   );
-  expect(screen.getByTestId('helper')).toHaveClass('custom');
+
+  expect(screen.getByTestId('container').firstChild).toBeVisible();
 });
 
-Object.values(['indeterminate', 'warning', 'success', 'error']).forEach((variant) => {
-  test(`Renders ${styles.modifiers[variant]} when variant = ${variant}`, () => {
-    render(
-      <HelperTextItem variant={variant as 'default' | 'indeterminate' | 'warning' | 'success' | 'error'}>
-        text
-      </HelperTextItem>
-    );
-    expect(screen.getByText('text').parentElement).toHaveClass(styles.modifiers[variant]);
-  });
+test('Renders default classes', () => {
+  render(<HelperText>text</HelperText>);
+  expect(screen.getByText('text')).toHaveClass(styles.helperText);
 });
 
-test('HelperTextItem renders custom className', () => {
-  render(
-    <HelperText>
-      <HelperTextItem className="custom">help test text 1</HelperTextItem>
-    </HelperText>
-  );
-  expect(screen.getByText('help test text 1').parentElement).toHaveClass('custom');
+test('Renders id when id is passed', () => {
+  render(<HelperText id="helper-id">text </HelperText>);
+  expect(screen.getByText('text')).toHaveAttribute('id');
 });
 
-test('Renders as ul when component = ul', () => {
+test('Renders aria-live when isLiveRegion is passed', () => {
+  render(<HelperText isLiveRegion>text </HelperText>);
+  expect(screen.getByText('text')).toHaveAttribute('aria-live');
+});
+
+test('Spreads additional props when passed', () => {
+  render(<HelperText>text </HelperText>);
+  expect(screen.getByText('text')).toBeInTheDocument();
+});
+
+test('Renders custom className', () => {
+  render(<HelperText className="custom">text </HelperText>);
+  expect(screen.getByText('text')).toHaveClass('custom');
+});
+
+test('Renders with element passed to component prop', () => {
+  render(<HelperText component="ul">text</HelperText>);
+  expect(screen.getByText('text').tagName).toBe('UL');
+});
+
+test('Renders aria-label and role when component = ul', () => {
   render(
     <HelperText component="ul" aria-label="helper">
-      <HelperTextItem component="li">help test text 1</HelperTextItem>
-      <HelperTextItem component="li">help test text 2</HelperTextItem>
+      text
     </HelperText>
   );
-  expect(screen.getByLabelText('helper').tagName).toBe('UL');
-  expect(screen.getByText('help test text 1').parentElement?.tagName).toBe('LI');
+  expect(screen.getByText('text')).toHaveAttribute('aria-label', 'helper');
+  expect(screen.getByText('text')).toHaveAttribute('role', 'list');
 });
 
-test('Renders custom icon', () => {
-  render(
-    <HelperText>
-      <HelperTextItem icon={<div>test</div>}>help test text</HelperTextItem>
-    </HelperText>
-  );
-  expect(screen.getByText('test').parentElement).toHaveClass(styles.helperTextItemIcon);
-});
-
-test('Renders dynamic helper text', () => {
-  render(
-    <HelperText>
-      <HelperTextItem isDynamic>help test text</HelperTextItem>
-    </HelperText>
-  );
-  expect(screen.getByText('help test text').parentElement).toHaveClass(styles.modifiers.dynamic);
+test('Does not render aria-label and role when component != ul', () => {
+  render(<HelperText aria-label="helper">text</HelperText>);
+  expect(screen.getByText('text')).not.toHaveAttribute('aria-label', 'helper');
+  expect(screen.getByText('text')).not.toHaveAttribute('role', 'list');
 });
