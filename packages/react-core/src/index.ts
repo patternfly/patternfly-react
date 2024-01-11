c14c48c2acf901dcf7773420c14d5743aa8aedec
Vojtech Szocs <vszocs@redhat.com>
2024-01-11 19:27:12 +0100

    Update react-core dist/dynamic to include exported styles (#9939)

--- packages/react-core/src/index.ts
+++ packages/react-core/src/index.ts
@@ -2,4 +2,4 @@ export * from './components';
 export * from './demos';
 export * from './layouts';
 export * from './helpers';
-export { BaseSizes, DeviceSizes } from './styles/sizes';
+export * from './styles';
