b22767e57b6f7d27315805ed5df2248d031be2bd
Austin Sullivan <ausulliv@redhat.com>
2024-01-11 16:56:28 -0500

    feat(BackgroundImage): update background image for penta

--- packages/react-core/src/components/BackgroundImage/examples/BackgroundImageBasic.tsx
+++ packages/react-core/src/components/BackgroundImage/examples/BackgroundImageBasic.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { BackgroundImage } from '@patternfly/react-core';
 
 export const BackgroundImageBasic: React.FunctionComponent = () => (
-  <BackgroundImage src="/assets/images/pfbg-icon.svg" />
+  <BackgroundImage src="/assets/images/pf-background.svg" />
 );
