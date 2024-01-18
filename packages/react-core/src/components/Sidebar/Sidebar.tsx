34bf92e2417eb24f43588a7f58a059e67c864aad
Eric Olkowski <thatblindgeye@gmail.com>
2024-01-18 15:04:12 -0500

    Renamed enum

--- packages/react-core/src/components/Sidebar/Sidebar.tsx
+++ packages/react-core/src/components/Sidebar/Sidebar.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { css } from '@patternfly/react-styles';
 import styles from '@patternfly/react-styles/css/components/Sidebar/sidebar';
 
-export enum SidebarVariant {
+export enum SidebarBackgroundVariant {
   default = 'default',
   secondary = 'secondary'
 }
