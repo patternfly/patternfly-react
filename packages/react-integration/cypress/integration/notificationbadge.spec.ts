595d659f6b26bb7f3d99f2749df2d569320dbaa2
Austin Sullivan <ausulliv@redhat.com>
2024-01-18 14:09:39 -0500

    chore(NotificationBadge): update integration test

--- packages/react-integration/cypress/integration/notificationbadge.spec.ts
+++ packages/react-integration/cypress/integration/notificationbadge.spec.ts
@@ -4,15 +4,15 @@ describe('Notification Badge Demo Test', () => {
   });
 
   it('Verify notification badge is unread', () => {
-    cy.get('.pf-v5-c-notification-badge').first().should('have.class', 'pf-m-unread');
-    cy.get('.pf-v5-c-notification-badge').last().should('have.class', 'pf-m-attention');
-    cy.get('.pf-v5-c-notification-badge__count').contains('30');
+    cy.get('#notification-demo-badge1').should('have.class', 'pf-m-unread');
+    cy.get('#notification-demo-badge2').should('have.class', 'pf-m-attention');
+    cy.get('#notification-demo-badge2').contains('30');
   });
 
   it('Verify notification badge is read after click', () => {
     cy.get('#notification-demo-badge1').click();
-    cy.get('.pf-v5-c-notification-badge').first().should('have.class', 'pf-m-read');
+    cy.get('#notification-demo-badge1').should('have.class', 'pf-m-read');
     cy.get('#notification-demo-badge2').click();
-    cy.get('.pf-v5-c-notification-badge').last().should('have.class', 'pf-m-read');
+    cy.get('#notification-demo-badge2').should('have.class', 'pf-m-read');
   });
 });
