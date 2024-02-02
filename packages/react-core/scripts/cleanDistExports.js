2d2bfa662762769a5bd9eae37ef8d6c7505e0bd7
Austin Sullivan <ausulliv@redhat.com>
2024-02-02 15:41:33 -0500

    chore(build): update to remove demo exports from shipped TS files also

--- packages/react-core/scripts/cleanDistExports.js
+++ packages/react-core/scripts/cleanDistExports.js
@@ -2,8 +2,9 @@ const fs = require('fs');
 const path = require('path');
 
 const distIndexFiles = ['esm', 'js'].map((dir) => path.join(process.cwd(), 'dist', dir, 'index.js'));
+const distIndexDeclarationFiles = ['esm', 'js'].map((dir) => path.join(process.cwd(), 'dist', dir, 'index.d.ts'));
 
-distIndexFiles.forEach((file) => {
+[...distIndexFiles, ...distIndexDeclarationFiles].forEach((file) => {
   const fileContents = fs.readFileSync(file, 'utf8');
   const newFileContents = fileContents
     .split('\n')
