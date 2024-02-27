6af17a1b05e099bf3f601a5df9b216e81e7d22b3
Katie McFaul <kmcfaul@redhat.com>
2024-02-27 10:17:12 -0500

    feat(Login): use children for loginmainfooterlinksitem

--- packages/react-core/src/components/LoginPage/LoginMainFooterLinksItem.tsx
+++ packages/react-core/src/components/LoginPage/LoginMainFooterLinksItem.tsx
@@ -5,35 +5,17 @@ import { css } from '@patternfly/react-styles';
 export interface LoginMainFooterLinksItemProps extends React.HTMLProps<HTMLLIElement> {
   /** Content rendered inside the footer link item */
   children?: React.ReactNode;
-  /** HREF for footer link item */
-  href?: string;
-  /** Target for footer link item */
-  target?: string;
   /** Additional classes added to the footer link item  */
   className?: string;
-  /** Component used to render the footer link item */
-  linkComponent?: React.ReactNode;
-  /** Props for the LinkComponent */
-  linkComponentProps?: any;
 }
 
 export const LoginMainFooterLinksItem: React.FunctionComponent<LoginMainFooterLinksItemProps> = ({
   children = null,
-  href = '',
-  target,
   className = '',
-  linkComponent = 'a',
-  linkComponentProps,
   ...props
-}: LoginMainFooterLinksItemProps) => {
-  const LinkComponent = linkComponent as any;
-
-  return (
-    <li className={css(styles.loginMainFooterLinksItem, className)} {...props}>
-      <LinkComponent href={href} target={target} {...linkComponentProps}>
-        {children}
-      </LinkComponent>
-    </li>
-  );
-};
+}: LoginMainFooterLinksItemProps) => (
+  <li className={css(styles.loginMainFooterLinksItem, className)} {...props}>
+    {children}
+  </li>
+);
 LoginMainFooterLinksItem.displayName = 'LoginMainFooterLinksItem';
