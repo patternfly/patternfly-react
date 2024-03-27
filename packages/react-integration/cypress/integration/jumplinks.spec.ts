92da014955dfa278a6fd192a675067f66dd4d33e
Katie McFaul <kmcfaul@redhat.com>
2024-03-27 16:12:40 -0400

    fix integration

--- packages/react-integration/cypress/integration/jumplinks.spec.ts
+++ packages/react-integration/cypress/integration/jumplinks.spec.ts
@@ -4,13 +4,15 @@ describe('Jump Link Demo Test', () => {
   });
 
   it('Should jump on click', () => {
-    cy.get('#ts-demo-app-page-id').then(el => {
+    cy.get('#ts-demo-app-page-id').then((el) => {
       const firstScroll = el.scrollTop();
       expect(firstScroll).to.eq(0);
     });
-    cy.get('#heading-2').click();
-    cy.get('#ts-demo-app-page-id').then(el => {
-      expect(el.scrollTop()).to.not.eq(0);
-    });
+    cy.get('#heading-3').click();
+    cy.get('#ts-demo-app-page-id')
+      .parent()
+      .then((el) => {
+        expect(el.scrollTop()).to.not.eq(0);
+      });
   });
 });
