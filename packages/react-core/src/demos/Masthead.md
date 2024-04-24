a2a567e55e906aa2fe749f336b3cd3006ed41d29
ajaypratap003 <ajay.pratap233@gmail.com>
2024-04-24 16:58:11 +0530

    implement review comments

--- packages/react-core/src/demos/Masthead.md
+++ packages/react-core/src/demos/Masthead.md
@@ -3,28 +3,24 @@ id: Masthead
 section: components
 ---
 
-import BarsIcon from '@patternfly/react-icons/dist/esm/icons/bars-icon';
 import EllipsisVIcon from '@patternfly/react-icons/dist/esm/icons/ellipsis-v-icon';
 import BellIcon from '@patternfly/react-icons/dist/esm/icons/bell-icon';
 import CogIcon from '@patternfly/react-icons/dist/esm/icons/cog-icon';
 import HelpIcon from '@patternfly/react-icons/dist/esm/icons/help-icon';
-import ThIcon from '@patternfly/react-icons/dist/esm/icons/th-icon';
 import QuestionCircleIcon from '@patternfly/react-icons/dist/esm/icons/question-circle-icon';
 import imgAvatar from '@patternfly/react-core/src/components/assets/avatarImg.svg';
-import pfIcon from './assets/pf-logo-small.svg';
 import pfLogo from './assets/pf-logo.svg';
-import { Link } from '@reach/router';
 
 ## Demos
 
-### Masthead with utilities and user dropdown menu
+### Utilities and user dropdown menu
 
 Many mastheads contain utilities which collapse into a dropdown at smaller viewport widths. These utilities could include a notification badge to open and close a notification drawer, a link to app or user settings, a help icon which could launch quick starts or link to other resources, and many other utilities. In this demo, the notification badge does not collapse down into the mobile menu so that it is always visible even at narrower viewport widths.
 
 ```ts file='./examples/Masthead/MastheadWithUtilitiesAndUserDropdownMenu.tsx' isFullscreen
 
 ```
-### Masthead with horizontal nav
+### Horizontal nav
 ```ts file='./examples/Masthead/MastheadWithHorizontalNav.tsx' isFullscreen
 
 ```
--- packages/react-core/src/demos/examples/Masthead/MastheadWithHorizontalNav.tsx
+++ packages/react-core/src/demos/examples/Masthead/MastheadWithHorizontalNav.tsx
@@ -19,11 +19,6 @@ import {
   MastheadBrand,
   MastheadContent,
   MastheadMain,
-  MastheadToggle,
-  Menu,
-  MenuContent,
-  MenuList,
-  MenuItem,
   MenuToggle,
   MenuToggleElement,
   Nav,
@@ -32,30 +27,20 @@ import {
   Page,
   PageSection,
   PageSectionVariants,
-  PageToggleButton,
-  Popper,
   SkipToContent,
   Text,
   TextContent,
   Toolbar,
   ToolbarContent,
   ToolbarGroup,
-  ToolbarItem,
-  MenuGroup,
-  MenuSearchInput,
-  SearchInput,
-  Tooltip
+  ToolbarItem
 } from '@patternfly/react-core';
-import { Link } from '@reach/router';
-import BarsIcon from '@patternfly/react-icons/dist/esm/icons/bars-icon';
 import EllipsisVIcon from '@patternfly/react-icons/dist/esm/icons/ellipsis-v-icon';
 import BellIcon from '@patternfly/react-icons/dist/esm/icons/bell-icon';
 import CogIcon from '@patternfly/react-icons/dist/esm/icons/cog-icon';
 import HelpIcon from '@patternfly/react-icons/dist/esm/icons/help-icon';
-import ThIcon from '@patternfly/react-icons/dist/esm/icons/th-icon';
 import QuestionCircleIcon from '@patternfly/react-icons/dist/esm/icons/question-circle-icon';
 import imgAvatar from '@patternfly/react-core/src/components/assets/avatarImg.svg';
-import pfIcon from '@patternfly/react-core/src/demos/assets/pf-logo-small.svg';
 import pfLogo from '@patternfly/react-core/src/demos/assets/pf-logo.svg';
 
 interface NavOnSelectProps {
@@ -71,9 +56,6 @@ export const MastheadWithHorizontalNav: React.FunctionComponent = () => {
   const [activeItem, setActiveItem] = React.useState(0);
 
   const [isOpen, setIsOpen] = React.useState<boolean>(false);
-  const [refFullOptions, setRefFullOptions] = React.useState<Element[]>();
-  const [favorites, setFavorites] = React.useState<string[]>([]);
-  const [filteredIds, setFilteredIds] = React.useState<string[]>(['*']);
   const menuRef = React.useRef<HTMLDivElement>(null);
   const toggleRef = React.useRef<HTMLButtonElement>(null);
 
@@ -123,20 +105,6 @@ export const MastheadWithHorizontalNav: React.FunctionComponent = () => {
     }
   };
 
-  const onToggleClick = (ev: React.MouseEvent) => {
-    ev.stopPropagation(); // Stop handleClickOutside from handling
-    setTimeout(() => {
-      if (menuRef.current) {
-        const firstElement = menuRef.current.querySelector(
-          'li > button:not(:disabled), li > a:not(:disabled), input:not(:disabled)'
-        );
-        firstElement && (firstElement as HTMLElement).focus();
-        setRefFullOptions(Array.from(menuRef.current.querySelectorAll('li:not(li[role=separator])>*:first-child')));
-      }
-    }, 0);
-    setIsOpen(!isOpen);
-  };
-
   React.useEffect(() => {
     window.addEventListener('keydown', handleMenuKeys);
     window.addEventListener('click', handleClickOutside);
@@ -146,206 +114,6 @@ export const MastheadWithHorizontalNav: React.FunctionComponent = () => {
       window.removeEventListener('click', handleClickOutside);
     };
   }, [isOpen, menuRef]);
-
-  const toggle = (
-    <MenuToggle
-      aria-label="Toggle"
-      ref={toggleRef}
-      variant="plain"
-      onClick={onToggleClick}
-      isExpanded={isOpen}
-      style={{ width: 'auto' }}
-    >
-      <ThIcon />
-    </MenuToggle>
-  );
-
-  const menuItems = [
-    <MenuGroup key="group1" label="Group 1">
-      <MenuList>
-        <MenuItem itemId="0" id="0" isFavorited={favorites.includes('0')}>
-          Application 1
-        </MenuItem>
-        <MenuItem
-          itemId="1"
-          id="1"
-          isFavorited={favorites.includes('1')}
-          to="#default-link2"
-          onClick={(ev) => ev.preventDefault()}
-        >
-          Application 2
-        </MenuItem>
-      </MenuList>
-    </MenuGroup>,
-    <Divider key="group1-divider" />,
-    <MenuGroup key="group2" label="Group 2">
-      <MenuList>
-        <MenuItem
-          itemId="2"
-          id="2"
-          isFavorited={favorites.includes('2')}
-          component={(props) => <Link {...props} to="#router-link" />}
-        >
-          @reach/router Link
-        </MenuItem>
-        <MenuItem
-          itemId="3"
-          id="3"
-          isFavorited={favorites.includes('3')}
-          isExternalLink
-          icon={<img src={pfIcon} />}
-          component={(props) => <Link {...props} to="#router-link2" />}
-        >
-          @reach/router Link with icon
-        </MenuItem>
-      </MenuList>
-    </MenuGroup>,
-    <Divider key="group2-divider" />,
-    <MenuList key="other-items">
-      <MenuItem key="tooltip-app" isFavorited={favorites.includes('4')} itemId="4" id="4">
-        <Tooltip content={<div>Launch Application 3</div>} position="right">
-          <span>Application 3 with tooltip</span>
-        </Tooltip>
-      </MenuItem>
-      <MenuItem key="disabled-app" itemId="5" id="5" isDisabled>
-        Unavailable Application
-      </MenuItem>
-    </MenuList>
-  ];
-
-  const createFavorites = (favIds: string[]) => {
-    const favorites: unknown[] = [];
-
-    menuItems.forEach((item) => {
-      if (item.type === MenuList) {
-        item.props.children.filter((child) => {
-          if (favIds.includes(child.props.itemId)) {
-            favorites.push(child);
-          }
-        });
-      } else if (item.type === MenuGroup) {
-        item.props.children.props.children.filter((child) => {
-          if (favIds.includes(child.props.itemId)) {
-            favorites.push(child);
-          }
-        });
-      } else {
-        if (favIds.includes(item.props.itemId)) {
-          favorites.push(item);
-        }
-      }
-    });
-
-    return favorites;
-  };
-
-  const filterItems = (items: any[], filteredIds: string[]) => {
-    if (filteredIds.length === 1 && filteredIds[0] === '*') {
-      return items;
-    }
-    let keepDivider = false;
-    const filteredCopy = items
-      .map((group) => {
-        if (group.type === MenuGroup) {
-          const filteredGroup = React.cloneElement(group, {
-            children: React.cloneElement(group.props.children, {
-              children: group.props.children.props.children.filter((child) => {
-                if (filteredIds.includes(child.props.itemId)) {
-                  return child;
-                }
-              })
-            })
-          });
-          const filteredList = filteredGroup.props.children;
-          if (filteredList.props.children.length > 0) {
-            keepDivider = true;
-            return filteredGroup;
-          } else {
-            keepDivider = false;
-          }
-        } else if (group.type === MenuList) {
-          const filteredGroup = React.cloneElement(group, {
-            children: group.props.children.filter((child) => {
-              if (filteredIds.includes(child.props.itemId)) {
-                return child;
-              }
-            })
-          });
-          if (filteredGroup.props.children.length > 0) {
-            keepDivider = true;
-            return filteredGroup;
-          } else {
-            keepDivider = false;
-          }
-        } else {
-          if ((keepDivider && group.type === Divider) || filteredIds.includes(group.props.itemId)) {
-            return group;
-          }
-        }
-      })
-      .filter((newGroup) => newGroup);
-
-    if (filteredCopy.length > 0) {
-      const lastGroup = filteredCopy.pop();
-      if (lastGroup.type !== Divider) {
-        filteredCopy.push(lastGroup);
-      }
-    }
-
-    return filteredCopy;
-  };
-
-  const onTextChange = (textValue: string) => {
-    if (textValue === '') {
-      setFilteredIds(['*']);
-      return;
-    }
-
-    const filteredIds =
-      refFullOptions
-        ?.filter((item) => (item as HTMLElement).innerText.toLowerCase().includes(textValue.toString().toLowerCase()))
-        .map((item) => item.id) || [];
-    setFilteredIds(filteredIds);
-  };
-
-  const onFavorite = (event: any, itemId: string, actionId: string) => {
-    event.stopPropagation();
-    if (actionId === 'fav') {
-      const isFavorite = favorites.includes(itemId);
-      if (isFavorite) {
-        setFavorites(favorites.filter((fav) => fav !== itemId));
-      } else {
-        setFavorites([...favorites, itemId]);
-      }
-    }
-  };
-
-  const filteredFavorites = filterItems(createFavorites(favorites), filteredIds);
-  const filteredItems = filterItems(menuItems, filteredIds);
-  if (filteredItems.length === 0) {
-    filteredItems.push(<MenuItem key="no-items">No results found</MenuItem>);
-  }
-
-  const menu = (
-    // eslint-disable-next-line no-console
-    <Menu ref={menuRef} onActionClick={onFavorite} onSelect={(_ev, itemId) => console.log('selected', itemId)}>
-      <MenuSearchInput>
-        <SearchInput aria-label="Filter menu items" onChange={(_event, value) => onTextChange(value)} />
-      </MenuSearchInput>
-      <Divider />
-      <MenuContent>
-        {filteredFavorites.length > 0 && (
-          <React.Fragment>
-            <MenuGroup key="favorites-group" label="Favorites">
-              <MenuList>{filteredFavorites}</MenuList>
-            </MenuGroup>
-            <Divider key="favorites-divider" />
-          </React.Fragment>
-        )}
-        {filteredItems}
-      </MenuContent>
-    </Menu>
-  );
 
   const dashboardBreadcrumb = (
     <Breadcrumb>
@@ -388,9 +156,6 @@ export const MastheadWithHorizontalNav: React.FunctionComponent = () => {
             <Button aria-label="Notifications" variant={ButtonVariant.plain} icon={<BellIcon />} />
           </ToolbarItem>
           <ToolbarGroup variant="icon-button-group" visibility={{ default: 'hidden', lg: 'visible' }}>
-            <ToolbarItem visibility={{ default: 'hidden', md: 'hidden', lg: 'visible' }}>
-              <Popper trigger={toggle} triggerRef={toggleRef} popper={menu} popperRef={menuRef} isVisible={isOpen} />
-            </ToolbarItem>
             <ToolbarItem>
               <Button aria-label="Settings" variant={ButtonVariant.plain} icon={<CogIcon />} />
             </ToolbarItem>
@@ -500,11 +265,6 @@ export const MastheadWithHorizontalNav: React.FunctionComponent = () => {
 
   const masthead = (
     <Masthead>
-      <MastheadToggle>
-        <PageToggleButton variant="plain" aria-label="Global navigation">
-          <BarsIcon />
-        </PageToggleButton>
-      </MastheadToggle>
       <MastheadMain>
         <MastheadBrand>
           <Brand src={pfLogo} alt="PatternFly" heights={{ default: '36px' }} />
