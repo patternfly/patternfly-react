ac359ecad2e8d0dfe7d9158df7c37a09fe02895f
Sarah Rambacher <srambach@redhat.com>
2024-04-29 16:29:25 -0400

    feat(toolbar): add tests

--- packages/react-core/src/components/Toolbar/__tests__/Toolbar.test.tsx
+++ packages/react-core/src/components/Toolbar/__tests__/Toolbar.test.tsx
@@ -9,6 +9,7 @@ import { ToolbarContent } from '../ToolbarContent';
 import { ToolbarFilter } from '../ToolbarFilter';
 import { ToolbarGroup } from '../ToolbarGroup';
 import { Button } from '../../Button/Button';
+import styles from '@patternfly/react-styles/css/components/Toolbar/toolbar';
 
 describe('Toolbar', () => {
   it('should render inset', () => {
@@ -104,5 +105,62 @@ describe('Toolbar', () => {
     expect(asFragment()).toMatchSnapshot();
     expect(screen.getAllByRole('button', { name: 'Save filters' }).length).toBe(1);
     expect(screen.getAllByRole('button', { name: 'Clear all filters' }).length).toBe(1);
+  });
+
+  it('Renders with class ${styles.modifiers.noBackground} when colorVariant="no-background"', () => {
+    const items = (
+      <React.Fragment>
+        <ToolbarItem>Test</ToolbarItem>
+        <ToolbarItem>Test 2</ToolbarItem>
+        <ToolbarItem variant="separator" />
+        <ToolbarItem>Test 3 </ToolbarItem>
+      </React.Fragment>
+    );
+
+    render(
+      <Toolbar id="toolbar" colorVariant="no-background" data-testid="Toolbar-test-no-background-id">
+        <ToolbarContent>{items}</ToolbarContent>
+      </Toolbar>
+    );
+
+    expect(screen.getByTestId('Toolbar-test-no-background-id')).toHaveClass(styles.modifiers.noBackground);
+  });
+
+  it('Renders with class ${styles.modifiers.primary} when colorVariant="primary"', () => {
+    const items = (
+      <React.Fragment>
+        <ToolbarItem>Test</ToolbarItem>
+        <ToolbarItem>Test 2</ToolbarItem>
+        <ToolbarItem variant="separator" />
+        <ToolbarItem>Test 3 </ToolbarItem>
+      </React.Fragment>
+    );
+
+    render(
+      <Toolbar id="toolbar" colorVariant="primary" data-testid="Toolbar-test-primary-id">
+        <ToolbarContent>{items}</ToolbarContent>
+      </Toolbar>
+    );
+
+    expect(screen.getByTestId('Toolbar-test-primary-id')).toHaveClass(styles.modifiers.primary);
+  });
+
+  it('Renders with class ${styles.modifiers.secondary} when colorVariant="secondary"', () => {
+    const items = (
+      <React.Fragment>
+        <ToolbarItem>Test</ToolbarItem>
+        <ToolbarItem>Test 2</ToolbarItem>
+        <ToolbarItem variant="separator" />
+        <ToolbarItem>Test 3 </ToolbarItem>
+      </React.Fragment>
+    );
+
+    render(
+      <Toolbar id="toolbar" colorVariant="secondary" data-testid="Toolbar-test-secondary-id">
+        <ToolbarContent>{items}</ToolbarContent>
+      </Toolbar>
+    );
+
+    expect(screen.getByTestId('Toolbar-test-secondary-id')).toHaveClass(styles.modifiers.secondary);
   });
 });
