727933b230850e7b4033d472055df5adc8f0a722
adamviktora <viktora.adam@gmail.com>
2024-04-30 18:07:19 +0200

    fix(Select): set isScrollable internally

--- packages/react-core/src/components/Select/Select.tsx
+++ packages/react-core/src/components/Select/Select.tsx
@@ -160,7 +160,7 @@ const SelectBase: React.FunctionComponent<SelectProps & OUIAProps> = ({
       }}
       isPlain={isPlain}
       selected={selected}
-      isScrollable={isScrollable}
+      isScrollable={isScrollable ?? (menuHeight !== undefined || maxMenuHeight !== undefined)}
       {...getOUIAProps(
         Select.displayName,
         props.ouiaId !== undefined ? props.ouiaId : getDefaultOUIAId(Select.displayName),
