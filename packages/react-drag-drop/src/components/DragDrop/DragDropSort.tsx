c85ec9cd27f9fa3a80bbd0e8f734d071b0a57aec
kmcfaul <45077788+kmcfaul@users.noreply.github.com>
2024-05-22 15:53:41 -0400

    Update packages/react-drag-drop/src/components/DragDrop/DragDropSort.tsx
    Co-authored-by: Eric Olkowski <70952936+thatblindgeye@users.noreply.github.com>

--- packages/react-drag-drop/src/components/DragDrop/DragDropSort.tsx
+++ packages/react-drag-drop/src/components/DragDrop/DragDropSort.tsx
@@ -49,7 +49,7 @@ export interface DragDropSortProps extends DndContextProps {
   /** Callback when use begins dragging a draggable object */
   onDrag?: (event: DragDropSortDragStartEvent, oldIndex: number) => void;
   /** The variant determines which component wraps the draggable object.
-   * Default varaint wraps the draggable object in a div.
+   * Default variant wraps the draggable object in a div.
    * DataList vairant wraps the draggable object in a DataListItem
    * DualListSelectorList variant wraps the draggable objects in a DualListSelectorListItem and a div.pf-c-dual-list-selector__item-text element
    * TableComposable variant wraps the draggable objects in TODO
