26c101461b6d0eb237e9e4cf1371d052b87071e0
adamviktora <viktora.adam@gmail.com>
2024-06-13 15:27:41 +0200

    feat(Text): update API to support <hr> and prepare for Content rename

--- packages/react-core/src/components/Text/Text.tsx
+++ packages/react-core/src/components/Text/Text.tsx
@@ -14,25 +14,53 @@ export enum TextVariants {
   a = 'a',
   small = 'small',
   blockquote = 'blockquote',
-  pre = 'pre'
+  pre = 'pre',
+  hr = 'hr',
+  ul = 'ul',
+  ol = 'ol',
+  dl = 'dl',
+  li = 'li',
+  dt = 'dt',
+  dd = 'dd'
 }
 
 export interface TextProps extends React.HTMLProps<HTMLElement>, OUIAProps {
-  /** The text component */
-  component?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'a' | 'small' | 'blockquote' | 'pre';
-  /** Content rendered within the Text */
+  /** The content component. If none provided, it will be a 'div' and styling will be applied to all its child components. */
+  component?:
+    | 'h1'
+    | 'h2'
+    | 'h3'
+    | 'h4'
+    | 'h5'
+    | 'h6'
+    | 'p'
+    | 'a'
+    | 'small'
+    | 'blockquote'
+    | 'pre'
+    | 'hr'
+    | 'ul'
+    | 'ol'
+    | 'dl'
+    | 'li'
+    | 'dt'
+    | 'dd';
+  /** Children rendered within the Content. */
   children?: React.ReactNode;
-  /** Additional classes added to the Text */
+  /** Additional classes added to the Content. */
   className?: string;
-  /** Flag to indicate the link has visited styles applied if the browser determines the link has been visited */
+  /** Modifies the list (ul, ol and dl components) to have plain styling. */
+  isPlainList?: boolean;
+  /** Flag to indicate the link (or all links within the content) has visited styles applied if the browser determines the link has been visited. */
   isVisitedLink?: boolean;
-  /** Value to overwrite the randomly generated data-ouia-component-id.*/
+  /** Value to overwrite the randomly generated data-ouia-component-id. */
   ouiaId?: number | string;
   /** Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false. */
   ouiaSafe?: boolean;
 }
 
 const componentStyles = {
+  div: styles.content,
   h1: styles.contentH1,
   h2: styles.contentH2,
   h3: styles.contentH3,
@@ -43,20 +71,31 @@ const componentStyles = {
   a: styles.contentA,
   small: styles.contentSmall,
   blockquote: styles.contentBlockquote,
-  pre: styles.contentPre
+  pre: styles.contentPre,
+  hr: styles.contentHr,
+  ul: styles.contentUl,
+  ol: styles.contentOl,
+  dl: styles.contentDl,
+  li: styles.contentLi,
+  dt: styles.contentDt,
+  dd: styles.contentDd
 };
 
 export const Text: React.FunctionComponent<TextProps> = ({
-  children = null,
+  children,
   className = '',
-  component = TextVariants.p,
+  component,
+  isPlainList = false,
   isVisitedLink = false,
   ouiaId,
   ouiaSafe = true,
   ...props
 }: TextProps) => {
-  const Component: any = component;
+  const wrappingComponent = component ?? 'div';
+  const Component: any = wrappingComponent;
   const ouiaProps = useOUIAProps(Text.displayName, ouiaId, ouiaSafe);
+
+  const isList = ['ul', 'ol', 'dl'].includes(wrappingComponent);
 
   return (
     <Component
@@ -64,8 +103,9 @@ export const Text: React.FunctionComponent<TextProps> = ({
       {...props}
       data-pf-content
       className={css(
-        isVisitedLink && component === TextVariants.a && styles.modifiers.visited,
-        componentStyles[component],
+        componentStyles[wrappingComponent],
+        isList && isPlainList && styles.modifiers.plain,
+        isVisitedLink && styles.modifiers.visited,
         className
       )}
     >
