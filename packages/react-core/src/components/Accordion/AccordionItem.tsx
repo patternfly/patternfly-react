a2014d4ec895739306753232e2625743a09eb91c
nicolethoen <nthoen@redhat.com>
2024-06-28 16:59:32 -0400

    fix: accordion sub components spread props

--- packages/react-core/src/components/Accordion/AccordionItem.tsx
+++ packages/react-core/src/components/Accordion/AccordionItem.tsx
@@ -3,7 +3,7 @@ import { css } from '@patternfly/react-styles';
 import styles from '@patternfly/react-styles/css/components/Accordion/accordion';
 import { AccordionItemContext } from './AccordionContext';
 
-export interface AccordionItemProps {
+export interface AccordionItemProps extends React.HTMLProps<HTMLDivElement> {
   /** Content rendered inside the accordion item. */
   children?: React.ReactNode;
   /** Additional classes added to the accordion item. */
@@ -15,14 +15,17 @@ export interface AccordionItemProps {
 export const AccordionItem: React.FunctionComponent<AccordionItemProps> = ({
   children = null,
   className,
-  isExpanded: isExpandedProp = false
+  isExpanded: isExpandedProp = false,
+  ...props
 }: AccordionItemProps) => (
   <AccordionItemContext.Provider
     value={{
       isExpanded: isExpandedProp
     }}
   >
-    <div className={css(styles.accordionItem, isExpandedProp && styles.modifiers.expanded, className)}>{children}</div>
+    <div className={css(styles.accordionItem, isExpandedProp && styles.modifiers.expanded, className)} {...props}>
+      {children}
+    </div>
   </AccordionItemContext.Provider>
 );
 AccordionItem.displayName = 'AccordionItem';
