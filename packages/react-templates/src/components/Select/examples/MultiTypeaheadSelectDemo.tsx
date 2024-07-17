943e19dc049ee9a0f047c89b0e456cdc69c56547
Jeff Phillips <jephilli@redhat.com>
2024-07-17 16:38:58 -0400

    feat(MultiTypeaheadSelect): Add MultiTypeaheadSelect to react-templates (#10757)
    * feat(MultiTypeaheadSelect): Add MultiTypeaheadSelect to react-templates
    
    * Update packages/react-templates/src/components/Select/MultiTypeaheadSelect.tsx
    
    Co-authored-by: kmcfaul <45077788+kmcfaul@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Titani Labaj <39532947+tlabaj@users.noreply.github.com>
    Co-authored-by: kmcfaul <45077788+kmcfaul@users.noreply.github.com>

---
+++ packages/react-templates/src/components/Select/examples/MultiTypeaheadSelectDemo.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { MultiTypeaheadSelect, MultiTypeaheadSelectOption } from '@patternfly/react-templates';
+
+const Options = [
+  { content: 'Alabama', value: 'option1' },
+  { content: 'Arizona', value: 'option2' },
+  { content: 'California', value: 'option3' },
+  { content: 'Florida', value: 'option4' },
+  { content: 'Massachusetts', value: 'option5' },
+  { content: 'Maine', value: 'option6' },
+  { content: 'New Jersey', value: 'option7' },
+  { content: 'New Mexico', value: 'option8' },
+  { content: 'New York', value: 'option9' },
+  { content: 'North Carolina', value: 'option10' }
+];
+
+type SelectionsType = (string | number)[];
+
+export const MultiSelectTypeaheadDemo: React.FunctionComponent = () => {
+  const [selected, setSelected] = React.useState<SelectionsType>(['option5']);
+
+  const initialOptions = React.useMemo<MultiTypeaheadSelectOption[]>(
+    () => Options.map((o) => ({ ...o, selected: selected.includes(o.value) })),
+    [selected]
+  );
+
+  return (
+    <MultiTypeaheadSelect
+      initialOptions={initialOptions}
+      placeholder="Select a state"
+      noOptionsFoundMessage={(filter) => `No state was found for "${filter}"`}
+      onSelectionChange={(_ev, selections) => setSelected(selections)}
+    />
+  );
+};
