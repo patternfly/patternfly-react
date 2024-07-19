d51645c7328ed916e7c4e836acf0576453cef154
Titani <tlabaj@redhat.com>
2024-07-19 10:56:52 -0400

    update example

--- packages/react-core/src/components/Content/examples/ContentOrderedList.tsx
+++ packages/react-core/src/components/Content/examples/ContentOrderedList.tsx
@@ -3,12 +3,12 @@ import { Content, ContentVariants } from '@patternfly/react-core';
 
 export const ContentOrderedList: React.FunctionComponent = () => (
   <Content component={ContentVariants.ol}>
-    <Content>Donec blandit a lorem id convallis.</Content>
-    <Content>Cras gravida arcu at diam gravida gravida.</Content>
-    <Content>Integer in volutpat libero.</Content>
-    <Content>Donec a diam tellus.</Content>
-    <Content>Aenean nec tortor orci.</Content>
-    <Content>Quisque aliquam cursus urna, non bibendum massa viverra eget.</Content>
-    <Content>Vivamus maximus ultricies pulvinar.</Content>
+    <Content component={ContentVariants.li}>Donec blandit a lorem id convallis.</Content>
+    <Content component={ContentVariants.li}>Cras gravida arcu at diam gravida gravida.</Content>
+    <Content component={ContentVariants.li}>Integer in volutpat libero.</Content>
+    <Content component={ContentVariants.li}>Donec a diam tellus.</Content>
+    <Content component={ContentVariants.li}>Aenean nec tortor orci.</Content>
+    <Content component={ContentVariants.li}>Quisque aliquam cursus urna, non bibendum massa viverra eget.</Content>
+    <Content component={ContentVariants.li}>Vivamus maximus ultricies pulvinar.</Content>
   </Content>
 );
