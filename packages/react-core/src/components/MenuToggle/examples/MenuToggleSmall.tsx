af051d5e07fb0d4b24b92dff16c333d54d9224b0
Eric Olkowski <thatblindgeye@gmail.com>
2024-07-29 09:31:06 -0400

    feat(MenuToggle): added size prop for small modifier class

---
+++ packages/react-core/src/components/MenuToggle/examples/MenuToggleSmall.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { MenuToggle } from '@patternfly/react-core';
+
+export const MenuToggleStatus: React.FunctionComponent = () => <MenuToggle size="sm">Small toggle</MenuToggle>;
