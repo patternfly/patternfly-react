45577b728d6c7b053f056f08506258b298806200
Eric Olkowski <thatblindgeye@gmail.com>
2024-07-29 14:10:30 -0400

    Added example

---
+++ packages/react-core/src/components/TextInputGroup/examples/TextInputGroupValidation.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import {
+  TextInputGroup,
+  TextInputGroupMain,
+  TextInputGroupUtilities,
+  Button,
+  ValidatedOptions,
+  Flex,
+  FlexItem
+} from '@patternfly/react-core';
+import SearchIcon from '@patternfly/react-icons/dist/esm/icons/search-icon';
+import TimesIcon from '@patternfly/react-icons/dist/esm/icons/times-icon';
+
+export const TextInputGroupValidation: React.FunctionComponent = () => {
+  const [successValue, setSuccessValue] = React.useState('Success validation');
+  const [warningValue, setWarningValue] = React.useState('Warning validation with icon at start');
+  const [errorValue, setErrorValue] = React.useState('Error validation with icon at start and utilities');
+
+  /** show the input clearing button only when the input is not empty */
+  const showClearButton = !!errorValue;
+
+  /** render the utilities component only when a component it contains is being rendered */
+  const showUtilities = showClearButton;
+
+  /** callback for clearing the text input */
+  const clearInput = () => {
+    setErrorValue('');
+  };
+
+  return (
+    <Flex direction={{ default: 'column' }} rowGap={{ default: 'rowGapSm' }}>
+      <FlexItem>
+        <TextInputGroup status={ValidatedOptions.success}>
+          <TextInputGroupMain value={successValue} onChange={(_event, value) => setSuccessValue(value)} />
+        </TextInputGroup>
+      </FlexItem>
+      <FlexItem>
+        <TextInputGroup status={ValidatedOptions.warning}>
+          <TextInputGroupMain
+            icon={<SearchIcon />}
+            value={warningValue}
+            onChange={(_event, value) => setWarningValue(value)}
+          />
+        </TextInputGroup>
+      </FlexItem>
+      <FlexItem>
+        <TextInputGroup status={ValidatedOptions.error}>
+          <TextInputGroupMain
+            icon={<SearchIcon />}
+            value={errorValue}
+            onChange={(_event, value) => setErrorValue(value)}
+          />
+          {showUtilities && (
+            <TextInputGroupUtilities>
+              {showClearButton && (
+                <Button variant="plain" onClick={clearInput} aria-label="Clear button and input" icon={<TimesIcon />} />
+              )}
+            </TextInputGroupUtilities>
+          )}
+        </TextInputGroup>
+      </FlexItem>
+    </Flex>
+  );
+};
