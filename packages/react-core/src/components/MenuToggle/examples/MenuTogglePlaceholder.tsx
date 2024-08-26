cdd389fec181e91b228edab0b7f8759345944653
Katie McFaul <kmcfaul@redhat.com>
2024-08-26 15:06:24 -0400

    feat(MenuToggle): add placeholder support

---
+++ packages/react-core/src/components/MenuToggle/examples/MenuTogglePlaceholder.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { MenuToggle } from '@patternfly/react-core';
+
+export const MenuTogglePlaceholder: React.FunctionComponent = () => (
+  <MenuToggle isPlaceholder>Placeholder text</MenuToggle>
+);
