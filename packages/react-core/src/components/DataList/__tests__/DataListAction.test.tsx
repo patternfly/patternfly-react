8a10376dc10c71920258d297155e93d80030475d
Titani <tlabaj@redhat.com>
2024-09-06 15:44:03 -0400

    updated snapshot tests

--- packages/react-core/src/components/DataList/__tests__/DataListAction.test.tsx
+++ packages/react-core/src/components/DataList/__tests__/DataListAction.test.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
 import { DataListAction } from '../DataListAction';
 
 import styles from '@patternfly/react-styles/css/components/DataList/data-list';
@@ -15,38 +17,35 @@ test('Renders to match snapshot', () => {
 
 test(`Renders with default class ${styles.dataListItemAction}`, () => {
   render(
-    <DataListAction aria-label="Actions" aria-labelledby="ex-action" id="ex-action">
+    <DataListAction aria-label="Actions" data-testid="actions" aria-labelledby="ex-action" id="ex-action">
       test
     </DataListAction>
   );
-  expect(screen.getByText('test')).toHaveClass(styles.dataListItemAction, { exact: true });
+  expect(screen.getByTestId('actions')).toHaveClass(styles.dataListItemAction, { exact: true });
 });
 
 test(`Renders with custom class when className is passed`, () => {
   render(
-    <DataListAction aria-label="Actions" aria-labelledby="ex-action" id="ex-action" className="test-class">
+    <DataListAction
+      aria-label="Actions"
+      data-testid="actions"
+      aria-labelledby="ex-action"
+      id="ex-action"
+      className="test-class"
+    >
       test
     </DataListAction>
   );
-  expect(screen.getByText('test')).toHaveClass('test-class');
+  expect(screen.getByTestId('actions')).toHaveClass('test-class');
 });
 
 test(`Renders with spread props`, () => {
   render(
-    <DataListAction dir="rtl" aria-label="Actions" aria-labelledby="ex-action" id="ex-action">
+    <DataListAction dir="rtl" data-testid="actions" aria-label="Actions" aria-labelledby="ex-action" id="ex-action">
       test
     </DataListAction>
   );
-  expect(screen.getByText('test')).toHaveAttribute('dir', 'rtl');
-});
-
-test(`Renders with class ${styles.dataListAction} when isPlainButtonAction = true`, () => {
-  render(
-    <DataListAction id="id" aria-label="Actions" aria-labelledby="ex-action" isPlainButtonAction>
-      test
-    </DataListAction>
-  );
-  expect(screen.getByText('test')).toHaveClass(styles.dataListAction);
+  expect(screen.getByTestId('actions')).toHaveAttribute('dir', 'rtl');
 });
 
 ['hidden', 'visible'].forEach((vis) => {
@@ -58,18 +57,19 @@ test(`Renders with class ${styles.dataListAction} when isPlainButtonAction = tru
         aria-labelledby="check-action-item2 check-action-action2"
         id="check-action-action2"
         aria-label="Actions"
+        data-testid="actions"
       >
         test
       </DataListAction>
     );
 
     if (visMod === 'hidden') {
-      expect(screen.getByText('test')).toHaveClass(styles.modifiers[`${visMod}`]);
+      expect(screen.getByTestId('actions')).toHaveClass(styles.modifiers[`${visMod}`]);
     }
-    expect(screen.getByText('test')).toHaveClass(styles.modifiers[`${visMod}OnSm`]);
-    expect(screen.getByText('test')).toHaveClass(styles.modifiers[`${visMod}OnMd`]);
-    expect(screen.getByText('test')).toHaveClass(styles.modifiers[`${visMod}OnLg`]);
-    expect(screen.getByText('test')).toHaveClass(styles.modifiers[`${visMod}OnXl`]);
-    expect(screen.getByText('test')).toHaveClass(styles.modifiers[`${visMod}On_2xl`]);
+    expect(screen.getByTestId('actions')).toHaveClass(styles.modifiers[`${visMod}OnSm`]);
+    expect(screen.getByTestId('actions')).toHaveClass(styles.modifiers[`${visMod}OnMd`]);
+    expect(screen.getByTestId('actions')).toHaveClass(styles.modifiers[`${visMod}OnLg`]);
+    expect(screen.getByTestId('actions')).toHaveClass(styles.modifiers[`${visMod}OnXl`]);
+    expect(screen.getByTestId('actions')).toHaveClass(styles.modifiers[`${visMod}On_2xl`]);
   });
 });
