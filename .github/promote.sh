2ef64e03e9663d3b07f795771466fbfc549ff794
Donald Labaj <dlabaj@gmail.com>
2024-09-11 14:50:21 -0400

    Testing dryrun for path release.

--- .github/promote.sh
+++ .github/promote.sh
@@ -12,17 +12,21 @@ echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
 # Update their versions and changelogs according to angular commit guidelines
 # https://github.com/angular/angular/blob/master/CONTRIBUTING.md#commit
 
-if [[ ! -z "${PATTERNFLY_VERSION}" ]]; then
-  echo "Updating to @patternfly/patternfly: ${PATTERNFLY_VERSION}"
-  npm pkg set dependencies.@patternfly/patternfly=${PATTERNFLY_VERSION} --workspace @patternfly/react-docs
-  npm pkg set devDependencies.@patternfly/patternfly=${PATTERNFLY_VERSION} --workspace @patternfly/react-core --workspace @patternfly/react-styles --workspace @patternfly/react-tokens --workspace @patternfly/react-icons 
-fi
+# if [[ ! -z "${PATTERNFLY_VERSION}" ]]; then
+#   echo "Updating to @patternfly/patternfly: ${PATTERNFLY_VERSION}"
+#   npm pkg set dependencies.@patternfly/patternfly=${PATTERNFLY_VERSION} --workspace @patternfly/react-docs
+#   npm pkg set devDependencies.@patternfly/patternfly=${PATTERNFLY_VERSION} --workspace @patternfly/react-core --workspace @patternfly/react-styles --workspace @patternfly/react-tokens --workspace @patternfly/react-icons 
+# fi
 
 # publish to npm
-yarn run lerna publish --conventional-commits --conventional-graduate --no-private --dist-tag=latest --yes
+# yarn run lerna publish --conventional-commits --conventional-graduate --no-private --dist-tag=latest --yes
 
-# immediately after promote - set up repo for next prerelease
-# yarn run lerna version preminor --force-publish --conventional-commits --no-private --yes --preid prerelease
+# Promote - set up repo for alpha
+# yarn run lerna version preminor --force-publish --conventional-commits --no-private --yes --preid alpha
+
+# Promote - To set up repo for a alpha patch release
+# yarn run lerna version prepatch --force-publish --conventional-commits --no-private --yes --preid alpha
+yarn run lerna version prepatch --conventional-commits --no-private --yes --no-push --preid alpha
 
 # dry run
 # yarn run lerna version --conventional-commits --conventional-graduate --no-private --yes --no-git-tag-version --no-push
