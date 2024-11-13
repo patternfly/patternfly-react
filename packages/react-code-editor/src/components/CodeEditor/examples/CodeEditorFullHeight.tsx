6ddfc93376a48028fed46948b08a185e3c9617f6
Rebecca Alpert <ralpert@redhat.com>
2024-11-13 13:05:12 -0500

    fix(CodeEditor): Set default height (#11014)
    The docs state that the default height is 100%. However, the examples just break if you don't pass in a height. This should help.

---
+++ packages/react-code-editor/src/components/CodeEditor/examples/CodeEditorFullHeight.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { CodeEditor, Language } from '@patternfly/react-code-editor';
+
+export const CodeEditorFullHeight: React.FunctionComponent = () => {
+  const onEditorDidMount = (editor, monaco) => {
+    editor.layout();
+    editor.focus();
+    monaco.editor.getModels()[0].updateOptions({ tabSize: 5 });
+  };
+
+  const onChange = (value) => {
+    // eslint-disable-next-line no-console
+    console.log(value);
+  };
+
+  return (
+    <div style={{ height: '400px' }}>
+      <CodeEditor
+        code="Some example content"
+        onChange={onChange}
+        language={Language.javascript}
+        onEditorDidMount={onEditorDidMount}
+        isFullHeight
+      />
+    </div>
+  );
+};
