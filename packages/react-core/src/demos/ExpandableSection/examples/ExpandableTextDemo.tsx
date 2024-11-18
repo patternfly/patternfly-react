c82602c81b4cbe320386477d05b84dc2b11a8f98
Steve Goodwin <sgoodwin@redhat.com>
2024-11-18 14:18:47 -0500

    fix(expandable section): Remove returns within text content so that example truncates correctly

--- packages/react-core/src/demos/ExpandableSection/examples/ExpandableTextDemo.tsx
+++ packages/react-core/src/demos/ExpandableSection/examples/ExpandableTextDemo.tsx
@@ -8,13 +8,7 @@ export const ExpandableTextDemo: React.FunctionComponent = () => {
     setIsExpanded(isExpanded);
   };
 
-  const content = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec dignissim turpis, et tristique purus.
-  Phasellus efficitur ante quis dolor viverra imperdiet. Orci varius natoque penatibus et magnis dis parturient
-  montes, nascetur ridiculus mus. Pellentesque laoreet, sem ac elementum semper, lectus mauris vestibulum nulla,
-  eget volutpat massa neque vel turpis. Donec finibus enim eu leo accumsan consectetur. Praesent massa diam,
-  tincidunt eu dui ac, ullamcorper elementum est. Phasellus metus felis, venenatis vitae semper nec, porta a metus.
-  Vestibulum justo nisi, imperdiet id eleifend at, varius nec lorem. Fusce porttitor mollis nibh, ut elementum ante
-  commodo tincidunt. Integer tincidunt at ipsum non aliquet.`;
+  const content = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec dignissim turpis, et tristique purus. Phasellus efficitur ante quis dolor viverra imperdiet. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque laoreet, sem ac elementum semper, lectus mauris vestibulum nulla, eget volutpat massa neque vel turpis. Donec finibus enim eu leo accumsan consectetur. Praesent massa diam, tincidunt eu dui ac, ullamcorper elementum est. Phasellus metus felis, venenatis vitae semper nec, porta a metus. Vestibulum justo nisi, imperdiet id eleifend at, varius nec lorem. Fusce porttitor mollis nibh, ut elementum ante commodo tincidunt. Integer tincidunt at ipsum non aliquet.`;
 
   return (
     <ExpandableSection
