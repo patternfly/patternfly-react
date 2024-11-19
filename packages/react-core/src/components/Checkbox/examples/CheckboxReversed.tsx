47cb1de593ab6dc39e207786e817eb97805427df
adamviktora <84135613+adamviktora@users.noreply.github.com>
2024-11-19 19:32:30 +0100

    fix(CheckboxReversed example): fix prop (#11175)

--- packages/react-core/src/components/Checkbox/examples/CheckboxReversed.tsx
+++ packages/react-core/src/components/Checkbox/examples/CheckboxReversed.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { Checkbox } from '@patternfly/react-core';
 
 export const CheckboxReversed: React.FunctionComponent = () => (
-  <Checkbox isLabelBeforeButton label="Reversed checkbox example" id="checkbox-reversed" name="checkbox-reversed" />
+  <Checkbox labelPosition="start" label="Reversed checkbox example" id="checkbox-reversed" name="checkbox-reversed" />
 );
