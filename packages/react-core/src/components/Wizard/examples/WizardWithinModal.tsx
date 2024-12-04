099dd9d6050e61d6c6b79ff6343851a2c491c314
mfrances <mfrances@redhat.com>
2024-12-04 09:55:09 -0500

    rm modalbody from example and revise modal doc

--- packages/react-core/src/components/Wizard/examples/WizardWithinModal.tsx
+++ packages/react-core/src/components/Wizard/examples/WizardWithinModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Modal, ModalBody, ModalVariant, Wizard, WizardHeader, WizardStep } from '@patternfly/react-core';
+import { Button, Modal, ModalVariant, Wizard, WizardHeader, WizardStep } from '@patternfly/react-core';
 
 export const WizardWithinModal: React.FunctionComponent = () => {
   const [isModelOpen, setIsModalOpen] = React.useState(false);
@@ -14,34 +14,32 @@ export const WizardWithinModal: React.FunctionComponent = () => {
         onEscapePress={() => setIsModalOpen(false)}
         variant={ModalVariant.medium}
       >
-        <ModalBody>
-          <Wizard
-            height={400}
-            onClose={() => setIsModalOpen(false)}
-            title="In modal wizard"
-            header={
-              <WizardHeader
-                onClose={() => setIsModalOpen(false)}
-                title="Wizard in modal"
-                description="Simple wizard description"
-              />
-            }
+        <Wizard
+          height={400}
+          onClose={() => setIsModalOpen(false)}
+          title="In modal wizard"
+          header={
+            <WizardHeader
+              onClose={() => setIsModalOpen(false)}
+              title="Wizard in modal"
+              description="Simple wizard description"
+            />
+          }
+        >
+          <WizardStep name="Step 1" id="in-modal-step-1">
+            Step 1 content
+          </WizardStep>
+          <WizardStep name="Step 2" id="in-modal-step-2">
+            Step 2 content
+          </WizardStep>
+          <WizardStep
+            name="Review"
+            id="in-modal-review-step"
+            footer={{ nextButtonText: 'Finish', onNext: () => setIsModalOpen(false) }}
           >
-            <WizardStep name="Step 1" id="in-modal-step-1">
-              Step 1 content
-            </WizardStep>
-            <WizardStep name="Step 2" id="in-modal-step-2">
-              Step 2 content
-            </WizardStep>
-            <WizardStep
-              name="Review"
-              id="in-modal-review-step"
-              footer={{ nextButtonText: 'Finish', onNext: () => setIsModalOpen(false) }}
-            >
-              Review step content
-            </WizardStep>
-          </Wizard>
-        </ModalBody>
+            Review step content
+          </WizardStep>
+        </Wizard>
       </Modal>
     </>
   );
