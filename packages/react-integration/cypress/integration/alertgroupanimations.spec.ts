84056cf0425b8747b690cbd4daef1ab558494ffd
Eric Olkowski <thatblindgeye@gmail.com>
2025-02-17 08:23:19 -0500

    Updated animation test

--- packages/react-integration/cypress/integration/alertgroupanimations.spec.ts
+++ packages/react-integration/cypress/integration/alertgroupanimations.spec.ts
@@ -3,41 +3,14 @@ describe('Alert Group Animations Demo Test', () => {
     cy.visit('http://localhost:3000/alert-group-animations-demo-nav-link');
   });
 
-  it('Renders alert group item with animation', () => {
+  it('Renders alert group item with animation classes', () => {
     cy.clock();
     cy.get('#add-alert-button').click();
-    cy.get('.pf-v6-c-alert-group__item').then(($groupItem) => {
-      const currentItem = $groupItem.get(0);
-      const height = currentItem.offsetHeight;
-      expect(height).to.equal(0);
-    });
+    cy.get('.pf-v6-c-alert-group__item').should('have.class', 'pf-m-offstage-top');
     cy.tick(1000);
-    cy.get('.pf-v6-c-alert-group__item').then(($groupItem) => {
-      const currentItem = $groupItem.get(0);
-      const height = currentItem.offsetHeight;
-      expect(height).to.be.greaterThan(0);
-    });
-    cy.clock().invoke('restore');
-    cy.get('#alert-close-button').click({ waitForAnimations: true });
-    cy.get('.pf-v6-c-alert').should('not.exist');
-  });
-
-  it('Removes alert group item with animation', () => {
-    let left: number;
-    cy.clock();
-    cy.get('#add-alert-button').click();
-    cy.tick(1000);
-    cy.get('.pf-v6-c-alert-group__item').then(($groupItem) => {
-      const currentItem = $groupItem.get(0);
-      left = currentItem.getBoundingClientRect().left;
-    });
+    cy.get('.pf-v6-c-alert-group__item').should('not.have.class', 'pf-m-offstage-top');
     cy.get('#alert-close-button').click();
-    cy.tick(50);
-    cy.get('.pf-v6-c-alert-group__item').then(($groupItem) => {
-      const currentItem = $groupItem.get(0);
-      const leftAfterAnimation = currentItem.getBoundingClientRect().left;
-
-      expect(left).to.be.lessThan(leftAfterAnimation);
-    });
+    cy.tick(5);
+    cy.get('.pf-v6-c-alert-group__item').should('have.class', 'pf-m-offstage-right');
   });
 });
