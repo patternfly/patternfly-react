f8893d5c9a5b7c18c0e02404c9ca07dd08bad514
Katie McFaul <kmcfaul@redhat.com>
2025-02-20 16:23:03 -0500

    update example name

--- packages/react-core/src/components/Label/examples/LabelCustomRender.tsx
+++ packages/react-core/src/components/Label/examples/LabelCustomRender.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Label } from '@patternfly/react-core';
 import InfoCircleIcon from '@patternfly/react-icons/dist/esm/icons/info-circle-icon';
 
-export const LabelRouterLink: React.FunctionComponent = () => (
+export const LabelCustomRender: React.FunctionComponent = () => (
   <Label
     color="blue"
     icon={<InfoCircleIcon />}
@@ -11,14 +11,9 @@ export const LabelRouterLink: React.FunctionComponent = () => (
       <a className={className} ref={componentRef}>
         {content}
       </a>
-      /** A router link would look like the following:
-       * <Link to="/" className={className} ref={componentRef}>
-       *   {content}
-       * </Link>
-       */
     )}
     textMaxWidth="16ch"
-    isClickable // can be passed manually to remove the default underline text-decoration of links
+    isClickable
   >
     Blue label router link with icon that overflows
   </Label>
