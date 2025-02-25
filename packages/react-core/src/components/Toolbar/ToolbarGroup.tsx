1022e95a384fd5fd2de0291026136d90afc64870
mfrances <mfrances@redhat.com>
2025-02-25 07:57:42 -0500

    rm unsupported wrap reverse

--- packages/react-core/src/components/Toolbar/ToolbarGroup.tsx
+++ packages/react-core/src/components/Toolbar/ToolbarGroup.tsx
@@ -159,12 +159,12 @@ export interface ToolbarGroupProps extends Omit<React.HTMLProps<HTMLDivElement>,
   };
   /** Value to set for row wrapping at various breakpoints */
   rowWrap?: {
-    default?: 'wrap' | 'wrapReverse' | 'nowrap';
-    sm?: 'wrap' | 'wrapReverse' | 'nowrap';
-    md?: 'wrap' | 'wrapReverse' | 'nowrap';
-    lg?: 'wrap' | 'wrapReverse' | 'nowrap';
-    xl?: 'wrap' | 'wrapReverse' | 'nowrap';
-    '2xl'?: 'wrap' | 'wrapReverse' | 'nowrap';
+    default?: 'wrap' | 'nowrap';
+    sm?: 'wrap' | 'nowrap';
+    md?: 'wrap' | 'nowrap';
+    lg?: 'wrap' | 'nowrap';
+    xl?: 'wrap' | 'nowrap';
+    '2xl'?: 'wrap' | 'nowrap';
   };
   /** Content to be rendered inside the data toolbar group */
   children?: React.ReactNode;
