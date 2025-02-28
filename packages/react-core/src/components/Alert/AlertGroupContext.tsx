60ad3c0e440e18b7ee12c5b79ce2edadc557a0b7
Eric Olkowski <thatblindgeye@gmail.com>
2025-02-28 09:29:09 -0500

    Refactored transitionend handling

--- packages/react-core/src/components/Alert/AlertGroupContext.tsx
+++ packages/react-core/src/components/Alert/AlertGroupContext.tsx
@@ -2,6 +2,10 @@ import * as React from 'react';
 
 interface AlertGroupContext {
   hasAnimations?: boolean;
+  updateTransitionEnd?: (onTransitionEnd: React.Dispatch<React.SetStateAction<() => void>>) => void;
 }
 
-export const AlertGroupContext = React.createContext<AlertGroupContext>({ hasAnimations: false });
+export const AlertGroupContext = React.createContext<AlertGroupContext>({
+  hasAnimations: false,
+  updateTransitionEnd: () => {}
+});
