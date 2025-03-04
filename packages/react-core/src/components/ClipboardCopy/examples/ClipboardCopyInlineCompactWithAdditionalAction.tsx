ae6a7a72b61fdd2bb2c0c03bda22746f50d85a6f
Divyanshu Gupta <114072061+Mash707@users.noreply.github.com>
2025-03-05 02:31:47 +0530

    Chore(ClipboardCopy): add tooltip to play icon (#11583)
    * Chore(ClipboardCopy): add tooltip to play icon
    
    * renamed state variable and added a callback
    
    * added tooltip to code block examples

--- packages/react-core/src/components/ClipboardCopy/examples/ClipboardCopyInlineCompactWithAdditionalAction.tsx
+++ packages/react-core/src/components/ClipboardCopy/examples/ClipboardCopyInlineCompactWithAdditionalAction.tsx
@@ -1,18 +1,36 @@
 import React from 'react';
-import { ClipboardCopy, ClipboardCopyAction, Button } from '@patternfly/react-core';
+import { ClipboardCopy, ClipboardCopyAction, Button, Tooltip } from '@patternfly/react-core';
 import PlayIcon from '@patternfly/react-icons/dist/esm/icons/play-icon';
 
-export const ClipboardCopyInlineCompactWithAdditionalAction: React.FunctionComponent = () => (
-  <ClipboardCopy
-    hoverTip="Copy"
-    clickTip="Copied"
-    variant="inline-compact"
-    additionalActions={
-      <ClipboardCopyAction>
-        <Button variant="plain" hasNoPadding aria-label="Run in web terminal" icon={<PlayIcon />} />
-      </ClipboardCopyAction>
-    }
-  >
-    2.3.4-2-redhat
-  </ClipboardCopy>
-);
+export const ClipboardCopyInlineCompactWithAdditionalAction: React.FunctionComponent = () => {
+  const [isRunning, setisRunning] = React.useState(false);
+  const runText: string = 'Run in web terminal';
+  const doneRunText: string = 'Running in web terminal';
+  return (
+    <ClipboardCopy
+      hoverTip="Copy"
+      clickTip="Copied"
+      variant="inline-compact"
+      additionalActions={
+        <ClipboardCopyAction>
+          <Tooltip
+            aria="none"
+            aria-live="polite"
+            content={isRunning ? doneRunText : runText}
+            onTooltipHidden={() => setisRunning(false)}
+          >
+            <Button
+              variant="plain"
+              hasNoPadding
+              aria-label="Run in web terminal"
+              icon={<PlayIcon />}
+              onClick={() => setisRunning(!isRunning)}
+            />
+          </Tooltip>
+        </ClipboardCopyAction>
+      }
+    >
+      2.3.4-2-redhat
+    </ClipboardCopy>
+  );
+};
