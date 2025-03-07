b3b5435ad5f3898b22bc129a0a46d819997e1cb4
Divyanshu Gupta <divyanshugupta585@gmail.com>
2025-03-07 20:07:51 +0530

    updated aboutmodal.spec.ts file

--- packages/react-integration/cypress/integration/aboutmodal.spec.ts
+++ packages/react-integration/cypress/integration/aboutmodal.spec.ts
@@ -1,10 +1,7 @@
 describe('About Modal Demo Test', () => {
-  it('Navigate to demo section', () => {
-    cy.visit('http://localhost:3000/about-modal-demo-nav-link');
-  });
-
   it('Open + close about modal', () => {
-    cy.get('#modal-button').click();
+    cy.visit('http://localhost:3000/about-modal-demo-nav-link');
+    cy.get('#modal-button', { timeout: 10000 }).should('be.visible').click();
     cy.get('.pf-v6-c-about-modal-box').should('exist');
     cy.get('.pf-v6-c-button[aria-label="Close Dialog"]').click();
     cy.get('.pf-v6-c-about-modal-box').should('not.exist');
