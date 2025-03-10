4a01203255637ae7ef464d28f071a80be5b275e1
Divyanshu Gupta <divyanshugupta585@gmail.com>
2025-03-10 22:36:14 +0530

    added before() hook

--- packages/react-integration/cypress/integration/aboutmodal.spec.ts
+++ packages/react-integration/cypress/integration/aboutmodal.spec.ts
@@ -1,7 +1,9 @@
 describe('About Modal Demo Test', () => {
-  it('Open + close about modal', () => {
+  before(() => {
     cy.visit('http://localhost:3000/about-modal-demo-nav-link');
-    cy.get('#modal-button', { timeout: 10000 }).should('be.visible').click();
+  });
+  it('Open + close about modal', () => {
+    cy.get('#modal-button').click();
     cy.get('.pf-v6-c-about-modal-box').should('exist');
     cy.get('.pf-v6-c-button[aria-label="Close Dialog"]').click();
     cy.get('.pf-v6-c-about-modal-box').should('not.exist');
