ea4b10b0f2707d6887f7ae18ae494105d8d8ba5a
Divyanshu Gupta <divyanshugupta585@gmail.com>
2025-03-19 03:17:39 +0530

    chore(DescriptionList): added keyboard interaction cypress test

--- packages/react-integration/cypress/integration/descriptionlist.spec.ts
+++ packages/react-integration/cypress/integration/descriptionlist.spec.ts
@@ -10,5 +10,16 @@ describe('Description List Demo Test', () => {
       '#description-list-help-text > :nth-child(1) > .pf-v6-c-description-list__term .pf-v6-c-description-list__text'
     ).click();
     cy.get('.pf-v6-c-popover__content').should('exist');
+    cy.get('.pf-v6-c-popover__close').click();
+    cy.get('.pf-v6-c-popover__content').should('not.exist');
+  });
+
+  it('Verify list with help text using keyboard', () => {
+    cy.get('#description-list-help-text').should('exist').children('.pf-v6-c-description-list__group');
+    cy.get('.pf-v6-c-popover__content').should('not.exist');
+    cy.tab().type('{enter}');
+    cy.get('.pf-v6-c-popover__content').should('exist');
+    cy.get('.pf-v6-c-popover__close').type('{enter}');
+    cy.get('.pf-v6-c-popover__content').should('not.exist');
   });
 });
