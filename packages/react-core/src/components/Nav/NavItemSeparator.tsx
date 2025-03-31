435e3b592421b0688cb2823406b18688b0e585bf
Eric Olkowski <thatblindgeye@gmail.com>
2025-03-31 15:11:21 -0400

    fix(Nav): added role presentation to NavItemSeparator

--- packages/react-core/src/components/Nav/NavItemSeparator.tsx
+++ packages/react-core/src/components/Nav/NavItemSeparator.tsx
@@ -2,6 +2,7 @@ import { Divider, DividerProps } from '../Divider';
 
 export const NavItemSeparator: React.FunctionComponent<DividerProps> = ({
   component = 'li',
+  role = 'presentation',
   ...props
-}: DividerProps) => <Divider component={component} {...props} />;
+}: DividerProps) => <Divider component={component} role={role} {...props} />;
 NavItemSeparator.displayName = 'NavItemSeparator';
