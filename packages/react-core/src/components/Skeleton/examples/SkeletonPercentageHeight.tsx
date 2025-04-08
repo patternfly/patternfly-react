3ba79b0c6c6e750996f6134251dc6c6bd5eeb28a
Divyanshu Gupta <114072061+Mash707@users.noreply.github.com>
2025-04-09 01:14:35 +0530

    chore(skeleton): updated screen reader text (#11739)
    * chore(skeleton): updated screen reader text
    
    * updated text and shapes example
    
    * updated skeleton card demo

--- packages/react-core/src/components/Skeleton/examples/SkeletonPercentageHeight.tsx
+++ packages/react-core/src/components/Skeleton/examples/SkeletonPercentageHeight.tsx
@@ -2,11 +2,11 @@ import { Skeleton } from '@patternfly/react-core';
 
 export const SkeletonPercentageHeight: React.FunctionComponent = () => (
   <div style={{ height: '400px', display: 'flex', alignItems: 'flex-end', justifyContent: 'space-between' }}>
-    <Skeleton height="25%" width="15%" screenreaderText="Loaded 25% of content" />
-    <Skeleton height="33%" width="15%" screenreaderText="Loaded 33% of content" />
-    <Skeleton height="50%" width="15%" screenreaderText="Loaded 50% of content" />
-    <Skeleton height="66%" width="15%" screenreaderText="Loaded 66% of content" />
-    <Skeleton height="75%" width="15%" screenreaderText="Loaded 75% of content" />
-    <Skeleton height="100%" width="15%" screenreaderText="Loaded 100% of content" />
+    <Skeleton height="25%" width="15%" screenreaderText="Loading percentage height content" />
+    <Skeleton height="33%" width="15%" />
+    <Skeleton height="50%" width="15%" />
+    <Skeleton height="66%" width="15%" />
+    <Skeleton height="75%" width="15%" />
+    <Skeleton height="100%" width="15%" />
   </div>
 );
