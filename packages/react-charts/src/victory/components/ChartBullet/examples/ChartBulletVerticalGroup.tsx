bac56c49b7403df5b7d76b223f2a98b8244a2625
Divyanshu Gupta <divyanshugupta585@gmail.com>
2025-04-14 22:55:13 +0530

    added variables

--- packages/react-charts/src/victory/components/ChartBullet/examples/ChartBulletVerticalGroup.tsx
+++ packages/react-charts/src/victory/components/ChartBullet/examples/ChartBulletVerticalGroup.tsx
@@ -1,119 +1,124 @@
 import { ChartBullet, ChartContainer } from '@patternfly/react-charts/victory';
 
-export const ChartBulletVerticalGroup: React.FunctionComponent = () => (
-  <div style={{ height: '600px', width: '500px' }}>
-    <ChartContainer title="Bullet chart example" height={600} width={500}>
-      <ChartBullet
-        comparativeWarningMeasureData={[{ name: 'Warning', y: 78 }]}
-        comparativeWarningMeasureLegendData={[{ name: 'Warning' }]}
-        constrainToVisibleArea
-        height={600}
-        horizontal={false}
-        labels={({ datum }) => `${datum.name}: ${datum.y}`}
-        maxDomain={{ y: 100 }}
-        name="chart18"
-        padding={{
-          bottom: 125, // Adjusted to accommodate legend
-          left: 50,
-          right: 50,
-          top: 50
-        }}
-        primarySegmentedMeasureData={[
-          { name: 'Measure', y: 15 },
-          { name: 'Measure', y: 50 }
-        ]}
-        primarySegmentedMeasureLegendData={[{ name: 'Measure 1' }, { name: 'Measure 2' }]}
-        qualitativeRangeData={[
-          { name: 'Range', y: 40 },
-          { name: 'Range', y: 65 }
-        ]}
-        qualitativeRangeLegendData={[{ name: 'Range 1' }, { name: 'Range 2' }]}
-        standalone={false}
-        subTitle="Measure details"
-        title="Text label"
-        width={500}
-      />
-      <ChartBullet
-        comparativeWarningMeasureData={[{ name: 'Warning', y: 88 }]}
-        constrainToVisibleArea
-        height={600}
-        horizontal={false}
-        labels={({ datum }) => `${datum.name}: ${datum.y}`}
-        maxDomain={{ y: 100 }}
-        name="chart19"
-        padding={{
-          bottom: 125, // Adjusted to accommodate legend
-          left: 300,
-          right: 50,
-          top: 50
-        }}
-        primarySegmentedMeasureData={[
-          { name: 'Measure', y: 25 },
-          { name: 'Measure', y: 60 }
-        ]}
-        qualitativeRangeData={[
-          { name: 'Range', y: 50 },
-          { name: 'Range', y: 75 }
-        ]}
-        standalone={false}
-        subTitle="Measure details"
-        title="Text label"
-        width={500}
-      />
-      <ChartBullet
-        comparativeWarningMeasureData={[{ name: 'Warning', y: 98 }]}
-        constrainToVisibleArea
-        height={600}
-        horizontal={false}
-        labels={({ datum }) => `${datum.name}: ${datum.y}`}
-        maxDomain={{ y: 100 }}
-        name="chart20"
-        padding={{
-          bottom: 125, // Adjusted to accommodate legend
-          left: 550,
-          right: 50,
-          top: 50
-        }}
-        primarySegmentedMeasureData={[
-          { name: 'Measure', y: 35 },
-          { name: 'Measure', y: 70 }
-        ]}
-        qualitativeRangeData={[
-          { name: 'Range', y: 60 },
-          { name: 'Range', y: 85 }
-        ]}
-        standalone={false}
-        subTitle="Measure details"
-        title="Text label"
-        width={500}
-      />
-      <ChartBullet
-        comparativeWarningMeasureData={[{ name: 'Warning', y: 78 }]}
-        constrainToVisibleArea
-        height={600}
-        horizontal={false}
-        labels={({ datum }) => `${datum.name}: ${datum.y}`}
-        maxDomain={{ y: 100 }}
-        name="chart21"
-        padding={{
-          bottom: 125, // Adjusted to accommodate legend
-          left: 800,
-          right: 50,
-          top: 50
-        }}
-        primarySegmentedMeasureData={[
-          { name: 'Measure', y: 15 },
-          { name: 'Measure', y: 50 }
-        ]}
-        qualitativeRangeData={[
-          { name: 'Range', y: 40 },
-          { name: 'Range', y: 65 }
-        ]}
-        standalone={false}
-        subTitle="Measure details"
-        title="Text label"
-        width={500}
-      />
-    </ChartContainer>
-  </div>
-);
+interface ChartData {
+  name: string;
+  y?: number;
+}
+
+export const ChartBulletVerticalGroup: React.FunctionComponent = () => {
+  const comparativeWarningMeasureData: ChartData[] = [{ name: 'Warning', y: 78 }];
+  const primarySegmentedMeasureData: ChartData[] = [
+    { name: 'Measure', y: 15 },
+    { name: 'Measure', y: 50 }
+  ];
+  const qualitativeRangeData: ChartData[] = [
+    { name: 'Range', y: 40 },
+    { name: 'Range', y: 65 }
+  ];
+
+  return (
+    <div style={{ height: '600px', width: '500px' }}>
+      <ChartContainer title="Bullet chart example" height={600} width={500}>
+        <ChartBullet
+          comparativeWarningMeasureData={comparativeWarningMeasureData}
+          comparativeWarningMeasureLegendData={[{ name: 'Warning' }]}
+          constrainToVisibleArea
+          height={600}
+          horizontal={false}
+          labels={({ datum }) => `${datum.name}: ${datum.y}`}
+          maxDomain={{ y: 100 }}
+          name="chart18"
+          padding={{
+            bottom: 125, // Adjusted to accommodate legend
+            left: 50,
+            right: 50,
+            top: 50
+          }}
+          primarySegmentedMeasureData={primarySegmentedMeasureData}
+          primarySegmentedMeasureLegendData={[{ name: 'Measure 1' }, { name: 'Measure 2' }]}
+          qualitativeRangeData={qualitativeRangeData}
+          qualitativeRangeLegendData={[{ name: 'Range 1' }, { name: 'Range 2' }]}
+          standalone={false}
+          subTitle="Measure details"
+          title="Text label"
+          width={500}
+        />
+        <ChartBullet
+          comparativeWarningMeasureData={[{ name: 'Warning', y: 88 }]}
+          constrainToVisibleArea
+          height={600}
+          horizontal={false}
+          labels={({ datum }) => `${datum.name}: ${datum.y}`}
+          maxDomain={{ y: 100 }}
+          name="chart19"
+          padding={{
+            bottom: 125, // Adjusted to accommodate legend
+            left: 300,
+            right: 50,
+            top: 50
+          }}
+          primarySegmentedMeasureData={[
+            { name: 'Measure', y: 25 },
+            { name: 'Measure', y: 60 }
+          ]}
+          qualitativeRangeData={[
+            { name: 'Range', y: 50 },
+            { name: 'Range', y: 75 }
+          ]}
+          standalone={false}
+          subTitle="Measure details"
+          title="Text label"
+          width={500}
+        />
+        <ChartBullet
+          comparativeWarningMeasureData={[{ name: 'Warning', y: 98 }]}
+          constrainToVisibleArea
+          height={600}
+          horizontal={false}
+          labels={({ datum }) => `${datum.name}: ${datum.y}`}
+          maxDomain={{ y: 100 }}
+          name="chart20"
+          padding={{
+            bottom: 125, // Adjusted to accommodate legend
+            left: 550,
+            right: 50,
+            top: 50
+          }}
+          primarySegmentedMeasureData={[
+            { name: 'Measure', y: 35 },
+            { name: 'Measure', y: 70 }
+          ]}
+          qualitativeRangeData={[
+            { name: 'Range', y: 60 },
+            { name: 'Range', y: 85 }
+          ]}
+          standalone={false}
+          subTitle="Measure details"
+          title="Text label"
+          width={500}
+        />
+        <ChartBullet
+          comparativeWarningMeasureData={comparativeWarningMeasureData}
+          constrainToVisibleArea
+          height={600}
+          horizontal={false}
+          labels={({ datum }) => `${datum.name}: ${datum.y}`}
+          maxDomain={{ y: 100 }}
+          name="chart21"
+          padding={{
+            bottom: 125, // Adjusted to accommodate legend
+            left: 800,
+            right: 50,
+            top: 50
+          }}
+          primarySegmentedMeasureData={primarySegmentedMeasureData}
+          qualitativeRangeData={qualitativeRangeData}
+          standalone={false}
+          subTitle="Measure details"
+          title="Text label"
+          width={500}
+        />
+      </ChartContainer>
+    </div>
+  );
+};
--- packages/react-charts/src/victory/components/ChartBullet/examples/ChartBulletVerticalGroupTitle.tsx
+++ packages/react-charts/src/victory/components/ChartBullet/examples/ChartBulletVerticalGroupTitle.tsx
@@ -1,121 +1,126 @@
 import { ChartBullet, ChartContainer } from '@patternfly/react-charts/victory';
 
-export const ChartBulletVerticalGroupTitle: React.FunctionComponent = () => (
-  <div style={{ height: '600px', width: '500px' }}>
-    <ChartContainer title="Bullet chart example" height={600} width={500}>
-      <ChartBullet
-        comparativeWarningMeasureData={[{ name: 'Warning', y: 78 }]}
-        comparativeWarningMeasureLegendData={[{ name: 'Warning' }]}
-        constrainToVisibleArea
-        groupSubTitle="Measure details"
-        groupTitle="Text label"
-        height={600}
-        horizontal={false}
-        labels={({ datum }) => `${datum.name}: ${datum.y}`}
-        maxDomain={{ y: 100 }}
-        name="chart26"
-        padding={{
-          bottom: 125, // Adjusted to accommodate legend
-          left: 50,
-          right: 50,
-          top: 150 // Adjusted to accommodate group labels
-        }}
-        primarySegmentedMeasureData={[
-          { name: 'Measure', y: 15 },
-          { name: 'Measure', y: 50 }
-        ]}
-        primarySegmentedMeasureLegendData={[{ name: 'Measure 1' }, { name: 'Measure 2' }]}
-        qualitativeRangeData={[
-          { name: 'Range', y: 40 },
-          { name: 'Range', y: 65 }
-        ]}
-        qualitativeRangeLegendData={[{ name: 'Range 1' }, { name: 'Range 2' }]}
-        standalone={false}
-        subTitle="Measure details"
-        title="Text label"
-        width={500}
-      />
-      <ChartBullet
-        comparativeWarningMeasureData={[{ name: 'Warning', y: 88 }]}
-        constrainToVisibleArea
-        height={600}
-        horizontal={false}
-        labels={({ datum }) => `${datum.name}: ${datum.y}`}
-        maxDomain={{ y: 100 }}
-        name="chart27"
-        padding={{
-          bottom: 125, // Adjusted to accommodate legend
-          left: 300,
-          right: 50,
-          top: 150 // Adjusted to accommodate group labels
-        }}
-        primarySegmentedMeasureData={[
-          { name: 'Measure', y: 25 },
-          { name: 'Measure', y: 60 }
-        ]}
-        qualitativeRangeData={[
-          { name: 'Range', y: 50 },
-          { name: 'Range', y: 75 }
-        ]}
-        standalone={false}
-        subTitle="Measure details"
-        title="Text label"
-        width={500}
-      />
-      <ChartBullet
-        comparativeWarningMeasureData={[{ name: 'Warning', y: 98 }]}
-        constrainToVisibleArea
-        height={600}
-        horizontal={false}
-        labels={({ datum }) => `${datum.name}: ${datum.y}`}
-        maxDomain={{ y: 100 }}
-        name="chart28"
-        padding={{
-          bottom: 125, // Adjusted to accommodate legend
-          left: 550,
-          right: 50,
-          top: 150 // Adjusted to accommodate group labels
-        }}
-        primarySegmentedMeasureData={[
-          { name: 'Measure', y: 35 },
-          { name: 'Measure', y: 70 }
-        ]}
-        qualitativeRangeData={[
-          { name: 'Range', y: 60 },
-          { name: 'Range', y: 85 }
-        ]}
-        standalone={false}
-        subTitle="Measure details"
-        title="Text label"
-        width={500}
-      />
-      <ChartBullet
-        comparativeWarningMeasureData={[{ name: 'Warning', y: 78 }]}
-        constrainToVisibleArea
-        height={600}
-        horizontal={false}
-        labels={({ datum }) => `${datum.name}: ${datum.y}`}
-        maxDomain={{ y: 100 }}
-        name="chart29"
-        padding={{
-          bottom: 125, // Adjusted to accommodate legend
-          left: 800,
-          right: 50,
-          top: 150 // Adjusted to accommodate group labels
-        }}
-        primarySegmentedMeasureData={[
-          { name: 'Measure', y: 15 },
-          { name: 'Measure', y: 50 }
-        ]}
-        qualitativeRangeData={[
-          { name: 'Range', y: 40 },
-          { name: 'Range', y: 65 }
-        ]}
-        standalone={false}
-        subTitle="Measure details"
-        title="Text label"
-        width={500}
-      />
-    </ChartContainer>
-  </div>
-);
+interface ChartData {
+  name: string;
+  y?: number;
+}
+
+export const ChartBulletVerticalGroupTitle: React.FunctionComponent = () => {
+  const comparativeWarningMeasureData: ChartData[] = [{ name: 'Warning', y: 78 }];
+  const primarySegmentedMeasureData: ChartData[] = [
+    { name: 'Measure', y: 15 },
+    { name: 'Measure', y: 50 }
+  ];
+  const qualitativeRangeData: ChartData[] = [
+    { name: 'Range', y: 40 },
+    { name: 'Range', y: 65 }
+  ];
+
+  return (
+    <div style={{ height: '600px', width: '500px' }}>
+      <ChartContainer title="Bullet chart example" height={600} width={500}>
+        <ChartBullet
+          comparativeWarningMeasureData={comparativeWarningMeasureData}
+          comparativeWarningMeasureLegendData={[{ name: 'Warning' }]}
+          constrainToVisibleArea
+          groupSubTitle="Measure details"
+          groupTitle="Text label"
+          height={600}
+          horizontal={false}
+          labels={({ datum }) => `${datum.name}: ${datum.y}`}
+          maxDomain={{ y: 100 }}
+          name="chart26"
+          padding={{
+            bottom: 125, // Adjusted to accommodate legend
+            left: 50,
+            right: 50,
+            top: 150 // Adjusted to accommodate group labels
+          }}
+          primarySegmentedMeasureData={primarySegmentedMeasureData}
+          primarySegmentedMeasureLegendData={[{ name: 'Measure 1' }, { name: 'Measure 2' }]}
+          qualitativeRangeData={qualitativeRangeData}
+          qualitativeRangeLegendData={[{ name: 'Range 1' }, { name: 'Range 2' }]}
+          standalone={false}
+          subTitle="Measure details"
+          title="Text label"
+          width={500}
+        />
+        <ChartBullet
+          comparativeWarningMeasureData={[{ name: 'Warning', y: 88 }]}
+          constrainToVisibleArea
+          height={600}
+          horizontal={false}
+          labels={({ datum }) => `${datum.name}: ${datum.y}`}
+          maxDomain={{ y: 100 }}
+          name="chart27"
+          padding={{
+            bottom: 125, // Adjusted to accommodate legend
+            left: 300,
+            right: 50,
+            top: 150 // Adjusted to accommodate group labels
+          }}
+          primarySegmentedMeasureData={[
+            { name: 'Measure', y: 25 },
+            { name: 'Measure', y: 60 }
+          ]}
+          qualitativeRangeData={[
+            { name: 'Range', y: 50 },
+            { name: 'Range', y: 75 }
+          ]}
+          standalone={false}
+          subTitle="Measure details"
+          title="Text label"
+          width={500}
+        />
+        <ChartBullet
+          comparativeWarningMeasureData={[{ name: 'Warning', y: 98 }]}
+          constrainToVisibleArea
+          height={600}
+          horizontal={false}
+          labels={({ datum }) => `${datum.name}: ${datum.y}`}
+          maxDomain={{ y: 100 }}
+          name="chart28"
+          padding={{
+            bottom: 125, // Adjusted to accommodate legend
+            left: 550,
+            right: 50,
+            top: 150 // Adjusted to accommodate group labels
+          }}
+          primarySegmentedMeasureData={[
+            { name: 'Measure', y: 35 },
+            { name: 'Measure', y: 70 }
+          ]}
+          qualitativeRangeData={[
+            { name: 'Range', y: 60 },
+            { name: 'Range', y: 85 }
+          ]}
+          standalone={false}
+          subTitle="Measure details"
+          title="Text label"
+          width={500}
+        />
+        <ChartBullet
+          comparativeWarningMeasureData={comparativeWarningMeasureData}
+          constrainToVisibleArea
+          height={600}
+          horizontal={false}
+          labels={({ datum }) => `${datum.name}: ${datum.y}`}
+          maxDomain={{ y: 100 }}
+          name="chart29"
+          padding={{
+            bottom: 125, // Adjusted to accommodate legend
+            left: 800,
+            right: 50,
+            top: 150 // Adjusted to accommodate group labels
+          }}
+          primarySegmentedMeasureData={primarySegmentedMeasureData}
+          qualitativeRangeData={qualitativeRangeData}
+          standalone={false}
+          subTitle="Measure details"
+          title="Text label"
+          width={500}
+        />
+      </ChartContainer>
+    </div>
+  );
+};
