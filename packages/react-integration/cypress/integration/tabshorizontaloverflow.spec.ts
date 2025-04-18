7fc661c7883e6e20a537f3f43bedac0063c8f1e9
Eric Olkowski <thatblindgeye@gmail.com>
2025-04-18 11:04:15 -0400

    Adjusted cypress tests again

--- packages/react-integration/cypress/integration/tabshorizontaloverflow.spec.ts
+++ packages/react-integration/cypress/integration/tabshorizontaloverflow.spec.ts
@@ -3,27 +3,6 @@ describe('Tab Demo Test', () => {
     cy.visit('http://localhost:3000/tabs-horizontal-overflow-demo-nav-link');
   });
 
-  it('Updates overflow tab title based on tab selection', () => {
-    cy.contains('.pf-v6-c-tabs__link', 'More').should('exist');
-    cy.contains('Tab item 9').should('not.exist');
-    cy.contains('.pf-v6-c-tabs__link', 'More').click();
-    cy.contains('Tab item 9').click();
-    cy.contains('More').should('not.exist');
-    cy.contains('.pf-v6-c-tabs__link', 'Tab item 9').should('exist');
-    cy.contains('Tab item 1').click();
-    cy.contains('Tab item 9').should('not.exist');
-    cy.contains('More').should('exist');
-  });
-
-  it('Allows selection of tabs from overflow tab menu', () => {
-    cy.contains('Tab 1 section').should('not.be.hidden');
-    cy.contains('Tab 9 section').should('be.hidden');
-    cy.contains('.pf-v6-c-tabs__link', 'More').click();
-    cy.contains('Tab item 9').click();
-    cy.contains('Tab 9 section').should('not.be.hidden');
-    cy.contains('Tab 1 section').should('be.hidden');
-  });
-
   // Re-enable once https://github.com/patternfly/patternfly/issues/7449 is resolved
   xit('Adjusts tabs showing on resize', () => {
     // shrink viewport and verify that tabs which would now overflow are moved to the overflow tab
@@ -55,5 +34,14 @@ describe('Tab Demo Test', () => {
       'Tab item 11'
     ].forEach((tab) => cy.contains('.pf-v6-c-tabs__link', tab).should('exist'));
     cy.contains('.pf-v6-c-tabs__link', 'More').should('not.exist');
+  });
+
+  it('Allows selection of tabs from overflow tab menu', () => {
+    cy.contains('Tab 1 section').should('not.be.hidden');
+    cy.contains('Tab 9 section').should('be.hidden');
+    cy.contains('.pf-v6-c-tabs__link', 'More').click();
+    cy.contains('Tab item 9').click();
+    cy.contains('Tab 9 section').should('not.be.hidden');
+    cy.contains('Tab 1 section').should('be.hidden');
   });
 });
