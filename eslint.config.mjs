c0ad0ecdf1dac67e106e8ce3d3654152ecbff47c
Divyanshu Gupta <divyanshugupta585@gmail.com>
2025-04-28 22:46:46 +0530

    chore(deps): update dependency typescript-eslint to ^8.22.0 (#11600)
    * allow shortcircuit and ternary
    
    * updated eslint config file
    
    * updated config file
    
    * resolve merge conflicts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -48,6 +48,10 @@ export default [
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      '@typescript-eslint/no-wrapper-object-types': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-unsafe-function-type': 'off',
+      '@typescript-eslint/no-unused-expressions': ['error', { allowShortCircuit: true, allowTernary: true }],
       '@typescript-eslint/ban-ts-comment': 'off',
       '@typescript-eslint/adjacent-overload-signatures': 'error',
       '@typescript-eslint/array-type': 'error',
@@ -67,11 +71,12 @@ export default [
       '@typescript-eslint/no-unused-vars': [
         'error',
         {
-          argsIgnorePattern: '^_'
+          argsIgnorePattern: '^_',
+          caughtErrors: 'none'
         }
       ],
       '@typescript-eslint/no-use-before-define': 'off',
-      '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
       '@typescript-eslint/prefer-for-of': 'error',
       '@typescript-eslint/prefer-function-type': 'error',
       '@typescript-eslint/prefer-namespace-keyword': 'error',
