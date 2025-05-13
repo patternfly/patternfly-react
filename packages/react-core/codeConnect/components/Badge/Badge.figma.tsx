4a21e1d965070c1a979fdfba8d915d16c072163e
Matt Nolting <matthew.nolting@gmail.com>
2025-05-13 10:58:44 -0400

    feat(CC-batch-2): added batch 2

---
+++ packages/react-core/codeConnect/components/Badge/Badge.figma.tsx
@@ -0,0 +1,25 @@
+import figma from '@figma/code-connect';
+import { Badge } from '@patternfly/react-core';
+
+/**
+ * PatternFly Badge component integration for Figma Code Connect
+ */
+
+figma.connect(
+  Badge,
+  'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=1259-1132&m=dev',
+  {
+    props: {
+      // strings
+      title: figma.string('Text', '00'),
+
+      // enums
+      type: figma.enum('Type', {
+        Unread: undefined,
+        Read: false,
+        disable: false
+      })
+    },
+    example: (props) => <Badge title={props.title} type={props.type} />
+  }
+);
