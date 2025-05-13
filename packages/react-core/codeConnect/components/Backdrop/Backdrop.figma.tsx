1f6b2380ba8075d7bc4b8b13f7aedac4b3843d28
Matt Nolting <matthew.nolting@gmail.com>
2025-05-13 10:58:44 -0400

    feat(CC-batch-2): added batch 2

---
+++ packages/react-core/codeConnect/components/Backdrop/Backdrop.figma.tsx
@@ -0,0 +1,14 @@
+import figma from '@figma/code-connect';
+import { Backdrop } from '@patternfly/react-core';
+
+/**
+ * PatternFly Backdrop component integration for Figma Code Connect
+ */
+
+figma.connect(
+  Backdrop,
+  'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6%3A-Components-Test?node-id=2873-2900&m=dev',
+  {
+    example: () => <Backdrop />
+  }
+);
