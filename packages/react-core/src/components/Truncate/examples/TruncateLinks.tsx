abc6eb4010896487f1c55553f13087c0d099e3c6
Eric Olkowski <thatblindgeye@gmail.com>
2025-05-14 08:37:08 -0400

    Added tests

--- packages/react-core/src/components/Truncate/examples/TruncateLinks.tsx
+++ packages/react-core/src/components/Truncate/examples/TruncateLinks.tsx
@@ -1,20 +1,12 @@
-import { useRef } from 'react';
-import { Truncate, Button } from '@patternfly/react-core';
+import { Truncate } from '@patternfly/react-core';
 
 export const TruncateLinks: React.FunctionComponent = () => {
-  const anchorRef = useRef(null);
-  const btnRef = useRef(null);
   const content = 'A very lengthy anchor text content to trigger truncation';
   return (
     <>
       <div>With default width-observing truncation:</div>
       <div className="truncate-example-resize">
-        <a ref={anchorRef} href="#">
-          <Truncate tooltipProps={{ triggerRef: anchorRef }} content={content} />
-        </a>
-        <Button variant="control" ref={btnRef}>
-          <Truncate tooltipProps={{ triggerRef: btnRef }} content={content} />
-        </Button>
+        <Truncate href="#" content={content} />
         <Truncate position="start" href="#" content={content} />
         <Truncate position="middle" href="#" content={content} />
       </div>
