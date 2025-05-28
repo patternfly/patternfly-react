2e8bda6fd12c3127ff5408c9c2c0b724dc8f1062
Eric Olkowski <thatblindgeye@gmail.com>
2025-05-28 14:21:27 -0400

    Test for more opt-in functionality

--- packages/react-core/src/components/ExpandableSection/ExpandableSection.tsx
+++ packages/react-core/src/components/ExpandableSection/ExpandableSection.tsx
@@ -68,6 +68,7 @@ export interface ExpandableSectionProps extends Omit<React.HTMLProps<HTMLDivElem
    * animation will not occur.
    */
   direction?: 'up' | 'down';
+  hasDetachedAnimations?: boolean;
 }
 
 interface ExpandableSectionState {
@@ -203,6 +204,7 @@ class ExpandableSection extends Component<ExpandableSectionProps, ExpandableSect
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
       truncateMaxLines,
       direction,
+      hasDetachedAnimations,
       ...props
     } = this.props;
 
@@ -255,6 +257,8 @@ class ExpandableSection extends Component<ExpandableSectionProps, ExpandableSect
       </div>
     );
 
+    const shouldApplyDetachedAnimations = hasDetachedAnimations && isDetached;
+
     return (
       <div
         className={css(
@@ -263,8 +267,10 @@ class ExpandableSection extends Component<ExpandableSectionProps, ExpandableSect
           displaySize === 'lg' && styles.modifiers.displayLg,
           isWidthLimited && styles.modifiers.limitWidth,
           isIndented && styles.modifiers.indented,
-          isDetached && 'pf-m-detached',
-          isDetached && direction && (direction === 'up' ? styles.modifiers.expandTop : 'pf-m-expand-bottom'),
+          shouldApplyDetachedAnimations && 'pf-m-detached',
+          shouldApplyDetachedAnimations &&
+            direction &&
+            (direction === 'up' ? styles.modifiers.expandTop : 'pf-m-expand-bottom'),
           variant === ExpandableSectionVariant.truncate && styles.modifiers.truncate,
           className
         )}
