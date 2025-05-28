93c7abc692e6cafad367f88145c00545269975e7
Divyanshu Gupta <divyanshugupta585@gmail.com>
2025-05-29 03:38:51 +0530

    fix icon import

--- packages/react-core/src/components/Button/examples/ButtonFavorite.tsx
+++ packages/react-core/src/components/Button/examples/ButtonFavorite.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Button } from '@patternfly/react-core';
-import TimesIcon from '@patternfly/react-icons/dist/esm/icons/star-icon';
+import TimesIcon from '@patternfly/react-icons/dist/esm/icons/times-icon';
 
 export const ButtonFavorite: React.FunctionComponent = () => {
   const [isFavorited, setIsFavorited] = useState(false);
