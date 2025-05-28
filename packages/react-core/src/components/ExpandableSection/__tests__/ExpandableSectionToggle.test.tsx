521a72c19ed9cc148eb8acac03a51080f4ad439c
Eric Olkowski <thatblindgeye@gmail.com>
2025-05-28 10:19:04 -0400

    fix(ExpandableSection): made animation opt-in for detached variant

---
+++ packages/react-core/src/components/ExpandableSection/__tests__/ExpandableSectionToggle.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { ExpandableSectionToggle } from '../ExpandableSectionToggle';
+import styles from '@patternfly/react-styles/css/components/ExpandableSection/expandable-section';
+
+test('Renders without children', () => {
+  render(<ExpandableSectionToggle></ExpandableSectionToggle>);
+
+  expect(screen.getByRole('button')).toBeInTheDocument();
+});
+
+test('Renders with children', () => {
+  render(<ExpandableSectionToggle>Toggle test</ExpandableSectionToggle>);
+
+  expect(screen.getByRole('button')).toHaveTextContent('Toggle test');
+});
+
+test('Renders with class pf-m-detached by default', () => {
+  render(<ExpandableSectionToggle data-testid="section-wrapper">Toggle test</ExpandableSectionToggle>);
+
+  expect(screen.getByTestId('section-wrapper')).toHaveClass('pf-m-detached');
+});
+
+test('Does not render with class pf-m-detached when isDetached is false', () => {
+  render(
+    <ExpandableSectionToggle isDetached={false} data-testid="section-wrapper">
+      Toggle test
+    </ExpandableSectionToggle>
+  );
+
+  expect(screen.getByTestId('section-wrapper')).not.toHaveClass('pf-m-detached');
+});
