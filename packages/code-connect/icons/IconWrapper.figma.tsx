e7a36b6b978bed2f8c1817a1bd99b93587afcbab
Matt Nolting <matthew.nolting@gmail.com>
2025-06-03 13:43:33 -0400

    feat(CC-icon): moved CC up one level, addressed code review comments

---
+++ packages/code-connect/icons/IconWrapper.figma.tsx
@@ -0,0 +1,31 @@
+import figma from '@figma/code-connect';
+import { Icon } from '@patternfly/react-core';
+
+figma.connect(
+  Icon,
+  'https://www.figma.com/design/VMEX8Xg2nzhBX8rfBx53jp/branch/9QzcMKMR7FX7HXlc0sRUSp/PatternFly-6--Components?node-id=198-1573&t=dP3Et2mI3FtI6IEP-11',
+  {
+    props: {
+      // // Icon instance from Figma
+      // icon: figma.instance('🖼️ Icon'),
+
+      // Comprehensive icon size mapping
+      size: figma.enum('Icon Size', {
+        'Body - Small': 'bodySm',
+        'Body - Default': 'bodyDefault',
+        'Body - Large': 'bodyLg',
+
+        // Heading Sizes
+        'Heading - H1': 'heading_3xl',
+        'Heading - H2': 'heading_2xl',
+        'Heading - H3': 'headingXl',
+        'Heading - H4': 'headingLg',
+        'Heading - H5': 'headingMd',
+        'Heading - H6': 'headingSm'
+      }),
+      children: figma.children('*')
+    },
+
+    example: (props) => <Icon size={props.size}>{props.children}</Icon>
+  }
+);
