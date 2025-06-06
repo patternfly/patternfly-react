521f26c0eb6dee5272c08cef84a672bfa1e77050
Eric Olkowski <thatblindgeye@gmail.com>
2025-06-06 08:58:43 -0400

    Updated existing tests

--- packages/react-core/src/components/Button/__tests__/Button.test.tsx
+++ packages/react-core/src/components/Button/__tests__/Button.test.tsx
@@ -2,17 +2,22 @@ import { render, screen } from '@testing-library/react';
 import { Button, ButtonState, ButtonVariant } from '../Button';
 import styles from '@patternfly/react-styles/css/components/Button/button';
 
+const { primary, secondary, tertiary, danger, warning, link, plain, control, stateful } = ButtonVariant;
+const validDangerVariants = [secondary, link];
+const invalidDangerVariants = [primary, tertiary, warning, plain, control, stateful];
+const invalidInlineVariants = [primary, secondary, tertiary, danger, warning, plain, control, stateful];
+
 Object.values(ButtonVariant).forEach((variant) => {
   if (variant !== 'primary') {
-    test(`Does not render with class pf-m-${variant} by default`, () => {
+    test(`Does not render with class ${styles.modifiers[variant]} by default`, () => {
       render(<Button>Not {variant} Button</Button>);
-      expect(screen.getByRole('button')).not.toHaveClass(`pf-m-${variant}`);
+      expect(screen.getByRole('button')).not.toHaveClass(styles.modifiers[variant]);
     });
   }
 
-  test(`Renders with class pf-m-${variant} when variant=${variant}`, () => {
+  test(`Renders with class ${styles.modifiers[variant]} when variant=${variant}`, () => {
     render(<Button variant={variant}>{variant} Button</Button>);
-    expect(screen.getByRole('button')).toHaveClass(`pf-m-${variant}`);
+    expect(screen.getByRole('button')).toHaveClass(styles.modifiers[variant]);
   });
 });
 
@@ -26,14 +31,14 @@ test('Renders without children', () => {
   expect(screen.getByTestId('container').firstChild).toBeVisible();
 });
 
-test('Renders with class pf-v6-c-button by default', () => {
+test(`Renders with class ${styles.button} by default`, () => {
   render(<Button>Button</Button>);
-  expect(screen.getByRole('button')).toHaveClass('pf-v6-c-button');
+  expect(screen.getByRole('button')).toHaveClass(styles.button);
 });
 
-test('Renders with class pf-m-primary by default', () => {
+test(`Renders with class ${styles.modifiers.primary} by default`, () => {
   render(<Button>Button</Button>);
-  expect(screen.getByText('Button').parentElement).toHaveClass('pf-m-primary');
+  expect(screen.getByText('Button').parentElement).toHaveClass(styles.modifiers.primary);
 });
 
 test('Renders with custom class', () => {
@@ -48,28 +53,28 @@ test('Renders with an aria-label', () => {
   expect(screen.getByLabelText(label)).toHaveAccessibleName('aria-label test');
 });
 
-test('Renders with class pf-m-block when isBlock = true', () => {
+test(`Renders with class ${styles.modifiers.block} when isBlock = true`, () => {
   render(<Button isBlock>Block Button</Button>);
-  expect(screen.getByRole('button')).toHaveClass('pf-m-block');
+  expect(screen.getByRole('button')).toHaveClass(styles.modifiers.block);
 });
 
-test('Renders with class pf-m-clicked when isClicked = true', () => {
+test(`Renders with class ${styles.modifiers.clicked} when isClicked = true`, () => {
   render(<Button isClicked>Clicked Button</Button>);
-  expect(screen.getByRole('button')).toHaveClass('pf-m-clicked');
+  expect(screen.getByRole('button')).toHaveClass(styles.modifiers.clicked);
 });
 
-test('Does not render with class pf-m-disabled by default when isDisabled = true', () => {
+test(`Does not render with class ${styles.modifiers.disabled} by default when isDisabled = true`, () => {
   render(<Button isDisabled>Disabled Button</Button>);
-  expect(screen.getByRole('button')).not.toHaveClass('pf-m-disabled');
+  expect(screen.getByRole('button')).not.toHaveClass(styles.modifiers.disabled);
 });
 
-test('Renders with class pf-m-disabled when isDisabled = true and component is not a button', () => {
+test(`Renders with class ${styles.modifiers.disabled} when isDisabled = true and component is not a button`, () => {
   render(
     <Button isDisabled component="a">
       Disabled Anchor Button
     </Button>
   );
-  expect(screen.getByText('Disabled Anchor Button').parentElement).toHaveClass('pf-m-disabled');
+  expect(screen.getByText('Disabled Anchor Button').parentElement).toHaveClass(styles.modifiers.disabled);
 });
 
 test(`aria-disabled and class ${styles.modifiers.ariaDisabled} are not rendered when isAriaDisabled is not passed by default`, () => {
@@ -105,120 +110,117 @@ test('Does not disable button when isDisabled = true and component = a', () => {
   expect(screen.getByText('Disabled yet focusable button')).not.toHaveProperty('disabled');
 });
 
-test('Renders with class pf-m-unread by default when variant = stateful', () => {
+test(`Renders with class ${styles.modifiers.unread} by default when variant = stateful`, () => {
   render(<Button variant="stateful">Stateful Button</Button>);
-  expect(screen.getByRole('button')).toHaveClass('pf-m-stateful', 'pf-m-unread');
+  expect(screen.getByRole('button')).toHaveClass(styles.modifiers.unread);
 });
 
 Object.values(ButtonState).forEach((state) => {
-  test(`Renders with class pf-m-${state} when state = ${state} and variant = stateful`, () => {
+  test(`Renders with class ${styles.modifiers[state]} when state = ${state} and variant = stateful`, () => {
     render(
       <Button variant="stateful" state={state}>
         Stateful Button - {state}
       </Button>
     );
-    expect(screen.getByRole('button')).toHaveClass('pf-m-stateful', `pf-m-${state}`);
+    expect(screen.getByRole('button')).toHaveClass(styles.modifiers[state]);
   });
 });
 
-test('Renders with class pf-m-danger when isDanger = true and variant = secondary', () => {
-  render(
-    <Button variant="secondary" isDanger>
-      Danger Button
-    </Button>
-  );
-  expect(screen.getByRole('button')).toHaveClass('pf-m-danger', 'pf-m-secondary');
+Object.values(validDangerVariants).forEach((validDangerVariant) => {
+  test(`Renders with class ${styles.modifiers.danger} when isDanger is true and variant = ${validDangerVariant}`, () => {
+    render(
+      <Button variant={validDangerVariant} isDanger>
+        Danger Button
+      </Button>
+    );
+    expect(screen.getByRole('button')).toHaveClass(styles.modifiers.danger);
+  });
 });
 
-test('Renders with class pf-m-danger when isDanger = true and variant = link', () => {
-  render(
-    <Button variant="link" isDanger>
-      Danger Button
-    </Button>
-  );
-  expect(screen.getByRole('button')).toHaveClass('pf-m-danger', 'pf-m-link');
+Object.values(invalidDangerVariants).forEach((invalidDangerVariant) => {
+  test(`Does not render with class ${styles.modifiers.danger} when isDanger is true and variant = ${invalidDangerVariant}`, () => {
+    render(
+      <Button variant={invalidDangerVariant} isDanger>
+        Danger Button
+      </Button>
+    );
+    expect(screen.getByRole('button')).not.toHaveClass(styles.modifiers.danger);
+  });
 });
 
-test('Does not render with class pf-m-danger when isDanger = true and variant != secondary or link', () => {
-  render(
-    <Button variant="primary" isDanger>
-      Danger Button
-    </Button>
-  );
-  expect(screen.getByRole('button')).not.toHaveClass('pf-m-danger');
-});
-
-test('Does not render with class pf-m-danger when isDanger = true and variant = tertiary', () => {
-  render(
-    <Button variant="tertiary" isDanger>
-      Danger Button
-    </Button>
-  );
-  expect(screen.getByRole('button')).not.toHaveClass('pf-m-danger');
-});
-
-test('Does not render with class pf-m-danger when isDanger = true and variant = control', () => {
-  render(
-    <Button variant="control" isDanger>
-      Danger Button
-    </Button>
-  );
-  expect(screen.getByRole('button')).not.toHaveClass('pf-m-danger');
-});
-
-test('Renders with class pf-m-inline when isInline = true and variant = link', () => {
+test(`Renders with class ${styles.modifiers.inline} when isInline = true and variant = link`, () => {
   render(
     <Button variant={ButtonVariant.link} isInline>
-      Hovered Button
+      Inline Button
     </Button>
   );
-  expect(screen.getByRole('button')).toHaveClass('pf-m-inline');
+  expect(screen.getByRole('button')).toHaveClass(styles.modifiers.inline);
 });
 
-test('Renders with class pf-m-small when size = sm', () => {
+Object.values(invalidInlineVariants).forEach((invalidInlineVariant) => {
+  test(`Does not render with class ${styles.modifiers.inline} when isInline is true and variant = ${invalidInlineVariants}`, () => {
+    render(
+      <Button variant={invalidInlineVariants} isInline>
+        Inline Button
+      </Button>
+    );
+    expect(screen.getByRole('button')).not.toHaveClass(styles.modifiers.inline);
+  });
+});
+
+test(`Renders with class ${styles.modifiers.small} when size = sm`, () => {
   render(<Button size="sm">Small Button</Button>);
-  expect(screen.getByRole('button')).toHaveClass('pf-m-small');
+  expect(screen.getByRole('button')).toHaveClass(styles.modifiers.small);
 });
 
-test('Renders with class pf-m-display-lg when size = lg', () => {
+test(`Renders with class ${styles.modifiers.displayLg} when size = lg`, () => {
   render(<Button size="lg">Large Button</Button>);
-  expect(screen.getByRole('button')).toHaveClass('pf-m-display-lg');
+  expect(screen.getByRole('button')).toHaveClass(styles.modifiers.displayLg);
 });
 
-test('Renders with class pf-m-in-progress when isLoading = true', () => {
+test(`Renders with classes ${styles.modifiers.inProgress} when isLoading = true`, () => {
   render(
     <Button isLoading spinnerAriaValueText="Loading">
       Loading Button
     </Button>
   );
-  expect(screen.getByRole('button')).toHaveClass('pf-m-in-progress');
+  expect(screen.getByRole('button')).toHaveClass(styles.modifiers.inProgress);
 });
 
-test('Renders with class pf-m-progress when isLoading is defined and isLoading = false', () => {
+test(`Renders with class ${styles.modifiers.progress} when isLoading is true`, () => {
+  render(
+    <Button isLoading spinnerAriaValueText="Loading">
+      Loading Button
+    </Button>
+  );
+  expect(screen.getByRole('button')).toHaveClass(styles.modifiers.progress);
+});
+
+test(`Renders with class ${styles.modifiers.progress} when isLoading is defined and isLoading = false`, () => {
   render(
     <Button isLoading={false} spinnerAriaValueText="Loading">
       Loading Button
     </Button>
   );
-  expect(screen.getByRole('button')).toHaveClass('pf-m-progress');
+  expect(screen.getByRole('button')).toHaveClass(styles.modifiers.progress);
 });
 
-test('Renders without class pf-m-progress when isLoading = false and variant = plain', () => {
+test(`Renders without class ${styles.modifiers.progress} when isLoading = false and variant = plain`, () => {
   render(
     <Button variant="plain" isInline isLoading={false} aria-label="Loading" spinnerAriaValueText="Loading">
       Loading Button
     </Button>
   );
-  expect(screen.getByRole('button')).not.toHaveClass('pf-m-progress');
+  expect(screen.getByRole('button')).not.toHaveClass(styles.modifiers.progress);
 });
 
-test('Renders custom icon with class pf-m-in-progress when isLoading = true and icon is present', () => {
+test(`Renders custom icon with class ${styles.modifiers.inProgress} when isLoading = true and icon is present`, () => {
   render(
     <Button variant="plain" isLoading aria-label="Upload" spinnerAriaValueText="Loading" icon={<div>ICON</div>} />
   );
 
   expect(screen.getByText('ICON')).toBeVisible();
-  expect(screen.getByText('ICON').parentElement?.parentElement).toHaveClass('pf-m-in-progress');
+  expect(screen.getByText('ICON').parentElement?.parentElement).toHaveClass(styles.modifiers.inProgress);
 });
 
 test('Renders as custom component when component is passed', () => {
