ceb380e05c4901b8eea42d7db2f526d6f60ae201
Matt Nolting <matthew.nolting@gmail.com>
2025-06-11 09:10:41 -0400

    feat(CC-batch-4): updated props and comments

--- packages/code-connect/components/DescriptionList/DescriptionList.figma.tsx
+++ packages/code-connect/components/DescriptionList/DescriptionList.figma.tsx
@@ -1,26 +1,27 @@
 import { DescriptionList } from '@patternfly/react-core';
 import figma from '@figma/code-connect';
 
-/**
- * PatternFly DescriptionList component integration for Figma Code Connect
- */
-
 figma.connect(
   DescriptionList,
-  'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=1121-3946&m=dev',
+  'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=1121-3946',
   {
     props: {
       // enum
-      isCompact: figma.enum('Spacing', { Compact: true }),
-      isFluid: figma.enum('Orientation', { 'Horizontal - Fluid': true }),
+      isCompact: figma.enum('Spacing', {
+        Compact: true
+      }),
       isHorizontal: figma.enum('Orientation', {
         'Horizontal - Fixed': true,
+        'Horizontal - Fluid': true
+      }),
+      isFluid: figma.enum('Orientation', {
         'Horizontal - Fluid': true
       }),
 
       children: figma.children('*')
     },
     example: (props) => (
+      // Documentation for DescriptionList can be found at https://www.patternfly.org/components/description-list
       <DescriptionList isFluid={props.isFluid} isCompact={props.isCompact} isHorizontal={props.isHorizontal}>
         {props.children}
       </DescriptionList>
