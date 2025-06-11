7e28b0ad0eadecf8ff0486ed5e2ee68cf75f3c94
Matt Nolting <matthew.nolting@gmail.com>
2025-06-11 08:12:27 -0400

    feat(CC-batch-3): updated urls, added required

--- packages/code-connect/components/CodeBlock/CodeBlock.figma.tsx
+++ packages/code-connect/components/CodeBlock/CodeBlock.figma.tsx
@@ -1,18 +1,31 @@
 import figma from '@figma/code-connect';
-import { CodeBlock } from '@patternfly/react-core';
-
-/**
- * PatternFly CodeBlock component integration for Figma Code Connect
- */
+import { CodeBlock, ExpandableSectionToggle } from '@patternfly/react-core';
 
 figma.connect(
   CodeBlock,
-  'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=9802-5857&t=IzSunfrnw18ti37Y-11',
+  'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=9802-5857',
   {
     props: {
-      isExpandable: figma.boolean('Expandable'),
-      children: figma.children('*')
+      // TODO: FIGMA: configure options for actions
+      actions: `action1, action2, action3, action4, action5`,
+
+      // TODO: FIGMA: configure CodeBlockCode
+      expandableSection: figma.boolean('Expandable', {
+        true: (
+          <ExpandableSectionToggle ontoggle={() => {}} contentId="code-block-id" direction="up">
+            {isExpanded ? 'Show Less' : 'Show More'}
+          </ExpandableSectionToggle>
+        ),
+        false: undefined
+      })
+      // children: figma.children('*')
     },
-    example: (props) => <CodeBlock>{props.children}</CodeBlock>
+    example: (props) => (
+      // Documentation for CodeBlock can be found at https://www.patternfly.org/components/code-block
+      <CodeBlock actions={props.actions}>
+        <CodeBlock>{/* {props.codeBlockCode} */}</CodeBlock>
+        {props.expandableSection}
+      </CodeBlock>
+    )
   }
 );
