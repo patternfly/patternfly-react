a1d09982b7b825844c7029384a2910dab985fae3
Matt Nolting <matthew.nolting@gmail.com>
2025-06-13 10:12:58 -0400

    feat(CC-batch-3): updated code editor config

--- packages/code-connect/components/CodeBlock/CodeBlock.figma.tsx
+++ packages/code-connect/components/CodeBlock/CodeBlock.figma.tsx
@@ -1,29 +1,33 @@
 import figma from '@figma/code-connect';
-import { CodeBlock, ExpandableSectionToggle } from '@patternfly/react-core';
+import { CodeBlock, CodeBlockCode, ExpandableSectionToggle } from '@patternfly/react-core';
 
 figma.connect(
   CodeBlock,
-  'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=9802-5857',
+  'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=9802-5857&m=dev',
   {
     props: {
-      // TODO: FIGMA: configure options for actions
+      // TODO: FIGMA: Configure Editor Text as a base component for CodeBlock
+      // Currently using placeholder string, but should be a multi-select property with options like "Copy", "Download", "Edit", "Share", "Delete"
+      editorText: 'code block code',
+
+      // TODO: FIGMA: Configure Actions layer to contain CodeBlockAction components
+      // Currently using placeholder string, but should be a multi-select property with options like "Copy", "Download", "Edit", "Share", "Delete"
       actions: `action1, action2, action3, action4, action5`,
 
-      // TODO: FIGMA: configure CodeBlockCode
+      // TODO: FIGMA: ExpandableSectionToggle needs proper configuration
+      // Current implementation has undefined variables and function calls
       expandableSection: figma.boolean('Expandable', {
         true: (
-          <ExpandableSectionToggle ontoggle={() => {}} contentId="code-block-id" direction="up">
-            {isExpanded ? 'Show Less' : 'Show More'}
+          <ExpandableSectionToggle onToggle={() => {}} contentId="code-block-id">
+            Show More
           </ExpandableSectionToggle>
         ),
         false: undefined
       })
-      // children: figma.children('*')
     },
     example: (props) => (
-      // Documentation for CodeBlock can be found at https://www.patternfly.org/components/code-block
       <CodeBlock actions={props.actions}>
-        <CodeBlock>{/* {props.codeBlockCode} */}</CodeBlock>
+        <CodeBlockCode>{props.editorText}</CodeBlockCode>
         {props.expandableSection}
       </CodeBlock>
     )
