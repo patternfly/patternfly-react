300bfd79b056bee47fe32c2c71db118939b78577
Eric Olkowski <thatblindgeye@gmail.com>
2025-06-13 14:06:28 -0400

    Updated hamburgericon class to styles object

--- packages/react-core/src/components/Button/hamburgerIcon.tsx
+++ packages/react-core/src/components/Button/hamburgerIcon.tsx
@@ -4,7 +4,7 @@ import { css } from '@patternfly/react-styles';
 // Because this is such a specific icon that requires being wrapped in a pf-v[current version]-c-button element,
 // we don't want to export this to consumers nor include it in the react-icons package as a custom icon.
 export const hamburgerIcon = (
-  <svg viewBox="0 0 10 10" className="pf-v6-c-button--hamburger-icon pf-v6-svg" width="1em" height="1em">
+  <svg viewBox="0 0 10 10" className={css(styles.buttonHamburgerIcon, 'pf-v6-svg')} width="1em" height="1em">
     <path className={css(styles.buttonHamburgerIconTop)} d="M1,1 L9,1"></path>
     <path className={css(styles.buttonHamburgerIconMiddle)} d="M1,5 L9,5"></path>
     <path className={css(styles.buttonHamburgerIconArrow)} d="M1,5 L1,5 L1,5"></path>
