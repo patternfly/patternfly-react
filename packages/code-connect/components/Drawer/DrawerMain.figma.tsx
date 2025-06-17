dfecb036712b7bfc065e0fa8010656fa80b55d91
Matt Nolting <matthew.nolting@gmail.com>
2025-06-17 09:19:09 -0400

    feat(CC-batch-4): updated required props

---
+++ packages/code-connect/components/Drawer/DrawerMain.figma.tsx
@@ -0,0 +1,13 @@
+import figma from '@figma/code-connect';
+import { DrawerContent } from '@patternfly/react-core';
+
+figma.connect(
+  DrawerContent,
+  'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=35586-211216&m=dev',
+  {
+    props: {
+      children: figma.children('*')
+    },
+    example: (props) => <DrawerContent>{props.children}</DrawerContent>
+  }
+);
