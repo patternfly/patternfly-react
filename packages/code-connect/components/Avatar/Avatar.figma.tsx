a639b16c17a804d71de999dfcc1d233539f0c905
Matt Nolting <matthew.nolting@gmail.com>
2025-06-17 09:13:11 -0400

    feat(CC-batch-2): updated required props

--- packages/code-connect/components/Avatar/Avatar.figma.tsx
+++ packages/code-connect/components/Avatar/Avatar.figma.tsx
@@ -1,15 +1,17 @@
 import figma from '@figma/code-connect';
 import { Avatar } from '@patternfly/react-core';
 
+// Documentation for Avatar can be found at https://www.patternfly.org/components/avatar
+
 figma.connect(
   Avatar,
   'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6%3A-Components-Test?node-id=1561-4342',
   {
     props: {
-      // booleans
+      // boolean
       isBordered: figma.boolean('Bordered'),
 
-      // enums
+      // enum
       size: figma.enum('Size', {
         small: 'sm',
         med: 'md',
@@ -18,7 +20,6 @@ figma.connect(
       })
     },
     example: (props) => (
-      // Documentation for Avatar can be found at https://www.patternfly.org/components/avatar
       <Avatar alt="Avatar" src="/assets/images/avatar.svg" isBordered={props.isBordered} size={props.size} />
     )
   }
