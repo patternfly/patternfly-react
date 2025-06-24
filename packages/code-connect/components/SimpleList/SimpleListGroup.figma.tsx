beb486ff630183a4ecc84a384be26d744d19df51
Matt Nolting <matthew.nolting@gmail.com>
2025-06-24 11:20:20 -0400

    feat(CC-batch-7): verification complete

--- packages/code-connect/components/SimpleList/SimpleListGroup.figma.tsx
+++ packages/code-connect/components/SimpleList/SimpleListGroup.figma.tsx
@@ -1,6 +1,8 @@
 import figma from '@figma/code-connect';
 import { SimpleListGroup } from '@patternfly/react-core';
 
+// Documentation for SimpleListGroup can be found at https://www.patternfly.org/components/simple-list
+
 figma.connect(
   SimpleListGroup,
   'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=4410-20708',
@@ -8,9 +10,6 @@ figma.connect(
     props: {
       children: figma.children('*')
     },
-    example: (props) => (
-      // Documentation for SimpleListGroup can be found at https://www.patternfly.org/components/simple-list
-      <SimpleListGroup>{props.children}</SimpleListGroup>
-    )
+    example: (props) => <SimpleListGroup>{props.children}</SimpleListGroup>
   }
 );
