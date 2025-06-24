21b5732399f7043b14ab24ee6077fe1f83dfd4ea
Matt Nolting <matthew.nolting@gmail.com>
2025-06-24 11:19:14 -0400

    feat(CC-batch-6): verification complete

--- packages/code-connect/components/JumpLinks/JumplinksVertical.figma.tsx
+++ packages/code-connect/components/JumpLinks/JumplinksVertical.figma.tsx
@@ -1,21 +1,22 @@
 import figma from '@figma/code-connect';
 import { JumpLinks } from '@patternfly/react-core';
 
+// Documentation for JumpLinks can be found at https://www.patternfly.org/components/jump-links
+
 figma.connect(
   JumpLinks,
-  'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=5426-8542&t=IzSunfrnw18ti37Y-11',
+  'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=5426-8542',
   {
     props: {
       // boolean
       label: figma.boolean('Show Label', {
-        true: figma.children('Label'),
+        true: 'Vertical jump links',
         false: undefined
       }),
 
       children: figma.children('*')
     },
     example: (props) => (
-      // Documentation for JumpLinks can be found at https://www.patternfly.org/components/jump-links
       <JumpLinks isVertical label={props.label}>
         {props.children}
       </JumpLinks>
--- packages/code-connect/components/Label/LabelStatus.figma.tsx
+++ packages/code-connect/components/Label/LabelStatus.figma.tsx
@@ -1,28 +1,33 @@
 import figma from '@figma/code-connect';
 import { Label } from '@patternfly/react-core';
 
+// Documentation for Label can be found at https://www.patternfly.org/components/label
+
 figma.connect(
   Label,
   'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=2800-488',
   {
     props: {
+      // string
       text: figma.string('Text'),
-      status: figma.enum('State', {
+      children: figma.children('*'),
+
+      // enum
+      isCompact: figma.enum('Size', { Compact: true }),
+      variant: figma.enum('Type', {
+        Filled: 'filled',
+        Outlined: 'outline'
+      }),
+      status: figma.enum('Status', {
         Danger: 'danger',
         Warning: 'warning',
         Success: 'success',
         Info: 'info',
         Custom: 'custom'
-      }),
-      type: figma.enum('Type', {
-        Filled: 'filled',
-        Outlined: 'outline'
-      }),
-      isCompact: figma.enum('Size', { Compact: true })
+      })
     },
     example: (props) => (
-      // Documentation for Label can be found at https://www.patternfly.org/components/label
-      <Label isCompact={props.isCompact} status={props.status} variant={props.type}>
+      <Label isCompact={props.isCompact} status={props.status} variant={props.variant}>
         {props.text}
       </Label>
     )
