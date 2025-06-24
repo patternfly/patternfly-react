77dea8d7c5ad8e9e8922301ebc9c2770a9c0dcc7
Matt Nolting <matthew.nolting@gmail.com>
2025-06-24 11:14:37 -0400

    feat(CC-batch-3): verification complete

--- packages/code-connect/components/Brand/MastheadLogo.figma.tsx
+++ packages/code-connect/components/Brand/MastheadLogo.figma.tsx
@@ -1,19 +1,20 @@
 import figma from '@figma/code-connect';
 import { Brand } from '@patternfly/react-core';
 
+// Documentation for Brand can be found at https://www.patternfly.org/components/brand
+
 figma.connect(
   Brand,
   'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=2104-3644',
   {
     example: () => (
-      // Documentation for Brand can be found at https://www.patternfly.org/components/brand
-      <Brand src={`pfLogo`} alt="Patternfly" widths={{ default: '40px', sm: '60px', md: '220px' }}>
-        <source media="(min-width: 1200px)" srcSet={`pfLogo`} />
-        <source media="(min-width: 992px)" srcSet={`pfLogo`} />
-        <source media="(min-width: 768px)" srcSet={`pfLogo`} />
-        <source media="(min-width: 576px)" srcSet={`pfLogo`} />
-        <source media="(min-width: 320px)" srcSet={`pfLogo`} />
-        <source srcSet={`pfLogo`} />
+      <Brand src="<path-to-logo>" alt="Patternfly" widths={{ default: '40px', sm: '60px', md: '220px' }}>
+        <source media="(min-width: 1200px)" srcSet="<path-to-logo>" />
+        <source media="(min-width: 992px)" srcSet="<path-to-logo>" />
+        <source media="(min-width: 768px)" srcSet="<path-to-logo>" />
+        <source media="(min-width: 576px)" srcSet="<path-to-logo>" />
+        <source media="(min-width: 320px)" srcSet="<path-to-logo>" />
+        <source srcSet="<path-to-logo>" />
       </Brand>
     )
   }
