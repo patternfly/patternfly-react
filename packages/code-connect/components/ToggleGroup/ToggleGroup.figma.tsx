a31b48b37b5d0e733bb9ae27774915a72fba28c8
Matt Nolting <matthew.nolting@gmail.com>
2025-06-24 11:21:21 -0400

    feat(CC-batch-8): verification complete

--- packages/code-connect/components/ToggleGroup/ToggleGroup.figma.tsx
+++ packages/code-connect/components/ToggleGroup/ToggleGroup.figma.tsx
@@ -1,6 +1,8 @@
 import figma from '@figma/code-connect';
 import { ToggleGroup } from '@patternfly/react-core';
 
+// Documentation for ToggleGroup can be found at https://www.patternfly.org/components/toggle-group
+
 figma.connect(
   ToggleGroup,
   'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=7644-56919',
