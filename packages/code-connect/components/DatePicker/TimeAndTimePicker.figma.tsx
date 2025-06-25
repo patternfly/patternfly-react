36a2a9681c966a2b4212f5659a71fc60d62c8b23
Matt Nolting <matthew.nolting@gmail.com>
2025-06-24 22:29:08 -0400

    feat(CC-date-and-time): added and validated date/time picker

---
+++ packages/code-connect/components/DatePicker/TimeAndTimePicker.figma.tsx
@@ -0,0 +1,15 @@
+import figma from '@figma/code-connect';
+import { TimePicker } from '@patternfly/react-core';
+
+// Documentation for TimePicker can be found at https://www.patternfly.org/components/time-picker
+
+figma.connect(
+  TimePicker,
+  'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=7958-136824',
+  {
+    props: {
+      children: figma.children('*')
+    },
+    example: (props) => <TimePicker>{props.children}</TimePicker>
+  }
+);
