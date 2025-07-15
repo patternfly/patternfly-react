c8cd2b374d9970cbce7aafbc5d0984015a455d6b
Matt Nolting <matthew.nolting@gmail.com>
2025-07-15 15:22:31 -0400

    feat(CC-batch-4): review-athon round 2 followup

--- packages/code-connect/components/DescriptionList/DescriptionListInCard.figma.tsx
+++ packages/code-connect/components/DescriptionList/DescriptionListInCard.figma.tsx
@@ -1,8 +1,10 @@
 import figma from '@figma/code-connect';
 import { Card, DescriptionList, DescriptionListTerm, DescriptionListDescription } from '@patternfly/react-core';
 
+// Note: DescriptionList displaySize prop is set by displaySize.
 // Note: DescriptionList label and description are not dynamic. DescriptionList list label composition doesn't currently align with the React component.
 // Note: DescriptionListLabel requires updates to the React component to support dynamic composition.
+// TODO: DESIGN: Separate card and description list size properties.
 // Documentation for DescriptionList can be found at https://www.patternfly.org/components/description-list-in-card
 
 figma.connect(
@@ -10,14 +12,21 @@ figma.connect(
   'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=4421-28234&m=dev',
   {
     props: {
+      displaySize: figma.enum('Card size', {
+        Small: 'default',
+        Default: 'lg',
+        Large: '2xl'
+      }),
+
       isCompact: figma.enum('Card size', { Small: true }),
       isLarge: figma.enum('Card size', { Large: true }),
+
       isHorizontal: figma.enum('Orientation', { Horizontal: true }),
 
       children: figma.children('*')
     },
     example: (props) => (
-      <DescriptionList isHorizontal={props.isHorizontal}>
+      <DescriptionList isHorizontal={props.isHorizontal} displaySize={props.displaySize}>
         <Card component="div" isCompact={props.isCompact} isLarge={props.isLarge}>
           <DescriptionListTerm>Input label</DescriptionListTerm>
           <DescriptionListDescription>Description list item</DescriptionListDescription>
