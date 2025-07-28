b5306aee54020a44c2d8288dee5a41062f5f9247
Nicole Thoen <nthoen@redhat.com>
2025-07-28 16:50:17 -0400

    Update upload-preview.mjs

--- .github/upload-preview.mjs
+++ .github/upload-preview.mjs
@@ -18,7 +18,7 @@ if (!uploadFolder) {
 }
 
 const uploadFolderName = path.basename(uploadFolder);
-let uploadURL = `${repo}-${prnum ? `pr-${prnum}` : prbranch}`.replace(/[/|.]/g, '-');
+let uploadURL = `pf-react-${prnum ? `pr-${prnum}` : prbranch}`.replace(/[/|.]/g, '-');
 
 switch (uploadFolderName) {
   case 'coverage':
