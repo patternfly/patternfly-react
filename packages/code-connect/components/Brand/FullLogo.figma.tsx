c3b3ab0ea696c4ebc6442365e0c33597b2b144a6
Matt Nolting <matthew.nolting@gmail.com>
2025-07-30 15:47:05 -0400

    feat(CC-batch-3): added batch 3 (#11829)
    * feat(CC-batch-3): added batch 3
    
    * feat(CC-batch-3): lint fix
    
    * feat(CC-batch-3): moved directory, updated for review
    
    * feat(CC-batch-3): removed previous codeConnect dir from react-core
    
    * feat(CC-batch-3): updated urls, added required
    
    * feat(CC-batch-3): added code and children to codeEditor
    
    * feat(CC-batch-2): updated breadcrumb
    
    * feat(CC-batch-3): updated code editor config
    
    * feat(CC-batch-3): updated required props
    
    * feat(CC-batch-3): verification complete
    
    * feat(CC-batch-3): added sr text to status banner
    
    * feat(CC-batch-3): review-athon round 1 followup
    
    * feat(CC-batch-3): review-athon round 2 followup
    
    * feat(CC-batch-3): removed redundant dropdown itesm

---
+++ packages/code-connect/components/Brand/FullLogo.figma.tsx
@@ -0,0 +1,14 @@
+import figma from '@figma/code-connect';
+import { Brand } from '@patternfly/react-core';
+
+// Documentation for Brand can be found at https://www.patternfly.org/components/brand
+
+figma.connect(
+  Brand,
+  'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=2104-3689',
+  {
+    example: () => (
+      <Brand src="<path-to-logo>" alt="Patternfly" widths={{ default: '200px', md: '400px', xl: '600px' }} />
+    )
+  }
+);
