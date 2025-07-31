0373c15bb7d43a50837781a2d52cb0ecbc8d547a
Matt Nolting <matthew.nolting@gmail.com>
2025-07-31 15:29:56 -0400

    feat(CC-batch-5): group review 4 followup

--- packages/code-connect/components/DatePicker/CalendarMonth.figma.tsx
+++ packages/code-connect/components/DatePicker/CalendarMonth.figma.tsx
@@ -1,16 +1,7 @@
 import figma from '@figma/code-connect';
-import { CalendarMonth, CalendarMonthInlineProps, Title } from '@patternfly/react-core';
+import { CalendarMonth, Title } from '@patternfly/react-core';
 
 // Documentation for CalendarMonth can be found at https://www.patternfly.org/components/calendar-month
-const inlineProps: CalendarMonthInlineProps = {
-  component: 'article',
-  title: (
-    <Title headingLevel="h4" id="favorite-date">
-      Select your favorite date
-    </Title>
-  ),
-  ariaLabelledby: 'favorite-date'
-};
 
 figma.connect(
   CalendarMonth,
@@ -20,7 +11,12 @@ figma.connect(
       date: '2025-06-16'
     },
     example: (props) => (
-      <CalendarMonth date={props.date} inlineProps={inlineProps} onChange={() => {}} onMonthChange={() => {}} />
+      <CalendarMonth
+        date={props.date}
+        inlineProps={{ component: 'article', ariaLabelledby: 'favorite-date' }}
+        onChange={() => {}}
+        onMonthChange={() => {}}
+      />
     )
   }
 );
@@ -31,17 +27,22 @@ figma.connect(
   {
     variant: { Type: 'With date range' },
     props: {
-      startDate: new Date(2020, 10, 11),
-      endDate: new Date(2020, 10, 24)
+      rangeStart: new Date(2020, 10, 11),
+      date: new Date(2020, 10, 24)
     },
     example: (props) => (
-      <CalendarMonth
-        date={props.startDate}
-        inlineProps={inlineProps}
-        onChange={() => {}}
-        onMonthChange={() => {}}
-        rangeStart={props.endDate}
-      />
+      <>
+        <Title headingLevel="h4" id="favorite-date">
+          Select your favorite date
+        </Title>
+        <CalendarMonth
+          date={props.date}
+          inlineProps={{ component: 'article', ariaLabelledby: 'favorite-date' }}
+          onChange={() => {}}
+          onMonthChange={() => {}}
+          rangeStart={props.rangeStart}
+        />
+      </>
     )
   }
 );
