386b7ef8f66936e45387d81c59db014bb88ee93e
Matt Nolting <matthew.nolting@gmail.com>
2025-09-05 14:45:09 -0400

    feat(CC-batch-7): updated notification group/list

--- packages/code-connect/components/NotificationDrawer/NotificationDrawer.figma.tsx
+++ packages/code-connect/components/NotificationDrawer/NotificationDrawer.figma.tsx
@@ -1,5 +1,32 @@
 import figma from '@figma/code-connect';
-import { NotificationDrawer, NotificationDrawerBody, NotificationDrawerList } from '@patternfly/react-core';
+import {
+  NotificationDrawer,
+  NotificationDrawerBody,
+  NotificationDrawerGroupList,
+  NotificationDrawerList
+} from '@patternfly/react-core';
+
+figma.connect(
+  NotificationDrawer,
+  'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=7172-99015',
+  {
+    variant: { Type: 'Grouped' },
+    props: {
+      // children
+      notificationDrawerHeader: figma.children('Notification drawer header'),
+      notificationDrawerGroup: figma.children('Notification drawer groups')
+    },
+    example: (props) => (
+      // Documentation for NotificationDrawer can be found at https://www.patternfly.org/components/notification-drawer
+      <NotificationDrawer>
+        {props.notificationDrawerHeader}
+        <NotificationDrawerBody>
+          <NotificationDrawerGroupList>{props.notificationDrawerGroup}</NotificationDrawerGroupList>
+        </NotificationDrawerBody>
+      </NotificationDrawer>
+    )
+  }
+);
 
 figma.connect(
   NotificationDrawer,
@@ -8,16 +35,14 @@ figma.connect(
     props: {
       // children
       notificationDrawerHeader: figma.children('Notification drawer header'),
-      notificationDrawerGroup: figma.children('Notification drawer group'),
-      notificationDrawerNotifications: figma.children(['Notifications', 'Notification drawer item'])
+      notificationDrawerItems: figma.children(['Notifications', 'Notification drawer item'])
     },
     example: (props) => (
       // Documentation for NotificationDrawer can be found at https://www.patternfly.org/components/notification-drawer
       <NotificationDrawer>
         {props.notificationDrawerHeader}
         <NotificationDrawerBody>
-          {props.notificationDrawerGroup}
-          <NotificationDrawerList>{props.notificationDrawerNotifications}</NotificationDrawerList>
+          <NotificationDrawerList>{props.notificationDrawerItems}</NotificationDrawerList>
         </NotificationDrawerBody>
       </NotificationDrawer>
     )
