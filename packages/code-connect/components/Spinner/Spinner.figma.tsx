b1d93754eae9c956ef2f5f4a909025a954d48b2a
Matt Nolting <matthew.nolting@gmail.com>
2025-09-14 22:32:46 -0400

    feat(CC-batch-8): refactored toggle-group

--- packages/code-connect/components/Spinner/Spinner.figma.tsx
+++ packages/code-connect/components/Spinner/Spinner.figma.tsx
@@ -3,19 +3,15 @@ import { Spinner } from '@patternfly/react-core';
 
 // Documentation for Spinner can be found at https://www.patternfly.org/components/spinner
 
-figma.connect(
-  Spinner,
-  'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=790-332&t=IzSunfrnw18ti37Y-11',
-  {
-    props: {
-      // enum
-      size: figma.enum('Size', {
-        sm: 'sm',
-        md: 'md',
-        lg: 'lg',
-        xl: 'xl'
-      })
-    },
-    example: (props) => <Spinner aria-label="Loading..." size={props.size} />
-  }
-);
+figma.connect(Spinner, 'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components?node-id=790-332', {
+  props: {
+    // enum
+    size: figma.enum('Size', {
+      sm: 'sm',
+      md: 'md',
+      lg: 'lg',
+      xl: 'xl'
+    })
+  },
+  example: (props) => <Spinner aria-label="Loading..." size={props.size} />
+});
--- packages/code-connect/components/Tabs/HorizontalSubTabs.figma.tsx
+++ packages/code-connect/components/Tabs/HorizontalSubTabs.figma.tsx
@@ -3,27 +3,17 @@ import { Tabs } from '@patternfly/react-core';
 
 // Documentation for Tabs can be found at https://www.patternfly.org/components/tabs
 
-figma.connect(
-  Tabs,
-  'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=14327-6583',
-  {
-    props: {
-      children: figma.children('*'),
-      isBox: figma.enum('Type', {
-        Boxed: true,
-        'Boxed Light': true
-      })
-    },
-    example: (props) => (
-      <Tabs
-        aria-label="Horizontal subtabs example"
-        isSubtab={true}
-        isBox={props.isBox}
-        onSelect={() => {}}
-        role="region"
-      >
-        {props.children}
-      </Tabs>
-    )
-  }
-);
+figma.connect(Tabs, 'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components?node-id=14327-6583', {
+  props: {
+    children: figma.children('*'),
+    isBox: figma.enum('Type', {
+      Boxed: true,
+      'Boxed Light': true
+    })
+  },
+  example: (props) => (
+    <Tabs aria-label="Horizontal subtabs example" isSubtab={true} isBox={props.isBox} onSelect={() => {}} role="region">
+      {props.children}
+    </Tabs>
+  )
+});
