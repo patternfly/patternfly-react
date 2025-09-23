af3eefe8fade6f21c8031aa82895d4d36b9df30f
Matt Nolting <matthew.nolting@gmail.com>
2025-09-23 16:07:37 -0400

    feat(CC-batch-6): group review 9/23 updates

--- packages/code-connect/components/Label/LabelGroups.figma.tsx
+++ packages/code-connect/components/Label/LabelGroups.figma.tsx
@@ -3,14 +3,170 @@ import { Label, LabelGroup } from '@patternfly/react-core';
 
 // Documentation for LabelGroups can be found at https://www.patternfly.org/components/label
 
+const sharedProps = {
+  closableLabels: (
+    <>
+      <Label onClose={() => {}}>Label</Label>
+      <Label onClose={() => {}}>Label</Label>
+      <Label onClose={() => {}}>Label</Label>
+      <Label onClose={() => {}}>Label</Label>
+      <Label onClose={() => {}}>Label</Label>
+      <Label onClose={() => {}}>Label</Label>
+      <Label onClose={() => {}}>Label</Label>
+      <Label onClose={() => {}}>Label</Label>
+      <Label onClose={() => {}}>Label</Label>
+    </>
+  ),
+  editableLabels: (
+    <>
+      <Label
+        color="grey"
+        isCompact
+        onClose={() => {}}
+        closeBtnAriaLabel="Custom close button for compact editable label"
+        onEditCancel={() => {}}
+        onEditComplete={() => {}}
+        isEditable
+        editableProps={{
+          'aria-label': `Editable compact label with text`,
+          id: 'compact-editable-label1'
+        }}
+      >
+        Label
+      </Label>
+      <Label
+        color="grey"
+        isCompact
+        onClose={() => {}}
+        closeBtnAriaLabel="Custom close button for compact editable label"
+        onEditCancel={() => {}}
+        onEditComplete={() => {}}
+        isEditable
+        editableProps={{
+          'aria-label': `Editable compact label with text`,
+          id: 'compact-editable-label1'
+        }}
+      >
+        Label
+      </Label>
+      <Label
+        color="grey"
+        isCompact
+        onClose={() => {}}
+        closeBtnAriaLabel="Custom close button for compact editable label"
+        onEditCancel={() => {}}
+        onEditComplete={() => {}}
+        isEditable
+        editableProps={{
+          'aria-label': `Editable compact label with text`,
+          id: 'compact-editable-label2'
+        }}
+      >
+        Label
+      </Label>
+      <Label
+        color="grey"
+        isCompact
+        onClose={() => {}}
+        closeBtnAriaLabel="Custom close button for compact editable label"
+        onEditCancel={() => {}}
+        onEditComplete={() => {}}
+        isEditable
+        editableProps={{
+          'aria-label': `Editable compact label with text`,
+          id: 'compact-editable-label3'
+        }}
+      >
+        Label
+      </Label>
+      <Label
+        color="grey"
+        isCompact
+        onClose={() => {}}
+        closeBtnAriaLabel="Custom close button for compact editable label"
+        onEditCancel={() => {}}
+        onEditComplete={() => {}}
+        isEditable
+        editableProps={{
+          'aria-label': `Editable compact label with text`,
+          id: 'compact-editable-label4'
+        }}
+      >
+        Label
+      </Label>
+      <Label
+        color="grey"
+        isCompact
+        onClose={() => {}}
+        closeBtnAriaLabel="Custom close button for compact editable label"
+        onEditCancel={() => {}}
+        onEditComplete={() => {}}
+        isEditable
+        editableProps={{
+          'aria-label': `Editable compact label with text`,
+          id: 'compact-editable-label5'
+        }}
+      >
+        Label
+      </Label>
+      <Label
+        color="grey"
+        isCompact
+        onClose={() => {}}
+        closeBtnAriaLabel="Custom close button for compact editable label"
+        onEditCancel={() => {}}
+        onEditComplete={() => {}}
+        isEditable
+        editableProps={{
+          'aria-label': `Editable compact label with text`,
+          id: 'compact-editable-label6'
+        }}
+      >
+        Label
+      </Label>
+      <Label
+        color="grey"
+        isCompact
+        onClose={() => {}}
+        closeBtnAriaLabel="Custom close button for compact editable label"
+        onEditCancel={() => {}}
+        onEditComplete={() => {}}
+        isEditable
+        editableProps={{
+          'aria-label': `Editable compact label with text`,
+          id: 'compact-editable-label7'
+        }}
+      >
+        Label
+      </Label>
+      <Label
+        color="grey"
+        isCompact
+        onClose={() => {}}
+        closeBtnAriaLabel="Custom close button for compact editable label"
+        onEditCancel={() => {}}
+        onEditComplete={() => {}}
+        isEditable
+        editableProps={{
+          'aria-label': `Editable compact label with text`,
+          id: 'compact-editable-label8'
+        }}
+      >
+        Label
+      </Label>
+    </>
+  )
+};
+
 figma.connect(
   LabelGroup,
-  'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=2800-1075',
+  'https://www.figma.com/design/VMEX8Xg2nzhBX8rfBx53jp/PatternFly-6--Components?node-id=2800-1075',
   {
     props: {
+      ...sharedProps,
       // boolean
       numLabels: figma.boolean('Has collapsed labels', {
-        true: 4,
+        true: 6,
         false: undefined
       }),
 
@@ -21,17 +177,7 @@ figma.connect(
             Add label
           </Label>
         )
-      }),
-
-      children: figma.children([
-        'Label in group 1',
-        'Label in group 2',
-        'Label in group 3',
-        'Label in group 4',
-        'Label in group 5',
-        'Label in group 6',
-        'Label Non-status'
-      ])
+      })
     },
     example: (props) => (
       <LabelGroup
@@ -41,7 +187,43 @@ figma.connect(
         numLabels={props.numLabels}
         onClick={() => {}}
       >
-        {props.children}
+        {props.closableLabels}
+      </LabelGroup>
+    )
+  }
+);
+
+figma.connect(
+  LabelGroup,
+  'https://www.figma.com/design/VMEX8Xg2nzhBX8rfBx53jp/PatternFly-6--Components?node-id=2800-1075',
+  {
+    variant: { Type: 'Label management' },
+    props: {
+      ...sharedProps,
+      // boolean
+      numLabels: figma.boolean('Has collapsed labels', {
+        true: 6,
+        false: undefined
+      }),
+
+      // enum
+      addLabelControl: figma.enum('Type', {
+        'Label management': (
+          <Label variant="add" onClick={() => {}}>
+            Add label
+          </Label>
+        )
+      })
+    },
+    example: (props) => (
+      <LabelGroup
+        addLabelControl={props.addLabelControl}
+        categoryName="Group label"
+        isClosable
+        numLabels={props.numLabels}
+        onClick={() => {}}
+      >
+        {props.editableLabels}
       </LabelGroup>
     )
   }
--- packages/code-connect/components/Label/LabelInGroup.figma.tsx
+++ packages/code-connect/components/Label/LabelInGroup.figma.tsx
@@ -1,5 +1,5 @@
 import figma from '@figma/code-connect';
-import { Label } from '@patternfly/react-core';
+import { Badge, Label } from '@patternfly/react-core';
 
 // TODO: DESIGN: Reconfigure how numlabels is set. Currently, the text string "more" is not customizable
 // TODO: DESIGN: Label group should contain label components
@@ -7,17 +7,22 @@ import { Label } from '@patternfly/react-core';
 // NOTE: "Label overflow" doesn't appear to be necessary
 // Documentation for Label can be found at https://www.patternfly.org/components/label-group
 
-figma.connect(
-  Label,
-  'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=2800-1067',
-  {
-    props: {
-      labelText: figma.string('Label text'),
-      isCloseable: figma.boolean('Has Close button', {
-        true: `{() => {}}`,
-        false: undefined
-      })
-    },
-    example: (props) => <Label onClose={props.isCloseable}>{props.labelText}</Label>
-  }
-);
+figma.connect(Label, 'https://www.figma.com/design/VMEX8Xg2nzhBX8rfBx53jp/PatternFly-6--Components?node-id=2800-1067', {
+  props: {
+    labelText: figma.string('Label text'),
+    isCloseable: figma.boolean('Has Close button', {
+      true: `{() => {}}`,
+      false: undefined
+    }),
+    badge: figma.boolean('Has counter', {
+      true: <Badge>06</Badge>,
+      false: undefined
+    })
+  },
+  example: (props) => (
+    <Label onClose={props.isCloseable}>
+      {props.labelText}
+      {props.badge}
+    </Label>
+  )
+});
