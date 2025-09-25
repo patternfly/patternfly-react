cac6127e4f21f3ad547cf98930f484a7e495fefa
Michael Coker <35148959+mcoker@users.noreply.github.com>
2025-09-25 10:00:36 -0500

    chore(table): remove card that wraps table content (#11959)

--- packages/react-table/src/demos/examples/TableEmptyStateDefault.tsx
+++ packages/react-table/src/demos/examples/TableEmptyStateDefault.tsx
@@ -2,7 +2,6 @@ import { Table, Thead, Tr, Th, Tbody, Td } from '@patternfly/react-table';
 import {
   Bullseye,
   Button,
-  Card,
   EmptyState,
   EmptyStateVariant,
   EmptyStateBody,
@@ -16,42 +15,40 @@ import { DashboardWrapper } from '@patternfly/react-table/dist/esm/demos/Dashboa
 export const TableEmptyStateDefault: React.FunctionComponent = () => (
   <DashboardWrapper hasPageTemplateTitle>
     <PageSection padding={{ default: 'noPadding', xl: 'padding' }} aria-label="Empty state table data">
-      <Card component="div">
-        <Table aria-label="Empty state table">
-          <Thead>
-            <Tr>
-              <Th>Repositories</Th>
-              <Th>Branches</Th>
-              <Th>Pull requests</Th>
-              <Th>Workspaces</Th>
-              <Th>Last commit</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            <Tr>
-              <Td colSpan={8}>
-                <Bullseye>
-                  <EmptyState
-                    titleText="No results found"
-                    icon={SearchIcon}
-                    headingLevel="h2"
-                    variant={EmptyStateVariant.sm}
-                  >
-                    <EmptyStateBody>
-                      No results match this filter criteria. Clear all filters and try again.
-                    </EmptyStateBody>
-                    <EmptyStateFooter>
-                      <EmptyStateActions>
-                        <Button variant="link">Clear all filters</Button>
-                      </EmptyStateActions>
-                    </EmptyStateFooter>
-                  </EmptyState>
-                </Bullseye>
-              </Td>
-            </Tr>
-          </Tbody>
-        </Table>
-      </Card>
+      <Table aria-label="Empty state table">
+        <Thead>
+          <Tr>
+            <Th>Repositories</Th>
+            <Th>Branches</Th>
+            <Th>Pull requests</Th>
+            <Th>Workspaces</Th>
+            <Th>Last commit</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td colSpan={8}>
+              <Bullseye>
+                <EmptyState
+                  titleText="No results found"
+                  icon={SearchIcon}
+                  headingLevel="h2"
+                  variant={EmptyStateVariant.sm}
+                >
+                  <EmptyStateBody>
+                    No results match this filter criteria. Clear all filters and try again.
+                  </EmptyStateBody>
+                  <EmptyStateFooter>
+                    <EmptyStateActions>
+                      <Button variant="link">Clear all filters</Button>
+                    </EmptyStateActions>
+                  </EmptyStateFooter>
+                </EmptyState>
+              </Bullseye>
+            </Td>
+          </Tr>
+        </Tbody>
+      </Table>
     </PageSection>
   </DashboardWrapper>
 );
--- packages/react-table/src/demos/examples/TableStickyHeader.tsx
+++ packages/react-table/src/demos/examples/TableStickyHeader.tsx
@@ -1,4 +1,4 @@
-import { Card, Label, PageSection } from '@patternfly/react-core';
+import { Label, PageSection } from '@patternfly/react-core';
 import { Table, Thead, Tr, Th, Tbody, Td, TableText } from '@patternfly/react-table';
 import { DashboardWrapper } from '@patternfly/react-table/dist/esm/demos/DashboardWrapper';
 import { rows, columns } from '@patternfly/react-table/dist/esm/demos/sampleData';
@@ -20,40 +20,38 @@ export const TableStickyHeader: React.FunctionComponent = () => {
   return (
     <DashboardWrapper hasPageTemplateTitle>
       <PageSection padding={{ default: 'noPadding', xl: 'padding' }} aria-label="Sticky header table data">
-        <Card component="div">
-          <Table aria-label="Sticky Header Table Demo" isStickyHeader>
-            <Thead>
-              <Tr>
-                <Th width={15}>{columns[0]}</Th>
-                <Th width={10}>{columns[1]}</Th>
-                <Th width={10}>{columns[2]}</Th>
-                <Th width={10}>{columns[3]}</Th>
-                <Th width={10}>{columns[4]}</Th>
-                <Th width={15}>{columns[5]}</Th>
-                <Th width={15}>{columns[6]}</Th>
-                <Th width={10}>{columns[7]}</Th>
+        <Table aria-label="Sticky Header Table Demo" isStickyHeader>
+          <Thead>
+            <Tr>
+              <Th width={15}>{columns[0]}</Th>
+              <Th width={10}>{columns[1]}</Th>
+              <Th width={10}>{columns[2]}</Th>
+              <Th width={10}>{columns[3]}</Th>
+              <Th width={10}>{columns[4]}</Th>
+              <Th width={15}>{columns[5]}</Th>
+              <Th width={15}>{columns[6]}</Th>
+              <Th width={10}>{columns[7]}</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {rows.map((row) => (
+              <Tr key={row.name}>
+                <Td dataLabel={columns[0]}>{row.name}</Td>
+                <Td dataLabel={columns[1]}>{row.threads}</Td>
+                <Td dataLabel={columns[2]}>{row.applications}</Td>
+                <Td dataLabel={columns[3]}>{row.workspaces}</Td>
+                <Td dataLabel={columns[4]}>{renderLabel(row.status)}</Td>
+                <Td dataLabel={columns[5]}>{row.location}</Td>
+                <Td dataLabel={columns[6]}>{row.lastModified}</Td>
+                <Td dataLabel={columns[7]}>
+                  <a href="#">
+                    <TableText wrapModifier="truncate">{row.url} </TableText>
+                  </a>
+                </Td>
               </Tr>
-            </Thead>
-            <Tbody>
-              {rows.map((row) => (
-                <Tr key={row.name}>
-                  <Td dataLabel={columns[0]}>{row.name}</Td>
-                  <Td dataLabel={columns[1]}>{row.threads}</Td>
-                  <Td dataLabel={columns[2]}>{row.applications}</Td>
-                  <Td dataLabel={columns[3]}>{row.workspaces}</Td>
-                  <Td dataLabel={columns[4]}>{renderLabel(row.status)}</Td>
-                  <Td dataLabel={columns[5]}>{row.location}</Td>
-                  <Td dataLabel={columns[6]}>{row.lastModified}</Td>
-                  <Td dataLabel={columns[7]}>
-                    <a href="#">
-                      <TableText wrapModifier="truncate">{row.url} </TableText>
-                    </a>
-                  </Td>
-                </Tr>
-              ))}
-            </Tbody>
-          </Table>
-        </Card>
+            ))}
+          </Tbody>
+        </Table>
       </PageSection>
     </DashboardWrapper>
   );
