f14ed74ec219fd14480dc0310d79c3ca7f8f5e10
gitdallas <5322142+gitdallas@users.noreply.github.com>
2025-10-05 09:28:40 -0500

    update cypress test
    Signed-off-by: gitdallas <5322142+gitdallas@users.noreply.github.com>

--- packages/react-integration/cypress/integration/page.spec.ts
+++ packages/react-integration/cypress/integration/page.spec.ts
@@ -6,11 +6,11 @@ describe('Page Demo Test', () => {
   it('Test Page elements', () => {
     cy.get('#page-demo').within(() => {
       cy.get('#nav-toggle').then((hamburgerIcon: JQuery<HTMLDivElement>) => {
-        cy.get('.pf-v6-c-page__sidebar.pf-m-expanded').should('exist');
-        cy.get('.pf-v6-c-page__sidebar.pf-m-collapsed').should('not.exist');
-        cy.wrap(hamburgerIcon).click();
-        cy.get('.pf-v6-c-page__sidebar.pf-m-collapsed').should('exist');
         cy.get('.pf-v6-c-page__sidebar.pf-m-expanded').should('not.exist');
+        cy.get('.pf-v6-c-page__sidebar.pf-m-collapsed').should('exist');
+        cy.wrap(hamburgerIcon).click();
+        cy.get('.pf-v6-c-page__sidebar.pf-m-collapsed').should('not.exist');
+        cy.get('.pf-v6-c-page__sidebar.pf-m-expanded').should('exist');
       });
       cy.get('#page-demo-masthead').should('not.have.attr', 'role');
       cy.get('#page-demo-page-id').should('not.have.attr', 'role');
