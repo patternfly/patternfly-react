d316364a8871f1468db90438d2b88a6ac2398dcb
Matt Nolting <matthew.nolting@gmail.com>
2025-10-24 10:41:58 -0400

    feat(CC-batch-6): added batch 6 (#11868)
    * feat(CC-batch-6): added batch 6 components
    
    * update
    
    * feat(CC-batch-6): updated required props
    
    * feat(CC-batch-6): verification complete
    
    * feat(CC-batch-6): review-athon round 1 followup
    
    * feat(CC-batch-6): group review 6 updates
    
    * feat(CC-batch-6): removed isEditable from label group
    
    * feat(CC-batch-6): @kmcfaul review 7 updates
    
    * feat(CC-batch-6): @kmcfaul review 7 updates
    
    * feat(CC-batch-6): review 7 updated modal onClose()
    
    * feat(CC-batch-6): group review 9/23 updates

---
+++ packages/code-connect/components/JumpLinks/JumplinksHorizontal.figma.tsx
@@ -0,0 +1,25 @@
+import figma from '@figma/code-connect';
+import { JumpLinks } from '@patternfly/react-core';
+
+// Documentation for JumpLinks can be found at https://www.patternfly.org/components/jump-links
+
+figma.connect(
+  JumpLinks,
+  'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=8644-150823',
+  {
+    props: {
+      // boolean
+      label: figma.boolean('Show label', {
+        true: 'Jump to section',
+        false: undefined
+      }),
+
+      children: figma.children('*')
+    },
+    example: (props) => (
+      <JumpLinks aria-label={props.label} label={props.label}>
+        {props.children}
+      </JumpLinks>
+    )
+  }
+);
