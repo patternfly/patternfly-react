142c8ba8a9e0082c8b6dec809444b364dcf0b10f
Donald Labaj <dlabaj@redhat.com>
2025-11-04 10:57:54 -0500

    Update packages/react-core/src/components/Tabs/examples/TabsSiteNav.tsx
    Co-authored-by: Eric Olkowski <70952936+thatblindgeye@users.noreply.github.com>

--- packages/react-core/src/components/Tabs/examples/TabsSiteNav.tsx
+++ packages/react-core/src/components/Tabs/examples/TabsSiteNav.tsx
@@ -11,7 +11,7 @@ export const TabsSiteNav: React.FunctionComponent = () => {
     setActiveTabKey(tabIndex);
   };
   return (
-    <Tabs activeKey={activeTabKey} onSelect={handleTabClick} isNav={true} aria-label="Tabs in the nav element example">
+    <Tabs activeKey={activeTabKey} onSelect={handleTabClick} isNav={true} aria-label="Site navigation with nav styling example">
       <Tab eventKey={0} title={<TabTitleText>Users</TabTitleText>} href="#users" aria-label="Nav element content users">
         Users
       </Tab>
