4b5f5eb9d2b77168c229b884b3abe149ef99a3c6
Austin Sullivan <ausulliv@redhat.com>
2025-11-13 14:26:26 -0500

    Tweak interfaces to not duplicate props from HTMLDivElement

--- packages/react-core/src/components/Compass/CompassNavContent.tsx
+++ packages/react-core/src/components/Compass/CompassNavContent.tsx
@@ -1,11 +1,8 @@
 import styles from '@patternfly/react-styles/css/components/Compass/compass';
 import { css } from '@patternfly/react-styles';
-
-interface CompassNavContentProps extends React.HTMLProps<HTMLDivElement> {
+export interface CompassNavContentProps extends React.HTMLProps<HTMLDivElement> {
   /** Content of the nav content wrapper. */
   children: React.ReactNode;
-  /** Additional classes added to the nav content. */
-  className?: string;
 }
 
 export const CompassNavContent: React.FunctionComponent<CompassNavContentProps> = ({
