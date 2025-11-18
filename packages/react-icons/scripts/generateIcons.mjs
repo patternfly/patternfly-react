c1b959515a28ad56ded8da510d4e133c07dcb094
mcoker <mcoker@redhat.com>
2025-11-17 18:17:41 -0600

    feat(react-icons): add rhds icons

--- packages/react-icons/scripts/generateIcons.mjs
+++ packages/react-icons/scripts/generateIcons.mjs
@@ -1,6 +1,10 @@
 import faIcons from './icons/fontawesomeIcons.mjs';
 import patternflyIcons from './icons/patternflyIcons.mjs';
 import customIcons from './icons/customIcons.mjs';
+import rhdsIconsMicrons from './icons/rhdsIconsMicrons.mjs';
+import rhdsIconsStandard from './icons/rhdsIconsStandard.mjs';
+import rhdsIconsSocial from './icons/rhdsIconsSocial.mjs';
+import rhdsIconsUI from './icons/rhdsIconsUI.mjs';
 
 /**
  * Generates icons from FontAwesome, PatternFly CSS, and custom icons in this repo.
@@ -9,6 +13,10 @@ export function generateIcons() {
   return {
     ...faIcons,
     ...patternflyIcons,
-    ...customIcons
+    ...customIcons,
+    ...rhdsIconsMicrons,
+    ...rhdsIconsStandard,
+    ...rhdsIconsSocial,
+    ...rhdsIconsUI
   };
 }
