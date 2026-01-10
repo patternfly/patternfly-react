94a2c42e6a62874b7142e34ad1b72fc6721a6a91
Mohamed Fall <ps.hackmaster@gmail.com>
2026-01-10 07:08:48 +0000

    test(LoginPage): enhance brand image tests with aria-label and className attributes
    Signed-off-by: Mohamed Fall <ps.hackmaster@gmail.com>

--- packages/react-core/src/components/LoginPage/__tests__/LoginPage.test.tsx
+++ packages/react-core/src/components/LoginPage/__tests__/LoginPage.test.tsx
@@ -1,5 +1,5 @@
 import { Fragment } from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import { LoginPage } from '../LoginPage';
 import { ListVariant } from '../../List';
@@ -38,8 +38,28 @@ test('brand is present with brandImgSrc prop', () => {
 });
 
 test('brandImgProps successfully renders brand with props', () => {
-  const { asFragment } = render(
-    <LoginPage brandImgProps={{ src: 'Brand src', alt: 'Pf-logo' }} loginTitle="Log into your account" />
+  render(
+    <LoginPage
+      brandImgProps={{ src: 'Brand src', alt: 'Pf-logo', 'aria-label': 'PatternFly logo', className: 'custom-class' }}
+      loginTitle="Log into your account"
+    />
   );
-  expect(asFragment()).toMatchSnapshot();
+  const brandImg = screen.getByRole('img', { name: 'PatternFly logo' });
+  expect(brandImg).toHaveAttribute('src', 'Brand src');
+  expect(brandImg).toHaveAttribute('alt', 'Pf-logo');
+  expect(brandImg).toHaveAttribute('aria-label', 'PatternFly logo');
+  expect(brandImg).toHaveClass('custom-class');
+});
+
+test('Brand is rendered correctly with both brandImgSrc and brandImgProps, prioritizing brandImgProps.src', () => {
+  render(
+    <LoginPage
+      brandImgSrc="Brand-src-that-should-be-ignored"
+      brandImgProps={{ src: 'Brand-src-from-props', alt: 'Pf-logo from props' }}
+      loginTitle="Log into your account"
+    />
+  );
+  const brandImg = screen.getByRole('img', { name: 'Pf-logo from props' });
+  expect(brandImg).toHaveAttribute('src', 'Brand-src-from-props');
+  expect(brandImg).toHaveAttribute('alt', 'Pf-logo from props');
 });
