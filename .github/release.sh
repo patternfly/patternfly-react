3945ce7b6b3dc1ba68fab9f5df92251101111d4e
Donald Labaj <donald.labaj@gmail.com>
2026-01-29 02:15:06 -0500

    chore: updated to patch release dry run in release.sh

--- .github/release.sh
+++ .github/release.sh
@@ -17,10 +17,14 @@ echo "Doing a release..."
 LOG=$(git log --format="%s" -1 | grep -Poe "#\d+")
 PR_NUM=${LOG:1}
 
-yarn run lerna publish prerelease --preid=prerelease --dist-tag=prerelease --yes 2>&1 | tee lerna-output.txt
+#yarn run lerna publish prerelease --preid=prerelease --dist-tag=prerelease --yes 2>&1 | tee lerna-output.txt
+# Patch release
+#yarn run lerna publish patch --dist-tag=prerelease-patch --no-private --yes 2>&1 | tee lerna-output.txt
 
 # use lerna command below for dry run
 #yarn run lerna version prerelease --preid=prerelease --yes --no-git-tag-version --no-push | tee lerna-output.txt
+# use lerna command below for dry run for patch release
+yarn run lerna version patch --yes --no-git-tag-version --no-push --no-private | tee lerna-output.txt
 
 if grep -i "Successfully published" lerna-output.txt; # Leave a Github comment
 then
