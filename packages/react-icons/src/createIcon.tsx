3268e74802bc9eaaee72eb09b1a3efdc5ea3d627
Katie McFaul <kmcfaul@redhat.com>
2026-02-16 09:46:48 -0500

    feat(icon): pull in core bump and clean up PR

--- packages/react-icons/src/createIcon.tsx
+++ packages/react-icons/src/createIcon.tsx
@@ -25,11 +25,10 @@ export interface SVGIconProps extends Omit<React.HTMLProps<SVGElement>, 'ref'> {
   title?: string;
   className?: string;
   /* Indicates the icon should render using alternate svg data for unified theme */
-  set?: 'default' | 'unified';
+  set?: 'default' | 'rh-ui';
 }
 
 let currentId = 0;
-// const canUseDOM = !!(typeof window !== 'undefined' && window.document && window.document.createElement);
 
 const createSvg = (icon: IconDefinition, iconClassName: string) => {
   const { xOffset, yOffset, width, height, svgPathData, svgClassName } = icon ?? {};
@@ -67,49 +66,13 @@ const createSvg = (icon: IconDefinition, iconClassName: string) => {
  */
 export function createIcon({ name, icon, rhUiIcon = null }: CreateIconProps): React.ComponentClass<SVGIconProps> {
   return class SVGIcon extends Component<SVGIconProps> {
-    // return class SVGIcon extends Component<SVGIconProps, { themeClassVersion: number }> {
     static displayName = name;
 
     id = `icon-title-${currentId++}`;
-    // private observer: MutationObserver | null = null;
 
     constructor(props: SVGIconProps) {
       super(props);
-      // this.state = { themeClassVersion: 0 };
     }
-
-    // componentDidMount() {
-    //   if (rhUiIcon !== null && canUseDOM) {
-    //     this.observer = new MutationObserver((mutations) => {
-    //       for (const mutation of mutations) {
-    //         if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
-    //           const target = mutation.target as HTMLElement;
-    //           const hadClass = (mutation.oldValue || '').includes('pf-v6-theme-unified');
-    //           const hasClass = target.classList.contains('pf-v6-theme-unified');
-
-    //           if (hadClass !== hasClass && this.props.set === undefined) {
-    //             this.setState((prevState) => ({
-    //               themeClassVersion: prevState.themeClassVersion + 1
-    //             }));
-    //           }
-    //         }
-    //       }
-    //     });
-
-    //     this.observer.observe(document.documentElement, {
-    //       attributes: true,
-    //       attributeFilter: ['class'],
-    //       attributeOldValue: true
-    //     });
-    //   }
-    // }
-
-    // componentWillUnmount() {
-    //   if (this.observer) {
-    //     this.observer.disconnect();
-    //     this.observer = null;
-    //   }
-    // }
 
     render() {
       const { title, className: propsClassName, set, ...props } = this.props;
@@ -122,7 +85,7 @@ export function createIcon({ name, icon, rhUiIcon = null }: CreateIconProps): Re
       }
 
       if ((set === undefined && rhUiIcon === null) || set !== undefined) {
-        const iconData = set !== undefined && set === 'unified' && rhUiIcon !== null ? rhUiIcon : icon;
+        const iconData = set !== undefined && set === 'rh-ui' && rhUiIcon !== null ? rhUiIcon : icon;
         const { xOffset, yOffset, width, height, svgPathData, svgClassName } = iconData ?? {};
         const _xOffset = xOffset ?? 0;
         const _yOffset = yOffset ?? 0;
@@ -162,13 +125,13 @@ export function createIcon({ name, icon, rhUiIcon = null }: CreateIconProps): Re
         let rhUiIconClassName;
 
         const shouldRenderDefault = set === 'default' || (set === undefined && rhUiIcon !== null);
-        const shouldRenderRhUi = set === 'unified' || (set === undefined && rhUiIcon !== null);
+        const shouldRenderRhUi = set === 'rh-ui' || (set === undefined && rhUiIcon !== null);
 
         if (shouldRenderDefault) {
           defaultIconClassName = 'pf-v6-icon-default';
         }
         if (shouldRenderRhUi) {
-          rhUiIconClassName = 'pf-v6-icon-unified';
+          rhUiIconClassName = 'pf-v6-icon-rh-ui';
         }
 
         return (
@@ -188,53 +151,6 @@ export function createIcon({ name, icon, rhUiIcon = null }: CreateIconProps): Re
           </svg>
         );
       }
-
-      // React method w/ MutationObserver
-      // const shouldUseAltData =
-      //   rhUiIcon !== null &&
-      //   (set === 'unified' ||
-      //     (set === undefined && canUseDOM && document.documentElement.classList.contains('pf-v6-theme-unified')));
-
-      // const iconData = shouldUseAltData ? rhUiIcon : icon;
-      // const { xOffset, yOffset, width, height, svgClassName, svgPathData } = iconData ?? {};
-      // const _xOffset = xOffset ?? 0;
-      // const _yOffset = yOffset ?? 0;
-
-      // const hasTitle = Boolean(title);
-      // const viewBox = [_xOffset, _yOffset, width, height].join(' ');
-
-      // const classNames = ['pf-v6-svg'];
-      // if (svgClassName) {
-      //   classNames.push(svgClassName);
-      // }
-      // if (propsClassName) {
-      //   classNames.push(propsClassName);
-      // }
-
-      // const svgPaths = Array.isArray(svgPathData) ? (
-      //   svgPathData.map((pathObject, index) => (
-      //     <path className={pathObject.className} key={`${pathObject.path}-${index}`} d={pathObject.path} />
-      //   ))
-      // ) : (
-      //   <path d={svgPathData as string} />
-      // );
-
-      // return (
-      //   <svg
-      //     className={classNames.join(' ')}
-      //     viewBox={viewBox}
-      //     fill="currentColor"
-      //     aria-labelledby={hasTitle ? this.id : null}
-      //     aria-hidden={hasTitle ? null : true}
-      //     role="img"
-      //     width="1em"
-      //     height="1em"
-      //     {...(props as Omit<React.SVGProps<SVGElement>, 'ref'>)} // Lie.
-      //   >
-      //     {hasTitle && <title id={this.id}>{title}</title>}
-      //     {svgPaths}
-      //   </svg>
-      // );
     }
   };
 }
