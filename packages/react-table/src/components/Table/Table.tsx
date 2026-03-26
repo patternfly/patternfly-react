e96381a51714eeb79496f28fe9efec51522720e6
Katie McFaul <kmcfaul@redhat.com>
2026-03-26 10:39:31 -0400

    add warn for using both plain props at once

--- packages/react-table/src/components/Table/Table.tsx
+++ packages/react-table/src/components/Table/Table.tsx
@@ -50,7 +50,7 @@ export interface TableProps extends React.HTMLProps<HTMLTableElement>, OUIAProps
   role?: string;
   /** @beta Flag indicating if the table should have plain styling with a transparent background */
   isPlain?: boolean;
-  /** @beta Flag indicating if the table should not have plain styling when in the glass theme */
+  /** @beta Flag indicating if the table should not have plain styling when in the glass theme. If isPlain is also set, this property will be ignored. */
   isNoPlainOnGlass?: boolean;
   /** If set to true, the table header sticks to the top of its container */
   isStickyHeader?: boolean;
@@ -123,6 +123,13 @@ const TableBase: React.FunctionComponent<TableProps> = ({
 
   const [hasSelectableRows, setHasSelectableRows] = useState(false);
   const [tableCaption, setTableCaption] = useState<React.JSX.Element | undefined>();
+
+  if (isPlain && isNoPlainOnGlass) {
+    // eslint-disable-next-line no-console
+    console.warn(
+      "Table: When both isPlain and isNoPlainOnGlass are true, isPlain will take precedence and isNoPlainOnGlass will have no effect. It's recommended to pass only one prop according to the current theme."
+    );
+  }
 
   useEffect(() => {
     document.addEventListener('keydown', handleKeys);
