8088eb3607c9a0a33394e99ebe5e5795b2cfb73f
Andrew Ronaldson <aronalds@redhat.com>
2026-07-23 13:06:17 -0300

    chore(notification badge): add plain variant to demos

--- packages/react-core/src/demos/examples/Nav/NavFlyout.tsx
+++ packages/react-core/src/demos/examples/Nav/NavFlyout.tsx
@@ -134,7 +134,7 @@ export const NavFlyout: React.FunctionComponent = () => {
           gap={{ default: 'gapNone', md: 'gapMd' }}
         >
           <ToolbarItem>
-            <NotificationBadge aria-label="Notifications" variant={NotificationBadgeVariant.read} onClick={() => {}} />
+            <NotificationBadge aria-label="Notifications" variant={NotificationBadgeVariant.plain} onClick={() => {}} />
           </ToolbarItem>
           <ToolbarGroup variant="action-group-plain" visibility={{ default: 'hidden', lg: 'visible' }}>
             <ToolbarItem>
